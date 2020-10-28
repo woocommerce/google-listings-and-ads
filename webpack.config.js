29a0a826180d86e9270e0eea797e7b249e65ea36
Jason Conroy <jason@findingsimple.com>
2020-10-28 15:29:27 +1030

    Load WooCommerce components from the global scope

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,24 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 const path = require( 'path' );
 
+const requestToExternal = ( request ) => {
+	const wcDepMap = {
+		'@woocommerce/components': [ 'wc', 'components' ],
+	};
+	if ( wcDepMap[ request ] ) {
+		return wcDepMap[ request ];
+	}
+};
+
+const requestToHandle = ( request ) => {
+	const wcHandleMap = {
+		'@woocommerce/components': 'wc-components',
+	};
+	if ( wcHandleMap[ request ] ) {
+		return wcHandleMap[ request ];
+	}
+};
+
 module.exports = {
 	...defaultConfig,
 	plugins: [
@@ -9,7 +27,11 @@ module.exports = {
 			( plugin ) =>
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
-		new DependencyExtractionWebpackPlugin(),
+		new DependencyExtractionWebpackPlugin( {
+			injectPolyfill: true,
+			requestToExternal,
+			requestToHandle,
+		} ),
 	],
 	entry: {
 		index: path.resolve( process.cwd(), 'js/src', 'index.js' ),
