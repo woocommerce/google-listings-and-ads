3724d5f3d09d33a53f7b1ef4b8b445ba8805f7cc
Jason Conroy <jason@findingsimple.com>
2020-10-29 09:51:13 +1030

    Add initial Google menu item

--- js/src/index.js
+++ js/src/index.js
@@ -1,6 +1,31 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
+
+/**
  * Internal dependencies
  */
 import './index.scss';
+import ConnectAccountPage from './connect-account-page';
 
-console.log('hello from the Woogle plugin');
+addFilter(
+	'woocommerce_admin_pages_list',
+	'woocommerce-marketing',
+	( pages ) => {
+		return [
+			...pages,
+			{
+				breadcrumbs: [
+					[ '', wcSettings.woocommerceTranslation ],
+					['/marketing', __( 'Marketing', 'google-for-woocommerce' ) ],
+					__( 'Connect', 'google-for-woocommerce' ) ],
+				title: __( 'Connect', 'google-for-woocommerce' ),
+				container: ConnectAccountPage,
+				path: '/google/connect',
+				wpOpenMenu: 'toplevel_page_woocommerce-marketing',
+			},
+		];
+	},
+);
