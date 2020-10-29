c35b82f3df0db2808a1c41b0879122d0ac981a76
Jeremy Pry <jeremy.pry@gmail.com>
2020-10-29 17:30:25 -0400

    Create InvalidURI exception

---
+++ src/Exception/InvalidURI.php
@@ -0,0 +1,25 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleForWC\Exception;
+
+use InvalidArgumentException;
+
+/**
+ * Class InvalidURI
+ *
+ * @package Automattic\WooCommerce\GoogleForWC\Exception
+ */
+class InvalidURI extends InvalidArgumentException implements GoogleForWCException {
+
+	/**
+	 * Create a new exception for an unreadable asset.
+	 *
+	 * @param string $path
+	 *
+	 * @return static
+	 */
+	public static function asset_path( string $path ) {
+		return new static( sprintf( 'The asset "%s" is unreadable. Do build scripts need to be run?', $path ) );
+	}
+}
