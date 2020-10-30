fbabfe9f558edd0bc2ebe93605e486bb53e66bc9
Jeremy Pry <jeremy.pry@gmail.com>
2020-10-30 16:56:29 -0400

    Create asset classes for scripts and admin scripts

---
+++ src/Assets/AdminScriptAsset.php
@@ -0,0 +1,14 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleForWC\Assets;
+
+/**
+ * Class AdminScriptAsset
+ *
+ * @package Automattic\WooCommerce\GoogleForWC\Assets
+ */
+class AdminScriptAsset extends AdminBaseAsset {
+
+	use ScriptHelper;
+}
---
+++ src/Assets/ScriptAsset.php
@@ -0,0 +1,14 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleForWC\Assets;
+
+/**
+ * Class ScriptAsset
+ *
+ * @package Automattic\WooCommerce\GoogleForWC\Assets
+ */
+class ScriptAsset extends BaseAsset {
+
+	use ScriptHelper;
+}
