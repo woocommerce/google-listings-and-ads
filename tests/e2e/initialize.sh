b2279fa18a0102ca1307faa5ab6e7feb4f8bf847
engchin <engchin@engchinmbp16.local>
2020-11-10 00:36:07 +0800

    Move plugin installation into .wp-env.json instead.

--- tests/e2e/initialize.sh
+++ tests/e2e/initialize.sh
@@ -12,13 +12,8 @@ npm run wp-env run tests-cli 'wp option set woocommerce_product_type "both"'
 echo "Importing WooCommerce shop pages..."
 npm run wp-env run tests-cli "wp wc --user=admin tool run install_pages"
 
-echo "Installing and activating the WordPress Importer plugin"
-npm run wp-env run tests-cli "wp plugin install wordpress-importer --activate"
-echo "Importing the WooCommerce sample data..."
+echo "Importing the WooCommerce sample data using the WordPress Importer plugin..."
 npm run wp-env run tests-cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
-
-# # This plugin is required for WooCommerce e2e-utils to work
-npm run wp-env run tests-cli "wp plugin install https://github.com/WP-API/Basic-Auth/archive/master.zip --activate"
 
 # Create the page which is used to determine if the test environment has been setup
 npm run wp-env run tests-cli "wp post create --post_type=page --post_status=publish --post_title='Ready' --post_content='E2E-tests.'"
