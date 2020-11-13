ddf5a563642411a9adc7ff2173daca1e2dce7cc7
Jeremy Pry <jeremy.pry@gmail.com>
2020-11-13 18:03:24 -0500

    Let TracksEventInterface be a simple alias of Registerable

--- src/Tracking/Events/TracksEventInterface.php
+++ src/Tracking/Events/TracksEventInterface.php
@@ -4,19 +4,10 @@ declare( strict_types=1 );
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tracking\Events;
 
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Registerable;
-use Automattic\WooCommerce\GoogleListingsAndAds\Tracking\TracksInterface;
 
 /**
  * Interface describing an event tracker class.
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\Tracking
  */
-interface TracksEventInterface extends Registerable {
-
-	/**
-	 * Set the Tracks object that will be used for tracking.
-	 *
-	 * @param TracksInterface $tracks
-	 */
-	public function set_tracks( TracksInterface $tracks );
-}
+interface TracksEventInterface extends Registerable {}
