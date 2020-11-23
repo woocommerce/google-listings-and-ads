c6c6cbb8c2392a15dd697bd35f0a8184b551af9c
Jeremy Pry <jeremy.pry@gmail.com>
2020-11-23 16:27:34 -0500

    Create an exception to handle WP_Error objects.

---
+++ src/Exception/WPErrorTrait.php
@@ -0,0 +1,31 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Exception;
+
+use WP_Error;
+
+/**
+ * Trait WPErrorTrait
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Exception
+ */
+trait WPErrorTrait {
+
+	/**
+	 * Check for a WP_Error object and throw an exception if we found one.
+	 *
+	 * @param mixed|WP_Error $maybe_error
+	 *
+	 * @throws WPError when the object is a WP_Error.
+	 */
+	protected function check_for_wp_error( $maybe_error ) {
+		if ( ! $maybe_error instanceof WP_Error ) {
+			return;
+		}
+
+		if ( $maybe_error->has_errors() ) {
+			throw WPError::from_error( $maybe_error );
+		}
+	}
+}
