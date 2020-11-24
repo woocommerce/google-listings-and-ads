e0dd6fd05ec136411206b74581198cf487aa589c
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-11-25 01:34:01 +0800

    Use @emotion/css to generate class for GetStartedCard.

---
+++ js/src/get-started-page/get-started-card/styled.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import { css } from '@emotion/css';
+
+/**
+ * Internal dependencies
+ */
+import breakpoints from '../../utils/breakpoint-values';
+
+export default css`
+	.components-flex {
+		flex-direction: column;
+		align-items: flex-end;
+
+		@media ( min-width: ${ breakpoints.large } ) {
+			flex-direction: row;
+			align-items: center;
+		}
+
+		.motivation-text {
+			margin: calc( var( --main-gap ) * 2 );
+
+			.title {
+				margin-bottom: calc( var( --main-gap ) / 2 );
+			}
+
+			.description {
+				margin-bottom: var( --main-gap );
+			}
+		}
+
+		.motivation-image {
+			text-align: right;
+			margin-left: calc( var( --main-gap ) * 2 );
+			margin-bottom: -4px;
+
+			@media ( min-width: ${ breakpoints.large } ) {
+				margin-top: calc( var( --main-gap ) * 1.5 );
+			}
+
+			svg {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+`;
