7d850be26ec68de594970edc354aeccc044adb2f
Jeremy Pry <jeremy.pry@gmail.com>
2020-11-24 14:49:16 -0500

    Include the Http class in the proxy service provider

--- src/Internal/DependencyManagement/ProxyServiceProvider.php
+++ src/Internal/DependencyManagement/ProxyServiceProvider.php
@@ -3,6 +3,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Internal\DependencyManagement;
 
+use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\Http;
 use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\RESTServer;
 use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\Tracks;
 
@@ -19,7 +20,24 @@ class ProxyServiceProvider extends AbstractServiceProvider {
 	 * @var array
 	 */
 	protected $provides = [
+		Http::class       => true,
 		RESTServer::class => true,
 		Tracks::class     => true,
 	];
+
+	/**
+	 * Use the register method to register items with the container via the
+	 * protected $this->leagueContainer property or the `getLeagueContainer` method
+	 * from the ContainerAwareTrait.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// The Http class should be a new object every time.
+		$this->add( Http::class );
+
+		// Our other classes can be shared like normal.
+		$this->share( RESTServer::class );
+		$this->share( Tracks::class );
+	}
 }
