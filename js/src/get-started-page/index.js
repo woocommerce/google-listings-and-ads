02d9a17b2361b98f1844414a3812bd0757f83f62
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-11-27 00:22:09 +0800

    Export and use SCSS variables in JavaScript.

--- js/src/get-started-page/index.js
+++ js/src/get-started-page/index.js
@@ -4,12 +4,24 @@
 import './index.scss';
 import Faqs from './faqs';
 import GetStartedCard from './get-started-card';
+import exp from '../export.scss';
 
 const GetStartedPage = () => {
+	console.log( 'exp: ', exp );
+
 	return (
 		<div className="woocommerce-marketing-google-get-started-page">
 			<GetStartedCard />
 			<Faqs></Faqs>
+			<div
+				style={ {
+					color: exp.alertyellow,
+					backgroundColor: exp.black,
+					width: exp.gridunit60,
+				} }
+			>
+				hello
+			</div>
 		</div>
 	);
 };
