892d8708cf8449a26d7e7ccbfeac09a84f8bb6f2
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-01 01:46:38 +0800

    Display last updated date time and summaries in product feed.

--- js/src/product-feed/index.js
+++ js/src/product-feed/index.js
@@ -1,10 +1,30 @@
 /**
+ * External dependencies
+ */
+import GridiconHelpOutline from 'gridicons/dist/help-outline';
+
+/**
  * Internal dependencies
  */
 import TabNav from '../tab-nav';
+import Summaries from './summaries';
+import './index.scss';
+
+/* TODO: The last updated date and time need to come from backend API. */
+const lastUpdatedDateTime = new Date();
 
 const ProductFeed = () => {
-	return <TabNav initialName="product-feed" />;
+	return (
+		<div className="gla-product-feed">
+			<TabNav initialName="product-feed" />
+			<div className="gla-product-feed__last-updated">
+				{ /* TODO: Find the right WC way to format it. */ }
+				Last updated: { lastUpdatedDateTime.toLocaleString() }
+				<GridiconHelpOutline size={ 16 } />
+			</div>
+			<Summaries />
+		</div>
+	);
 };
 
 export default ProductFeed;
