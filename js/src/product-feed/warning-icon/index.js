ba6832785392d46e85463e0e4ed1c4c417dc1463
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-01 23:56:38 +0800

    Add warning icon.

---
+++ js/src/product-feed/warning-icon/index.js
@@ -0,0 +1,15 @@
+/**
+ * External dependencies
+ */
+import GridiconNoticeOutline from 'gridicons/dist/notice-outline';
+
+/**
+ * Internal dependencies
+ */
+import './index.scss';
+
+const WarningIcon = () => {
+	return <GridiconNoticeOutline className="gla-warning-icon" size={ 18 } />;
+};
+
+export default WarningIcon;
