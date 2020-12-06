1439e4dce17c96d827d1379da5ac616134f9ed07
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-07 01:01:44 +0800

    Add date range filter in dashboard page.

--- js/src/dashboard/index.js
+++ js/src/dashboard/index.js
@@ -1,13 +1,32 @@
 /**
+ * External dependencies
+ */
+import { DateRangeFilterPicker } from '@woocommerce/components';
+import { updateQueryString } from '@woocommerce/navigation';
+
+/**
  * Internal dependencies
  */
 import TabNav from '../tab-nav';
+import getDateQuery from './getDateQuery';
+
+const isoDateFormat = 'YYYY-MM-DD';
 
 const Dashboard = () => {
+	const dateQuery = getDateQuery();
+
+	const handleRangeSelect = ( data ) => {
+		updateQueryString( data );
+	};
+
 	return (
 		<div>
 			<TabNav initialName="dashboard" />
-			<div>Content</div>
+			<DateRangeFilterPicker
+				dateQuery={ dateQuery }
+				onRangeSelect={ handleRangeSelect }
+				isoDateFormat={ isoDateFormat }
+			/>
 		</div>
 	);
 };
