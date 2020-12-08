cfdaa66c5768348c0e5d59a915f55ea97673aa4c
Jeremy Pry <jeremy.pry@gmail.com>
2020-12-08 17:47:52 -0500

    Remove extra stuff from the third party provider

--- src/Internal/DependencyManagement/ThirdPartyServiceProvider.php
+++ src/Internal/DependencyManagement/ThirdPartyServiceProvider.php
@@ -5,13 +5,7 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\Internal\DependencyManagem
 
 use Automattic\Jetpack\Config;
 use Automattic\Jetpack\Connection\Manager;
-use Automattic\WooCommerce\GoogleListingsAndAds\Exception\WPError;
-use Automattic\WooCommerce\GoogleListingsAndAds\Exception\WPErrorTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\PluginHelper;
-use Google\Client as GoogleClient;
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\ClientInterface;
-use Jetpack_Options;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -23,7 +17,6 @@ defined( 'ABSPATH' ) || exit;
 class ThirdPartyServiceProvider extends AbstractServiceProvider {
 
 	use PluginHelper;
-	use WPErrorTrait;
 
 	/**
 	 * Array of classes provided by this container.
@@ -33,9 +26,8 @@ class ThirdPartyServiceProvider extends AbstractServiceProvider {
 	 * @var array
 	 */
 	protected $provides = [
-		Config::class       => true,
-		Manager::class      => true,
-		GoogleClient::class => true,
+		Config::class  => true,
+		Manager::class => true,
 	];
 
 	/**
@@ -59,67 +51,5 @@ class ThirdPartyServiceProvider extends AbstractServiceProvider {
 				],
 			]
 		);
-
-		$this->share_interface( ClientInterface::class, GuzzleClient::class )
-			->addArgument( $this->get_guzzle_arguments() );
-
-		$this->share( GoogleClient::class )->addMethodCall( 'setHttpClient', [ ClientInterface::class ] );
-	}
-
-	/**
-	 * Get arguments for the Guzzle client.
-	 *
-	 * @return array Array of arguments for the client.
-	 */
-	protected function get_guzzle_arguments(): array {
-		try {
-			$auth_header = $this->generate_guzzle_auth_header();
-			return [
-				'headers' => [
-					'Authorization' => $auth_header,
-				],
-			];
-		} catch ( WPError $error ) {
-			return [];
-		}
-	}
-
-	/**
-	 * Generate the authorization header for the Guzzle client.
-	 *
-	 * @return string
-	 */
-	protected function generate_guzzle_auth_header(): string {
-		/** @var Manager $manager */
-		$manager = $this->getLeagueContainer()->get( Manager::class );
-		$token   = $manager->get_access_token( false, false, false );
-		$this->check_for_wp_error( $token );
-
-		[ $key, $secret ] = explode( '.', $token->secret );
-
-		$key       = sprintf(
-			'%s:%d:%d',
-			$key,
-			defined( 'JETPACK__API_VERSION' ) ? JETPACK__API_VERSION : 1,
-			$token->external_user_id
-		);
-		$timestamp = time() + (int) Jetpack_Options::get_option( 'time_diff' );
-		$nonce     = wp_generate_password( 10, false );
-
-		$request   = join( "\n", [ $key, $timestamp, $nonce, '' ] );
-		$signature = base64_encode( hash_hmac( 'sha1', $request, $secret, true ) );
-		$auth      = [
-			'token'     => $key,
-			'timestamp' => $timestamp,
-			'nonce'     => $nonce,
-			'signature' => $signature,
-		];
-
-		$pieces = [ 'X_JP_AUTH' ];
-		foreach ( $auth as $key => $value ) {
-			$pieces[] = sprintf( '%s="%s"', $key, $value );
-		}
-
-		return join( ' ', $pieces );
 	}
 }
