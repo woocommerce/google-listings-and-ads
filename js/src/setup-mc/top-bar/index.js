90dcdedd0fc7e1c2f5f0400632bc3ff04335c0c5
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-13 18:00:11 +0800

    Add TopBar component for setup MC full screen page.

---
+++ js/src/setup-mc/top-bar/index.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { Link } from '@woocommerce/components';
+import { getNewPath } from '@woocommerce/navigation';
+import { __ } from '@wordpress/i18n';
+import GridiconChevronLeft from 'gridicons/dist/chevron-left';
+import GridiconHelpOutline from 'gridicons/dist/help-outline';
+
+/**
+ * Internal dependencies
+ */
+import AppIconButton from '../../components/app-icon-button';
+import './index.scss';
+
+const TopBar = () => {
+	return (
+		<div className="gla-setup-mc-top-bar">
+			<Link
+				className="back-button"
+				href={ getNewPath( {}, '/google/dashboard' ) }
+				type="wc-admin"
+			>
+				<GridiconChevronLeft />
+			</Link>
+			<span className="title">
+				{ __(
+					'Get started with Google Listings & Ads',
+					'google-listings-and-ads'
+				) }
+			</span>
+			<AppIconButton
+				className="help-button"
+				icon={ <GridiconHelpOutline /> }
+				text={ __( 'Help', 'google-listings-and-ads' ) }
+			></AppIconButton>
+		</div>
+	);
+};
+
+export default TopBar;
