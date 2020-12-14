0f313ee11799b5b715e1ed16266dae30ec95fd60
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-14 22:30:34 +0800

    Add header section into SetupAccounts.

--- js/src/setup-mc/setup-stepper/setup-accounts/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/index.js
@@ -1,10 +1,32 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import './index.scss';
 
 const SetupAccounts = () => {
-	return <div>Setup Accounts</div>;
+	return (
+		<div className="gla-setup-accounts">
+			<div className="header">
+				<p className="step">
+					{ __( 'STEP ONE', 'google-listings-and-ads' ) }
+				</p>
+				<h1>
+					{ __( 'Set up your accounts', 'google-listings-and-ads' ) }
+				</h1>
+				<p className="description">
+					{ __(
+						'Connect your Wordpress.com account, Google account, and Google Merchant Center account to use Google Listings & Ads.',
+						'google-listings-and-ads'
+					) }
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default SetupAccounts;
