23ce83a9aa329303abbeffcb2b2dc8c8d0f7e4ca
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-17 23:46:44 +0800

    Make GoogleAccount able to be disabled.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-account/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-account/index.js
@@ -3,6 +3,7 @@
  */
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import DisabledDiv from '../../../../components/disabled-div';
 
 /**
  * Internal dependencies
@@ -11,34 +12,38 @@ import Section from '../../../../wcdl/section';
 import Subsection from '../../../../wcdl/subsection';
 import ContentButtonLayout from '../content-button-layout';
 
-const GoogleAccount = () => {
+const GoogleAccount = ( props ) => {
+	const { disabled = false } = props;
+
 	// TODO: call backend API upon clicking Connect button.
 	const handleConnectClick = () => {};
 
 	return (
-		<Section
-			title={ __( 'Google account', 'google-listings-and-ads' ) }
-			description={ __(
-				'WooCommerce uses your Google account to sync with Google Merchant Center and Google Ads.',
-				'google-listings-and-ads'
-			) }
-		>
-			<Section.Card>
-				<Section.Card.Body>
-					<ContentButtonLayout>
-						<Subsection.Title>
-							{ __(
-								'Connect your Google account',
-								'google-listings-and-ads'
-							) }
-						</Subsection.Title>
-						<Button isSecondary onClick={ handleConnectClick }>
-							{ __( 'Connect', 'google-listings-and-ads' ) }
-						</Button>
-					</ContentButtonLayout>
-				</Section.Card.Body>
-			</Section.Card>
-		</Section>
+		<DisabledDiv disabled={ disabled }>
+			<Section
+				title={ __( 'Google account', 'google-listings-and-ads' ) }
+				description={ __(
+					'WooCommerce uses your Google account to sync with Google Merchant Center and Google Ads.',
+					'google-listings-and-ads'
+				) }
+			>
+				<Section.Card>
+					<Section.Card.Body>
+						<ContentButtonLayout>
+							<Subsection.Title>
+								{ __(
+									'Connect your Google account',
+									'google-listings-and-ads'
+								) }
+							</Subsection.Title>
+							<Button isSecondary onClick={ handleConnectClick }>
+								{ __( 'Connect', 'google-listings-and-ads' ) }
+							</Button>
+						</ContentButtonLayout>
+					</Section.Card.Body>
+				</Section.Card>
+			</Section>
+		</DisabledDiv>
 	);
 };
 
