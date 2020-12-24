03cb8aa0ac1f6e146cd76cdefd07668dc86c9cad
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2020-12-24 15:33:59 +0330

    Create a WooCommerce product adapter class
    This class maps the attributes of a WooCommerce product into the Google's product class. It will later be used as an input for Google's API client to represent a WooCommerce Product object.

---
+++ src/Product/WCProductAdapter.php
@@ -0,0 +1,325 @@
+<?php
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Product;
+
+/**
+ * Class WCProductAdapter
+ *
+ * This class adapts the WooCommerce Product class to the Google's Product class by mapping their attributes.
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Product
+ */
+class WCProductAdapter extends \Google_Service_ShoppingContent_Product {
+	const AVAILABILITY_IN_STOCK = 'in stock';
+	const AVAILABILITY_OUT_OF_STOCK = 'out of stock';
+
+	const IMAGE_SIZE_FULL = 'full';
+
+	const CHANNEL_ONLINE = 'online';
+
+	/**
+	 * @var \WC_Product WooCommerce product object
+	 */
+	protected $WC_product;
+
+	/**
+	 * Initialize this object's properties from an array.
+	 *
+	 * @param array $array Used to seed this object's properties.
+	 * @return void
+	 */
+	public function mapTypes( $array ) {
+		parent::mapTypes( $array );
+
+		if ( ! empty( $array['wc_product_id'] ) ) {
+			$this->WC_product = new \WC_Product( $array['wc_product_id'] );
+			$this->map_woocommerce_product();
+		}
+	}
+
+	/**
+	 * Map the WooCommerce product attributes to the current class.
+	 *
+	 * @return void
+	 */
+	protected function map_woocommerce_product() {
+
+
+		// store currency
+		$currency = get_woocommerce_currency();
+
+		$dimension_unit = get_option( 'woocommerce_dimension_unit' );
+		$weight_unit    = get_option( 'woocommerce_weight_unit' );
+
+		$this->setChannel( self::CHANNEL_ONLINE );
+
+		// todo: this is temporary, modify or remove this when the GTIN, MPN etc. functionalities are implemented.
+		$this->setIdentifierExists( false );
+
+		// set target country
+		$base_country = WC()->countries->get_base_country();
+		$this->setTargetCountry( $base_country );
+
+		// todo: maybe set this using the site locale?
+		// set content language
+		$this->setContentLanguage( 'en' );
+
+		// tax is excluded from price in US and CA
+		$tax_excluded = in_array( $base_country, array( 'US', 'CA' ), true );
+
+		// set general product attributes
+		$this->map_wc_general_attributes();
+
+		// set full size product image
+		$this->map_wc_product_image(self::IMAGE_SIZE_FULL );
+
+		// set availability
+		$this->map_wc_availability();
+
+		// set shipping dimensions
+		$this->map_wc_shipping_dimensions( $dimension_unit );
+
+		// set shipping weight
+		$this->map_wc_shipping_weight( $weight_unit );
+
+		// set price
+		$this->map_wc_product_price( $currency, $tax_excluded );
+
+	}
+
+	/**
+	 * Map the general WooCommerce product attributes.
+	 *
+	 * @return void
+	 */
+	protected function map_wc_general_attributes() {
+		$this->setOfferId( $this->WC_product->get_sku() ?? $this->WC_product->get_id() );
+		$this->setTitle( $this->WC_product->get_title() );
+		$this->setDescription( $this->WC_product->get_description() );
+		$this->setLink( $this->WC_product->get_permalink() );
+
+		if ( $this->is_variation() ) {
+			$parent_product    = wc_get_product( $this->WC_product->get_parent_id() );
+			$parent_product_id = $parent_product->get_sku() ?? $parent_product->get_id();
+			$this->setItemGroupId( $parent_product_id );
+		}
+	}
+
+	/**
+	 * Map the WooCommerce product images.
+	 *
+	 * @param string $image_size
+	 *
+	 * @return void
+	 */
+	protected function map_wc_product_image( $image_size = self::IMAGE_SIZE_FULL ) {
+		$image_id          = $this->WC_product->get_image_id();
+		$gallery_image_ids = $this->WC_product->get_gallery_image_ids();
+
+		// check if we can use the parent product image if it's a variation
+		if ( $this->is_variation() ) {
+			$parent_product = wc_get_product( $this->WC_product->get_parent_id() );
+			$image_id = $image_id ?? $this->WC_product->get_image_id();
+			$gallery_image_ids = ! empty( $gallery_image_ids ) ? $gallery_image_ids : $parent_product->get_gallery_image_ids();
+		}
+
+		// use a gallery image as the main product image if no main image is available
+		if ( empty( $image_id ) && ! empty( $gallery_image_ids[0] ) ) {
+			$image_id = $gallery_image_ids[0];
+
+			// remove the recently set main image from the list of gallery images
+			unset( $gallery_image_ids[0] );
+		}
+
+		// set main image
+		$image_link = wp_get_attachment_image_url( $image_id, $image_size, false );
+		$this->setImageLink( $image_link );
+
+		// set additional images
+		$gallery_image_links = array_map( function ( $gallery_image_id ) use ( $image_size ) {
+			return wp_get_attachment_image_url( $gallery_image_id, $image_size, false );
+		}, $gallery_image_ids );
+		// Uniquify the set of additional images
+		$gallery_image_links = array_unique( $gallery_image_links, SORT_REGULAR );
+		$this->setAdditionalImageLinks( $gallery_image_links );
+	}
+
+	/**
+	 * Map the general WooCommerce product attributes.
+	 *
+	 * @return void
+	 */
+	protected function map_wc_availability() {
+		// todo: include 'preorder' status (maybe a new field for products / or using an extension?)
+		$availability = $this->WC_product->is_in_stock() ? self::AVAILABILITY_IN_STOCK : self::AVAILABILITY_OUT_OF_STOCK;
+		$this->setAvailability( $availability );
+	}
+
+	/**
+	 * Map the measurements for the WooCommerce product.
+	 *
+	 * @param string $unit
+	 *
+	 * @return void
+	 */
+	protected function map_wc_shipping_dimensions( $unit = 'cm' ) {
+		$length = $this->WC_product->get_length();
+		$width  = $this->WC_product->get_width();
+		$height = $this->WC_product->get_height();
+
+		// Use cm if the unit isn't supported.
+		if ( ! in_array( $unit, [ 'in', 'cm' ], true ) ) {
+			$unit = 'cm';
+		}
+		$length = wc_get_dimension( (float) $length, $unit );
+		$width  = wc_get_dimension( (float) $width, $unit );
+		$height = wc_get_dimension( (float) $height, $unit );
+
+		if ( $length > 0 && $width > 0 && $height > 0 ) {
+			$this->setShippingLength( new \Google_Service_ShoppingContent_ProductShippingDimension([
+				'unit' => $unit,
+				'value' => $length,
+			]) );
+			$this->setShippingWidth( new \Google_Service_ShoppingContent_ProductShippingDimension([
+				'unit' => $unit,
+				'value' => $width,
+			]) );
+			$this->setShippingHeight( new \Google_Service_ShoppingContent_ProductShippingDimension([
+				'unit' => $unit,
+				'value' => $height,
+			]) );
+		}
+	}
+
+	/**
+	 * Map the weight for the WooCommerce product.
+	 *
+	 * @param string $unit
+	 *
+	 * @return void
+	 */
+	protected function map_wc_shipping_weight( $unit = 'g' ) {
+
+		// Use g if the unit isn't supported.
+		if ( ! in_array( $unit, [ 'g', 'lbs', 'oz' ], true ) ) {
+			$unit = 'g';
+		}
+
+		$weight = wc_get_weight( $this->WC_product->get_weight(), $unit );
+		$this->setShippingWeight( new \Google_Service_ShoppingContent_ProductShippingWeight( [
+			'unit' => $unit,
+			'value' => $weight,
+		] ) );
+	}
+
+	/**
+	 * Map the prices (base and sale price) for the WooCommerce product.
+	 *
+	 * @param string $currency
+	 * @param bool   $tax_excluded
+	 *
+	 * @return void
+	 */
+	protected function map_wc_product_price( string $currency, bool $tax_excluded ) {
+		// set regular price
+		$regular_price = $this->WC_product->get_regular_price();
+		if ( '' !== $regular_price ) {
+			$price = $tax_excluded ?
+				wc_get_price_excluding_tax( $this->WC_product->get_id(), [ 'price' => $regular_price ] ) :
+				wc_get_price_including_tax( $this->WC_product->get_id(), [ 'price' => $regular_price ] );
+
+			$this->setPrice( new \Google_Service_ShoppingContent_Price( [
+				'currency' => $currency,
+				'value' => $price,
+			] ) );
+		}
+		// set sale price
+		$this->map_wc_sale_price( $currency, $tax_excluded );
+	}
+
+	/**
+	 * Map the sale price and sale effective date for the WooCommerce product.
+	 *
+	 * @param string $currency
+	 * @param bool   $tax_excluded
+	 *
+	 * @return void
+	 */
+	protected function map_wc_sale_price( string $currency, bool $tax_excluded ) {
+		// Grab the sale price of the base product. Some plugins (Dyanmic
+		// pricing as an example) filter the active price, but not the sale
+		// price. If the active price < the regular price treat it as a sale
+		// price.
+		$regular_price = $this->WC_product->get_regular_price();
+		$sale_price   = $this->WC_product->get_sale_price();
+		$active_price = $this->WC_product->get_price();
+		if ( ( empty( $sale_price ) && $active_price < $regular_price ) ||
+		     ( ! empty( $sale_price ) && $active_price < $sale_price ) ) {
+			$sale_price = $active_price;
+		}
+
+		// set sale price and sale effective date if any
+		if ( '' !== $sale_price ) {
+			$sale_price = $tax_excluded ?
+				wc_get_price_excluding_tax( $this->WC_product->get_id(), [ 'price' => $sale_price ] ) :
+				wc_get_price_including_tax( $this->WC_product->get_id(), [ 'price' => $sale_price ] );
+
+			// If the sale price dates no longer apply, make sure we don't include a sale price.
+			$now                 = new \WC_DateTime();
+			$sale_price_end_date = $this->WC_product->get_date_on_sale_to();
+			if ( empty( $sale_price_end_date ) || $sale_price_end_date >= $now ) {
+				$this->setSalePrice( new \Google_Service_ShoppingContent_Price( [
+					'currency' => $currency,
+					'value' => $sale_price,
+				] ) );
+
+				$this->setSalePriceEffectiveDate( $this->get_wc_sale_price_effective_date() );
+			}
+		}
+	}
+
+	/**
+	 * Return the sale effective dates for the WooCommerce product.
+	 *
+	 * @return string
+	 */
+	protected function get_wc_sale_price_effective_date(): string {
+		$start_date = $this->WC_product->get_date_on_sale_from();
+		$end_date   = $this->WC_product->get_date_on_sale_to();
+
+		$now = new \WC_DateTime();
+		// if we have a sale end date in the future, but no start date, set the start date to now()
+		if ( ! empty( $end_date ) &&
+		     $end_date > $now &&
+		     empty( $start_date )
+		) {
+			$start_date = $now;
+		}
+		// if we have a sale start date in the past, but no end date, do not include the start date.
+		if ( ! empty( $start_date ) &&
+		     $start_date < $now &&
+		     empty( $end_date )
+		) {
+			$start_date = null;
+		}
+		// if we have a start date in the future, but no end date, assume a one-day sale.
+		if ( ! empty( $start_date ) &&
+		     $start_date > $now &&
+		     empty( $end_date )
+		) {
+			$end_date = clone $start_date;
+			$end_date->add( new \DateInterval( 'P1D' ) );
+		}
+
+		return sprintf( '%s/%s', (string) $start_date, (string) $end_date);
+	}
+
+	/**
+	 * Return whether the WooCommerce product is a variation.
+	 *
+	 * @return bool
+	 */
+	protected function is_variation(): bool {
+		return $this->WC_product instanceof \WC_Product_Variation;
+	}
+}
