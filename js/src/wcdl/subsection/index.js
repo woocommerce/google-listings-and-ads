b315daa188b2ee14a4006f619c8c501eb1d3b468
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-30 02:43:21 +0800

    Add Subsection component.

--- js/src/wcdl/subsection/index.js
+++ js/src/wcdl/subsection/index.js
@@ -5,8 +5,19 @@ import Title from './title';
 import Body from './body';
 import HelperText from './helper-text';
 
-export default {
-	Title,
-	Body,
-	HelperText,
+/**
+ * Internal dependencies
+ */
+import './index.scss';
+
+const Subsection = ( props ) => {
+	const { className = '', ...rest } = props;
+
+	return <div className={ `wcdl-subsection ${ className }` } { ...rest } />;
 };
+
+Subsection.Title = Title;
+Subsection.Body = Body;
+Subsection.HelperText = HelperText;
+
+export default Subsection;
