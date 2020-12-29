dbe331de7f53880813907b9faf6e89f2e7be2798
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-29 19:24:43 +0800

    Display Audience section.

--- js/src/setup-mc/setup-stepper/choose-audience/index.js
+++ js/src/setup-mc/setup-stepper/choose-audience/index.js
@@ -1,11 +1,13 @@
 /**
  * External dependencies
  */
+import { Link } from '@woocommerce/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import Section from '../../../wcdl/section';
 import StepContentHeader from '../components/step-content-header';
 import './index.scss';
 
@@ -23,6 +25,32 @@ const ChooseAudience = () => {
 					'google-listings-and-ads'
 				) }
 			/>
+			<Section
+				title={ __( 'Audience', 'google-listings-and-ads' ) }
+				description={
+					<div>
+						<p>
+							{ __(
+								'Your store must have the appropriate shipping and tax rates (if required) for customers in all your selected countries.',
+								'google-listings-and-ads'
+							) }
+						</p>
+						<p>
+							<Link
+								type="external"
+								href="https://docs.woocommerce.com/documentation/plugins/woocommerce/getting-started/shipping/core-shipping-options/"
+								target="_blank"
+							>
+								{ __( 'Read more', 'google-listings-and-ads' ) }
+							</Link>
+						</p>
+					</div>
+				}
+			>
+				<Section.Card>
+					<Section.Card.Body>TODO:</Section.Card.Body>
+				</Section.Card>
+			</Section>
 		</div>
 	);
 };
