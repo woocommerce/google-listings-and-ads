4aeb57b1664e5a1c01aea1a0d80f31d612bc7c85
Gan Eng Chin <ecgan@users.noreply.github.com>
2020-12-31 01:17:32 +0800

    SetupFreeListings component with hero image.

--- js/src/setup-mc/setup-stepper/setup-free-listings/index.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/index.js
@@ -1,11 +1,48 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
+import StepContentHeader from '../components/step-content-header';
+import { ReactComponent as GoogleFreeListingImage } from './google-free-listing.svg';
 import './index.scss';
 
 const SetupFreeListings = () => {
 	// TODO:
-	return <div>Setup Free Listings</div>;
+	return (
+		<div className="gla-setup-free-listings">
+			<div className="hero">
+				<StepContentHeader
+					className="hero__header"
+					step={ __( 'STEP THREE', 'google-listings-and-ads' ) }
+					title={ __(
+						'Configure your product listings',
+						'google-listings-and-ads'
+					) }
+					description={
+						<div>
+							<p className="subtitle">
+								{ __(
+									'Your free product listings will look something like this.',
+									'google-listings-and-ads'
+								) }
+							</p>
+							<p className="body">
+								{ __(
+									'Your product details, estimated shipping info and tax details will be displayed across Google.',
+									'google-listings-and-ads'
+								) }
+							</p>
+						</div>
+					}
+				/>
+				<GoogleFreeListingImage viewBox="0 0 720 319"></GoogleFreeListingImage>
+			</div>
+		</div>
+	);
 };
 
 export default SetupFreeListings;
