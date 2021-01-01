ff342b64202151622a18741d3eb76c52a62cb315
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-01-01 14:40:58 +0330

    fix PHPCS code style

--- src/Product/WCProductAdapter.php
+++ src/Product/WCProductAdapter.php
@@ -20,17 +20,17 @@ use WC_Product_Variation;
  * @package Automattic\WooCommerce\GoogleListingsAndAds\Product
  */
 class WCProductAdapter extends Google_Service_ShoppingContent_Product {
-	const AVAILABILITY_IN_STOCK = 'in stock';
-	const AVAILABILITY_OUT_OF_STOCK = 'out of stock';
+	public const AVAILABILITY_IN_STOCK     = 'in stock';
+	public const AVAILABILITY_OUT_OF_STOCK = 'out of stock';
 
-	const IMAGE_SIZE_FULL = 'full';
+	public const IMAGE_SIZE_FULL = 'full';
 
-	const CHANNEL_ONLINE = 'online';
+	public const CHANNEL_ONLINE = 'online';
 
 	/**
 	 * @var WC_Product WooCommerce product object
 	 */
-	protected $WC_product;
+	protected $wc_product;
 
 	/**
 	 * @var bool Whether tax is excluded from product price
@@ -45,7 +45,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 */
 	public function mapTypes( $array ) {
 		if ( ! empty( $array['wc_product_id'] ) ) {
-			$this->WC_product = wc_get_product( $array['wc_product_id'] );
+			$this->wc_product = wc_get_product( $array['wc_product_id'] );
 			$this->map_woocommerce_product();
 
 			// Google doesn't expect this field, so it's best to remove it
@@ -69,7 +69,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$this->setTargetCountry( $base_country );
 
 		// tax is excluded from price in US and CA
-		$this->tax_excluded = in_array( $base_country, array( 'US', 'CA' ), true );
+		$this->tax_excluded = in_array( $base_country, [ 'US', 'CA' ], true );
 		$this->tax_excluded = apply_filters( 'woocommerce_gla_tax_excluded', $this->tax_excluded );
 
 		$this->setChannel( self::CHANNEL_ONLINE );
@@ -85,7 +85,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$this->map_wc_general_attributes();
 
 		// set full size product image
-		$this->map_wc_product_image(self::IMAGE_SIZE_FULL );
+		$this->map_wc_product_image( self::IMAGE_SIZE_FULL );
 
 		// set availability
 		$this->map_wc_availability();
@@ -106,16 +106,16 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return void
 	 */
 	protected function map_wc_general_attributes() {
-		$offer_id = $this->WC_product->get_sku() ?? $this->WC_product->get_id();
+		$offer_id = $this->wc_product->get_sku() ?? $this->wc_product->get_id();
 		$this->setOfferId( $offer_id );
 
-		$this->setTitle( $this->WC_product->get_title() );
+		$this->setTitle( $this->wc_product->get_title() );
 		$this->setDescription( $this->get_wc_product_description() );
-		$this->setLink( $this->WC_product->get_permalink() );
+		$this->setLink( $this->wc_product->get_permalink() );
 
 		// set item group id for variations and variable products
 		if ( $this->is_variation() ) {
-			$parent_product    = wc_get_product( $this->WC_product->get_parent_id() );
+			$parent_product    = wc_get_product( $this->wc_product->get_parent_id() );
 			$parent_product_id = $parent_product->get_sku() ?? $parent_product->get_id();
 			$this->setItemGroupId( $parent_product_id );
 		} elseif ( $this->is_variable() ) {
@@ -128,12 +128,12 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 *
 	 * @return string
 	 */
-	protected function get_wc_product_description() {
-		$description = $this->WC_product->get_description() ?? $this->WC_product->get_short_description();
+	protected function get_wc_product_description(): string {
+		$description = $this->wc_product->get_description() ?? $this->wc_product->get_short_description();
 
 		// prepend the parent product description to the variation product
-		if ( $this->is_variation() && ! empty( $this->WC_product->get_parent_id() ) ) {
-			$parent_product     = wc_get_product( $this->WC_product->get_parent_id() );
+		if ( $this->is_variation() && ! empty( $this->wc_product->get_parent_id() ) ) {
+			$parent_product     = wc_get_product( $this->wc_product->get_parent_id() );
 			$parent_description = $parent_product->get_description() ?? $parent_product->get_short_description();
 			$new_line           = ! empty( $description ) && ! empty( $parent_description ) ? PHP_EOL : '';
 			$description        = $parent_description . $new_line . $description;
@@ -156,14 +156,14 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 *
 	 * @return void
 	 */
-	protected function map_wc_product_image( $image_size = self::IMAGE_SIZE_FULL ) {
-		$image_id          = $this->WC_product->get_image_id();
-		$gallery_image_ids = $this->WC_product->get_gallery_image_ids();
+	protected function map_wc_product_image( string $image_size ) {
+		$image_id          = $this->wc_product->get_image_id();
+		$gallery_image_ids = $this->wc_product->get_gallery_image_ids();
 
 		// check if we can use the parent product image if it's a variation
 		if ( $this->is_variation() ) {
-			$parent_product = wc_get_product( $this->WC_product->get_parent_id() );
-			$image_id = $image_id ?? $this->WC_product->get_image_id();
+			$parent_product    = wc_get_product( $this->wc_product->get_parent_id() );
+			$image_id          = $image_id ?? $this->wc_product->get_image_id();
 			$gallery_image_ids = ! empty( $gallery_image_ids ) ? $gallery_image_ids : $parent_product->get_gallery_image_ids();
 		}
 
@@ -180,9 +180,12 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$this->setImageLink( $image_link );
 
 		// set additional images
-		$gallery_image_links = array_map( function ( $gallery_image_id ) use ( $image_size ) {
-			return wp_get_attachment_image_url( $gallery_image_id, $image_size, false );
-		}, $gallery_image_ids );
+		$gallery_image_links = array_map(
+			function ( $gallery_image_id ) use ( $image_size ) {
+				return wp_get_attachment_image_url( $gallery_image_id, $image_size, false );
+			},
+			$gallery_image_ids
+		);
 		// Uniquify the set of additional images
 		$gallery_image_links = array_unique( $gallery_image_links, SORT_REGULAR );
 		$this->setAdditionalImageLinks( $gallery_image_links );
@@ -195,7 +198,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 */
 	protected function map_wc_availability() {
 		// todo: include 'preorder' status (maybe a new field for products / or using an extension?)
-		$availability = $this->WC_product->is_in_stock() ? self::AVAILABILITY_IN_STOCK : self::AVAILABILITY_OUT_OF_STOCK;
+		$availability = $this->wc_product->is_in_stock() ? self::AVAILABILITY_IN_STOCK : self::AVAILABILITY_OUT_OF_STOCK;
 		$this->setAvailability( $availability );
 	}
 
@@ -207,9 +210,9 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return void
 	 */
 	protected function map_wc_shipping_dimensions( $unit = 'cm' ) {
-		$length = $this->WC_product->get_length();
-		$width  = $this->WC_product->get_width();
-		$height = $this->WC_product->get_height();
+		$length = $this->wc_product->get_length();
+		$width  = $this->wc_product->get_width();
+		$height = $this->wc_product->get_height();
 
 		// Use cm if the unit isn't supported.
 		if ( ! in_array( $unit, [ 'in', 'cm' ], true ) ) {
@@ -220,18 +223,30 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$height = wc_get_dimension( (float) $height, $unit );
 
 		if ( $length > 0 && $width > 0 && $height > 0 ) {
-			$this->setShippingLength( new Google_Service_ShoppingContent_ProductShippingDimension([
-				'unit' => $unit,
-				'value' => $length,
-			]) );
-			$this->setShippingWidth( new Google_Service_ShoppingContent_ProductShippingDimension([
-				'unit' => $unit,
-				'value' => $width,
-			]) );
-			$this->setShippingHeight( new Google_Service_ShoppingContent_ProductShippingDimension([
-				'unit' => $unit,
-				'value' => $height,
-			]) );
+			$this->setShippingLength(
+				new Google_Service_ShoppingContent_ProductShippingDimension(
+					[
+						'unit'  => $unit,
+						'value' => $length,
+					]
+				)
+			);
+			$this->setShippingWidth(
+				new Google_Service_ShoppingContent_ProductShippingDimension(
+					[
+						'unit'  => $unit,
+						'value' => $width,
+					]
+				)
+			);
+			$this->setShippingHeight(
+				new Google_Service_ShoppingContent_ProductShippingDimension(
+					[
+						'unit'  => $unit,
+						'value' => $height,
+					]
+				)
+			);
 		}
 	}
 
@@ -248,11 +263,15 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 			$unit = 'g';
 		}
 
-		$weight = wc_get_weight( $this->WC_product->get_weight(), $unit );
-		$this->setShippingWeight( new Google_Service_ShoppingContent_ProductShippingWeight( [
-			'unit' => $unit,
-			'value' => $weight,
-		] ) );
+		$weight = wc_get_weight( $this->wc_product->get_weight(), $unit );
+		$this->setShippingWeight(
+			new Google_Service_ShoppingContent_ProductShippingWeight(
+				[
+					'unit'  => $unit,
+					'value' => $weight,
+				]
+			)
+		);
 	}
 
 	/**
@@ -261,7 +280,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return void
 	 */
 	protected function map_wc_prices() {
-		$this->map_wc_product_price( $this->WC_product );
+		$this->map_wc_product_price( $this->wc_product );
 
 		if ( $this->is_variable() ) {
 			// use the cheapest child price for the main product
@@ -275,15 +294,15 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return void
 	 */
 	protected function maybe_map_wc_children_prices() {
-		if ( ! $this->WC_product->has_child() ) {
+		if ( ! $this->wc_product->has_child() ) {
 			return;
 		}
 
-		$current_price = '' === $this->WC_product->get_regular_price() ?
+		$current_price = '' === $this->wc_product->get_regular_price() ?
 			null :
-			wc_get_price_including_tax( $this->WC_product, [ 'price' => $this->WC_product->get_regular_price() ] );
+			wc_get_price_including_tax( $this->wc_product, [ 'price' => $this->wc_product->get_regular_price() ] );
 
-		$children = $this->WC_product->get_children();
+		$children = $this->wc_product->get_children();
 		foreach ( $children as $child ) {
 			$child_product = wc_get_product( $child );
 			if ( ! $child_product ) {
@@ -308,16 +327,16 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	/**
 	 * Whether the given WooCommerce product is visible in store.
 	 *
-	 * @param WC_Product $WC_product
+	 * @param WC_Product $wc_product
 	 *
 	 * @return bool
 	 */
-	protected static function is_wc_product_visible( WC_Product $WC_product ): bool {
-		if ( $WC_product instanceof WC_Product_Variation ) {
-			return $WC_product->variation_is_visible();
+	protected static function is_wc_product_visible( WC_Product $wc_product ): bool {
+		if ( $wc_product instanceof WC_Product_Variation ) {
+			return $wc_product->variation_is_visible();
 		}
 
-		return $WC_product->is_visible();
+		return $wc_product->is_visible();
 	}
 
 	/**
@@ -335,10 +354,14 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 				wc_get_price_excluding_tax( $product, [ 'price' => $regular_price ] ) :
 				wc_get_price_including_tax( $product, [ 'price' => $regular_price ] );
 
-			$this->setPrice( new Google_Service_ShoppingContent_Price( [
-				'currency' => get_woocommerce_currency(),
-				'value' => $price,
-			] ) );
+			$this->setPrice(
+				new Google_Service_ShoppingContent_Price(
+					[
+						'currency' => get_woocommerce_currency(),
+						'value'    => $price,
+					]
+				)
+			);
 		}
 		// set sale price
 		$this->map_wc_product_sale_price( $product );
@@ -352,7 +375,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return void
 	 */
 	protected function map_wc_product_sale_price( WC_Product $product ) {
-		// Grab the sale price of the base product. Some plugins (Dyanmic
+		// Grab the sale price of the base product. Some plugins (Dynamic
 		// pricing as an example) filter the active price, but not the sale
 		// price. If the active price < the regular price treat it as a sale
 		// price.
@@ -360,7 +383,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$sale_price    = $product->get_sale_price();
 		$active_price  = $product->get_price();
 		if ( ( empty( $sale_price ) && $active_price < $regular_price ) ||
-		     ( ! empty( $sale_price ) && $active_price < $sale_price ) ) {
+			( ! empty( $sale_price ) && $active_price < $sale_price ) ) {
 			$sale_price = $active_price;
 		}
 
@@ -374,10 +397,14 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 			$now                 = new WC_DateTime();
 			$sale_price_end_date = $product->get_date_on_sale_to();
 			if ( empty( $sale_price_end_date ) || $sale_price_end_date >= $now ) {
-				$this->setSalePrice( new Google_Service_ShoppingContent_Price( [
-					'currency' => get_woocommerce_currency(),
-					'value' => $sale_price,
-				] ) );
+				$this->setSalePrice(
+					new Google_Service_ShoppingContent_Price(
+						[
+							'currency' => get_woocommerce_currency(),
+							'value'    => $sale_price,
+						]
+					)
+				);
 
 				$this->setSalePriceEffectiveDate( $this->get_wc_product_sale_price_effective_date( $product ) );
 			}
@@ -398,28 +425,28 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 		$now = new WC_DateTime();
 		// if we have a sale end date in the future, but no start date, set the start date to now()
 		if ( ! empty( $end_date ) &&
-		     $end_date > $now &&
-		     empty( $start_date )
+			$end_date > $now &&
+			empty( $start_date )
 		) {
 			$start_date = $now;
 		}
 		// if we have a sale start date in the past, but no end date, do not include the start date.
 		if ( ! empty( $start_date ) &&
-		     $start_date < $now &&
-		     empty( $end_date )
+			$start_date < $now &&
+			empty( $end_date )
 		) {
 			$start_date = null;
 		}
 		// if we have a start date in the future, but no end date, assume a one-day sale.
 		if ( ! empty( $start_date ) &&
-		     $start_date > $now &&
-		     empty( $end_date )
+			$start_date > $now &&
+			empty( $end_date )
 		) {
 			$end_date = clone $start_date;
 			$end_date->add( new DateInterval( 'P1D' ) );
 		}
 
-		return sprintf( '%s/%s', (string) $start_date, (string) $end_date);
+		return sprintf( '%s/%s', (string) $start_date, (string) $end_date );
 	}
 
 	/**
@@ -428,7 +455,7 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return bool
 	 */
 	protected function is_variation(): bool {
-		return $this->WC_product instanceof WC_Product_Variation;
+		return $this->wc_product instanceof WC_Product_Variation;
 	}
 
 	/**
@@ -437,6 +464,6 @@ class WCProductAdapter extends Google_Service_ShoppingContent_Product {
 	 * @return bool
 	 */
 	protected function is_variable(): bool {
-		return $this->WC_product instanceof WC_Product_Variable;
+		return $this->wc_product instanceof WC_Product_Variable;
 	}
 }
