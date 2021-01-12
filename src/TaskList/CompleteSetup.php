8fb0cdef6af54e786d257c44c7f6e159a8fa3b23
Jeremy Pry <jeremy.pry@gmail.com>
2021-01-12 16:18:48 -0500

    Enqueue conditionally, and remove old code

--- src/TaskList/CompleteSetup.php
+++ src/TaskList/CompleteSetup.php
@@ -46,7 +46,6 @@ class CompleteSetup implements Deactivateable, Service, Registerable {
 	 */
 	public function register(): void {
 		$this->register_assets();
-		$this->enqueue_assets();
 
 		add_action(
 			'admin_enqueue_scripts',
@@ -54,6 +53,8 @@ class CompleteSetup implements Deactivateable, Service, Registerable {
 				if ( ! $this->should_register_tasks() ) {
 					return;
 				}
+
+				$this->enqueue_assets();
 
 				// argument matches the task "key" property
 				do_action( 'add_woocommerce_extended_task_list_item', 'gla_complete_setup' );
@@ -84,53 +85,12 @@ class CompleteSetup implements Deactivateable, Service, Registerable {
 	}
 
 	/**
-	 * Add task list item
-	 */
-	protected function register_task(): void {
-		$script_path       = '/js/build/task-complete-setup.js';
-		$script_asset_path = "{$this->get_root_dir()}/js/build/task-complete-setup.asset.php";
-
-		$script_asset = file_exists( $script_asset_path )
-			? require $script_asset_path
-			: [
-				'dependencies' => [],
-				'version'      => filemtime( "{$this->get_root_dir()}/js/build/task-complete-setup.js" ),
-			];
-
-		$script_url = $this->get_plugin_url( $script_path );
-
-		wp_register_script(
-			'gla-task-complete-setup',
-			$script_url,
-			$script_asset['dependencies'],
-			$script_asset['version'],
-			true
-		);
-
-		$client_data = [
-			'isComplete' => get_option( 'gla_mc_setup_complete', false ),
-		];
-
-		wp_localize_script( 'gla-task-complete-setup', 'glaTaskData', $client_data );
-		wp_enqueue_script( 'gla-task-complete-setup' );
-
-		// argument matches the task "key" property
-		do_action(
-			'add_woocommerce_extended_task_list_item',
-			'gla_complete_setup'
-		);
-	}
-
-	/**
 	 * Deactivate the service.
 	 *
 	 * @return void
 	 */
 	public function deactivate(): void {
 		// argument matches the task "key" property
-		do_action(
-			'remove_woocommerce_extended_task_list_item',
-			'gla_complete_setup'
-		);
+		do_action( 'remove_woocommerce_extended_task_list_item', 'gla_complete_setup' );
 	}
 }
