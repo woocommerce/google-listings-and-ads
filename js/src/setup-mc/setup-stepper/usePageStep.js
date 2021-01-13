259819f8ef08c155cf10d37f5dad82b2f5a44f99
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-01-13 21:37:05 +0800

    Add documentation.

--- js/src/setup-mc/setup-stepper/usePageStep.js
+++ js/src/setup-mc/setup-stepper/usePageStep.js
@@ -6,12 +6,19 @@ import { useDispatch } from '@wordpress/data';
 import { useState } from '@wordpress/element';
 
 /**
+ * @typedef {Object} UsePageStepResult
+ * @property {string} pageStep - The value to control the stepper that the user is seeing.
+ * @property {Function} updatePageStep - Function to update pageStep and save it with Options API when needed.
+ */
+
+/**
  * A custom React hook to get the following:
  *
  * - `pageStep`: a value to control the stepper that the user is seeing.
- * - `updatePageStep`: a function to update the pageStep value and save it with Options API.
+ * - `updatePageStep(step`: a function to update the pageStep value and save it with Options API.
  *
  * @param {string} savedStep Initial pageStep value. This should come from a previously saved step value from Options API.
+ * @return {UsePageStepResult} usePageStepResult.
  */
 const usePageStep = ( savedStep ) => {
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
@@ -21,12 +28,12 @@ const usePageStep = ( savedStep ) => {
 	// pageStep should always be <= savedStep.
 	const [ pageStep, setPageStep ] = useState( savedStep );
 
-	const updatePageStep = ( stepKey ) => {
-		setPageStep( stepKey );
+	const updatePageStep = ( step ) => {
+		setPageStep( step );
 
-		if ( parseInt( savedStep, 10 ) < parseInt( stepKey, 10 ) ) {
+		if ( parseInt( savedStep, 10 ) < parseInt( step, 10 ) ) {
 			updateOptions( {
-				gla_setup_mc_saved_step: stepKey,
+				gla_setup_mc_saved_step: step,
 			} );
 		}
 	};
