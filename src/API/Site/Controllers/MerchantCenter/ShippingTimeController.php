3f4566f10293f1a58c4f4bd92cc2a4ef244d49ff
Jeremy Pry <jeremy.pry@gmail.com>
2021-01-13 18:10:42 -0500

    Add schema and traits to shipping times controller

--- src/API/Site/Controllers/MerchantCenter/ShippingTimeController.php
+++ src/API/Site/Controllers/MerchantCenter/ShippingTimeController.php
@@ -4,6 +4,8 @@ declare( strict_types=1 );
 namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\MerchantCenter;
 
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\BaseOptionsController;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\ControllerTrait;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\CountryCodeTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\TransportMethods;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsInterface;
 use WP_REST_Request;
@@ -17,6 +19,9 @@ defined( 'ABSPATH' ) || exit;
  */
 class ShippingTimeController extends BaseOptionsController {
 
+	use ControllerTrait;
+	use CountryCodeTrait;
+
 	/**
 	 * Register rest routes with WordPress.
 	 */
@@ -29,6 +34,7 @@ class ShippingTimeController extends BaseOptionsController {
 					'callback'            => $this->get_read_times_callback(),
 					'permission_callback' => $this->get_permission_callback(),
 				],
+				'schema' => $this->get_api_response_schema_callback(),
 			]
 		);
 	}
@@ -51,5 +57,47 @@ class ShippingTimeController extends BaseOptionsController {
 	 */
 	protected function get_shipping_times_option(): array {
 		return $this->options->get( OptionsInterface::SHIPPING_TIMES, [] );
+	}
+
+	/**
+	 * Get the item schema for the controller.
+	 *
+	 * @return array
+	 */
+	protected function get_item_schema(): array {
+		return [
+			'country'      => [
+				'type'        => 'string',
+				'description' => __( 'Country in which the shipping time applies.', 'google-listings-and-ads' ),
+				'context'     => [ 'view' ],
+				'readonly'    => true,
+			],
+			'country_code' => [
+				'type'              => 'string',
+				'description'       => __( 'Country code in ISO 3166-1 alpha-2 format.', 'google-listings-and-ads' ),
+				'context'           => [ 'view', 'edit' ],
+				'sanitize_callback' => $this->get_country_code_sanitize_callback(),
+				'validate_callback' => $this->get_country_code_validate_callback(),
+				'required'          => true,
+			],
+			'time'         => [
+				'type'              => 'integer',
+				'description'       => __( 'The shipping time in days.', 'google-listings-and-ads' ),
+				'context'           => [ 'view', 'edit' ],
+				'validate_callback' => 'rest_validate_request_arg',
+				'required'          => true,
+			],
+		];
+	}
+
+	/**
+	 * Get the item schema name for the controller.
+	 *
+	 * Used for building the API response schema.
+	 *
+	 * @return string
+	 */
+	protected function get_item_schema_name(): string {
+		return 'shipping_times';
 	}
 }
