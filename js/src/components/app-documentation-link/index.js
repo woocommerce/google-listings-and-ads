e5cbbbf893fc0c959747f5aab9ca70f4eb6360c5
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-01-14 23:53:50 +0800

    Add AppDocumentationLink component.

---
+++ js/src/components/app-documentation-link/index.js
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import TrackableLink from '../trackable-link';
+
+/**
+ * Renders a `TrackableLink` that will open the link in new tab and
+ * call `recordEvent` with `gla_documentation_link_click` event name.
+ *
+ * - `eventName="gla_documentation_link_click"`
+ * - `eventProps={ { context, link_id: linkId, href } }`
+ * - `type="external"` by default.
+ * - `target="_blank"` by default.
+ *
+ * @param {Object} props Props.
+ */
+const AppDocumentationLink = ( props ) => {
+	const { context, linkId, href, ...rest } = props;
+
+	return (
+		<TrackableLink
+			eventName="gla_documentation_link_click"
+			eventProps={ { context, link_id: linkId, href } }
+			type="external"
+			target="_blank"
+			href={ href }
+			{ ...rest }
+		/>
+	);
+};
+
+AppDocumentationLink.propTypes = {
+	/**
+	 * The screen or page of this DocumentationLink. This will be sent as part of track event properties.
+	 */
+	context: PropTypes.string.isRequired,
+
+	/**
+	 * An identifier for this DocumentationLink. This will be sent as part of track event properties.
+	 */
+	linkId: PropTypes.string.isRequired,
+};
+
+export default AppDocumentationLink;
