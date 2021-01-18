e3fc3ddfcd5f13b258a45ed7537d159f401f6355
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-01-18 10:37:09 +0800

    Better JSDoc comment with @link.

--- js/src/components/trackable-link/index.js
+++ js/src/components/trackable-link/index.js
@@ -9,9 +9,9 @@ import { Link } from '@woocommerce/components';
 import recordEvent from '../../utils/recordEvent';
 
 /**
- * A `@woocommerce/components` `Link` component that will call `recordEvent` with `eventName` and `eventProps` parameters upon click.
+ * A {@link module:@woocommerce/components~Link} component that will call `recordEvent` with `eventName` and `eventProps` parameters upon click.
  *
- * @param {Object} props Props to be forwarded to {@link Link}.
+ * @param {Object} props Props to be forwarded to {@link module:@woocommerce/components~Link}.
  * @param {string} props.eventName The eventName used in calling `recordEvent`.
  * @param {Object} [props.eventProps] Event properties to include in the event.
  */
