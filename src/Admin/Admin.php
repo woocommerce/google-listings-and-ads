55290ae517931f747cc20fd17a14a7c44980b635
Jason Conroy <jason@findingsimple.com>
2021-01-21 12:01:16 +1030

    Add additional plugin links for GLA to wp-admin plugins page

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -46,6 +46,13 @@ class Admin implements Service, Registerable, Conditional {
 				}
 			}
 		);
+
+		add_action(
+			'plugin_action_links_' . $this->get_plugin_basename(),
+			function( $links ) {
+				return $this->add_plugin_links( $links );
+			}
+		);
 	}
 
 	/**
@@ -75,5 +82,22 @@ class Admin implements Service, Registerable, Conditional {
 			defined( 'WC_ADMIN_PLUGIN_FILE' ) ? [ 'wc-admin-app' ] : [],
 			(string) filemtime( "{$this->get_root_dir()}/js/build/index.css" )
 		) );
+	}
+
+	/**
+	 * Adds links to the plugin's row in the "Plugins" wp-admin page.
+	 *
+	 * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+	 * @param array $links The existing list of links that will be rendered.
+	 */
+	protected function add_plugin_links( $links ): array {
+		$plugin_links = [];
+
+		// If setup complete, add settings link.
+		// $plugin_links[] = '<a href="' . esc_attr( $this->get_settings_url() ) . '">' . esc_html__( 'Settings', 'google-listings-and-ads' ) . '</a>';
+
+		$plugin_links[] = '<a href="' . esc_attr( $this->get_documentation_url() ) . '">' . esc_html__( 'Documentation', 'google-listings-and-ads' ) . '</a>';
+
+		return array_merge( $plugin_links, $links );
 	}
 }
