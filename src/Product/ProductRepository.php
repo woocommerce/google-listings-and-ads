0ce3b6087922e9bc23419419dba8dab89ceff0c7
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-01-26 17:21:54 +0330

    Set default product query limit to -1 (unlimited)

--- src/Product/ProductRepository.php
+++ src/Product/ProductRepository.php
@@ -20,76 +20,91 @@ class ProductRepository implements Service {
 	/**
 	 * Find and return an array of WooCommerce product objects based on the provided arguments.
 	 *
-	 * @param array $args Array of WooCommerce args (except 'return'), and product metadata.
+	 * @param array $args   Array of WooCommerce args (except 'return'), and product metadata.
+	 * @param int   $limit  Maximum number of results to retrieve or -1 for unlimited.
+	 * @param int   $offset Amount to offset product results.
+	 *
 	 * @see execute_woocommerce_query For more information about the arguments.
 	 *
 	 * @return WC_Product[] Array of WooCommerce product objects
 	 */
-	public function find( array $args = [] ): array {
+	public function find( array $args = [], int $limit = -1, int $offset = 0 ): array {
 		$args['return'] = 'objects';
 
-		return $this->execute_woocommerce_query( $args );
+		return $this->execute_woocommerce_query( $args, $limit, $offset );
 	}
 
 	/**
 	 * Find and return an array of WooCommerce product IDs based on the provided arguments.
 	 *
-	 * @param array $args Array of WooCommerce args (except 'return'), and product metadata.
+	 * @param array $args   Array of WooCommerce args (except 'return'), and product metadata.
+	 * @param int   $limit  Maximum number of results to retrieve or -1 for unlimited.
+	 * @param int   $offset Amount to offset product results.
+	 *
 	 * @see execute_woocommerce_query For more information about the arguments.
 	 *
 	 * @return int[] Array of WooCommerce product IDs
 	 */
-	public function find_ids( array $args = [] ): array {
+	public function find_ids( array $args = [], int $limit = -1, int $offset = 0 ): array {
 		$args['return'] = 'ids';
 
-		return $this->execute_woocommerce_query( $args );
+		return $this->execute_woocommerce_query( $args, $limit, $offset );
 	}
 
 	/**
 	 * Find and return an array of WooCommerce product objects based on the provided product IDs.
 	 *
-	 * @param int[] $ids Array of WooCommerce product IDs
+	 * @param int[] $ids    Array of WooCommerce product IDs
+	 * @param int   $limit  Maximum number of results to retrieve or -1 for unlimited.
+	 * @param int   $offset Amount to offset product results.
 	 *
 	 * @return WC_Product[] Array of WooCommerce product objects
 	 */
-	public function find_by_ids( array $ids = [] ): array {
+	public function find_by_ids( array $ids, int $limit = -1, int $offset = 0 ): array {
 		$args['include'] = $ids;
 
-		return $this->find( $args );
+		return $this->find( $args, $limit, $offset );
 	}
 
 	/**
 	 * Find and return an array of WooCommerce product IDs already submitted to Google Merchant Center.
 	 *
-	 * @param array $args Array of WooCommerce args (except 'return'), and product metadata.
-	 * @see execute_woocommerce_query For more information about the arguments.
+	 * @param int $limit  Maximum number of results to retrieve or -1 for unlimited.
+	 * @param int $offset Amount to offset product results.
 	 *
 	 * @return int[] Array of WooCommerce product IDs
 	 */
-	public function find_synced_product_ids( array $args = [] ): array {
+	public function find_synced_product_ids( int $limit = -1, int $offset = 0 ): array {
 		$google_id_key          = ProductMetaHandler::KEY_GOOGLE_IDS;
 		$compare_key            = ProductMetaHandler::get_meta_compare_key( $google_id_key );
 		$args[ $google_id_key ] = '';
 		$args[ $compare_key ]   = 'EXISTS';
 
-		return $this->find_ids( $args );
+		return $this->find_ids( $args, $limit, $offset );
 	}
 
 	/**
 	 * Find and return an array of WooCommerce products based on the provided arguments.
 	 *
-	 * @param array $args Array of WooCommerce args (see below), and product metadata.
+	 * @param array $args   Array of WooCommerce args (see below), and product metadata.
+	 * @param int   $limit  Maximum number of results to retrieve or -1 for unlimited.
+	 * @param int   $offset Amount to offset product results.
+	 *
 	 * @link https://github.com/woocommerce/woocommerce/wiki/wc_get_products-and-WC_Product_Query
 	 * @see ProductMetaHandler::VALID_KEYS For the list of meta data that can be used as query arguments.
 	 *
 	 * @return WC_Product[]|int[] Array of WooCommerce product objects or IDs, depending on the 'return' argument.
 	 */
-	protected function execute_woocommerce_query( array $args = [] ): array {
+	protected function execute_woocommerce_query( array $args = [], int $limit = -1, int $offset = 0 ): array {
+		$args['limit']  = $limit;
+		$args['offset'] = $offset;
+
 		return wc_get_products( $this->prepare_query_args( $args ) );
 	}
 
 	/**
 	 * @param array $args Array of WooCommerce args (except 'return'), and product metadata.
+	 *
 	 * @see execute_woocommerce_query For more information about the arguments.
 	 *
 	 * @return array
