08d3a094eb34fa4398a7386f23ef88d85631cf63
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-01-29 00:57:49 +0800

    Add AppButton component.

---
+++ js/src/components/app-button/index.js
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { Button } from '@wordpress/components';
+import { Spinner } from '@woocommerce/components';
+
+/**
+ * Internal dependencies
+ */
+import './index.scss';
+
+/**
+ * Renders a Button component with extra props.
+ *
+ * Set `loading` to `true` and it will render a disabled Button with a loading spinner indicator.
+ *
+ * ## Usage
+ *
+ * ```jsx
+ * <AppButton loading>
+ * 		Click Me
+ * </AppButton>
+ * ```
+ *
+ * @param {*} props Props to be forwarded to {@link Button}.
+ * @param {boolean} props.loading If true, the button will be disabled and will display a loading spinner indicator beside the button text.
+ */
+const AppButton = ( props ) => {
+	const { className = '', loading, children, ...rest } = props;
+
+	return (
+		<Button
+			className={ `app-button ${ className }` }
+			disabled={ loading }
+			{ ...rest }
+		>
+			{ loading && <Spinner /> }
+			{ children }
+		</Button>
+	);
+};
+
+export default AppButton;
