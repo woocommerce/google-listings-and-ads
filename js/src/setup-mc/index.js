1fa3a2f0b2aa568bd1398093fde30d753b884368
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-02-04 01:01:23 +0800

    Add a "root" import alias.

--- js/src/setup-mc/index.js
+++ js/src/setup-mc/index.js
@@ -1,7 +1,11 @@
 /**
+ * External dependencies
+ */
+import FullScreen from 'root/components/full-screen';
+
+/**
  * Internal dependencies
  */
-import FullScreen from '../components/full-screen';
 import TopBar from './top-bar';
 import SetupStepper from './setup-stepper';
 
