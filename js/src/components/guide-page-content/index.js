8a76ca5d1f4625e3d18fa6812fa87e0f62290408
Eason Su <eason.su.tw@gmail.com>
2021-02-04 11:33:29 +0800

    Add a shared component for the tracking link in guide-page-content

--- js/src/components/guide-page-content/index.js
+++ js/src/components/guide-page-content/index.js
@@ -6,6 +6,7 @@ import classnames from 'classnames';
 /**
  * Internal dependencies
  */
+import TrackableLink from '../trackable-link';
 import './index.scss';
 
 export default function GuidePageContent( { title, children } ) {
@@ -14,5 +15,22 @@ export default function GuidePageContent( { title, children } ) {
 			<h2 className="gla-guide__page-content__header">{ title }</h2>
 			<div className="gla-guide__page-content__body">{ children }</div>
 		</div>
+	);
+}
+
+export function ContentLink( props ) {
+	const { children, className, ...restProps } = props;
+	return (
+		<TrackableLink
+			className={ classnames(
+				'gla-guide__page-content__link',
+				className
+			) }
+			type="external"
+			target="_blank"
+			{ ...restProps }
+		>
+			{ children }
+		</TrackableLink>
 	);
 }
