48f375f7d87cc7c2a125b269e25b2e2143530a7d
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-02-06 01:19:15 +0800

    JSDoc for useAudienceSelectedCountryCodes.

--- js/src/hooks/useAudienceSelectedCountryCodes.js
+++ js/src/hooks/useAudienceSelectedCountryCodes.js
@@ -8,6 +8,11 @@ import { useDispatch, useSelect } from '@wordpress/data';
  */
 import { STORE_KEY } from '../data';
 
+/**
+ * Returns `[value, setValue]` for the selected country codes in the Step 2 Choose Audience page.
+ *
+ * Example format of `value`: `['US', 'AU', 'CN']`
+ */
 const useAudienceSelectedCountryCodes = () => {
 	const value = useSelect( ( select ) => {
 		return select( STORE_KEY ).getAudienceSelectedCountryCodes();
