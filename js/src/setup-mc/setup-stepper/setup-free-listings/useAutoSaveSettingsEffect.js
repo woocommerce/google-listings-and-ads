fdc2e205459a7f499ad328955d9840dd7e35335c
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-02-07 21:05:34 +0800

    JSDoc for useAutoSaveSettingsEffect.

--- js/src/setup-mc/setup-stepper/setup-free-listings/useAutoSaveSettingsEffect.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/useAutoSaveSettingsEffect.js
@@ -11,17 +11,25 @@ import { useAppDispatch } from '../../../data';
 
 const delay = 500;
 
+/**
+ * Automatically save settings value upon value change with a debounce delay.
+ * It does not save on first render since the first render is the loading of value from API.
+ *
+ * @param {Object} value Settings value object to be saved.
+ */
 const useAutoSaveSettingsEffect = ( value ) => {
 	const { saveSettings } = useAppDispatch();
 	const debouncedCallback = useDebouncedCallback( saveSettings, delay );
 	const ref = useRef( null );
 
 	useEffect( () => {
+		// do not save on first render.
 		if ( ! ref.current ) {
 			ref.current = value;
 			return;
 		}
 
+		// call the debounced callback.
 		debouncedCallback.callback( value );
 	}, [ debouncedCallback, value ] );
 };
