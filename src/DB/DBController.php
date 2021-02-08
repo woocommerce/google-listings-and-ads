d528ab75728132fbf65710d6938be034d933bfed
Jeremy Pry <jeremy.pry@gmail.com>
2021-02-08 18:19:21 -0500

    Add table handling to DB controller

--- src/DB/DBController.php
+++ src/DB/DBController.php
@@ -6,6 +6,7 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\DB;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Activateable;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Registerable;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
+use Psr\Container\ContainerInterface;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -17,12 +18,34 @@ defined( 'ABSPATH' ) || exit;
 class DBController implements Service, Registerable, Activateable {
 
 	/**
+	 * @var ContainerInterface
+	 */
+	protected $container;
+
+	/**
+	 * @var DBTable[]
+	 */
+	protected $tables;
+
+	/**
+	 * DBController constructor.
+	 *
+	 * @param ContainerInterface $container
+	 */
+	public function __construct( ContainerInterface $container ) {
+		$this->container = $container;
+	}
+
+	/**
 	 * Activate the service.
 	 *
 	 * @return void
 	 */
 	public function activate(): void {
-		// Handle setting up individual tables on activation.
+		$this->setup_table_controllers();
+		foreach ( $this->tables as $table ) {
+			$table->install();
+		}
 	}
 
 	/**
@@ -30,5 +53,12 @@ class DBController implements Service, Registerable, Activateable {
 	 */
 	public function register(): void {
 		// TODO: Implement register() method.
+	}
+
+	/**
+	 * Set up each of the table controllers.
+	 */
+	protected function setup_table_controllers() {
+		$this->tables = $this->container->get( 'db_table' );
 	}
 }
