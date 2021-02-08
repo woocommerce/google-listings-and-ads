4c72eb3b6e35656ac4068857209309291c933a44
Eason Su <eason.su.tw@gmail.com>
2021-02-08 14:06:30 +0800

    Add todo note and JSDoc to guide-page-content/index.js

--- js/src/components/guide-page-content/index.js
+++ js/src/components/guide-page-content/index.js
@@ -9,6 +9,16 @@ import classnames from 'classnames';
 import TrackableLink from '../trackable-link';
 import './index.scss';
 
+// TODO: These components are planned to be reused when implementing the Successful Campaign Creation Modal.
+//       Ref: https://github.com/woocommerce/google-listings-and-ads/issues/180
+
+/**
+ * Renders header title and content within a [Guide modal](https://wordpress.github.io/gutenberg/?path=/docs/components-guide--default).
+ *
+ * @param {Object} props Component props.
+ * @param {string} props.title Guide's title.
+ * @param {Array<JSX.Element>} props.children Array of react element to be rendered inside the Guide as content.
+ */
 export default function GuidePageContent( { title, children } ) {
 	return (
 		<div className="gla-guide__page-content">
@@ -18,6 +28,13 @@ export default function GuidePageContent( { title, children } ) {
 	);
 }
 
+/**
+ * Renders a TrackableLink component with preset props and additional styling. This link should be a link within the content of GuidePageContent.
+ *
+ * @param {Object} props Props to be forwarded to TrackableLink.
+ * @param {Array<JSX.Element>} props.children Array of react element to be rendered inside the TrackableLink.
+ * @param {string} [props.className] Additional CSS class name to be appended.
+ */
 export function ContentLink( props ) {
 	const { children, className, ...restProps } = props;
 	return (
