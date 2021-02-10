81e9fd445cecde1aad55fe7d262e26390d0ce2c0
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-02-11 02:43:13 +0800

    Code refactor with useJetpackAccount hook.

--- js/src/setup-mc/setup-stepper/setup-accounts/wordpressdotcom-account/card-content.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/wordpressdotcom-account/card-content.js
@@ -2,25 +2,17 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import AppSpinner from '../../../../components/app-spinner';
-import { STORE_KEY } from '../../../../data/constants';
 import TitleButtonLayout from '../title-button-layout';
 import ConnectAccount from './connect-account';
+import useJetpackAccount from '.~/hooks/useJetpackAccount';
 
 const CardContent = () => {
-	const { jetpack, isResolving } = useSelect( ( select ) => {
-		const acc = select( STORE_KEY ).getJetpackAccount();
-		const resolving = select( STORE_KEY ).isResolving(
-			'getJetpackAccount'
-		);
-
-		return { jetpack: acc, isResolving: resolving };
-	} );
+	const { jetpack, isResolving } = useJetpackAccount();
 
 	if ( isResolving ) {
 		return <AppSpinner />;
