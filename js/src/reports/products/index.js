957a9696d1783803bbf2a0a1e6496d71643bd5b4
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-02-11 18:52:10 +0100

    Add Reports' sub navigation.
    Use existing `.subsubsub` class, to share styling with the existing component written with jQuery.
    
    Closes https://github.com/woocommerce/google-listings-and-ads/issues/98.

--- js/src/reports/products/index.js
+++ js/src/reports/products/index.js
@@ -2,11 +2,13 @@
  * Internal dependencies
  */
 import TabNav from '../../tab-nav';
+import SubNav from '../sub-nav';
 
 const ProductsReport = () => {
 	return (
 		<div>
 			<TabNav initialName="reports" />
+			<SubNav initialName="products" />
 		</div>
 	);
 };
