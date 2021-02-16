50c0f55101e423d76f46d1e7a81fe805987cedc4
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-02-16 22:26:02 +0100

    Add FullContainer workaround,
    to make EditFreeCampaign page occupy entire pane.
    Similar to FullPage.

--- js/src/edit-free-campaign/index.js
+++ js/src/edit-free-campaign/index.js
@@ -1,3 +1,12 @@
+/**
+ * Internal dependencies
+ */
+import FullContainer from '.~/components/full-container';
+
 export default function EditFreeCampaign() {
-	return <h3>Configure your free listings.</h3>;
-}
+	return (
+		<FullContainer>
+			<h3>Configure your free listings.</h3>
+		</FullContainer>
+	);
+};
