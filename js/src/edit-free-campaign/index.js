d84601123b3d6ad15d7b061f18443b436f8087c4
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-02-17 01:03:26 +0100

    Provide basic Top bar, and Stepper,
    mock Steps with the ones from SetupMC.

--- js/src/edit-free-campaign/index.js
+++ js/src/edit-free-campaign/index.js
@@ -1,12 +1,88 @@
 /**
+ * External dependencies
+ */
+import { Link, Stepper } from '@woocommerce/components';
+import { getQuery, getNewPath, getHistory } from '@woocommerce/navigation';
+import { __ } from '@wordpress/i18n';
+import GridiconChevronLeft from 'gridicons/dist/chevron-left';
+
+/**
  * Internal dependencies
  */
 import FullContainer from '.~/components/full-container';
+import SetupFreeListings from '../setup-mc/setup-stepper/setup-free-listings';
+import ChooseAudience from '../setup-mc/setup-stepper/choose-audience';
+import './index.scss';
 
+/**
+ * Page Component to edit free campaigns.
+ * Provides two steps:
+ *  - Choose your audience
+ *  - Configure your free listings
+ * Given the user is editing an existing campaign, both steps should be available.
+ * The displayed step is driven by `pageStep` URL parameter, to make it easier to permalink and navigate back and forth.
+ */
 export default function EditFreeCampaign() {
+	const { pageStep = '1' } = getQuery();
+	const dashboardURL = getNewPath( {}, '/google/dashboard' );
+
+	const handleChooseAudienceContinue = () => {
+		getHistory().push( getNewPath( { pageStep: '2' } ) );
+	};
+
+	const handleSetupFreeListingsContinue = () => {
+		// TODO: provide a param to highlight edited program.
+		// TODO: Make SetupFreeListings actually call this callback.
+		getHistory().push( dashboardURL );
+	};
+
+	const handleStepClick = ( key ) => {
+		getHistory().push( getNewPath( { pageStep: key } ) );
+	};
 	return (
 		<FullContainer>
-			<h3>Configure your free listings.</h3>
+			<div className="gla-edit-campaign-top-bar">
+				<Link
+					className="back-button"
+					href={ dashboardURL }
+					type="wc-admin"
+				>
+					<GridiconChevronLeft size={ 20 } />
+					{ __( 'Dashboard', 'google-listings-and-ads' ) }
+				</Link>
+			</div>
+			<Stepper
+				className="gla-setup-stepper"
+				currentStep={ pageStep }
+				steps={ [
+					{
+						key: '1',
+						label: __(
+							'Choose your audience',
+							'google-listings-and-ads'
+						),
+						content: (
+							<ChooseAudience
+								onContinue={ handleChooseAudienceContinue }
+							/>
+						),
+						onClick: handleStepClick,
+					},
+					{
+						key: '2',
+						label: __(
+							'Configure your free listings',
+							'google-listings-and-ads'
+						),
+						content: (
+							<SetupFreeListings
+								onContinue={ handleSetupFreeListingsContinue }
+							/>
+						),
+						onClick: handleStepClick,
+					},
+				] }
+			/>
 		</FullContainer>
 	);
-};
+}
