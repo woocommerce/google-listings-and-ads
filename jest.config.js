03ad4bcc84d61165099f397d456bff816b6e7c78
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-02-22 13:58:22 +0100

    Extend WP-scripts jest config,
    instead of overwriting it.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+// Import WP-scripts presets to extend them,
+// see https://developer.wordpress.org/block-editor/packages/packages-scripts/#advanced-information-11.
+const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
+
 module.exports = {
+	...defaultConfig,
 	moduleNameMapper: {
-        "\\.~/(.*)$": "<rootDir>/js/src/$1",
+		// Transform our `.~/` alias.
+		'\\.~/(.*)$': '<rootDir>/js/src/$1',
 	},
 };
