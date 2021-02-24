d6b8e85127bfeaee4535207482ad58549e0c867d
Eason Su <eason.su.tw@gmail.com>
2021-02-24 17:00:42 +0800

    Add the FeaturesCard to Get Started page

--- js/src/get-started-page/index.js
+++ js/src/get-started-page/index.js
@@ -4,11 +4,13 @@
 import './index.scss';
 import Faqs from './faqs';
 import GetStartedCard from './get-started-card';
+import FeaturesCard from './features-card';
 
 const GetStartedPage = () => {
 	return (
 		<div className="woocommerce-marketing-google-get-started-page">
 			<GetStartedCard />
+			<FeaturesCard />
 			<Faqs></Faqs>
 		</div>
 	);
