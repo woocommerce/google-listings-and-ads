42b75ccc5cf26349f18df6e39d1273d43fbc9043
mikkamp <michael.kamp@automattic.com>
2021-02-26 15:23:25 +0000

    Add create/link functions

--- src/API/Site/Controllers/Ads/AccountController.php
+++ src/API/Site/Controllers/Ads/AccountController.php
@@ -3,12 +3,14 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\Ads;
 
-use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\BaseController;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\BaseOptionsController;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\TransportMethods;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Proxy as Middleware;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\AdsAccountState;
+use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\RESTServer;
 use Exception;
+use Psr\Container\ContainerInterface;
 use WP_REST_Request as Request;
 use WP_REST_Response as Response;
 
@@ -19,7 +21,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\Ads
  */
-class AccountController extends BaseController {
+class AccountController extends BaseOptionsController {
 
 	/**
 	 * @var ContainerInterface
@@ -110,12 +112,13 @@ class AccountController extends BaseController {
 	protected function create_or_link_account_callback(): callable {
 		return function( Request $request ) {
 			try {
-				$link_id    = absint( $request['id'] );
-				$account_id = $link_id ?
-					$this->middleware->link_ads_account( $link_id ) :
-					$this->middleware->create_ads_account();
+				$link_id = absint( $request['id'] );
+				if ( $link_id ) {
+					$this->use_existing_account( $link_id );
+				}
 
-				return $this->prepare_item_for_response( [ 'id' => $account_id ], $request );
+				$account = $this->setup_account();
+				return $this->prepare_item_for_response( $account, $request );
 			} catch ( Exception $e ) {
 				return new Response( [ 'message' => $e->getMessage() ], $e->getCode() ?: 400 );
 			}
@@ -175,5 +178,79 @@ class AccountController extends BaseController {
 	 */
 	protected function get_schema_title(): string {
 		return 'account';
+	}
+
+	/**
+	 * Use an existing Ads account. Mark the 'set_id' step as done and sets the Ads ID.
+	 *
+	 * @param int $account_id The Ads account ID to use.
+	 *
+	 * @throws Exception If there is already an Ads account ID.
+	 */
+	private function use_existing_account( int $account_id ) {
+		$ads_id = $this->options->get( OptionsInterface::ADS_ID );
+		if ( $ads_id && $ads_id !== $account_id ) {
+			throw new Exception(
+				/* translators: 1: is a numeric account ID */
+				sprintf( __( 'Ads account %1$d already connected.', 'google-listings-and-ads' ), $ads_id )
+			);
+		}
+
+		$state = $this->account_state->get();
+
+		// Don't do anything if this step was already finished.
+		if ( AdsAccountState::STEP_DONE === $state['set_id']['status'] ) {
+			return;
+		}
+
+		$this->middleware->link_ads_account( $account_id );
+		$state['set_id']['status'] = AdsAccountState::STEP_DONE;
+		$this->account_state->update( $state );
+	}
+
+	/**
+	 * Performs the steps necessary to setup an ads account.
+	 * Should always resume up at the last pending or unfinished step.
+	 * If the Ads account has already been created, the ID is simply returned.
+	 *
+	 * @return array The newly created (or pre-existing) Ads ID.
+	 * @throws Exception If an error occurs during any step.
+	 */
+	protected function setup_account(): array {
+		$state  = $this->account_state->get();
+		$ads_id = $this->options->get( OptionsInterface::ADS_ID );
+
+		foreach ( $state as $name => &$step ) {
+			if ( AdsAccountState::STEP_DONE === $step['status'] ) {
+				continue;
+			}
+
+			try {
+				switch ( $name ) {
+					case 'set_id':
+						// Just in case, don't create another Ads ID.
+						if ( ! empty( $ads_id ) ) {
+							break;
+						}
+						$ads_id = $this->middleware->create_ads_account();
+						break;
+					default:
+						throw new Exception(
+							/* translators: 1: is a string representing an unknown step name */
+							sprintf( __( 'Unknown ads account creation step %1$s', 'google-listings-and-ads' ), $name )
+						);
+				}
+				$step['status']  = AdsAccountState::STEP_DONE;
+				$step['message'] = '';
+				$this->account_state->update( $state );
+			} catch ( Exception $e ) {
+				$step['status']  = AdsAccountState::STEP_ERROR;
+				$step['message'] = $e->getMessage();
+				$this->account_state->update( $state );
+				throw $e;
+			}
+		}
+
+		return [ 'id' => $ads_id ];
 	}
 }
