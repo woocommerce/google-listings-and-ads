75573a54ed74d7219aa341243ec873672f0b756e
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-03-01 16:43:24 +0330

    Add interface for recurring ActionScheduler jobs

---
+++ src/Jobs/RecurringJobInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Jobs;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Interface RecurringJobInterface
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Jobs
+ */
+interface RecurringJobInterface extends StartOnHookInterface {
+
+	/**
+	 * Return the recurring job's interval in seconds.
+	 *
+	 * @return int
+	 */
+	public function get_interval(): int;
+
+}
