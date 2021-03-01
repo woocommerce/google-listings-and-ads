3ede4c0cf3402c5b8f3f98dac8f7823c810db980
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-01 17:39:51 +0800

    Dispatch receive data to store; display documentation link for claim URL.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/reclaim-url-card.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/reclaim-url-card.js
@@ -7,16 +7,19 @@ import { createInterpolateElement } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import Section from '.~/wcdl/section';
-import ContentButtonLayout from '../content-button-layout';
-import Subsection from '.~/wcdl/subsection';
+import AppDocumentationLink from '.~/components/app-documentation-link';
 import AppButton from '.~/components/app-button';
-import AccountId from './account-id';
+import Section from '.~/wcdl/section';
+import Subsection from '.~/wcdl/subsection';
 import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
+import { useAppDispatch } from '.~/data';
+import ContentButtonLayout from '../content-button-layout';
+import AccountId from './account-id';
 
 const ReclaimUrlCard = () => {
 	const { createNotice } = useDispatchCoreNotices();
+	const { receiveMCAccount } = useAppDispatch();
 	const [ fetchClaimOverwrite, { loading } ] = useApiFetchCallback( {
 		path: `/wc/gla/mc/accounts/claim-overwrite`,
 		method: 'POST',
@@ -28,7 +31,9 @@ const ReclaimUrlCard = () => {
 
 	const handleReclaimClick = async () => {
 		try {
-			await fetchClaimOverwrite();
+			const data = await fetchClaimOverwrite();
+
+			receiveMCAccount( data );
 		} catch ( e ) {
 			createNotice(
 				'error',
@@ -75,6 +80,18 @@ const ReclaimUrlCard = () => {
 					</AppButton>
 				</ContentButtonLayout>
 			</Section.Card.Body>
+			<Section.Card.Footer>
+				<AppDocumentationLink
+					context="setup-mc"
+					linkId="claim-url"
+					href="https://support.google.com/merchants/answer/176793"
+				>
+					{ __(
+						'Read more about claiming URLs',
+						'google-listings-and-ads'
+					) }
+				</AppDocumentationLink>
+			</Section.Card.Footer>
 		</Section.Card>
 	);
 };
