02a84937749737d21ceaaaf08e47118f982a788d
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-03 01:29:51 +0800

    JSDoc for CountryNames.

--- js/src/setup-mc/setup-stepper/setup-free-listings/components/country-names.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/components/country-names.js
@@ -10,6 +10,23 @@ import useCountryKeyNameMap from '.~/hooks/useCountryKeyNameMap';
 import useTargetAudienceFinalCountryCodes from '.~/hooks/useTargetAudienceFinalCountryCodes';
 import More from './more';
 
+/**
+ * Accepts an array of country codes and renders a comma-separated string of country names.
+ *
+ * If the array of country codes is the same as target audience countries, it will render a string `'all countries'`.
+ *
+ * If the country codes is more than `firstN`, it will render the `firstN` country names, and then follow with `'+ <remainingCount> more'`.
+ *
+ * Usage:
+ *
+ * ```
+ * <CountryNames countries={ ['AU', 'US' ] } />
+ * ```
+ *
+ * @param {Object} props
+ * @param {Array<string>} props.countries Array of country codes.
+ * @param {number} props.firstN Number of country names to be shown before displaying "+ <remainingCount> more".
+ */
 const CountryNames = ( props ) => {
 	const { countries, firstN = 5 } = props;
 	const { data: selectedCountryCodes } = useTargetAudienceFinalCountryCodes();
