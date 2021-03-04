73cdbbc8eee01a1ffa7f0ab6109fc615ce1f966b
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-03-04 16:22:15 +0330

    Add schedule cron to ActionScheduler interface

--- src/ActionScheduler/ActionScheduler.php
+++ src/ActionScheduler/ActionScheduler.php
@@ -77,6 +77,33 @@ class ActionScheduler implements ActionSchedulerInterface, Service {
 	}
 
 	/**
+	 * Schedule an action that recurs on a cron-like schedule.
+	 *
+	 * @param int    $timestamp The first instance of the action will be scheduled to run at a time
+	 *                          calculated after this timestamp matching the cron expression. This
+	 *                          can be used to delay the first instance of the action.
+	 * @param string $schedule  A cron-link schedule string
+	 * @param string $hook      The hook to trigger.
+	 * @param array  $args      Arguments to pass when the hook triggers.
+	 *
+	 * @return int The action ID.
+	 *
+	 * @see http://en.wikipedia.org/wiki/Cron
+	 *   *    *    *    *    *    *
+	 *   ┬    ┬    ┬    ┬    ┬    ┬
+	 *   |    |    |    |    |    |
+	 *   |    |    |    |    |    + year [optional]
+	 *   |    |    |    |    +----- day of week (0 - 7) (Sunday=0 or 7)
+	 *   |    |    |    +---------- month (1 - 12)
+	 *   |    |    +--------------- day of month (1 - 31)
+	 *   |    +-------------------- hour (0 - 23)
+	 *   +------------------------- min (0 - 59)
+	 */
+	public function schedule_cron( int $timestamp, string $schedule, string $hook, array $args = [] ): int {
+		return as_schedule_cron_action( $timestamp, $schedule, $hook, $args, $this->get_slug() );
+	}
+
+	/**
 	 * Enqueue an action to run one time, as soon as possible
 	 *
 	 * @param string $hook  The hook to trigger.
