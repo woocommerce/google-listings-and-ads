e893db75aea2bae8eb30d38f605bc0260947d421
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-08 23:03:39 +0800

    Display GoogleAccount in AdsStepper.

--- js/src/setup-ads/ads-stepper/setup-accounts/index.js
+++ js/src/setup-ads/ads-stepper/setup-accounts/index.js
@@ -1,5 +1,49 @@
-const SetupAccounts = () => {
-	return <div>setup account</div>;
+/**
+ * External dependencies
+ */
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import StepContent from '.~/components/stepper/step-content';
+import StepContentHeader from '.~/components/stepper/step-content-header';
+import StepContentFooter from '.~/components/stepper/step-content-footer';
+import GoogleAccount from '.~/components/google-account';
+
+const SetupAccounts = ( props ) => {
+	const { onContinue = () => {} } = props;
+
+	// TODO: call backend API to check and set the following to true/false.
+	const isContinueButtonDisabled = false;
+
+	return (
+		<StepContent>
+			<StepContentHeader
+				step={ __( 'STEP ONE', 'google-listings-and-ads' ) }
+				title={ __(
+					'Set up your accounts',
+					'google-listings-and-ads'
+				) }
+				description={ __(
+					'Connect your Google account and your Google Ads account to set up a paid Smart Shopping campaign.',
+					'google-listings-and-ads'
+				) }
+			/>
+			<GoogleAccount />
+			{ /* TODO: Google Ads Account setup here. */ }
+			<StepContentFooter>
+				<Button
+					isPrimary
+					disabled={ isContinueButtonDisabled }
+					onClick={ onContinue }
+				>
+					{ __( 'Continue', 'google-listings-and-ads' ) }
+				</Button>
+			</StepContentFooter>
+		</StepContent>
+	);
 };
 
 export default SetupAccounts;
