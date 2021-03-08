e6765c1a558f519d207e1aa40ecd66f74b2c8658
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-08 23:09:24 +0800

    Add audience section in CreateCampaign.

---
+++ js/src/setup-ads/ads-stepper/create-campaign/audience-section.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import Section from '.~/wcdl/section';
+
+const AudienceSection = () => {
+	return (
+		<Section
+			title={ __( 'Audience', 'google-listings-and-ads' ) }
+			description={
+				<p>
+					{ __(
+						'Choose where do you want your product ads to appear.',
+						'google-listings-and-ads'
+					) }
+				</p>
+			}
+		>
+			<Section.Card>
+				<Section.Card.Body>
+					{ /* TODO: labels and helptext. */ }
+					{ /* TODO: better component composition to use country single select. */ }
+					{ /* <AppCountryMultiSelect multiple={ false } /> */ }
+				</Section.Card.Body>
+			</Section.Card>
+		</Section>
+	);
+};
+
+export default AudienceSection;
