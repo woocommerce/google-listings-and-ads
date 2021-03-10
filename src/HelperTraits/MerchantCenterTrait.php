1e76c5e3d488730ac3b52a8f7ec2b6f16dafb628
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-03-10 16:15:51 +0330

    Add helper function to return store's target countries
    Defaults to store's base country.

--- src/HelperTraits/MerchantCenterTrait.php
+++ src/HelperTraits/MerchantCenterTrait.php
@@ -3,6 +3,8 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\HelperTraits;
 
+use Automattic\WooCommerce\GoogleListingsAndAds\GoogleHelper;
+use Automattic\WooCommerce\GoogleListingsAndAds\Options\Options;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsAwareTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsInterface;
 
@@ -16,6 +18,8 @@ defined( 'ABSPATH' ) || exit;
 trait MerchantCenterTrait {
 
 	use OptionsAwareTrait;
+
+	use GoogleHelper;
 
 	/**
 	 * Get whether Merchant Center setup is completed.
@@ -58,5 +62,26 @@ trait MerchantCenterTrait {
 		}
 
 		return false;
+	}
+
+	/**
+	 * @return string[] List of target countries specified in options. Defaults to WooCommerce store base country.
+	 */
+	protected function get_target_countries(): array {
+		$target_countries = [ WC()->countries->get_base_country() ];
+
+		$target_audience = $this->options->get( Options::TARGET_AUDIENCE );
+		if ( empty( $target_audience['location'] ) && empty( $target_audience['countries'] ) ) {
+			return $target_countries;
+		}
+
+		$location = strtolower( $target_audience['location'] );
+		if ( 'all' === $location ) {
+			$target_countries = $this->get_mc_supported_countries();
+		} elseif ( 'selected' === $location && ! empty( $target_audience['countries'] ) ) {
+			$target_countries = $target_audience['countries'];
+		}
+
+		return $target_countries;
 	}
 }
