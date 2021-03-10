ea0e147e10d7574bc3fdbedf674038ca7c76989d
Justin Palmer <justin.palmer@automattic.com>
2021-03-10 18:14:16 +0100

    Swap aw_ for ads_ in variables

--- src/Google/GlobalSiteTag.php
+++ src/Google/GlobalSiteTag.php
@@ -54,20 +54,20 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 			return;
 		}
 
-		$aw_conversion_id    = $conversion_action['conversion_id'];
-		$aw_conversion_label = $conversion_action['conversion_label'];
+		$ads_conversion_id    = $conversion_action['conversion_id'];
+		$ads_conversion_label = $conversion_action['conversion_label'];
 
 		add_action(
 			'wp_head',
-			function() use ( $aw_conversion_id ) {
-				$this->activate_global_site_tag( $aw_conversion_id );
+			function() use ( $ads_conversion_id ) {
+				$this->activate_global_site_tag( $ads_conversion_id );
 			},
 			999998
 		);
 		add_action(
 			'wp_head',
-			function() use ( $aw_conversion_id, $aw_conversion_label ) {
-				$this->maybe_display_event_snippet( $aw_conversion_id, $aw_conversion_label );
+			function() use ( $ads_conversion_id, $ads_conversion_label ) {
+				$this->maybe_display_event_snippet( $ads_conversion_id, $ads_conversion_label );
 			},
 			1000000
 		);
@@ -78,22 +78,22 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 	 * - Insert GST code, or
 	 * - Include the Google Ads conversion ID in WooCommerce Google Analytics Integration output, if available
 	 *
-	 * @param string $aw_conversion_id Google Ads account conversion ID.
+	 * @param string $ads_conversion_id Google Ads account conversion ID.
 	 */
-	public function activate_global_site_tag( string $aw_conversion_id ) {
+	public function activate_global_site_tag( string $ads_conversion_id ) {
 		if ( $this->container->get( GoogleGtagJs::class )->is_adding_framework() ) {
 			add_filter(
 				'woocommerce_gtag_snippet',
-				function( $gtag_snippet ) use ( $aw_conversion_id ) {
+				function( $gtag_snippet ) use ( $ads_conversion_id ) {
 					return preg_replace(
 						'~(\s)</script>~',
-						"\tgtag('config', '" . $aw_conversion_id . "');\n$1</script>",
+						"\tgtag('config', '" . $ads_conversion_id . "');\n$1</script>",
 						$gtag_snippet
 					);
 				}
 			);
 		} else {
-			$this->display_global_site_tag( $aw_conversion_id );
+			$this->display_global_site_tag( $ads_conversion_id );
 		}
 	}
 
@@ -101,20 +101,20 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 	/**
 	 * Display the JavaScript code to load the Global Site Tag framework.
 	 *
-	 * @param string $aw_conversion_id Google Ads account conversion ID.
+	 * @param string $ads_conversion_id Google Ads account conversion ID.
 	 */
-	protected function display_global_site_tag( string $aw_conversion_id ) {
+	protected function display_global_site_tag( string $ads_conversion_id ) {
 		// phpcs:disable WordPress.WP.EnqueuedResources.NonEnqueuedScript
 		?>
-		<!-- Global site tag (gtag.js) - Google Ads: <?php echo esc_js( $aw_conversion_id ); ?> -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_js( $aw_conversion_id ); ?>"></script>
+		<!-- Global site tag (gtag.js) - Google Ads: <?php echo esc_js( $ads_conversion_id ); ?> -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_js( $ads_conversion_id ); ?>"></script>
 		<script>
 			window.dataLayer = window.dataLayer || [];
 			function gtag(){dataLayer.push(arguments);}
 			gtag('js', new Date());
 			gtag('set', 'developer_id.<?php echo esc_js( self::DEVELOPER_ID ); ?>', true);
 
-			gtag('config','<?php echo esc_js( $aw_conversion_id ); ?>');
+			gtag('config','<?php echo esc_js( $ads_conversion_id ); ?>');
 		</script>
 		<?php
 		// phpcs:enable WordPress.WP.EnqueuedResources.NonEnqueuedScript
@@ -123,10 +123,10 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 	/**
 	 * Display the JavaScript code to track conversions on the order confirmation page.
 	 *
-	 * @param string $aw_conversion_id Google Ads account conversion ID.
-	 * @param string $aw_conversion_label Google Ads conversion label.
+	 * @param string $ads_conversion_id Google Ads account conversion ID.
+	 * @param string $ads_conversion_label Google Ads conversion label.
 	 */
-	public function maybe_display_event_snippet( string $aw_conversion_id, string $aw_conversion_label ): void {
+	public function maybe_display_event_snippet( string $ads_conversion_id, string $ads_conversion_label ): void {
 		// Only display on the order confirmation page.
 		if ( ! is_order_received_page() ) {
 			return;
@@ -147,7 +147,7 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 			?>
 	<script>
 		gtag('event', 'conversion', {
-			'send_to': '<?php echo esc_js( $aw_conversion_id ); ?>/<?php echo esc_js( $aw_conversion_label ); ?>',
+			'send_to': '<?php echo esc_js( $ads_conversion_id ); ?>/<?php echo esc_js( $ads_conversion_label ); ?>',
 			'value': '<?php echo esc_js( $order->get_total() ); ?>',
 			'currency': '<?php echo esc_js( $order->get_currency() ); ?>',
 			'transaction_id': '<?php echo esc_js( $order->get_transaction_id() ); ?>'
