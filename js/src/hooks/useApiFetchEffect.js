8afdb087b9d66b813457aa6c9cf94c0192a6ed05
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-12 03:02:12 +0800

    JSDoc for effect hooks.

--- js/src/hooks/useApiFetchEffect.js
+++ js/src/hooks/useApiFetchEffect.js
@@ -3,15 +3,23 @@
  */
 import { useEffect, useRef } from '@wordpress/element';
 import isEqual from 'lodash/isEqual';
+import { APIFetchOptions } from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
 import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 
+/**
+ * Calls apiFetch as a side effect. This uses useApiFetchCallback in a useEffect hook.
+ *
+ * If you pass falsey options value (e.g. undefined or null), the apiFetch effect won't be fired. This can be useful for initial rendering.
+ *
+ * @param {APIFetchOptions} options Options to be used in apiFetch call.
+ * @return {Object} FetchResult from useApiFetchCallback.
+ */
 const useApiFetchEffect = ( options ) => {
 	const optionsRef = useRef( options );
-
 	if ( ! isEqual( optionsRef.current, options ) ) {
 		optionsRef.current = options;
 	}
--- js/src/setup-ads/ads-stepper/create-campaign/budget-section/useFetchBudgetRecommendationEffect.js
+++ js/src/setup-ads/ads-stepper/create-campaign/budget-section/useFetchBudgetRecommendationEffect.js
@@ -3,6 +3,12 @@
  */
 import useApiFetchEffect from '.~/hooks/useApiFetchEffect';
 
+/**
+ * Fetch the budget recommendation for a country in a side effect.
+ *
+ * @param {string} countryCode Country code string, e.g. 'US'.
+ * @return {Object} Budget recommendation.
+ */
 const useFetchBudgetRecommendationEffect = ( countryCode ) => {
 	const options = countryCode && {
 		path: `/wc/gla/ads/campaigns/budget-recommendation/${ countryCode }`,
