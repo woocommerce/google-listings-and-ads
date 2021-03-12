e03712e2673c2406e148ec2ce450e94de709da2a
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-03-12 13:27:56 +0330

    Add helper methods to generate ID maps and batch requests

--- src/Google/BatchProductRequestEntry.php
+++ src/Google/BatchProductRequestEntry.php
@@ -4,6 +4,7 @@ declare( strict_types=1 );
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Google;
 
 use Automattic\WooCommerce\GoogleListingsAndAds\Product\WCProductAdapter;
+use Automattic\WooCommerce\GoogleListingsAndAds\Value\ProductIDMap;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -46,5 +47,33 @@ class BatchProductRequestEntry {
 	 */
 	public function get_product() {
 		return $this->product;
+	}
+
+	/**
+	 * @param ProductIDMap $product_id_map
+	 *
+	 * @return BatchProductRequestEntry[]
+	 */
+	public static function create_from_id_map( ProductIDMap $product_id_map ): array {
+		$product_entries = [];
+		foreach ( $product_id_map as $google_product_id => $wc_product_id ) {
+			$product_entries[] = new BatchProductRequestEntry( $wc_product_id, $google_product_id );
+		}
+
+		return $product_entries;
+	}
+
+	/**
+	 * @param BatchProductRequestEntry[] $request_entries
+	 *
+	 * @return ProductIDMap $product_id_map
+	 */
+	public static function convert_to_id_map( array $request_entries ): ProductIDMap {
+		$id_map = [];
+		foreach ( $request_entries as $request_entry ) {
+			$id_map[ (string) $request_entry->get_product() ] = $request_entry->get_wc_product_id();
+		}
+
+		return new ProductIDMap( $id_map );
 	}
 }
