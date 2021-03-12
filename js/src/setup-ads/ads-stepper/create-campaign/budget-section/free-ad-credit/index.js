920a871e980013e86e69d2a0f20cca7b12776e2e
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-03-12 16:34:34 +0800

    Change free ad credit description link.

--- js/src/setup-ads/ads-stepper/create-campaign/budget-section/free-ad-credit/index.js
+++ js/src/setup-ads/ads-stepper/create-campaign/budget-section/free-ad-credit/index.js
@@ -25,27 +25,32 @@ const FreeAdCredit = () => {
 				<div className="gla-free-ad-credit__description">
 					{ createInterpolateElement(
 						__(
-							'Whatever you spend over the next 30 days will be added back to your Google Ads account. <checklink>Check how much credit you can receive in your country</checklink>. <termslink>Terms and conditions apply</termslink>.',
+							'Whatever you spend over the next 30 days will be added back to your Google Ads account. Check how much credit you can receive in your country <checklink>here</checklink>.',
 							'google-listings-and-ads'
 						),
 						{
 							checklink: (
-								// TODO: what is the correct link?
+								// TODO: need to change this.
+								// Upon click, this should upon up a free credit modal, instead of going to an external site.
 								<AppDocumentationLink
 									context="setup-ads"
 									linkId="check-free-ad-credit-in-country"
 									href="https://support.google.com/adspolicy/answer/54818"
 								/>
 							),
-							termslink: (
-								<AppDocumentationLink
-									context="setup-ads"
-									linkId="free-ad-credit-terms"
-									href="https://www.google.com/ads/coupons/terms/"
-								/>
-							),
 						}
 					) }
+					<br />
+					<AppDocumentationLink
+						context="setup-ads"
+						linkId="free-ad-credit-terms"
+						href="https://www.google.com/ads/coupons/terms/"
+					>
+						{ __(
+							'Terms and conditions apply.',
+							'google-listings-and-ads'
+						) }
+					</AppDocumentationLink>
 				</div>
 			</div>
 		</div>
