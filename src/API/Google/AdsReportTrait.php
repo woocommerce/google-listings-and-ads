ccbfaa3c7dee307549b9516e1dd2f10cb11d44c7
mikkamp <michael.kamp@automattic.com>
2021-03-16 14:56:49 +0000

    Common variable for report data

--- src/API/Google/AdsReportTrait.php
+++ src/API/Google/AdsReportTrait.php
@@ -7,6 +7,7 @@ use Automattic\WooCommerce\Admin\API\Reports\TimeInterval;
 use DateTime;
 use Exception;
 use Google\Ads\GoogleAds\V6\Common\Segments;
+use Google\Ads\GoogleAds\V6\Services\GoogleAdsRow;
 use Google\ApiCore\ApiException;
 
 /**
@@ -15,6 +16,9 @@ use Google\ApiCore\ApiException;
  * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Google
  */
 trait AdsReportTrait {
+
+	/** @var array $report_data */
+	private $report_data = [];
 
 	/**
 	 * Get report data for campaigns.
@@ -26,73 +30,81 @@ trait AdsReportTrait {
 	 */
 	public function get_report_data( array $args ): array {
 		try {
-			$response  = $this->query( $this->get_report_query( $args ) );
-			$spend     = 0;
-			$sales     = 0;
-			$campaigns = [];
-			$intervals = [];
+			$this->report_data = [
+				'campaigns' => [],
+				'intervals' => [],
+				'spend'     => 0,
+				'sales'     => 0,
+			];
 
+			$response = $this->query( $this->get_report_query( $args ) );
 			foreach ( $response->iterateAllElements() as $row ) {
-				$campaign_spend = 0;
-				$campaign_sales = 0;
-				$campaign       = $row->getCampaign();
-				$metrics        = $row->getMetrics();
-				$segments       = $row->getSegments();
-
-				if ( $metrics ) {
-					$campaign_spend = $this->from_micro( $metrics->getCostMicros() );
-					$campaign_sales = $metrics->getConversionsValue();
-				}
-
-				if ( $segments && ! empty( $args['interval'] ) ) {
-					$interval = $this->get_segment_interval( $args['interval'], $segments );
-
-					if ( ! isset( $intervals[ $interval ] ) ) {
-						$intervals[ $interval ] = [
-							'interval'  => $interval,
-							'subtotals' => [
-								'spend' => $campaign_spend,
-								'sales' => $campaign_sales,
-							],
-						];
-					} else {
-						$intervals[ $interval ]['subtotals']['spend'] += $campaign_spend;
-						$intervals[ $interval ]['subtotals']['sales'] += $campaign_sales;
-					}
-				}
-
-				$campaign_id = $campaign->getId();
-				if ( ! isset( $campaigns[ $campaign_id ] ) ) {
-					$campaigns[ $campaign_id ] = [
-						'id'        => $campaign_id,
-						'name'      => $campaign->getName(),
-						'status'    => CampaignStatus::label( $campaign->getStatus() ),
-						'subtotals' => [
-							'spend' => $campaign_spend,
-							'sales' => $campaign_sales,
-						],
-					];
-				} else {
-					$campaigns[ $campaign_id ]['subtotals']['spend'] += $campaign_spend;
-					$campaigns[ $campaign_id ]['subtotals']['sales'] += $campaign_sales;
-				}
-
-				$spend += $campaign_spend;
-				$sales += $campaign_sales;
+				$this->add_report_row( $row, $args['interval'] ?? '' );
 			}
 
-			return [
-				'campaigns' => $campaigns,
-				'intervals' => $intervals,
-				'spend'     => $spend,
-				'sales'     => $sales,
-			];
+			return $this->report_data;
 		} catch ( ApiException $e ) {
 			do_action( 'gla_ads_client_exception', $e, __METHOD__ );
 
 			/* translators: %s Error message */
 			throw new Exception( sprintf( __( 'Unable to retrieve campaign report data: %s', 'google-listings-and-ads' ), $e->getBasicMessage() ) );
 		}
+	}
+
+	/**
+	 * Add data for a report row.
+	 *
+	 * @param GoogleAdsRow $row      Report row.
+	 * @param string       $interval Segment interval.
+	 */
+	protected function add_report_row( GoogleAdsRow $row, string $interval ) {
+		$campaign_spend = 0;
+		$campaign_sales = 0;
+
+		$campaign = $row->getCampaign();
+		$metrics  = $row->getMetrics();
+		$segments = $row->getSegments();
+
+		if ( $metrics ) {
+			$campaign_spend = $this->from_micro( $metrics->getCostMicros() );
+			$campaign_sales = $metrics->getConversionsValue();
+		}
+
+		if ( $segments && ! empty( $interval ) ) {
+			$index = $this->get_segment_interval( $interval, $segments );
+
+			if ( ! isset( $this->report_data['intervals'][ $index ] ) ) {
+				$this->report_data['intervals'][ $index ] = [
+					'interval'  => $index,
+					'subtotals' => [
+						'spend' => $campaign_spend,
+						'sales' => $campaign_sales,
+					],
+				];
+			} else {
+				$this->report_data['intervals'][ $index ]['subtotals']['spend'] += $campaign_spend;
+				$this->report_data['intervals'][ $index ]['subtotals']['sales'] += $campaign_sales;
+			}
+		}
+
+		$campaign_id = $campaign->getId();
+		if ( ! isset( $this->report_data['campaigns'][ $campaign_id ] ) ) {
+			$this->report_data['campaigns'][ $campaign_id ] = [
+				'id'        => $campaign_id,
+				'name'      => $campaign->getName(),
+				'status'    => CampaignStatus::label( $campaign->getStatus() ),
+				'subtotals' => [
+					'spend' => $campaign_spend,
+					'sales' => $campaign_sales,
+				],
+			];
+		} else {
+			$this->report_data['campaigns'][ $campaign_id ]['subtotals']['spend'] += $campaign_spend;
+			$this->report_data['campaigns'][ $campaign_id ]['subtotals']['sales'] += $campaign_sales;
+		}
+
+		$this->report_data['spend'] += $campaign_spend;
+		$this->report_data['sales'] += $campaign_sales;
 	}
 
 	/**
