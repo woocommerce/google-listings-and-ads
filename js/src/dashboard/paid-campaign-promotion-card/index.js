7c83fc1defe1252f0832735e2b6c2d55199617c6
Eason Su <eason.su.tw@gmail.com>
2021-03-17 18:47:28 +0800

    Show different promotion text for not-new ads account

--- js/src/dashboard/paid-campaign-promotion-card/index.js
+++ js/src/dashboard/paid-campaign-promotion-card/index.js
@@ -8,39 +8,64 @@ import {
 	CardHeader,
 	__experimentalText as Text,
 } from '@wordpress/components';
+import { Spinner } from '@woocommerce/components';
 
 /**
  * Internal dependencies
  */
+import useGoogleAdsAccount from '.~/hooks/useGoogleAdsAccount';
 import TrackableLink from '.~/components/trackable-link';
 import './index.scss';
 
-function PaidCampaignPromotionCard( { title } ) {
+const PromotionContent = ( { adsAccount } ) => {
 	const href = getNewPath( null, '/google/setup-ads' );
+	const showFreeCredit =
+		adsAccount.sub_account || adsAccount.status === 'disconnected';
+
+	let promoteText = __(
+		'Create your first campaign',
+		'google-listings-and-ads'
+	);
+
+	if ( showFreeCredit ) {
+		promoteText += __(
+			' and get up to $150* free',
+			'google-listings-and-ads'
+		);
+	}
+
+	return (
+		<>
+			<Text variant="body">{ promoteText }</Text>
+			<TrackableLink
+				className="components-button is-secondary is-small"
+				eventName="gla_dashboard_link_clicked"
+				eventProps={ {
+					context: 'add-paid-campaign-promotion',
+					href,
+				} }
+				href={ href }
+			>
+				{ __( 'Add paid campaign', 'google-listings-and-ads' ) }
+			</TrackableLink>
+		</>
+	);
+};
+
+function PaidCampaignPromotionCard( { title } ) {
+	const { googleAdsAccount } = useGoogleAdsAccount();
 
 	return (
 		<Card>
 			<CardHeader size="medium">
 				<Text variant="title.small">{ title }</Text>
 			</CardHeader>
-			<div className="gla-dashboard__performance__promotion-content">
-				<Text variant="body">
-					{ __(
-						'Create your first campaign and get up to $150* free',
-						'google-listings-and-ads'
-					) }
-				</Text>
-				<TrackableLink
-					className="components-button is-secondary is-small"
-					eventName="gla_dashboard_link_clicked"
-					eventProps={ {
-						context: 'add-paid-campaign-promotion',
-						href,
-					} }
-					href={ href }
-				>
-					{ __( 'Add paid campaign', 'google-listings-and-ads' ) }
-				</TrackableLink>
+			<div className="gla-dashboard__performance__promotion-container">
+				{ googleAdsAccount ? (
+					<PromotionContent adsAccount={ googleAdsAccount } />
+				) : (
+					<Spinner />
+				) }
 			</div>
 		</Card>
 	);
