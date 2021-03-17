29cf25bc9943ade5f41696f10e231232482197dc
Jeremy Pry <jeremy.pry@gmail.com>
2021-03-17 12:11:12 -0400

    Update google ads library and remove v3 from the removal script

--- bin/googleads-remove-older-versions.sh
+++ bin/googleads-remove-older-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo Removing googleads lib versions 3,4,5
-rm -rf vendor/googleads/google-ads-php/metadata/Google/Ads/GoogleAds/V{3,4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/V{3,4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Util/V{3,4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Lib/V{3,4,5}
+echo Removing googleads lib versions 4,5
+rm -rf vendor/googleads/google-ads-php/metadata/Google/Ads/GoogleAds/V{4,5}
+rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/V{4,5}
+rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Util/V{4,5}
+rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Lib/V{4,5}
