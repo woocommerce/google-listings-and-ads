a6aa02057f09144efcf9b9730ecd731fb3fe828a
Eason Su <eason.su.tw@gmail.com>
2021-03-24 17:26:51 +0800

    Add a DisconnectAccounts component for accounts disconnection section

---
+++ js/src/settings/disconnect-accounts/disconnect-section.js
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import AppSpinner from '.~/components/app-spinner';
+import Section from '.~/wcdl/section';
+
+export default function DisconnectSection( { isLoading, children } ) {
+	return (
+		<Section
+			title={ __( 'Linked accounts', 'google-listings-and-ads' ) }
+			description={ __(
+				'A Wordpress.com account, Google account, and Google Merchant Center account are required to use this extension in WooCommerce.',
+				'google-listings-and-ads'
+			) }
+		>
+			<Section.Card>
+				{ isLoading ? <AppSpinner /> : children }
+			</Section.Card>
+		</Section>
+	);
+}
---
+++ js/src/settings/disconnect-accounts/index.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import DisconnectSection from './disconnect-section';
+
+export default function DisconnectAccounts() {
+	return <DisconnectSection />;
+}
