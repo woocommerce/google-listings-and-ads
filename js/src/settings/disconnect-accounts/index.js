f49c8c1175c93248a53563ea5b5e7f6e49ca5192
Eason Su <eason.su.tw@gmail.com>
2021-03-24 18:12:12 +0800

    Add onClick events for disconnect buttons

--- js/src/settings/disconnect-accounts/index.js
+++ js/src/settings/disconnect-accounts/index.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
-import { createInterpolateElement } from '@wordpress/element';
+import { createInterpolateElement, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -15,6 +15,7 @@ import useGoogleAdsAccount from '.~/hooks/useGoogleAdsAccount';
 import Section from '.~/wcdl/section';
 import DisconnectSection from './disconnect-section';
 import AccountSubsection from './account-subsection';
+import { ALL_ACCOUNTS, ADS_ACCOUNT } from './constants';
 
 const withPrefix = ( id ) => {
 	return createInterpolateElement(
@@ -39,6 +40,16 @@ export default function DisconnectAccounts() {
 	);
 	const hasAdsAccount = googleAdsAccount?.status === 'connected';
 
+	const [ openedModal, setOpenedModal ] = useState( null );
+	const openDisconnectAllAccountsModal = () => setOpenedModal( ALL_ACCOUNTS );
+	const openDisconnectAdsAccountModal = () => setOpenedModal( ADS_ACCOUNT );
+	const dismissModal = () => setOpenedModal( null );
+
+	if ( openedModal ) {
+		// TODO: disconnect modal will be implemented by another PR.
+		return <Button onClick={ dismissModal }>Back</Button>;
+	}
+
 	return (
 		<DisconnectSection isLoading={ isLoading }>
 			<Section.Card.Body>
@@ -62,7 +73,11 @@ export default function DisconnectAccounts() {
 						title={ __( 'Google Ads', 'google-listings-and-ads' ) }
 						info={ withPrefix( googleAdsAccount?.id ) }
 					>
-						<Button isDestructive isLink>
+						<Button
+							isDestructive
+							isLink
+							onClick={ openDisconnectAdsAccountModal }
+						>
 							{ __(
 								'Disconnect Google Ads account only',
 								'google-listings-and-ads'
@@ -72,7 +87,10 @@ export default function DisconnectAccounts() {
 				) }
 			</Section.Card.Body>
 			<Section.Card.Footer>
-				<Button isDestructive>
+				<Button
+					isDestructive
+					onClick={ openDisconnectAllAccountsModal }
+				>
 					{ __(
 						'Disconnect all accounts',
 						'google-listings-and-ads'
