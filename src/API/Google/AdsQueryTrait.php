b12722e908d40ee7f77c4b522d845479c3c43718
mikkamp <michael.kamp@automattic.com>
2021-03-29 13:41:28 +0100

    Send pagination arguments

--- src/API/Google/AdsQueryTrait.php
+++ src/API/Google/AdsQueryTrait.php
@@ -17,11 +17,12 @@ trait AdsQueryTrait {
 	 * Run a Google Ads Query.
 	 *
 	 * @param string $query Query to run.
+	 * @param array  $args  Arguments to send to the query.
 	 *
 	 * @return PagedListResponse
 	 * @throws ApiException If the search call fails.
 	 */
-	protected function query( string $query ): PagedListResponse {
-		return $this->client->getGoogleAdsServiceClient()->search( $this->options->get_ads_id(), $query );
+	protected function query( string $query, array $args = [] ): PagedListResponse {
+		return $this->client->getGoogleAdsServiceClient()->search( $this->options->get_ads_id(), $query, $args );
 	}
 }
