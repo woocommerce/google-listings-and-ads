4f3cb0d74c98dfa0069f62dcd4d928449cdfc092
Justin Palmer <justin.palmer@automattic.com>
2021-04-01 11:15:08 +0200

    Fix route

--- bin/link-pre-push-phpcs.sh
+++ bin/link-pre-push-phpcs.sh
@@ -3,5 +3,5 @@
 # Symlink the git pre-push hook to its destination.
 if [ ! -h ".git/hooks/pre-push" ] ; then
   echo Linking pre-push hook script
-  ln -s "bin/pre-push" ".git/hooks/pre-push"
+  ln -s "../../bin/pre-push" ".git/hooks/pre-push"
 fi
