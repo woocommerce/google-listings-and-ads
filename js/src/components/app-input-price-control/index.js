6be008ff351d8f6d8a2f9b055c4f3b8736a17703
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-01 16:45:57 +0800

    JSDoc for AppInputPriceControl.

--- js/src/components/app-input-price-control/index.js
+++ js/src/components/app-input-price-control/index.js
@@ -6,6 +6,19 @@ import useStoreCurrency from '.~/hooks/useStoreCurrency';
 import AppInputControl from '.~/components/app-input-control';
 import parseStringToNumber from './parseStringToNumber';
 
+/**
+ * Renders an AppInputControl that formats number value
+ * into string displayed to the user,
+ * and parses user input string into number.
+ *
+ * Formatting and parsing is based on the store's currency setting.
+ *
+ * The AppInputControl will have the store's currency code as its suffix.
+ *
+ * @param {Object} props Props that will be passed to AppInputControl.
+ * @param {number} props.value Value of number type.
+ * @param {(value: number) => void} props.onChange onChange callback with value of number type.
+ */
 const AppInputPriceControl = ( props ) => {
 	const { value, onChange = () => {}, onBlur = () => {}, ...rest } = props;
 	const currencySetting = useStoreCurrency();
