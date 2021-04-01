03d6b15a0109ce228b97a9e7f1cfc5515103d2e1
Eason Su <eason.su.tw@gmail.com>
2021-04-01 19:32:25 +0800

    Remove unsaved prompt after form submitted

--- js/src/setup-ads/setup-ads-form.js
+++ js/src/setup-ads/setup-ads-form.js
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+import isEqual from 'lodash/isEqual';
+import { __ } from '@wordpress/i18n';
 import { useState, useEffect } from '@wordpress/element';
 import { Form } from '@woocommerce/components';
 import { getNewPath } from '@woocommerce/navigation';
@@ -8,6 +10,7 @@ import { getNewPath } from '@woocommerce/navigation';
 /**
  * Internal dependencies
  */
+import useNavigateAwayPromptEffect from '.~/hooks/useNavigateAwayPromptEffect';
 import SetupAdsFormContent from './setup-ads-form-content';
 import useSetupCompleteCallback from './useSetupCompleteCallback';
 
@@ -19,6 +22,7 @@ const initialValues = {
 };
 
 const SetupAdsForm = () => {
+	const [ editedValues, setEditedValues ] = useState( initialValues );
 	const [ isSubmitted, setSubmitted ] = useState( false );
 	const [ handleSetupComplete, isSubmitting ] = useSetupCompleteCallback();
 
@@ -41,10 +45,24 @@ const SetupAdsForm = () => {
 		}
 	}, [ isSubmitted ] );
 
+	const didCampaignChanged = ! isEqual( initialValues, editedValues );
+	const shouldPreventLeave = didCampaignChanged && ! isSubmitted;
+
+	useNavigateAwayPromptEffect(
+		__(
+			'You have unsaved campaign data. Are you sure you want to leave?',
+			'google-listings-and-ads'
+		),
+		shouldPreventLeave
+	);
+
 	return (
 		<Form
 			initialValues={ initialValues }
 			validate={ handleValidate }
+			onChangeCallback={ ( _, values ) => {
+				setEditedValues( values );
+			} }
 			onSubmitCallback={ ( values ) => {
 				const { amount, country: countryArr } = values;
 				const country = countryArr && countryArr[ 0 ];
