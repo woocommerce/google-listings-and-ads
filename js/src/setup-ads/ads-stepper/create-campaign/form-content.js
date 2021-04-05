fd31dcb5797ad1f43ceb7babe74e17d0c0aa0d7f
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-06 00:28:04 +0800

    Move AudienceSection to shared paid ads component.

--- js/src/setup-ads/ads-stepper/create-campaign/form-content.js
+++ js/src/setup-ads/ads-stepper/create-campaign/form-content.js
@@ -1,8 +1,13 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import StepContentFooter from '.~/components/stepper/step-content-footer';
-import AudienceSection from './audience-section';
+import AudienceSection from '.~/components/paid-ads/audience-section';
 import BudgetSection from './budget-section';
 import FaqsSection from './faqs-section';
 
@@ -11,7 +16,13 @@ const FormContent = ( props ) => {
 
 	return (
 		<>
-			<AudienceSection formProps={ formProps } />
+			<AudienceSection
+				formProps={ formProps }
+				countrySelectHelperText={ __(
+					'You can only select one country per campaign. ',
+					'google-listings-and-ads'
+				) }
+			/>
 			<BudgetSection formProps={ formProps } />
 			<FaqsSection />
 			<StepContentFooter>{ submitButton }</StepContentFooter>
