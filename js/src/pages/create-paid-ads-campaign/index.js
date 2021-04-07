da30cb77fa2bfb1e5a96ccb3fb257aa5bc8b9a37
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-08 00:24:56 +0800

    Add create paid ads campaign page.

---
+++ js/src/pages/create-paid-ads-campaign/index.js
@@ -0,0 +1,15 @@
+/**
+ * Internal dependencies
+ */
+import FullContainer from '.~/components/full-container';
+
+const CreatePaidAdsCampaign = () => {
+	// TODO: just a dummy render below.
+	return (
+		<FullContainer>
+			<div>Create paid ads campaign</div>
+		</FullContainer>
+	);
+};
+
+export default CreatePaidAdsCampaign;
