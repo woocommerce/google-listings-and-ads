f3f9005bb9740d613923669bb2117bc8016e3d65
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-10 02:20:52 +0800

    Check for adsSetupComplete in useAdsCampaigns to prevent error when ads setup is not completed yet.

--- js/src/hooks/useAdsCampaigns.js
+++ js/src/hooks/useAdsCampaigns.js
@@ -7,9 +7,22 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import { STORE_KEY } from '.~/data';
+import { glaData } from '.~/constants';
 
 const useAdsCampaigns = () => {
 	return useSelect( ( select ) => {
+		// TODO: ideally adsSetupComplete should be retrieved from API endpoint
+		// and then put into wp-data.
+		// With that in place, then we don't need to depend on glaData
+		// which requires force reload using window.location.href.
+		const { adsSetupComplete } = glaData;
+		if ( ! adsSetupComplete ) {
+			return {
+				loading: false,
+				data: [],
+			};
+		}
+
 		const { getAdsCampaigns, isResolving } = select( STORE_KEY );
 
 		const data = getAdsCampaigns();
