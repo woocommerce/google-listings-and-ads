63566641c3ff25f2dbd0685484d13abc57e26f67
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-14 00:43:30 +0800

    Display PauseProgramModal when toggling ProgramToggleControl.

--- js/src/dashboard/all-programs-table-card/program-toggle-control/index.js
+++ js/src/dashboard/all-programs-table-card/program-toggle-control/index.js
@@ -7,23 +7,54 @@ import { useState } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import PauseProgramModal from './pause-program-modal';
 import './index.scss';
 
 const ProgramToggleControl = ( props ) => {
 	const { program } = props;
 	const [ checked, setChecked ] = useState( program.active );
+	const [ showModal, setShowModal ] = useState( false );
 
-	// TODO: check value and show popup modal.
 	const handleChange = ( v ) => {
+		if ( v === false ) {
+			setShowModal( true );
+			return;
+		}
+
 		setChecked( v );
+		// TODO: fire API request to enable campaign.
+	};
+
+	const handleModalRequestClose = () => {
+		setShowModal( false );
+	};
+
+	const handleKeepActive = () => {
+		setShowModal( false );
+	};
+
+	const handlePauseCampaign = () => {
+		setShowModal( false );
+		setChecked( false );
+		// TODO: fire api request to pause campaign.
 	};
 
 	return (
-		<ToggleControl
-			className="gla-program-toggle-control"
-			checked={ checked }
-			onChange={ handleChange }
-		/>
+		<>
+			<ToggleControl
+				className="gla-program-toggle-control"
+				checked={ checked }
+				onChange={ handleChange }
+			/>
+			{ showModal && (
+				<PauseProgramModal
+					programId={ program.id }
+					onKeepActive={ handleKeepActive }
+					onPauseCampaign={ handlePauseCampaign }
+					onRequestClose={ handleModalRequestClose }
+				/>
+			) }
+		</>
 	);
 };
 
