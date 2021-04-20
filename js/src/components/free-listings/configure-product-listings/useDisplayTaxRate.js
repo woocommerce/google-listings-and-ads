7c6c14fbd0721e17352d09e70bc6aaddb5398a4e
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-04-20 15:34:53 +0200

    Fix `useDisplayTaxRate` condition to cater for `null` state.
    For example when audience countries are still resolving.
    Make it return `null` if we cannot tell yet due to some data being unresolved and the other not containing `US`.
    
    Addresses https://github.com/woocommerce/google-listings-and-ads/pull/491#discussion_r614528931.

--- js/src/components/free-listings/configure-product-listings/useDisplayTaxRate.js
+++ js/src/components/free-listings/configure-product-listings/useDisplayTaxRate.js
@@ -12,24 +12,25 @@ import useStoreCountry from '.~/hooks/useStoreCountry';
  *
  * The Tax Rate section should be displayed when the store's country code or one of the target audience country codes is `'US'`.
  *
- * @param {Array<CountryCode>} [audienceCountries=[]] If no audience countries are given, check only store's one.
+ * @param {Array<CountryCode>} [audienceCountries] If no audience countries are given, check store's one.
+ * @return {boolean | null} `true` if the Tax Rate section should be displayed, `false` if shouldn't, `null` we cannot tell - we still wait for store or audience to be determined.
  */
-const useDisplayTaxRate = ( audienceCountries = [] ) => {
+const useDisplayTaxRate = ( audienceCountries = null ) => {
 	const { code: storeCountry } = useStoreCountry();
 
-	return shouldDisplayTaxRate( [ ...audienceCountries, storeCountry ] );
+	// If any country is US return `true`.
+	if ( storeCountry === 'US' ) {
+		return true;
+	}
+	if ( audienceCountries && audienceCountries.includes( 'US' ) ) {
+		return true;
+	}
+	// If we cannot tell yet, return `null`.
+	if ( storeCountry === null || audienceCountries === null ) {
+		return null;
+	}
+	// Store's and audience countries are available and were checked, none contains `US`.
+	return false;
 };
 
 export default useDisplayTaxRate;
-
-/**
- * Checks whether the Tax Rate section should be displayed,
- * for a given list of involved countries (audience and store's).
- * The Tax Rate section should be displayed when there is `'US'` country code in the countries list.
- *
- * @param {Array<CountryCode>} countries
- * @return {boolean} `true` if the Tax Rate section should be displayed, `false` otherwise.
- */
-export function shouldDisplayTaxRate( countries ) {
-	return countries.includes( 'US' );
-}
