06f0cb1f676235db691451c244a4959e5d74089b
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-22 23:37:37 +0800

    Better loading and error UI.

--- js/src/product-feed/product-statistics/index.js
+++ js/src/product-feed/product-statistics/index.js
@@ -3,82 +3,92 @@
  */
 import { format as formatDate } from '@wordpress/date';
 import { __ } from '@wordpress/i18n';
-import { SummaryList, SummaryNumber } from '@woocommerce/components';
+import {
+	SummaryList,
+	SummaryListPlaceholder,
+	SummaryNumber,
+} from '@woocommerce/components';
 
 /**
  * Internal dependencies
  */
-import AppSpinner from '.~/components/app-spinner';
 import useMCProductStatistics from '.~/hooks/useMCProductStatistics';
 import ProductStatusHelpPopover from './product-status-help-popover';
 import './index.scss';
 
 const ProductStatistics = () => {
 	const { hasFinishedResolution, data } = useMCProductStatistics();
-
-	if ( ! hasFinishedResolution ) {
-		return <AppSpinner />;
-	}
-
-	/**
-	 * Check for error.
-	 */
-	if ( hasFinishedResolution && ! data ) {
-		return null;
-	}
-
-	const { timestamp, statistics } = data;
-	const lastUpdatedDateTime = new Date( timestamp * 1000 );
+	const loading = ! hasFinishedResolution;
+	const error = hasFinishedResolution && ! data;
 
 	return (
 		<div className="gla-product-statistics">
 			<div className="gla-product-statistics__last-updated">
-				{ __( 'Last updated: ', 'google-listings-and-ads' ) }
-				{ formatDate( 'Y-m-d H:i:s', lastUpdatedDateTime ) }
-				<ProductStatusHelpPopover />
+				{ loading && __( 'Updating…', 'google-listings-and-ads' ) }
+				{ error &&
+					__(
+						'An error occurred while loading product statistics. Please try again later.',
+						'google-listings-and-ads'
+					) }
+				{ data && (
+					<>
+						{ __( 'Last updated: ', 'google-listings-and-ads' ) }
+						{ formatDate(
+							'Y-m-d H:i:s',
+							new Date( data.timestamp * 1000 )
+						) }
+						<ProductStatusHelpPopover />
+					</>
+				) }
 			</div>
 			<div className="gla-product-statistics__summaries">
-				<SummaryList>
-					{ () => [
-						<SummaryNumber
-							key="active"
-							label={ __(
-								'Active / Partially Active',
-								'google-listings-and-ads'
-							) }
-							value={ statistics.active }
-						/>,
-						<SummaryNumber
-							key="expiring"
-							label={ __(
-								'Expiring',
-								'google-listings-and-ads'
-							) }
-							value={ statistics.expiring }
-						/>,
-						<SummaryNumber
-							key="pending"
-							label={ __( 'Pending', 'google-listings-and-ads' ) }
-							value={ statistics.pending }
-						/>,
-						<SummaryNumber
-							key="disapproved"
-							label={ __(
-								'Disapproved',
-								'google-listings-and-ads'
-							) }
-							value={ statistics.disapproved }
-						/>,
-						<SummaryNumber
-							key="not_synced"
-							label={ __(
-								'Not Synced',
-								'google-listings-and-ads'
-							) }
-							value={ statistics.not_synced }
-						/>,
-					] }
-				</SummaryList>
+				{ loading && <SummaryListPlaceholder numberOfItems={ 5 } /> }
+				{ data && (
+					<SummaryList>
+						{ () => [
+							<SummaryNumber
+								key="active"
+								label={ __(
+									'Active / Partially Active',
+									'google-listings-and-ads'
+								) }
+								value={ data.statistics.active }
+							/>,
+							<SummaryNumber
+								key="expiring"
+								label={ __(
+									'Expiring',
+									'google-listings-and-ads'
+								) }
+								value={ data.statistics.expiring }
+							/>,
+							<SummaryNumber
+								key="pending"
+								label={ __(
+									'Pending',
+									'google-listings-and-ads'
+								) }
+								value={ data.statistics.pending }
+							/>,
+							<SummaryNumber
+								key="disapproved"
+								label={ __(
+									'Disapproved',
+									'google-listings-and-ads'
+								) }
+								value={ data.statistics.disapproved }
+							/>,
+							<SummaryNumber
+								key="not_synced"
+								label={ __(
+									'Not Synced',
+									'google-listings-and-ads'
+								) }
+								value={ data.statistics.not_synced }
+							/>,
+						] }
+					</SummaryList>
+				) }
 			</div>
 		</div>
 	);
