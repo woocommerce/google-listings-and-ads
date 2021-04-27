17d251339263ddb11504c36301fefa0db44fa796
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-27 20:50:56 +0800

    Add hasFinishedResolution to useGoogleMCAccount.

--- js/src/hooks/useGoogleMCAccount.js
+++ js/src/hooks/useGoogleMCAccount.js
@@ -10,13 +10,14 @@ import { STORE_KEY } from '.~/data/constants';
 import useGoogleAccount from './useGoogleAccount';
 
 const useGoogleMCAccount = () => {
-	const { google, isResolving } = useGoogleAccount();
+	const { google, isResolving, hasFinishedResolution } = useGoogleAccount();
 
 	return useSelect( ( select ) => {
 		if ( ! google || google.active === 'no' ) {
 			return {
 				googleMCAccount: undefined,
 				isResolving,
+				hasFinishedResolution,
 			};
 		}
 
@@ -24,10 +25,14 @@ const useGoogleMCAccount = () => {
 		const isResolvingGoogleMCAccount = select( STORE_KEY ).isResolving(
 			'getGoogleMCAccount'
 		);
+		const hasFinishedResolutionGoogleMCAccount = select(
+			STORE_KEY
+		).hasFinishedResolution( 'getGoogleMCAccount' );
 
 		return {
 			googleMCAccount: acc,
 			isResolving: isResolvingGoogleMCAccount,
+			hasFinishedResolution: hasFinishedResolutionGoogleMCAccount,
 		};
 	} );
 };
