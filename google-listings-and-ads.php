8486fe932e42135b342230108c1058828075ea16
mikkamp <michael.kamp@automattic.com>
2021-04-27 16:41:20 +0100

    Release version 0.5.1

--- google-listings-and-ads.php
+++ google-listings-and-ads.php
@@ -3,7 +3,7 @@
  * Plugin Name: Google Listings and Ads
  * Plugin URL: https://woocommerce.com/
  * Description: Native integration with Google that allows merchants to easily display their products across Google’s network.
- * Version: 0.5.0
+ * Version: 0.5.1
  * Author: WooCommerce
  * Author URI: https://woocommerce.com/
  * Text Domain: google-listings-and-ads
@@ -26,7 +26,7 @@ use Psr\Container\ContainerInterface;
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'GLA_VERSION', '0.5.0' ); // WRCS: DEFINED_VERSION.
+define( 'GLA_VERSION', '0.5.1' ); // WRCS: DEFINED_VERSION.
 
 // Load and initialize the autoloader.
 require_once __DIR__ . '/src/Autoloader.php';
