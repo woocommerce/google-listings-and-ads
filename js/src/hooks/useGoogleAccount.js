aaae1d059a1d4c9ae8e809b20be147aa2d9b70f9
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-28 22:38:07 +0800

    Temp fix for hasFinishedResolution is always false.

--- js/src/hooks/useGoogleAccount.js
+++ js/src/hooks/useGoogleAccount.js
@@ -7,12 +7,20 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import { STORE_KEY } from '.~/data/constants';
-import useJetpackAccount from './useJetpackAccount';
+// import useJetpackAccount from './useJetpackAccount';
 
 const useGoogleAccount = () => {
-	const { jetpack, isResolving, hasFinishedResolution } = useJetpackAccount();
+	// const { jetpack, isResolving, hasFinishedResolution } = useJetpackAccount();
 
 	return useSelect( ( select ) => {
+		const jetpack = select( STORE_KEY ).getJetpackAccount();
+		const isResolving = select( STORE_KEY ).isResolving(
+			'getJetpackAccount'
+		);
+		const hasFinishedResolution = select( STORE_KEY ).hasFinishedResolution(
+			'getJetpackAccount'
+		);
+
 		if ( ! jetpack || jetpack.active === 'no' ) {
 			return {
 				google: undefined,
@@ -21,19 +29,27 @@ const useGoogleAccount = () => {
 			};
 		}
 
-		const acc = select( STORE_KEY ).getGoogleAccount();
-		const isResolvingGoogle = select( STORE_KEY ).isResolving(
+		const {
+			getGoogleAccount,
+			isResolving: isResolvingFn,
+			hasFinishedResolution: hasFinishedResolutionFn,
+		} = select( STORE_KEY );
+
+		const acc = getGoogleAccount();
+		const isResolvingGoogle = isResolvingFn( 'getGoogleAccount' );
+		const hasFinishedResolutionGoogle = hasFinishedResolutionFn(
 			'getGoogleAccount'
 		);
-		const hasFinishedResolutionGoogle = select(
-			STORE_KEY
-		).hasFinishedResolution( 'getGoogleAccount' );
 
-		return {
+		const result = {
 			google: acc,
 			isResolving: isResolvingGoogle,
 			hasFinishedResolution: hasFinishedResolutionGoogle,
 		};
+
+		console.log( 'result google: ', result );
+
+		return result;
 	} );
 };
 
