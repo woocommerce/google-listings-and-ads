a75341d97a71e34b1725e98d7b348c61034c6ccb
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-04-30 17:28:00 +0200

    Connect Programs filter to data source.
    Create `ProgramsReportFilters` to use customized `ReportsFilter`, that will use Date, Programs, and
    Compare filters consistently with other wc-admin analytics.
    Provide custom autocompleter for programs.
    
    Implements part of https://github.com/woocommerce/google-listings-and-ads/issues/242.

--- js/src/reports/programs/filter-config.js
+++ js/src/reports/programs/filter-config.js
@@ -2,86 +2,110 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-/**
- * Internal dependencies
- */
-import { getProgramLabels } from './mocked-programs-data';
+import { applyFilters } from '@wordpress/hooks';
+import { getIdsFromQuery } from '@woocommerce/navigation';
 
-export const programsFilterConfig = {
-	label: __( 'Show', 'google-listings-and-ads' ),
-	staticParams: [ 'period' ],
-	param: 'programs',
-	filters: [
-		{
-			label: __( 'All Google programs', 'google-listings-and-ads' ),
-			value: 'all',
+export const programsFilterConfig = ( adsCampaigns ) => {
+	const autocompleter = {
+		name: 'programs',
+		// Promise.resolve will not be needed after
+		// https://github.com/woocommerce/woocommerce-admin/issues/6061
+		options: () => {
+			return Promise.resolve( adsCampaigns );
 		},
-		{
-			label: __( 'Single program', 'google-listings-and-ads' ),
-			value: 'single',
-			subFilters: [
-				{
-					component: 'Search',
-					value: 'single_program',
-					path: [ 'select_program' ],
-					settings: {
-						// TODO: setup config once following issues are fixed:
-						// https://github.com/woocommerce/woocommerce-admin/issues/6061
-						// https://github.com/woocommerce/woocommerce-admin/issues/6062
-						type: 'custom_is_broken',
-						param: 'custom_programs',
-						getLabels: getProgramLabels,
-						labels: {
-							placeholder: __(
-								'Type to search for a program',
-								'google-listings-and-ads'
-							),
-							button: __(
-								'Single Program',
-								'google-listings-and-ads'
-							),
-						},
-						autocompleter: {
-							name: 'fruit',
-							// The prefix that triggers this completer
-							triggerPrefix: '~',
-							// According to https://github.com/WordPress/gutenberg/tree/master/packages/components/src/autocomplete#usage
-							// this could be an Object,
-							// but wc-admin#Search https://woocommerce.github.io/woocommerce-admin/#/components/packages/search/README?id=search
-							// requires a function
-							options: [
-								{ visual: '🍎', name: 'Apple', id: 1 },
-								{ visual: '🍊', name: 'Orange', id: 2 },
-								{ visual: '🍇', name: 'Grapes', id: 3 },
-							],
-							// Returns a label for an option like "🍊 Orange"
-							getOptionLabel: ( option ) => (
-								<span>
-									<span className="icon">
-										{ option.visual }
-									</span>
-									{ option.name }
-								</span>
-							),
-							// Declares that options should be matched by their name
-							getOptionKeywords: ( option ) => [ option.name ],
-							// Declares that the Grapes option is disabled
-							isOptionDisabled: ( option ) =>
-								option.name === 'Grapes',
-							// Declares completions should be inserted as abbreviations
-							getOptionCompletion: ( option ) => (
-								<abbr title={ option.name }>
-									{ option.visual }
-								</abbr>
-							),
+		getOptionIdentifier: ( option ) => option.id,
+		getOptionLabel: ( option ) => option.name,
+		getOptionKeywords: ( option ) => [ option.name ],
+		// This is slightly different than gutenberg/Autocomplete, we don't support different methods
+		// of replace/insertion, so we can just return the value.
+		getOptionCompletion: ( program ) => ( {
+			key: program.id,
+			label: program.name,
+		} ),
+	};
+
+	function getLabels( param ) {
+		// TODO: cover that case
+		if ( ! adsCampaigns ) {
+			return Promise.resolve( [] );
+		}
+		// Get program id(s) from query parameter.
+		const ids = new Set( getIdsFromQuery( param ) );
+		const result = adsCampaigns
+			.filter( ( campaign ) => ids.has( campaign.id ) )
+			.map( ( campaign ) => ( {
+				key: campaign.id,
+				label: campaign.name,
+			} ) );
+		return Promise.resolve( result );
+	}
+
+	return {
+		label: __( 'Show', 'google-listings-and-ads' ),
+		staticParams: [ 'period', 'chartType', 'paged', 'per_page' ],
+		param: 'filter',
+		showFilters: () => true,
+		filters: [
+			{
+				label: __( 'All Google programs', 'google-listings-and-ads' ),
+				value: 'all',
+			},
+			{
+				label: __( 'Single program', 'google-listings-and-ads' ),
+				value: 'select_program',
+				subFilters: [
+					{
+						component: 'Search',
+						value: 'single_program',
+						// This needs to match value of the parent filter.
+						path: [ 'select_program' ],
+						settings: {
+							type: 'custom',
+							param: 'programs',
+							getLabels,
+							labels: {
+								placeholder: __(
+									'Type to search for a program',
+									'google-listings-and-ads'
+								),
+								button: __(
+									'Single Program',
+									'google-listings-and-ads'
+								),
+							},
+							autocompleter,
 						},
 					},
+				],
+			},
+			{
+				label: __( 'Comparison', 'google-listings-and-ads' ),
+				chartMode: 'item-comparison',
+				value: 'compare-programs',
+				settings: {
+					type: 'custom',
+					param: 'programs',
+					getLabels,
+					labels: {
+						helpText: __(
+							'Check at least two programs below to compare',
+							'google-listings-and-ads'
+						),
+						placeholder: __(
+							'Search for programs to compare',
+							'google-listings-and-ads'
+						),
+						title: __(
+							'Compare Programs',
+							'google-listings-and-ads'
+						),
+						update: __( 'Compare', 'google-listings-and-ads' ),
+					},
+					autocompleter,
 				},
-			],
-		},
-		{
-			label: __( 'Comparison', 'google-listings-and-ads' ),
-			value: 'comparison',
-		},
-	],
+			},
+		],
+	};
 };
+
+export const programsFilter = programsFilterConfig;
