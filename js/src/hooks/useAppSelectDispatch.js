4bce5598ce5b76e2feb67f4e4582034bd0f1402d
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-04-30 23:52:25 +0800

    Pass args to useSelect in useAppSelectDispatch.

--- js/src/hooks/useAppSelectDispatch.js
+++ js/src/hooks/useAppSelectDispatch.js
@@ -10,24 +10,30 @@ import { useCallback } from '@wordpress/element';
 import { STORE_KEY } from '.~/data/constants';
 import { useAppDispatch } from '.~/data';
 
-const useAppSelectDispatch = ( selector, args = [] ) => {
+const useAppSelectDispatch = ( selector, ...args ) => {
 	const { invalidateResolution } = useAppDispatch();
 
 	const invalidateResolutionCallback = useCallback( () => {
-		invalidateResolution( selector, args );
-	}, [ args, invalidateResolution, selector ] );
+		invalidateResolution( selector, [ ...args ] );
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ invalidateResolution, selector, ...args ] );
 
-	return useSelect( ( select ) => {
-		const { hasFinishedResolution } = select( STORE_KEY );
+	return useSelect(
+		( select ) => {
+			const { hasFinishedResolution } = select( STORE_KEY );
 
-		const data = select( STORE_KEY )[ selector ]( ...args );
+			const data = select( STORE_KEY )[ selector ]( ...args );
 
-		return {
-			hasFinishedResolution: hasFinishedResolution( selector, args ),
-			data,
-			invalidateResolution: invalidateResolutionCallback,
-		};
-	} );
+			return {
+				hasFinishedResolution: hasFinishedResolution( selector, [
+					...args,
+				] ),
+				data,
+				invalidateResolution: invalidateResolutionCallback,
+			};
+		},
+		[ ...args ]
+	);
 };
 
 export default useAppSelectDispatch;
