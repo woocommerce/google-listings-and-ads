0debda11d8664a6ee374d31f4478e725331171ed
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-05-03 17:49:47 +0800

    Simplify call-to-action button in GetStartedCard.
    We just show one piece of text 'Set up free listings in Google' regardless of whether the user has already started setup or not. With this, we do not need to call API and wait for the response.

--- js/src/get-started-page/get-started-card/index.js
+++ js/src/get-started-page/get-started-card/index.js
@@ -21,10 +21,8 @@ import { ReactComponent as GoogleShoppingImage } from './image.svg';
 import { glaData } from '.~/constants';
 import './index.scss';
 import AppButton from '.~/components/app-button';
-import useGoogleAccount from '.~/hooks/useGoogleAccount';
 
 const GetStartedCard = () => {
-	const { hasFinishedResolution, google } = useGoogleAccount();
 	const disableNextStep = ! glaData.mcSupportedLanguage;
 	const handleClick = () => {
 		recordSetupMCEvent( 'get_started' );
@@ -49,13 +47,13 @@ const GetStartedCard = () => {
 					<AppButton
 						isPrimary
 						disabled={ disableNextStep }
-						loading={ ! hasFinishedResolution }
 						href={ getNewPath( {}, '/google/setup-mc' ) }
 						onClick={ handleClick }
 					>
-						{ hasFinishedResolution && google?.active === 'yes'
-							? __( 'Continue setup', 'google-listings-and-ads' )
-							: __( 'Get started', 'google-listings-and-ads' ) }
+						{ __(
+							'Set up free listings in Google',
+							'google-listings-and-ads'
+						) }
 					</AppButton>
 					<Text className="woocommerce-marketing-google-get-started-card__terms-notice">
 						{ createInterpolateElement(
