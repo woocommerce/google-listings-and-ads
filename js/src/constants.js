0507fb0e5ebbc791cf2084574f7c0bab8ca5f174
Eason Su <eason.su.tw@gmail.com>
2021-05-05 17:57:51 +0800

    Add and apply constants of report types and param name

--- js/src/constants.js
+++ js/src/constants.js
@@ -1,3 +1,8 @@
 export const glaData = window.glaData;
 
 export const FREE_LISTINGS_PROGRAM_ID = 0;
+
+export const REPORT_SOURCE_PARAM = 'report-source';
+export const REPORT_SOURCE_PAID = 'paid';
+export const REPORT_SOURCE_FREE = 'free';
+export const REPORT_SOURCE_DEFAULT = REPORT_SOURCE_PAID;
