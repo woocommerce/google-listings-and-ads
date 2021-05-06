040624fd166c47dd5a574e8b77fbea7ea76c7a3b
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2021-05-06 17:21:36 +0430

    Use AbstractAttributesForm and a helper method to add attributes

---
+++ src/Admin/Product/Attributes/AbstractAttributesForm.php
@@ -0,0 +1,76 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Admin\Product\Attributes;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\Form;
+use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\InputInterface;
+use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\Select;
+use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\SelectWithTextInput;
+use Automattic\WooCommerce\GoogleListingsAndAds\Exception\ValidateInterface;
+use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\AttributeInterface;
+use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\WithValueOptionsInterface;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class AbstractAttributesForm
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Admin\Product\Attributes
+ */
+abstract class AbstractAttributesForm extends Form {
+
+	use ValidateInterface;
+
+	/**
+	 * @param InputInterface     $input
+	 * @param AttributeInterface $attribute
+	 *
+	 * @return InputInterface
+	 */
+	protected function init_input( InputInterface $input, AttributeInterface $attribute ) {
+		$input->set_id( $attribute::get_id() )
+			  ->set_label( $attribute::get_name() )
+			  ->set_description( $attribute::get_description() )
+			  ->set_name( $attribute::get_id() );
+
+		$value_options = [];
+		if ( $attribute instanceof WithValueOptionsInterface ) {
+			$value_options = $attribute->get_value_options();
+		}
+		$value_options = apply_filters( "gla_product_attribute_value_options_{$attribute::get_id()}", $value_options );
+
+		if ( ! empty( $value_options ) ) {
+			if ( ! $input instanceof Select && ! $input instanceof SelectWithTextInput ) {
+				return $this->init_input( new SelectWithTextInput(), $attribute );
+			}
+
+			// add a 'default' value option
+			$value_options = [ '' => 'Default' ] + $value_options;
+
+			$input->set_options( $value_options );
+		}
+
+		return $input;
+	}
+
+
+	/**
+	 * Add an attribute to the form
+	 *
+	 * @param string $attribute_type An attribute class extending AttributeInterface
+	 * @param string $input_type     An input class extending InputInterface
+	 *
+	 * @return AbstractAttributesForm
+	 */
+	protected function add_attribute( string $attribute_type, string $input_type ): AbstractAttributesForm {
+		$this->validate_interface( $attribute_type, AttributeInterface::class );
+		$this->validate_interface( $input_type, InputInterface::class );
+
+		$attribute_input = $this->init_input( new $input_type(), new $attribute_type() );
+		$attribute_id    = call_user_func( [ $attribute_type, 'get_id' ] );
+		$this->add( $attribute_input, $attribute_id );
+
+		return $this;
+	}
+}
--- src/Admin/Product/Attributes/AttributesForm.php
+++ src/Admin/Product/Attributes/AttributesForm.php
@@ -3,15 +3,9 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Admin\Product\Attributes;
 
-use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\Form;
-use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\InputInterface;
-use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\Select;
-use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\SelectWithTextInput;
 use Automattic\WooCommerce\GoogleListingsAndAds\Admin\Input\Text;
-use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\AttributeInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\GTIN;
 use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\MPN;
-use Automattic\WooCommerce\GoogleListingsAndAds\Product\Attributes\WithValueOptionsInterface;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -20,7 +14,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\Admin\Product\Attributes
  */
-class AttributesForm extends Form {
+class AttributesForm extends AbstractAttributesForm {
 
 	/**
 	 * AttributesForm constructor.
@@ -30,41 +24,9 @@ class AttributesForm extends Form {
 	public function __construct( array $data = [] ) {
 		$this->set_name( 'attributes' );
 
-		$this->add( $this->init_input( new Text(), new GTIN() ), GTIN::get_id() )
-			 ->add( $this->init_input( new Text(), new MPN() ), MPN::get_id() );
+		$this->add_attribute( GTIN::class, Text::class )
+			 ->add_attribute( MPN::class, Text::class );
 
 		parent::__construct( $data );
-	}
-
-	/**
-	 * @param InputInterface     $input
-	 * @param AttributeInterface $attribute
-	 *
-	 * @return InputInterface
-	 */
-	protected function init_input( InputInterface $input, AttributeInterface $attribute ) {
-		$input->set_id( $attribute::get_id() )
-			  ->set_name( $attribute::get_id() )
-			  ->set_label( $attribute::get_name() )
-			  ->set_description( $attribute::get_description() );
-
-		$value_options = [];
-		if ( $attribute instanceof WithValueOptionsInterface ) {
-			$value_options = $attribute->get_value_options();
-		}
-		$value_options = apply_filters( "gla_product_attribute_value_options_{$attribute::get_id()}", $value_options );
-
-		if ( ! empty( $value_options ) ) {
-			if ( ! $input instanceof Select && ! $input instanceof SelectWithTextInput ) {
-				return $this->init_input( new SelectWithTextInput(), $attribute );
-			}
-
-			// add a 'default' value option
-			$value_options = [ '' => 'Default' ] + $value_options;
-
-			$input->set_options( $value_options );
-		}
-
-		return $input;
 	}
 }
