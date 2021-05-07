a2e2dda50c08897b6349813344796df66d3bdbe0
Eason Su <eason.su.tw@gmail.com>
2021-05-07 17:01:27 +0800

    Change selected metric and display selected style when clicking on SummarySection metrics

--- js/src/reports/products/summary-section.js
+++ js/src/reports/products/summary-section.js
@@ -2,10 +2,12 @@
  * External dependencies
  */
 import { SummaryList } from '@woocommerce/components';
+import { getNewPath } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
  */
+import useUrlQuery from '.~/hooks/useUrlQuery';
 import MetricNumber from '../metric-number';
 import getMetricsData from './mocked-metrics-data';
 
@@ -16,16 +18,22 @@ import getMetricsData from './mocked-metrics-data';
  * @param {[[string, string, boolean]]} props.metrics Metrics array of each metric tuple [key, label, isCurrency=false].
  */
 export default function SummarySection( { metrics } ) {
+	const query = useUrlQuery();
 	const metricsData = getMetricsData();
+	const { orderby = metrics[ 0 ][ 0 ] } = query;
 
 	return (
 		<SummaryList>
 			{ () =>
 				metrics.map( ( [ key, label, isCurrency = false ] ) => {
+					const selected = orderby === key;
+					const href = getNewPath( { orderby: key } );
 					return (
 						<MetricNumber
 							key={ key }
 							label={ label }
+							href={ href }
+							selected={ selected }
 							isCurrency={ isCurrency }
 							data={ metricsData[ key ] }
 						/>
