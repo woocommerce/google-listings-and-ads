2828f6a856739626c6755fa2a9c8f86a0d733f7d
Eason Su <eason.su.tw@gmail.com>
2021-05-10 10:24:30 +0800

    ChartSection - enable chart type switching and tweak chart props to fit with visual design

--- js/src/reports/chart-section.js
+++ js/src/reports/chart-section.js
@@ -1,13 +1,20 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { useMemo } from '@wordpress/element';
 import { Chart } from '@woocommerce/components';
+import { getChartTypeForQuery } from '@woocommerce/date';
 
 /**
  * Internal dependencies
  */
 import useUrlQuery from '.~/hooks/useUrlQuery';
+
+const emptyMessage = __(
+	'No data for the selected date range',
+	'google-listings-and-ads'
+);
 
 /**
  * Renders a report chart.
@@ -22,6 +29,8 @@ export default function ChartSection( { metrics, report } ) {
 	const { key, label } = orderby
 		? metrics.find( ( metric ) => metric.key === orderby )
 		: metrics[ 0 ];
+
+	const chartType = getChartTypeForQuery( query );
 
 	const {
 		loaded,
@@ -49,10 +58,12 @@ export default function ChartSection( { metrics, report } ) {
 			data={ chartData }
 			title={ label }
 			query={ query }
+			chartType={ chartType }
 			isRequesting={ ! loaded }
+			emptyMessage={ emptyMessage }
 			layout="time-comparison"
-			interactiveLegend="false"
-			showHeaderControls="false"
+			// 'hidden' is NOT a valid `legendPosition` value, but it can hack to hide the legend.
+			legendPosition="hidden"
 		/>
 	);
 }
