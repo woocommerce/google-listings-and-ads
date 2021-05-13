695f91d96c57db54c21dcaa97f792155b59624f2
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-05-13 14:03:32 +0200

    Connect programs report's `ChartSection` to the API,
    use reused component.
    Make `ChartSection` acceptempty `metrics` prop, so it could render a placeholder, when `metrics` data is being loaded/resolved.

--- js/src/reports/chart-section.js
+++ js/src/reports/chart-section.js
@@ -32,9 +32,16 @@ export default function ChartSection( { metrics, loaded, intervals } ) {
 	const { formatAmount } = useCurrencyFactory();
 
 	const { selectedMetric } = query;
-	const { key, label, isCurrency = false } = selectedMetric
-		? metrics.find( ( metric ) => metric.key === selectedMetric )
-		: metrics[ 0 ];
+	let visibleMetric;
+	if ( metrics.length ) {
+		visibleMetric = selectedMetric
+			? metrics.find( ( metric ) => metric.key === selectedMetric )
+			: metrics[ 0 ];
+	} else {
+		visibleMetric = { key: undefined, label: undefined };
+	}
+
+	const { key, label, isCurrency = false } = visibleMetric;
 
 	const chartType = getChartTypeForQuery( query );
 	const valueType = isCurrency ? 'currency' : 'number';
--- js/src/reports/programs/index.js
+++ js/src/reports/programs/index.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Chart } from '@woocommerce/components';
 import { getQuery } from '@woocommerce/navigation';
 
 /**
@@ -13,6 +12,7 @@ import TabNav from '../../tab-nav';
 import SubNav from '../sub-nav';
 import ProgramsReportFilters from './programs-report-filters';
 import SummarySection from '../summary-section';
+import ChartSection from '../chart-section';
 import CompareProgramsTableCard from './compare-programs-table-card';
 import '../../dashboard/index.scss';
 
@@ -55,58 +55,6 @@ const ProgramsReport = () => {
 		data: { totals, intervals },
 	} = useProgramsReport();
 
-	const chartData = [
-		{
-			date: '2018-05-30T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 14205,
-			},
-		},
-		{
-			date: '2018-05-31T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 10581,
-			},
-		},
-		{
-			date: '2018-06-01T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 16307,
-			},
-		},
-		{
-			date: '2018-06-02T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 13481,
-			},
-		},
-		{
-			date: '2018-06-03T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 10581,
-			},
-		},
-		{
-			date: '2018-06-04T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 19874,
-			},
-		},
-		{
-			date: '2018-06-05T00:00:00',
-			Conversions: {
-				label: 'Conversions',
-				value: 20593,
-			},
-		},
-	];
-
 	// Show only available data.
 	// Until ~Q4 2021, free listings, may not have all metrics.
 	const availableMetrics = performanceMetrics.filter(
@@ -126,12 +74,10 @@ const ProgramsReport = () => {
 					expectedLength={ performanceMetrics.length }
 					totals={ totals }
 				/>
-				<Chart
-					data={ chartData }
-					title="Conversions"
-					layout="time-comparison"
-					interactiveLegend="false"
-					showHeaderControls="false"
+				<ChartSection
+					metrics={ availableMetrics }
+					loaded={ loaded }
+					intervals={ intervals }
 				/>
 			</div>
 			<div className="gla-dashboard__programs">
