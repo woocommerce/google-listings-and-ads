f635ece5b2b442d808e2f74cf1c8416334ca1b11
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-05-13 18:39:21 +0800

    Update statusLabelMap based on API changes.
    The API changes is done in PR #562.

--- js/src/product-feed/product-feed-table-card/statusLabelMap.js
+++ js/src/product-feed/product-feed-table-card/statusLabelMap.js
@@ -4,10 +4,12 @@
 import { __ } from '@wordpress/i18n';
 
 const statusLabelMap = {
+	approved: __( 'Approved', 'google-listings-and-ads' ),
+	partially_approved: __( 'Partially approved', 'google-listings-and-ads' ),
+	expiring: __( 'Expiring', 'google-listings-and-ads' ),
 	pending: __( 'Pending', 'google-listings-and-ads' ),
-	synced: __( 'Synced', 'google-listings-and-ads' ),
-	'not-synced': __( 'Not synced', 'google-listings-and-ads' ),
-	'has-errors': __( 'Has errors', 'google-listings-and-ads' ),
+	disapproved: __( 'Disapproved', 'google-listings-and-ads' ),
+	not_synced: __( 'Not synced', 'google-listings-and-ads' ),
 };
 
 export default statusLabelMap;
