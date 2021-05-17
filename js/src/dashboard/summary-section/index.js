a821b15944438b2a3a31c90bd75a98310ed18ed4
Eason Su <eason.su.tw@gmail.com>
2021-05-17 14:17:50 +0800

    Use store currency setting for the performance section on the dashboard page

--- js/src/dashboard/summary-section/index.js
+++ js/src/dashboard/summary-section/index.js
@@ -3,13 +3,13 @@
  */
 import { __ } from '@wordpress/i18n';
 import { SummaryNumber } from '@woocommerce/components';
-import CurrencyFactory from '@woocommerce/currency';
-import { numberFormat } from '@woocommerce/number';
 
 /**
  * Internal dependencies
  */
 import { glaData, REPORT_SOURCE_PAID, REPORT_SOURCE_FREE } from '.~/constants';
+import useCurrencyFormat from '.~/hooks/useCurrencyFormat';
+import useCurrencyFactory from '.~/hooks/useCurrencyFactory';
 import usePerformance from './usePerformance';
 import AppSpinner from '.~/components/app-spinner';
 import SummaryCard from './summary-card';
@@ -21,15 +21,10 @@ const paidPerformanceTitle = __(
 	'google-listings-and-ads'
 );
 
-// Note: Since the `delta` prop of SummaryNumber component wouldn't apply the WC Settings' currency options,
-//       it uses '.' as the decimal separator when transforming to percentage format.
-//       In order to keep the format consistency of number and currency in the same block,
-//       the WC Settings' currency options are not applied in this SummarySection component.
-// ref: https://github.com/woocommerce/woocommerce-admin/blob/v1.6.0/packages/components/src/summary/number.js#L133-L136
-const formatNumber = ( number ) => numberFormat( { precision: 0 }, number );
-const { formatAmount } = CurrencyFactory();
+const numberFormatSetting = { precision: 0 };
 
 const FreePerformanceCard = () => {
+	const formatNumber = useCurrencyFormat( numberFormatSetting );
 	const { data, loaded } = usePerformance( REPORT_SOURCE_FREE );
 
 	return (
@@ -63,6 +58,7 @@ const FreePerformanceCard = () => {
 };
 
 const PaidPerformanceCard = () => {
+	const { formatAmount } = useCurrencyFactory();
 	const { data, loaded } = usePerformance( REPORT_SOURCE_PAID );
 
 	return (
