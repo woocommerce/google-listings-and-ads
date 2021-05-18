4386ed69704b4dce5ad60aa005dd0aa3f37783d6
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-05-18 16:41:02 +0200

    Remove not needed transform ifrom jest.config
    Addresses: https://github.com/woocommerce/google-listings-and-ads/pull/622#discussion_r634264178

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,6 @@ module.exports = {
 	...defaultConfig,
 	// Workaround https://github.com/woocommerce/woocommerce-admin/issues/6483.
 	transformIgnorePatterns: [ 'node_modules/(?!@woocommerce/date/build)' ],
-	transform: {
-		...( defaultConfig.transform || [] ),
-		'node_modules/@woocommerce/date/build/index.js': 'babel-jest',
-	},
 	moduleNameMapper: {
 		// Transform our `.~/` alias.
 		'\\.~/(.*)$': '<rootDir>/js/src/$1',
