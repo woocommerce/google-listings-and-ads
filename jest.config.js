39f9592696375f4f6ee5d0d166e80d5638a92970
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-05-18 07:44:41 +0200

    Transform `@woocommerce/data` build for jest,
    To make our tests run with `@woocommerce/date` or `@woocommerce/components` dependencies.
    
    Fixes https://github.com/woocommerce/google-listings-and-ads/pull/234.
    Works around https://github.com/woocommerce/woocommerce-admin/issues/6483.

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
 
 module.exports = {
 	...defaultConfig,
+	// Workaround https://github.com/woocommerce/woocommerce-admin/issues/6483.
+	transformIgnorePatterns: [ 'node_modules/(?!@woocommerce/date/build)' ],
+	transform: {
+		...( defaultConfig.transform || [] ),
+		'node_modules/@woocommerce/date/build/index.js': 'babel-jest',
+	},
 	moduleNameMapper: {
 		// Transform our `.~/` alias.
 		'\\.~/(.*)$': '<rootDir>/js/src/$1',
