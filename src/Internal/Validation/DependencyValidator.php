c5254e3f0d01cdd7f6dbf235d3f4483e577da1d2
Justin Palmer <justin.palmer@automattic.com>
2021-05-21 17:16:26 +0200

    Merge methods

--- src/Internal/Validation/DependencyValidator.php
+++ src/Internal/Validation/DependencyValidator.php
@@ -42,19 +42,10 @@ abstract class DependencyValidator implements Validatable {
 		add_action(
 			'admin_notices',
 			function() use ( $e ) {
-				$this->admin_notice( $e->getMessage() );
+				echo '<div class="notice notice-error">' . PHP_EOL;
+				echo '	<p>' . esc_html( $e->getMessage() ) . '</p>' . PHP_EOL;
+				echo '</div>' . PHP_EOL;
 			}
 		);
-	}
-
-	/**
-	 * Display an admin notice with the provided message.
-	 *
-	 * @param string $message
-	 */
-	protected function admin_notice( string $message ) {
-		echo '<div class="notice notice-error">' . PHP_EOL;
-		echo '	<p>' . esc_html( $message ) . '</p>' . PHP_EOL;
-		echo '</div>' . PHP_EOL;
 	}
 }
