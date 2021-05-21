d6381e7a12d83bcdf0fd49d7b6288b42ee0a918d
Eason Su <eason.su.tw@gmail.com>
2021-05-21 18:25:04 +0800

    Transpile all WC admin packages when running unit-tests

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,23 @@
 // see https://developer.wordpress.org/block-editor/packages/packages-scripts/#advanced-information-11.
 const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
 
+const wcPackagesNeedTransform = [
+	'components',
+	'components/node_modules/@woocommerce/navigation',
+	'currency',
+	'data',
+	'date',
+	'navigation',
+	'number',
+	'tracks',
+].join( '|' );
+
 module.exports = {
 	...defaultConfig,
 	// Workaround https://github.com/woocommerce/woocommerce-admin/issues/6483.
-	transformIgnorePatterns: [ 'node_modules/(?!@woocommerce/date/build)' ],
+	transformIgnorePatterns: [
+		`<rootDir>/node_modules/(?!@woocommerce/(${ wcPackagesNeedTransform })/build/)`,
+	],
 	moduleNameMapper: {
 		// Transform our `.~/` alias.
 		'\\.~/(.*)$': '<rootDir>/js/src/$1',
