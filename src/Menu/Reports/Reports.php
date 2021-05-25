f58c4c6efdde4ba82c6ec557136fa6c676e536cd
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-05-25 23:03:05 +0800

    Refactor and add feature check and fix filter.

--- src/Menu/Reports/Reports.php
+++ src/Menu/Reports/Reports.php
@@ -3,6 +3,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Menu\Reports;
 
+use Automattic\WooCommerce\Admin\Features\Features;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Registerable;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
 
@@ -20,21 +21,31 @@ class Reports implements Service, Registerable {
 		add_action(
 			'admin_menu',
 			function() {
-				if ( apply_filters( 'gla_enable_reports', true ) ) {
-					wc_admin_register_page(
-						[
-							'title'    => __( 'Reports', 'google-listings-and-ads' ),
-							'parent'   => 'google-listings-and-ads-category',
-							'path'     => '/google/reports/programs',
-							'id'       => 'google-reports',
-							'nav_args' => [
-								'order'  => 20,
-								'parent' => 'google-listings-and-ads-category',
-							],
-						]
-					);
+				if (
+					apply_filters( 'gla_enable_reports', false ) &&
+					Features::is_enabled( 'navigation' )
+				) {
+					$this->register_navigation_pages();
 				}
 			}
+		);
+	}
+
+	/**
+	 * Register navigation pages for WC Navigation.
+	 */
+	protected function register_navigation_pages(): void {
+		wc_admin_register_page(
+			[
+				'title'    => __( 'Reports', 'google-listings-and-ads' ),
+				'parent'   => 'google-listings-and-ads-category',
+				'path'     => '/google/reports/programs',
+				'id'       => 'google-reports',
+				'nav_args' => [
+					'order'  => 20,
+					'parent' => 'google-listings-and-ads-category',
+				],
+			]
 		);
 	}
 }
