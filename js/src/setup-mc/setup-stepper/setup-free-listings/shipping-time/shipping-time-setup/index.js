9449ae0c20682040046ae36ad83b58a086edc73d
Jason Conroy <jason@findingsimple.com>
2021-05-27 19:57:06 +0930

    Remove share_shipping_time MC option

--- js/src/setup-mc/setup-stepper/setup-free-listings/shipping-time/shipping-time-setup/index.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/shipping-time/shipping-time-setup/index.js
@@ -1,16 +1,12 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { CheckboxControl } from '@wordpress/components';
-import { createInterpolateElement } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import AppSpinner from '.~/components/app-spinner';
-import AppDocumentationLink from '.~/components/app-documentation-link';
 import { STORE_KEY } from '.~/data';
 import useTargetAudienceFinalCountryCodes from '.~/hooks/useTargetAudienceFinalCountryCodes';
 import VerticalGapLayout from '.~/components/vertical-gap-layout';
@@ -19,10 +15,7 @@ import getCountriesTimeArray from './getCountriesTimeArray';
 import CountriesTimeInputForm from './countries-time-input-form';
 import './index.scss';
 
-const ShippingTimeSetup = ( props ) => {
-	const {
-		formProps: { getInputProps },
-	} = props;
+const ShippingTimeSetup = () => {
 	const shippingTimes = useSelect( ( select ) =>
 		select( STORE_KEY ).getShippingTimes()
 	);
@@ -68,24 +61,6 @@ const ShippingTimeSetup = ( props ) => {
 						) }
 					</VerticalGapLayout>
 				</div>
-				<CheckboxControl
-					label={ createInterpolateElement(
-						__(
-							'I allow Google to collect and calculate my shipping times for more accurate estimates. <link>Learn more</link>',
-							'google-listings-and-ads'
-						),
-						{
-							link: (
-								<AppDocumentationLink
-									context="setup-mc-shipping-time"
-									linkId="shipping-time-allow-google-data-collection"
-									href="https://www.google.com/retail/solutions/merchant-center/"
-								/>
-							),
-						}
-					) }
-					{ ...getInputProps( 'share_shipping_time' ) }
-				/>
 			</VerticalGapLayout>
 		</div>
 	);
