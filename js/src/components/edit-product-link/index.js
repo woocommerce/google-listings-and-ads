d496f5dbbf100824ad3e995cd1d8e3dc81d6fb89
Eason Su <eason.su.tw@gmail.com>
2021-06-01 11:11:55 +0800

    Add click event tracking to the EditProductLink component

--- js/src/components/edit-product-link/index.js
+++ js/src/components/edit-product-link/index.js
@@ -3,13 +3,28 @@
  */
 import { Link } from '@woocommerce/components';
 import { __ } from '@wordpress/i18n';
+import { queueRecordEvent } from '@woocommerce/tracks';
 
-const EditProductLink = ( props ) => {
-	const { productId } = props;
+const noop = () => {};
+
+/**
+ * Renders a link that points to the product edit page.
+ * Click event tracking would be attached if giving the `eventName` prop.
+ *
+ * @param {Object} props React props.
+ * @param {number} props.productId Product ID.
+ * @param {string} [props.eventName] The event name to record.
+ * @param {Object} [props.eventProps] Event properties to include in the event.
+ */
+const EditProductLink = ( { productId, eventName, eventProps } ) => {
 	const editProductLink = `post.php?action=edit&post=${ productId }`;
 
+	const handleClick = eventName
+		? () => queueRecordEvent( eventName, eventProps )
+		: noop;
+
 	return (
-		<Link href={ editProductLink } type="wp-admin">
+		<Link href={ editProductLink } onClick={ handleClick } type="wp-admin">
 			{ __( 'Edit', 'google-listings-and-ads' ) }
 		</Link>
 	);
