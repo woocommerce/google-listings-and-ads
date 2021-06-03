8f30ce3521db17732cc7c33950b459ca3c23df78
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-06-03 23:38:24 +0800

    Always register reports page, don't need condition anymore.

--- src/Menu/Reports/Reports.php
+++ src/Menu/Reports/Reports.php
@@ -21,31 +21,19 @@ class Reports implements Service, Registerable {
 		add_action(
 			'admin_menu',
 			function() {
-				if (
-					apply_filters( 'gla_enable_reports', true ) &&
-					Features::is_enabled( 'navigation' )
-				) {
-					$this->register_navigation_pages();
-				}
+				wc_admin_register_page(
+					[
+						'title'    => __( 'Reports', 'google-listings-and-ads' ),
+						'parent'   => 'google-listings-and-ads-category',
+						'path'     => '/google/reports',
+						'id'       => 'google-reports',
+						'nav_args' => [
+							'order'  => 20,
+							'parent' => 'google-listings-and-ads-category',
+						],
+					]
+				);
 			}
-		);
-	}
-
-	/**
-	 * Register navigation pages for WC Navigation.
-	 */
-	protected function register_navigation_pages(): void {
-		wc_admin_register_page(
-			[
-				'title'    => __( 'Reports', 'google-listings-and-ads' ),
-				'parent'   => 'google-listings-and-ads-category',
-				'path'     => '/google/reports/programs',
-				'id'       => 'google-reports',
-				'nav_args' => [
-					'order'  => 20,
-					'parent' => 'google-listings-and-ads-category',
-				],
-			]
 		);
 	}
 }
