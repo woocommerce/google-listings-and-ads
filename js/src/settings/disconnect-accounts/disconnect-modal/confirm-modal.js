95df3e8e4e609107e7b5393178d80286923b524b
Nima <73110514+nima-karimi@users.noreply.github.com>
2021-06-04 09:27:29 +0430

    change disconnect ads accounts modal text

--- js/src/settings/disconnect-accounts/disconnect-modal/confirm-modal.js
+++ js/src/settings/disconnect-accounts/disconnect-modal/confirm-modal.js
@@ -57,11 +57,11 @@ const textDict = {
 				'google-listings-and-ads'
 			),
 			__(
-				'Any paid campaigns created by this WooCommerce extension will be paused.',
+				'Any ongoing paid campaigns will continue to run. They can be managed, edited, or deleted manually from Google Ads (ads.google.com).',
 				'google-listings-and-ads'
 			),
 			__(
-				'Configurations for Google Ads created through WooCommerce may also be lost. This cannot be undone.',
+				'Some configurations for Google Ads created through WooCommerce may be lost. This cannot be undone.',
 				'google-listings-and-ads'
 			),
 		],
