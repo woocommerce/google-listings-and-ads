41d2a0da8ab53bbe47fee8b65d6141c5893e5ef5
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-06-04 23:40:10 +0200

    Show "Unavailable" and more descriptive tooltip,
    on the report pages when Google API fails to respond with data.
    
    Fixes https://github.com/woocommerce/google-listings-and-ads/issues/748

--- js/src/reports/metric-number.js
+++ js/src/reports/metric-number.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useMemo } from '@wordpress/element';
+import { useMemo, createInterpolateElement } from '@wordpress/element';
 import { SummaryNumber } from '@woocommerce/components';
 import GridiconInfoOutline from 'gridicons/dist/info-outline';
 
@@ -11,10 +11,14 @@ import GridiconInfoOutline from 'gridicons/dist/info-outline';
  */
 import './metric-number.scss';
 import AppTooltip from '.~/components/app-tooltip';
+import TrackableLink from '.~/components/trackable-link';
 import useCurrencyFormat from '.~/hooks/useCurrencyFormat';
 import useCurrencyFactory from '.~/hooks/useCurrencyFactory';
 
 const numberFormatSetting = { precision: 0 };
+
+const unavalable = __( 'Unavailable', 'google-listings-and-ads' );
+const googleMCURL = 'https://merchants.google.com/mc/reporting/dashboard';
 
 /**
  * SummeryNumber annotated about missing data.
@@ -48,19 +52,50 @@ const MetricNumber = ( {
 		const formatFn = isCurrency ? formatAmount : formatNumber;
 
 		return {
-			value: formatFn( value ),
+			value: value === null ? unavalable : formatFn( value ),
 			prevValue: formatFn( prevValue ),
 		};
 	}, [ isCurrency, value, prevValue, formatNumber, formatAmount ] );
 
 	let markedLabel = label;
+	let infoText;
 
 	// Until ~Q4 2021, metrics for all programs, may lack data for free listings.
-	if ( missingFreeListingsData ) {
-		const infoText = __(
-			'This data is available for paid campaigns only.',
+	// And Free Listings API may not respond with data.
+	if ( value === null ) {
+		infoText = (
+			<>
+				{ createInterpolateElement(
+					__(
+						'This data is currently available for paid campaigns only.<br/><br/>Please try again later, or go to <link>merchants.google.com</link> to track your performance for Google Free Listings. ',
+						'google-listings-and-ads'
+					),
+					{
+						br: <br />,
+						link: (
+							<TrackableLink
+								eventName="gla_google_mc_link_click"
+								eventProps={ {
+									context: 'reports',
+									href: googleMCURL,
+								} }
+								type="external"
+								target="_blank"
+								href={ googleMCURL }
+							/>
+						),
+					}
+				) }
+			</>
+		);
+	} else if ( missingFreeListingsData ) {
+		infoText = __(
+			'This data is currently available for paid campaigns only.',
 			'google-listings-and-ads'
 		);
+	}
+
+	if ( infoText ) {
 		markedLabel = (
 			<div className="gla-reports__metric-label">
 				{ label }
