1bc03fcf1cc369a980856f8676155389ae62c41c
mikkamp <michael.kamp@automattic.com>
2021-06-07 07:24:30 +0100

    Validate all plugins before loading services

---
+++ src/Internal/Requirements/PluginValidator.php
@@ -0,0 +1,41 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Internal\Requirements;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\Internal\Requirements\WCAdminValidator;
+use Automattic\WooCommerce\GoogleListingsAndAds\Internal\Requirements\WCBookingsValidator;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class PluginValidator
+ *
+ * Display admin notices for required and incompatible plugins.
+ *
+ * @package AutomatticWooCommerceGoogleListingsAndAdsInternalRequirements
+ */
+class PluginValidator {
+
+	private const PLUGINS = [
+		WCAdminValidator::class,
+		WCBookingsValidator::class,
+	];
+
+	/**
+	 * Validate all required and incompatible plugins.
+	 *
+	 * @return bool
+	 */
+	public static function validate(): bool {
+		$validated = true;
+
+		foreach ( self::PLUGINS as $plugin ) {
+			if ( ! $plugin::instance()->validate() ) {
+				$validated = false;
+			}
+		}
+
+		return $validated;
+	}
+}
