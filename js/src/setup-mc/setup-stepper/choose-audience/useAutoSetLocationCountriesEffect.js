6b0241673867d16f84ef1e584560dd6c82b53271
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-06-11 16:24:33 +0800

    Make sure that the storeCountryCode is in list of supported countries.

--- js/src/setup-mc/setup-stepper/choose-audience/useAutoSetLocationCountriesEffect.js
+++ js/src/setup-mc/setup-stepper/choose-audience/useAutoSetLocationCountriesEffect.js
@@ -7,10 +7,13 @@ import { useCallback, useEffect } from '@wordpress/element';
  * Internal dependencies
  */
 import useStoreCountry from '.~/hooks/useStoreCountry';
+import useGetCountries from '.~/hooks/useGetCountries';
 
 /**
  * Automatically set `location` to be `selected` and `countries` to be `[ storeCountryCode ]`
  * when `values.location === null && values.countries.length === 0` (i.e. when users first visit the page).
+ *
+ * If the `storeCountryCode` is not in the list of the MC supported countries, then `countries` would be set to empty array `[]`.
  *
  * This is done by calling `onChange` from `getInputProps`, simulating user's manual input action.
  *
@@ -19,14 +22,22 @@ import useStoreCountry from '.~/hooks/useStoreCountry';
 const useAutoSetLocationCountriesEffect = ( formProps ) => {
 	const { values, getInputProps } = formProps;
 	const { code: storeCountryCode } = useStoreCountry();
+	const { data } = useGetCountries();
 
 	const hasNoLocationCountries =
 		values.location === null && values.countries.length === 0;
 
 	const setLocationCountries = useCallback( () => {
+		if ( ! data ) {
+			return;
+		}
+
+		const countriesValue =
+			data && data[ storeCountryCode ] ? [ storeCountryCode ] : [];
+
 		getInputProps( 'location' ).onChange( 'selected' );
-		getInputProps( 'countries' ).onChange( [ storeCountryCode ] );
-	}, [ getInputProps, storeCountryCode ] );
+		getInputProps( 'countries' ).onChange( countriesValue );
+	}, [ data, getInputProps, storeCountryCode ] );
 
 	useEffect( () => {
 		if ( hasNoLocationCountries ) {
