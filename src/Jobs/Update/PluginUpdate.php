936b43492901d9bd6ef3a3e39bf64ae4b4d0c0ed
mikkamp <michael.kamp@automattic.com>
2021-06-16 13:53:37 +0100

    Run jobs on plugin update

---
+++ src/Jobs/Update/PluginUpdate.php
@@ -0,0 +1,74 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Jobs\Update;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
+use Automattic\WooCommerce\GoogleListingsAndAds\Internal\Interfaces\InstallableInterface;
+use Automattic\WooCommerce\GoogleListingsAndAds\Jobs\JobException;
+use Automattic\WooCommerce\GoogleListingsAndAds\Jobs\JobRepository;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Runs update jobs when the plugin is updated.
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Jobs\Update
+ */
+class PluginUpdate implements Service, InstallableInterface {
+
+	/**
+	 * @var JobRepository
+	 */
+	protected $job_repository;
+
+	/**
+	 * PluginUpdate constructor.
+	 *
+	 * @param JobRepository $job_repository
+	 */
+	public function __construct( JobRepository $job_repository ) {
+		$this->job_repository = $job_repository;
+	}
+
+	/**
+	 * Update Jobs that need to be run per version.
+	 *
+	 * @var array
+	 */
+	private $updates = [
+		'1.0.1' => [
+			CleanupProductTargetCountriesJob::class,
+		],
+	];
+
+	/**
+	 * Run installation logic for this class.
+	 *
+	 * @param string $old_version Previous version before updating.
+	 * @param string $new_version Current version after updating.
+	 */
+	public function install( string $old_version, string $new_version ): void {
+		foreach ( $this->updates as $version => $jobs ) {
+			if ( version_compare( $old_version, $version, '<' ) ) {
+				$this->start_jobs( $jobs );
+			}
+		}
+	}
+
+	/**
+	 * Start a list of jobs.
+	 *
+	 * @param array $jobs List of jobs
+	 */
+	protected function start_jobs( array $jobs ): void {
+		foreach ( $jobs as $job ) {
+			try {
+				$this->job_repository->get( $job )->start();
+			} catch ( JobException $e ) {
+				do_action( 'gla_exception', $e, __METHOD__ );
+			}
+		}
+	}
+
+}
