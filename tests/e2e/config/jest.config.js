7e2f383d9e9d4ac99ceb52032198f2bf380ca5d3
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-06-18 02:06:05 +0800

    Fix e2e jest config.

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -1,23 +1,27 @@
 const path = require( 'path' );
 const { useE2EJestConfig } = require( '@woocommerce/e2e-environment' );
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const config = useE2EJestConfig( {
 	moduleFileExtensions: [ 'js', 'ts' ],
 	roots: [ path.resolve( __dirname, '../specs' ) ],
 	testMatch: [ '**/*.(test|spec).(j|t)s', '*.(test|spec).(j|t)s' ],
 	testTimeout: 30000,
-	transform: {
-		'\\.[jt]sx?$': [
-			'babel-jest',
-			{
-				configFile: path.join(
-					__dirname,
-					'../../../',
-					'babel.config.js'
-				),
-			},
-		],
-	},
+
+	// The following should be not applicable to our project,
+	// we don't have babel.config.js at root.
+	// transform: {
+	// 	'\\.[jt]sx?$': [
+	// 		'babel-jest',
+	// 		{
+	// 			configFile: path.join(
+	// 				__dirname,
+	// 				'../../../',
+	// 				'babel.config.js'
+	// 			),
+	// 		},
+	// 	],
+	// },
 } );
 
 module.exports = config;
