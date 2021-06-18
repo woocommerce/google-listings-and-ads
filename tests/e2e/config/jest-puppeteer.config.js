8e13112f0109bf60aeb6c5ed1b5d19fff828f4a6
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-06-19 02:31:46 +0800

    Set defaultViewport for both headless and non-headless.
    Without this, the viewport was 800x600 in headless and 1280x800 in dev mode, causing some tests to fail in headless and pass in dev mode.

--- tests/e2e/config/jest-puppeteer.config.js
+++ tests/e2e/config/jest-puppeteer.config.js
@@ -1,9 +1,14 @@
 const { useE2EJestPuppeteerConfig } = require( '@woocommerce/e2e-environment' );
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const puppeteerConfig = useE2EJestPuppeteerConfig( {
 	launch: {
 		browserContext: 'incognito',
-		args: [ '--incognito' ],
+		args: [ '--incognito', '--window-size=1920,1080' ],
+		defaultViewport: {
+			width: 1280,
+			height: 800,
+		},
 	},
 } );
 
