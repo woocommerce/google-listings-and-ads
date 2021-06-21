903bfafdcc08dd3c633125791e1973ada5279f52
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-06-22 02:59:52 +0800

    e2e test for getting started to setup MC.

--- tests/e2e/specs/get-started.test.js
+++ tests/e2e/specs/get-started.test.js
@@ -6,12 +6,14 @@ describe( 'Merchant who is getting started', () => {
 	} );
 
 	it( 'Goes to GLA page, click on the call-to-action button and navigate to the Setup MC page', async () => {
+		// hover at the marketing link to open popup submenu.
 		const marketingLink = (
 			await page.$x( "//a[contains(., 'Marketing')]" )
 		 )[ 0 ];
 		await marketingLink.hover();
 
-		// when the submenu is opened, there will be an "opensub" CSS class for Marketing menu item.
+		// the submenu is in the DOM but not visible with the use of absolute positioning.
+		// we wait for submenu to open by checking for "opensub" CSS class in Marketing menu item.
 		await page.waitForSelector(
 			'li#toplevel_page_woocommerce-marketing.opensub'
 		);
@@ -24,8 +26,29 @@ describe( 'Merchant who is getting started', () => {
 		await glaLink.click();
 
 		await page.waitForNavigation();
-		await expect( page.title() ).resolves.toMatch(
-			'Google Listings & Ads'
+		const pageTitle = await page.title();
+		expect( pageTitle ).toContain( 'Google Listings & Ads' );
+
+		// click on the call-to-action button.
+		const setupButton = (
+			await page.$x( "//a[text()='Set up free listings in Google']" )
+		 )[ 0 ];
+		await setupButton.click();
+		await page.waitForNavigation();
+
+		// check we are in the Setup MC Step 1 page.
+		const topBarText = (
+			await page.$x(
+				"//*[text()='Get started with Google Listings & Ads']"
+			)
+		 )[ 0 ];
+		expect( topBarText ).toBeTruthy();
+
+		// there are some API calls running in the page before the steps are displayed.
+		// wait for the text in Step 1 to show up.
+		const step1Text = await page.waitForXPath(
+			"//*[text()='Set up your accounts']"
 		);
+		expect( step1Text ).toBeTruthy();
 	} );
 } );
