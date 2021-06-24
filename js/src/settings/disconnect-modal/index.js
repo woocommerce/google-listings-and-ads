34f233b517c263ef28a6906fbf47c0d9b915fe83
Eason Su <eason.su.tw@gmail.com>
2021-06-24 14:56:32 +0800

    Move DisconnectModal components to the upper folder for reuse

--- js/src/settings/disconnect-modal/index.js
+++ js/src/settings/disconnect-modal/index.js
@@ -4,6 +4,8 @@
 import ConfirmModal from './confirm-modal';
 import './index.scss';
 
+export * from './constants';
+
 export default function DisconnectModal( props ) {
 	// TODO: add a survey modal when the method of saving the survey results has been decided.
 	return <ConfirmModal { ...props } />;
