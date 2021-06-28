ab2889afc3d5c427e377a9d1779fc1549adf8b19
Eason Su <eason.su.tw@gmail.com>
2021-06-28 19:22:42 +0800

    Add test cases for the createErrorResponseCatcher middleware

---
+++ js/src/data/api-fetch-middlewares.test.js
@@ -0,0 +1,105 @@
+/**
+ * Internal dependencies
+ */
+import { createErrorResponseCatcher } from './api-fetch-middlewares';
+import { API_NAMESPACE } from './constants';
+
+describe( 'createErrorResponseCatcher', () => {
+	// Ref: https://github.com/WordPress/gutenberg/blob/%40wordpress/api-fetch%405.1.1/packages/api-fetch/src/index.js#L68-L81
+	const checkStatus = ( response ) => {
+		if ( response.status >= 200 && response.status < 300 ) {
+			return response;
+		}
+
+		throw response;
+	};
+
+	const createFetchHandler = ( status, jsonParsedData ) => () => {
+		const response = {
+			status,
+			json() {
+				return Promise.resolve( jsonParsedData );
+			},
+		};
+		return Promise.resolve( response ).then( checkStatus );
+	};
+
+	const passReject = ( response ) => Promise.reject( response );
+
+	const optionsDefaultParse = { path: `${ API_NAMESPACE }/hi` };
+	const optionsDontParse = { path: `${ API_NAMESPACE }/hi`, parse: false };
+
+	describe( 'force overwrite `parse` option to `false` and return the same result', () => {
+		let middleware;
+
+		beforeEach( () => {
+			middleware = createErrorResponseCatcher( passReject );
+		} );
+
+		it( 'should only handle requests where the path belongs to this plugin', async () => {
+			const data = {};
+			const optionsOtherNamespace = { path: '' };
+			const callback = jest.fn().mockImplementation( ( options ) => {
+				expect( options ).toBe( optionsOtherNamespace );
+				return data;
+			} );
+
+			const result = await middleware( optionsOtherNamespace, callback );
+			expect( callback ).toHaveBeenCalledTimes( 1 );
+			expect( result ).toBe( data );
+		} );
+
+		it( 'should resolve parsed response body by default in a successful response', async () => {
+			const handler = createFetchHandler( 200, { data: 'hi' } );
+			const result = await middleware( optionsDefaultParse, handler );
+			expect( result ).toEqual( { data: 'hi' } );
+		} );
+
+		it( 'should reject parsed response body by default in a failed response', async () => {
+			const handler = createFetchHandler( 418, { message: 'oops' } );
+			const promise = middleware( optionsDefaultParse, handler );
+			await expect( promise ).rejects.toEqual( { message: 'oops' } );
+		} );
+
+		it( 'should resolve response instance when indicating `parse: false` in a successful response', async () => {
+			const handler = createFetchHandler( 200, { data: 'hi' } );
+			const result = await middleware( optionsDontParse, handler );
+			expect( result.status ).toEqual( 200 );
+			expect( result.json ).toBeInstanceOf( Function );
+		} );
+
+		it( 'should reject response instance when indicating `parse: false` in a failed response', async () => {
+			const handler = createFetchHandler( 418, { message: 'oops' } );
+			const promise = middleware( optionsDontParse, handler );
+			await expect( promise ).rejects.toMatchObject( {
+				status: 418,
+				json: expect.any( Function ),
+			} );
+		} );
+	} );
+
+	describe( 'Catch error response', () => {
+		it( 'should invoke callback function with response instance regardless of the original `parse` option', async () => {
+			const callback = jest.fn().mockImplementation( passReject );
+			const middleware = createErrorResponseCatcher( callback );
+
+			let handler = createFetchHandler( 418, { message: 'oops' } );
+			let promise = middleware( optionsDefaultParse, handler );
+			await expect( promise ).rejects.toMatchObject( expect.anything() );
+			expect( callback ).toHaveBeenCalledTimes( 1 );
+			expect( callback ).toHaveBeenCalledWith( {
+				status: 418,
+				json: expect.any( Function ),
+			} );
+
+			handler = createFetchHandler( 500, { message: 'ouch' } );
+			promise = middleware( optionsDontParse, handler );
+			await expect( promise ).rejects.toMatchObject( expect.anything() );
+			expect( callback ).toHaveBeenCalledTimes( 2 );
+			expect( callback ).toHaveBeenCalledWith( {
+				status: 500,
+				json: expect.any( Function ),
+			} );
+		} );
+	} );
+} );
