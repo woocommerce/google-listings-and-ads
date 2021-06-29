432ed6a182e912e5bd921d22b7022a1a4ffc5a87
mikkamp <michael.kamp@automattic.com>
2021-06-29 13:57:55 +0100

    Replace script to remove older googleads versions

--- bin/googleads-remove-older-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Removing googleads lib versions 4,5
-rm -rf vendor/googleads/google-ads-php/metadata/Google/Ads/GoogleAds/V{4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/V{4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Util/V{4,5}
-rm -rf vendor/googleads/google-ads-php/src/Google/Ads/GoogleAds/Lib/V{4,5}
