34ed1c732f5def0194c715cd3c35acaa111d85c2
Nima <73110514+nima-karimi@users.noreply.github.com>
2021-07-02 20:56:09 +0430

    Add abstract UnitTest class

---
+++ tests/Framework/UnitTest.php
@@ -0,0 +1,14 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework;
+
+use WP_UnitTestCase;
+
+/**
+ * Class UnitTest
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework
+ */
+abstract class UnitTest extends WP_UnitTestCase {
+}
