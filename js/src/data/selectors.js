cd4301d1d995c16d3207c71d871cc0019f547f3b
Eason Su <eason.su.tw@gmail.com>
2021-07-21 12:41:48 +0800

    Add a util function to get the connected jetpack info

--- js/src/data/selectors.js
+++ js/src/data/selectors.js
@@ -22,6 +22,20 @@ export const getSettings = ( state ) => {
 	return state.mc.settings;
 };
 
+/**
+ * @typedef {Object} JetpackAccount
+ * @property {'yes'|'no'} active Whether jetpack is connected.
+ * @property {'yes'|'no'} owner Whether the current admin user is the jetpack owner.
+ * @property {string|''} email Owner email. Available for jetpack owner.
+ * @property {string|''} displayName Owner name. Available for jetpack owner.
+ */
+
+/**
+ * Select jetpack connection state.
+ *
+ * @param {Object} state The current store state will be injected by `wp.data`.
+ * @return {JetpackAccount|null} The jetpack connection state. It would return `null` before the data is fetched.
+ */
 export const getJetpackAccount = ( state ) => {
 	return state.mc.accounts.jetpack;
 };
--- js/src/data/utils.test.js
+++ js/src/data/utils.test.js
@@ -6,6 +6,7 @@ import {
 	getReportKey,
 	calculateDelta,
 	mapReportFieldsToPerformance,
+	getConnectedJetpackInfo,
 	freeFields,
 	paidFields,
 	MISSING_FREE_LISTINGS_DATA,
@@ -260,6 +261,37 @@ describe( 'mapReportFieldsToPerformance', () => {
 			a: {
 				missingFreeListingsData: MISSING_FREE_LISTINGS_DATA.FOR_REQUEST,
 			},
+		} );
+	} );
+} );
+
+describe( 'getConnectedJetpackInfo', () => {
+	it( 'When jetpack in not connected, should return an empty string', () => {
+		expect( getConnectedJetpackInfo( { active: 'no' } ) ).toBe( '' );
+	} );
+
+	describe( 'For connected jetpack', () => {
+		it( 'When current user is the jetpack owner, should return email', () => {
+			const jetpack = {
+				active: 'yes',
+				owner: 'yes',
+				email: 'i_am_jetpack_owner@example.com',
+			};
+
+			expect( getConnectedJetpackInfo( jetpack ) ).toBe( jetpack.email );
+		} );
+
+		it( 'When current user is not the jetpack owner, should return a successfully connected info', () => {
+			const jetpack = {
+				active: 'yes',
+				owner: 'no',
+				email: 'i_am_another_admin_user@example.com',
+			};
+
+			const info = getConnectedJetpackInfo( jetpack );
+
+			expect( info ).not.toBe( jetpack.email );
+			expect( info ).toMatchSnapshot();
 		} );
 	} );
 } );
