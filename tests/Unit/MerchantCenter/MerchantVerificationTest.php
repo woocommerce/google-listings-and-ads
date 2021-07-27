34db88bf712fea961fda66ae5d832cc10afea883
Justin Palmer <justin.palmer@automattic.com>
2021-07-27 17:39:01 +0200

    Don't test protected/private methods

--- tests/Unit/MerchantCenter/MerchantVerificationTest.php
+++ tests/Unit/MerchantCenter/MerchantVerificationTest.php
@@ -4,6 +4,7 @@ declare( strict_types=1 );
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\MerchantCenter;
 
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Merchant;
+use Automattic\WooCommerce\GoogleListingsAndAds\Exception\InvalidValue;
 use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\MerchantVerification;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\ContainerAwareUnitTest;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Tools\HelperTrait\MerchantTrait;
@@ -68,10 +69,9 @@ class MerchantVerificationTest extends ContainerAwareUnitTest {
 			$this->merchant_verification->get_phone_number()
 		);
 
-		// Test that invalid numbers work too (by design)
-		$new_phone_number = 'The quick brown fox';
-		$this->merchant_verification->update_phone_number( $new_phone_number );
-		$this->assertEquals( $new_phone_number, $this->merchant_verification->get_phone_number() );
+		// Test exception for bad incorrect values.
+		$this->expectException( InvalidValue::class );
+		$this->merchant_verification->update_phone_number( 'The quick brown fox' );
 	}
 
 	public function test_get_account_exception() {
@@ -81,6 +81,26 @@ class MerchantVerificationTest extends ContainerAwareUnitTest {
 
 		$this->expectExceptionObject( $this->get_account_exception() );
 		$this->merchant_verification->get_phone_number();
+	}
 
+	public function test_sanitize_phone_number() {
+		$this->assertEquals(
+			$this->valid_account_phone_number,
+			$this->merchant_verification->sanitize_phone_number( $this->valid_account_phone_number )
+		);
+
+		$this->assertEquals(
+			'123456789',
+			$this->merchant_verification->sanitize_phone_number( '(123) 45-6789' )
+		);
+	}
+
+	public function test_get_phone_number_validate_callback() {
+		$this->assertFalse( $this->merchant_verification->validate_phone_number( 'Bad number' ) );
+		$this->assertFalse( $this->merchant_verification->validate_phone_number( '[192] 123 123' ) );
+		$this->assertFalse( $this->merchant_verification->validate_phone_number( '' ) );
+		$this->assertTrue( $this->merchant_verification->validate_phone_number( $this->valid_account_phone_number ) );
+		$this->assertTrue( $this->merchant_verification->validate_phone_number( '197.123.5482' ) );
+		$this->assertTrue( $this->merchant_verification->validate_phone_number( '+001 (197) 123-5482' ) );
 	}
 }
