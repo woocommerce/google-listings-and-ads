34e6f62afcd6e1554e3d6df84da5746ba11b0f49
Eason Su <eason.su.tw@gmail.com>
2021-08-04 15:24:32 +0800

    Pass phone number data as phoneNumber prop to PhoneNumberCard
    Address https://github.com/woocommerce/google-listings-and-ads/pull/921#discussion_r681414104

--- js/src/components/contact-information/index.js
+++ js/src/components/contact-information/index.js
@@ -45,6 +45,7 @@ export function ContactInformationPreview() {
 					<PhoneNumberCard
 						isPreview
 						initEditing={ false }
+						phoneNumber={ phone }
 						onEditClick={ handleEditClick }
 					/>
 					<StoreAddressCard isPreview />
