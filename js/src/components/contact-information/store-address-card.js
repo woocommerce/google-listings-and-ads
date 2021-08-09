703ed63a74fb3aaa1c568be4140d09feefd37742
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-09 17:03:46 +0200

    Track "edit store address button clicked" event,
    Implements part of https://github.com/woocommerce/google-listings-and-ads/issues/863.

--- js/src/components/contact-information/store-address-card.js
+++ js/src/components/contact-information/store-address-card.js
@@ -6,6 +6,7 @@ import { CardDivider } from '@wordpress/components';
 import { Spinner } from '@woocommerce/components';
 import { external as externalIcon } from '@wordpress/icons';
 import GridiconRefresh from 'gridicons/dist/refresh';
+import { getPath, getQuery } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
@@ -17,8 +18,27 @@ import AccountCard, { APPEARANCE } from '.~/components/account-card';
 import AppButton from '.~/components/app-button';
 import './store-address-card.scss';
 
+/**
+ * "Edit MC store address" Tracking event
+ *
+ * @event gla_edit_mc_store_address
+ * @type {Object} TrackingEvent
+ * @property {string} path A page from which the link was clicked.
+ * @property {string|undefined} [subpath] A subpage from which the link was clicked.
+ */
+
+/**
+ * Renders a component with a given store address.
+ *
+ * @fires gla_edit_mc_store_address Whenever "Edit in Settings" is clicked.
+ *
+ * @param {Object} props React props
+ * @param {boolean} props.isPreview Is that a short preview or more formatted view?
+ * @return {JSX.Element} Filled AccountCard component.
+ */
 export default function StoreAddressCard( { isPreview } ) {
 	const { loaded, data, refetch } = useStoreAddress();
+	const { subpath } = getQuery();
 	const editButton = (
 		<AppButton
 			isSecondary
@@ -28,6 +48,8 @@ export default function StoreAddressCard( { isPreview } ) {
 			target="_blank"
 			href="admin.php?page=wc-settings"
 			text={ __( 'Edit in Settings', 'google-listings-and-ads' ) }
+			eventName="gla_edit_mc_store_address"
+			eventProps={ { path: getPath(), subpath } }
 		/>
 	);
 
