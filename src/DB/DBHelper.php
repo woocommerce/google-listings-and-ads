a170127e2c6211161933630930f27e7a29b49439
Nima <73110514+nima-karimi@users.noreply.github.com>
2021-08-12 17:30:53 +0430

    Add DBHelper class

---
+++ src/DB/DBHelper.php
@@ -0,0 +1,59 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\DB;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
+use wpdb;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class DBHelper
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\DB
+ *
+ * @since x.x.x
+ */
+class DBHelper implements Service {
+	/**
+	 * @var wpdb
+	 */
+	protected $wpdb;
+
+	/**
+	 * AbstractMigration constructor.
+	 *
+	 * @param wpdb $wpdb The wpdb object.
+	 */
+	public function __construct( wpdb $wpdb ) {
+		$this->wpdb = $wpdb;
+	}
+
+	/**
+	 * Checks whether a database table exists.
+	 *
+	 * @param string $table_name The table's name.
+	 *
+	 * @return bool True if the table exists and False if not.
+	 */
+	public function table_exists( string $table_name ): bool {
+		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		return $table_name === $this->wpdb->get_var( $this->wpdb->prepare( 'SHOW TABLES LIKE %s', [ $table_name ] ) );
+	}
+
+	/**
+	 * Checks whether an index exists for the given database table.
+	 *
+	 * @param string $table_name The table's name.
+	 * @param string $index_name The index name.
+	 *
+	 * @return bool True if the index exists on the table and False if not.
+	 */
+	public function index_exists( string $table_name, string $index_name ): bool {
+		// phpcs:disable WordPress.DB.PreparedSQL.NotPrepared
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		return ! empty( $this->wpdb->get_results( $this->wpdb->prepare( "SHOW INDEX FROM `{$this->wpdb->_escape( $table_name )}` WHERE Key_name = %s ", [ $index_name ] ) ) );
+		// phpcs:enable WordPress.DB.PreparedSQL.NotPrepared
+	}
+}
