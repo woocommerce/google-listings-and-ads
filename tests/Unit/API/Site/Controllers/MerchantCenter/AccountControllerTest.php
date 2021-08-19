b4cd5b44bd7e63fc442e020e1760e9f23b4200ab
mikkamp <michael.kamp@automattic.com>
2021-08-19 14:00:19 +0100

    Create new account test

--- tests/Unit/API/Site/Controllers/MerchantCenter/AccountControllerTest.php
+++ tests/Unit/API/Site/Controllers/MerchantCenter/AccountControllerTest.php
@@ -8,6 +8,7 @@ use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\SiteVerification;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\MerchantCenter\AccountController;
 use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\MerchantCenterService;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\MerchantAccountState;
+use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\RESTControllerUnitTest;
 use PHPUnit\Framework\MockObject\MockObject;
 
@@ -22,10 +23,11 @@ defined( 'ABSPATH' ) || exit;
  *
  * @property MockObject|Middleware            $middleware
  * @property MockObject|Merchant              $merchant
- * @property MockObject|MerchantAccountState  $account_state
  * @property MockObject|MerchantCenterService $mc_service
  * @property MockObject|SiteVerification      $site_verification
+ * @property MockObject|OptionsInterface      $options
  * @property AccountController                $controller
+ * @property MerchantAccountState             $account_state
  */
 class AccountControllerTest extends RESTControllerUnitTest {
 
@@ -60,9 +62,12 @@ class AccountControllerTest extends RESTControllerUnitTest {
 
 		$this->middleware        = $this->createMock( Middleware::class );
 		$this->merchant          = $this->createMock( Merchant::class );
-		$this->account_state     = $this->createMock( MerchantAccountState::class );
 		$this->mc_service        = $this->createMock( MerchantCenterService::class );
 		$this->site_verification = $this->createMock( SiteVerification::class );
+		$this->options           = $this->createMock( OptionsInterface::class );
+
+		$this->account_state = new MerchantAccountState();
+		$this->account_state->set_options_object( $this->options );
 
 		$this->controller = new AccountController(
 			$this->server,
@@ -72,6 +77,7 @@ class AccountControllerTest extends RESTControllerUnitTest {
 			$this->mc_service,
 			$this->site_verification
 		);
+		$this->controller->set_options_object( $this->options );
 
 		$this->register_test_routes();
 	}
@@ -90,11 +96,43 @@ class AccountControllerTest extends RESTControllerUnitTest {
 
 		$this->middleware->expects( $this->any() )
 			->method( 'get_merchant_ids' )
-			->willReturn(  $accounts );
+			->willReturn( $accounts );
 
 		$response = $this->do_request( '/wc/gla/mc/accounts', 'GET' );
 	 	$this->assertExpectedResponse( $response, 200 );
 	 	$this->assertEquals( $accounts, $response->data );
+	}
+
+	public function test_create_new_account() {
+		$merchant_id = 12345;
+
+		$this->options->expects( $this->any() )
+			->method( 'get_merchant_id' )
+			->willReturn( 0 );
+
+		$this->middleware->expects( $this->any() )
+			->method( 'create_merchant_account' )
+			->willReturn( $merchant_id );
+
+		$this->site_verification->expects( $this->any() )
+			->method( 'insert' )
+			->willReturn( true );
+
+		$this->options->expects( $this->any() )
+			->method( 'get' )
+			->will(
+            	$this->returnCallback(
+					function( $arg ) {
+                		if ( OptionsInterface::MERCHANT_ACCOUNT_STATE === $arg ) {
+							return [];
+    	            	}
+            		}
+				)
+			);
+
+		$response = $this->do_request( '/wc/gla/mc/accounts', 'POST' );
+		$this->assertExpectedResponse( $response, 200 );
+		$this->assertEquals( $merchant_id, $response->data['id'] );
 	}
 
 }
