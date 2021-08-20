5e77f8aa9fc489ccac4c78c3d618c631efa554d7
mikkamp <michael.kamp@automattic.com>
2021-08-20 13:32:49 +0100

    Tests for claiming website

--- tests/Unit/API/Google/MerchantTest.php
+++ tests/Unit/API/Google/MerchantTest.php
@@ -6,9 +6,12 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\API\Google;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Merchant;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\UnitTest;
+use Exception;
+use Google\Exception as GoogleException;
 use Google\Service\ShoppingContent;
 use Google\Service\ShoppingContent\Product;
 use Google\Service\ShoppingContent\ProductsListResponse;
+use Google\Service\ShoppingContent\Resource\Accounts;
 use Google\Service\ShoppingContent\Resource\Products;
 use PHPUnit\Framework\MockObject\MockObject;
 
@@ -30,15 +33,17 @@ class MerchantTest extends UnitTest {
 	 */
 	public function setUp() {
 		parent::setUp();
-		$this->service  = $this->createMock( ShoppingContent::class );
+		$this->service           = $this->createMock( ShoppingContent::class );
+		$this->service->accounts = $this->createMock( Accounts::class );
+		$this->service->products = $this->createMock( Products::class );
+
 		$this->options  = $this->createMock( OptionsInterface::class );
 		$this->merchant = new Merchant( $this->service );
 		$this->merchant->set_options_object( $this->options );
 	}
 
 	public function test_get_products_empty_list() {
-		$this->service->products = $this->createMock( Products::class );
-		$list_response           = $this->createMock( ProductsListResponse::class );
+		$list_response = $this->createMock( ProductsListResponse::class );
 
 		$this->service->products->expects( $this->any() )
 			->method( 'listProducts' )
@@ -49,8 +54,7 @@ class MerchantTest extends UnitTest {
 	}
 
 	public function test_get_products() {
-		$this->service->products = $this->createMock( Products::class );
-		$list_response           = $this->createMock( ProductsListResponse::class );
+		$list_response = $this->createMock( ProductsListResponse::class );
 
 		$product_list = [
 			$this->createMock( Product::class ),
@@ -73,8 +77,7 @@ class MerchantTest extends UnitTest {
 	}
 
 	public function test_get_products_multiple_pages() {
-		$this->service->products = $this->createMock( Products::class );
-		$list_response           = $this->createMock( ProductsListResponse::class );
+		$list_response = $this->createMock( ProductsListResponse::class );
 
 		$product_list = [
 			$this->createMock( Product::class ),
@@ -103,4 +106,34 @@ class MerchantTest extends UnitTest {
 		$this->assertCount( count( $product_list ) * 2, $products );
 	}
 
+	public function test_claim_website() {
+		$this->assertTrue( $this->merchant->claimwebsite() );
+	}
+
+	public function test_claimwebsite_error() {
+		$this->service->accounts->expects( $this->any() )
+			->method( 'claimwebsite' )
+			->will(
+				$this->throwException(
+					new GoogleException()
+				)
+			);
+
+		$this->expectException( Exception::class );
+		$this->merchant->claimwebsite();
+	}
+
+	public function test_website_already_claimed() {
+		$this->service->accounts->expects( $this->any() )
+			->method( 'claimwebsite' )
+			->will(
+				$this->throwException(
+					new GoogleException( 'claimed', 403 )
+				)
+			);
+
+		$this->expectException( Exception::class );
+        $this->expectExceptionCode( 403 );
+		$this->merchant->claimwebsite();
+	}
 }
