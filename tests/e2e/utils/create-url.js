bf950a9e45fb8e294c021a2f14719b0a36100ca3
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-24 16:44:31 +0200

    Rename `createURL` parameter, update JSDoc,
    to make the name reflect closer the behavior.
    
    Addresses https://github.com/woocommerce/google-listings-and-ads/pull/970#discussion_r693844751.

--- tests/e2e/utils/create-url.js
+++ tests/e2e/utils/create-url.js
@@ -11,9 +11,10 @@ import { createURL as originalCreateUrl } from '@woocommerce/e2e-utils'; // esli
 const WP_BASE_URL = originalCreateUrl( '' );
 
 /**
- * Creates new URL by parsing given WPPath and query params, relative to the WP base.
+ * Creates new absolute (full) URL string by parsing given
+ * `url` and `query` relative to the WP base.
  *
- * Given `query` params are merged with the ones already present in the path.
+ * Given `query` params are merged with the ones already present in the `url`.
  * Any overlapping ones will be overwritten.
  *
  * @example
@@ -23,20 +24,20 @@ const WP_BASE_URL = originalCreateUrl( '' );
  * // "http://localhost:8889/foo?a=b&bar=baz&fiz=a%2Fb%2Fc"
  * ```
  *
- * @param {string}              [WPPath=''] String to be serialized as pathname.
+ * @param {string}              [url=''] A valid URL string, usually a relative path, but could contain as well contain query, fragment or be absolute.
  * @param {string|Object|Array} [query]     Query parameters in any format supported by {@link https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/URLSearchParams | URLSearchParams}.
- * @return {string} String which represents full URL.
+ * @return {string} String which represents the full, absolute URL.
  */
-export function createURL( WPPath = '', query = '' ) {
-	const url = new URL( WPPath, WP_BASE_URL );
+export function createURL( url = '', query = '' ) {
+	const fullUrl = new URL( url, WP_BASE_URL );
 
 	// Merge query params if any.
 	if ( query ) {
 		for ( const [ key, value ] of new URLSearchParams( query ) ) {
 			// Overwrite any params if already present.
-			url.searchParams.set( key, value );
+			fullUrl.searchParams.set( key, value );
 		}
 	}
 
-	return url.href;
+	return fullUrl.href;
 }
