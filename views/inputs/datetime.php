49b7409d19b8fc91688851fa66948ff1aceca0c5
Nima <73110514+nima-karimi@users.noreply.github.com>
2021-08-25 15:07:50 +0430

    Remove unnecessary input variables

--- views/inputs/datetime.php
+++ views/inputs/datetime.php
@@ -19,28 +19,18 @@ defined( 'ABSPATH' ) || exit;
 $input = $this->input;
 
 $input['class']         = $input['class'] ?? '';
-$input['placeholder']   = $input['placeholder'] ?? '';
-$input['style']         = $input['style'] ?? '';
 $input['wrapper_class'] = $input['wrapper_class'] ?? '';
 $input['name']          = $input['name'] ?? $input['id'];
 $input['desc_tip']      = $input['desc_tip'] ?? false;
 $input['date']          = $input['date'] ?? '';
 $input['time']          = $input['time'] ?? '';
 
-// Custom attribute handling
-$custom_attributes = [];
-if ( ! empty( $input['custom_attributes'] ) && is_array( $input['custom_attributes'] ) ) {
-	foreach ( $input['custom_attributes'] as $attribute => $value ) {
-		$custom_attributes[] = esc_attr( $attribute ) . '="' . esc_attr( $value ) . '"';
-	}
-}
-
 echo '<p class="form-field ' . esc_attr( $input['id'] ) . '_field gla-input-datetime ' . esc_attr( $input['wrapper_class'] ) . '">
 		<label for="' . esc_attr( $input['id'] ) . '_date">' . wp_kses_post( $input['label'] ) . '</label>';
 
 // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-echo '<input type="date" pattern="\d{4}-\d{2}-\d{2}" class="' . esc_attr( $input['class'] ) . '" style="' . esc_attr( $input['style'] ) . '" name="' . esc_attr( $input['name'] ) . '[date]" id="' . esc_attr( $input['id'] ) . '_date" value="' . esc_attr( $input['date'] ) . '" placeholder="' . esc_attr( $input['placeholder'] ) . '" ' . implode( ' ', $custom_attributes ) . ' /> ';
-echo '<input type="time" pattern="[0-9]{2}:[0-9]{2}" class="' . esc_attr( $input['class'] ) . '" style="' . esc_attr( $input['style'] ) . '" name="' . esc_attr( $input['name'] ) . '[time]" id="' . esc_attr( $input['id'] ) . '_time" value="' . esc_attr( $input['time'] ) . '" placeholder="' . esc_attr( $input['placeholder'] ) . '" ' . implode( ' ', $custom_attributes ) . ' /> ';
+echo '<input type="date" pattern="\d{4}-\d{2}-\d{2}" class="' . esc_attr( $input['class'] ) . '" name="' . esc_attr( $input['name'] ) . '[date]" id="' . esc_attr( $input['id'] ) . '_date" value="' . esc_attr( $input['date'] ) . '" /> ';
+echo '<input type="time" pattern="[0-9]{2}:[0-9]{2}" class="' . esc_attr( $input['class'] ) . '" name="' . esc_attr( $input['name'] ) . '[time]" id="' . esc_attr( $input['id'] ) . '_time" value="' . esc_attr( $input['time'] ) . '" /> ';
 // phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 
 if ( ! empty( $input['description'] ) && false !== $input['desc_tip'] ) {
