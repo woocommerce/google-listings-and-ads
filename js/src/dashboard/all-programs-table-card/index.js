daf9f687cf44f609face656e3908ff3e64dd4876
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-08-26 12:54:33 +0200

    Render currency code instead of symbol in dashboard,
    to avoid ambiguity.
    
    Addresses https://github.com/woocommerce/google-listings-and-ads/pull/920#issuecomment-899437561.

--- js/src/dashboard/all-programs-table-card/index.js
+++ js/src/dashboard/all-programs-table-card/index.js
@@ -20,6 +20,7 @@ import { FREE_LISTINGS_PROGRAM_ID } from '.~/constants';
 import AddPaidCampaignButton from '.~/components/paid-ads/add-paid-campaign-button';
 import ProgramToggle from './program-toggle';
 import FreeListingsDisabledToggle from './free-listings-disabled-toggle';
+import formatAmountWithCode from './format-amount-with-code';
 
 const headers = [
 	{
@@ -56,13 +57,15 @@ const AllProgramsTableCard = ( props ) => {
 	// Budget is given in the currency that is used by Google Ads, which may differ from the current store's currency.
 	// We will still use the store's currency **formatting** settings.
 	const {
-		currency: { formatAmount },
+		currency: { getCurrencyConfig },
 	} = useAdsCurrency();
 	const {
 		data: finalCountryCodesData,
 	} = useTargetAudienceFinalCountryCodes();
 	const { data: adsCampaignsData } = useAdsCampaigns();
 	const map = useCountryKeyNameMap();
+
+	const adsCurrency = getCurrencyConfig();
 
 	if ( ! finalCountryCodesData || ! adsCampaignsData ) {
 		return <AppSpinner />;
@@ -90,7 +93,7 @@ const AllProgramsTableCard = ( props ) => {
 			return {
 				id: el.id,
 				title: el.name,
-				dailyBudget: formatAmount( el.amount ),
+				dailyBudget: formatAmountWithCode( adsCurrency, el.amount ),
 				country: map[ el.country ],
 				active: el.status === 'enabled',
 			};
