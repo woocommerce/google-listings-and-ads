d51dc814bb8e23260b6f1b553f666bbd3e16c969
chennyx <chennyx@google.com>
2021-09-17 18:16:11 +0000

    fix visibility box injection
    fix MC dependency in coupon visibility box
    
    fix

--- src/Admin/MetaBox/CouponChannelVisibilityMetaBox.php
+++ src/Admin/MetaBox/CouponChannelVisibilityMetaBox.php
@@ -8,6 +8,7 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Exception\InvalidValue;
 use Automattic\WooCommerce\GoogleListingsAndAds\Coupon\CouponHelper;
 use Automattic\WooCommerce\GoogleListingsAndAds\Coupon\CouponMetaHandler;
 use Automattic\WooCommerce\GoogleListingsAndAds\Coupon\CouponSyncer;
+use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\MerchantCenterService;
 use Automattic\WooCommerce\GoogleListingsAndAds\Value\ChannelVisibility;
 use WC_Coupon;
 use WP_Post;
@@ -32,15 +33,22 @@ class CouponChannelVisibilityMetaBox extends SubmittableMetaBox {
 	protected $coupon_helper;
 
 	/**
+	 * @var MerchantCenterService
+	 */
+	protected $merchant_center;
+	
+	/**
 	 * CouponChannelVisibilityMetaBox constructor.
 	 *
 	 * @param Admin              $admin
 	 * @param CouponMetaHandler $meta_handler
 	 * @param CouponHelper      $coupon_helper
+	 * @param MerchantCenterService $merchant_center
 	 */
-	public function __construct( Admin $admin, CouponMetaHandler $meta_handler, CouponHelper $coupon_helper ) {
+	public function __construct( Admin $admin, CouponMetaHandler $meta_handler, CouponHelper $coupon_helper, MerchantCenterService $merchant_center ) {
 		$this->meta_handler   = $meta_handler;
 		$this->coupon_helper = $coupon_helper;
+		$this->merchant_center = $merchant_center;
 		parent::__construct( $admin );
 	}
 
@@ -128,6 +136,8 @@ class CouponChannelVisibilityMetaBox extends SubmittableMetaBox {
 			'channel_visibility' => $this->coupon_helper->get_channel_visibility( $coupon ),
 			'sync_status'        => $this->meta_handler->get_sync_status( $coupon ),
 			'issues'             => $this->coupon_helper->get_validation_errors( $coupon ),
+		    'is_setup_complete'  => $this->merchant_center->is_setup_complete(),
+		    'get_started_url'    => $this->get_start_url(),
 		];
 	}
 
--- views/meta-box/coupon_channel_visibility.php
+++ views/meta-box/coupon_channel_visibility.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types = 1);
 
+use Automattic\WooCommerce\GoogleListingsAndAds\Coupon\CouponSyncer;
 use Automattic\WooCommerce\GoogleListingsAndAds\Value\ChannelVisibility;
 use Automattic\WooCommerce\GoogleListingsAndAds\Value\SyncStatus;
 use Automattic\WooCommerce\GoogleListingsAndAds\View\PHPView;
 
-defined('ABSPATH') || exit();
+defined( 'ABSPATH' ) || exit();
 
 /**
  *
@@ -16,10 +17,11 @@ defined('ABSPATH') || exit();
  *
  * @var int $coupon_id
  */
-$coupon_id = $this->coupons_id;
+$coupon_id = $this->coupon_id;
+
 /**
  *
- * @var WC_Coupon $product
+ * @var WC_Coupon $coupon
  */
 $coupon = $this->coupon;
 
@@ -33,77 +35,101 @@ $field_id = $this->field_id;
 
 /**
  *
+ * @var bool
+ */
+$is_setup_complete = $this->is_setup_complete;
+
+/**
+ *
+ * @var string
+ */
+$get_started_url = $this->get_started_url;
+
+/**
+ *
  * @var string $sync_status
  */
-if (SyncStatus::HAS_ERRORS === $this->sync_status) {
-    $sync_status = __('Issues detected', 'google-listings-and-ads');
-} elseif (! is_null($this->sync_status)) {
-    $sync_status = ucfirst(str_replace('-', ' ', $this->sync_status));
+if ( SyncStatus::HAS_ERRORS === $this->sync_status ) {
+    $sync_status = __( 'Issues detected', 'google-listings-and-ads' );
+} elseif ( ! is_null( $this->sync_status ) ) {
+    $sync_status = ucfirst( str_replace( '-', ' ', $this->sync_status ) );
 }
-$show_status = $channel_visibility === ChannelVisibility::SYNC_AND_SHOW &&
-    $this->sync_status !== SyncStatus::SYNCED;
+$show_status = 
+    $channel_visibility ===
+    ChannelVisibility::SYNC_AND_SHOW &&
+    $this->sync_status !==
+    SyncStatus::SYNCED;
 
 /**
  *
  * @var array $issues
  */
 $issues = $this->issues;
-$has_issues = ! empty($issues);
+$has_issues = ! empty( $issues );
 
 $input_description = '';
 $input_disabled = false;
-if ($coupon->get_virtual() || (! CouponSyncer::is_coupon_supported($coupon))) {
+if ( ! CouponSyncer::is_coupon_supported( $coupon ) ) {
     $channel_visibility = ChannelVisibility::DONT_SYNC_AND_SHOW;
     $show_status = false;
     $input_disabled = true;
-    $input_description = __(
+    $input_description = __( 
         $coupon->get_virtual() ? 'This coupon cannot be shown on public channel because it is hidden from your store.' : 'This coupon cannot be shown because the coupon restrictions are not supported to share in Google channel.',
-        'google-listings-and-ads');
+        'google-listings-and-ads' );
 }
 
 $custom_attributes = [];
-if ($input_disabled) {
+if ( $input_disabled ) {
     $custom_attributes['disabled'] = 'disabled';
 }
 ?>
 
 <div class="gla-channel-visibility-box">
-<?php
-woocommerce_wp_select(
-    [
-        'id' => $field_id,
-        'value' => $channel_visibility,
-        'label' => __('Google Listing & Ads', 'google-listings-and-ads'),
-        'description' => $input_description,
-        'desc_tip' => false,
-        'options' => [
-            ChannelVisibility::SYNC_AND_SHOW => __('Show coupon on Google',
-                'google-listings-and-ads'),
-            ChannelVisibility::DONT_SYNC_AND_SHOW => __(
-                'Don\'t show coupn on Google', 'google-listings-and-ads')
-        ],
-        'custom_attributes' => $custom_attributes
-    ]);
-?>
-	<?php if ( $show_status ) : ?>
-	<div class="sync-status notice-alt notice-large notice-warning"
+	<?php if ( $is_setup_complete ) : ?>
+        <?php
+    woocommerce_wp_select( 
+        [
+            'id' => $field_id,
+            'value' => $channel_visibility,
+            'label' => __( 'Google Listing & Ads', 'google-listings-and-ads' ),
+            'description' => $input_description,
+            'desc_tip' => false,
+            'options' => [
+                ChannelVisibility::SYNC_AND_SHOW => __( 
+                    'Show coupon on Google',
+                    'google-listings-and-ads' ),
+                ChannelVisibility::DONT_SYNC_AND_SHOW => __( 
+                    'Don\'t show coupn on Google',
+                    'google-listings-and-ads' )],
+            'custom_attributes' => $custom_attributes,
+            'wrapper_class' => 'form-row form-row-full'] );
+    ?>
+    	<?php if ( $show_status ) : ?>
+    	<div class="sync-status notice-alt notice-large notice-warning"
 		style="border-left-style: solid">
 		<p>
 			<strong><?php esc_html_e( 'Google sync status', 'google-listings-and-ads' ); ?></strong>
 		</p>
 		<p><?php echo esc_html( $sync_status ); ?></p>
-		<?php if ( $has_issues ) : ?>
-			<div class="gla-product-issues">
+    		<?php if ( $has_issues ) : ?>
+    			<div class="gla-product-issues">
 			<p>
 				<strong><?php esc_html_e( 'Issues', 'google-listings-and-ads' ); ?></strong>
 			</p>
 			<ul>
-					<?php foreach ( $issues as $issue ) : ?>
-					<li><?php echo esc_html( $issue ); ?></li>
-					<?php endforeach; ?>
-				</ul>
+    					<?php foreach ( $issues as $issue ) : ?>
+    					<li><?php echo esc_html( $issue ); ?></li>
+    					<?php endforeach; ?>
+    				</ul>
 		</div>
-		<?php endif; ?>
-	</div>
+    		<?php endif; ?>
+    	</div>
+    	<?php endif; ?>
+	<?php else : ?>
+		<p>
+		<strong><?php esc_html_e( 'Google Listings & Ads', 'google-listings-and-ads' ); ?></strong>
+	</p>
+	<p><?php esc_html_e( 'Complete setup to get your coupon listed on Google for free.', 'google-listings-and-ads' ); ?></p>
+	<a href="<?php echo esc_attr( $get_started_url ); ?>" class="button"><?php esc_html_e( 'Complete setup', 'google-listings-and-ads' ); ?></a>
 	<?php endif; ?>
 </div>
