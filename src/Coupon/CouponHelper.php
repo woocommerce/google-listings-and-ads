80763d605ead8caf3c7b5000a63b02203d7a8251
chennyx <chennyx@google.com>
2021-09-30 06:59:49 +0000

    Adjust coupon visibility metabox context to algin with UX mocks

--- src/Coupon/CouponHelper.php
+++ src/Coupon/CouponHelper.php
@@ -145,6 +145,7 @@ class CouponHelper implements Service {
 	 */
 	public function mark_as_pending( WC_Coupon $coupon ) {
 		$this->meta_handler->update_sync_status( $coupon, SyncStatus::PENDING );
+		$this->meta_handler->delete_errors( $coupon );
 	}
 
 	/**
@@ -244,24 +245,25 @@ class CouponHelper implements Service {
 	 *
 	 * @param WC_Coupon $coupon
 	 *
-	 * @return string|null
+	 * @return string
 	 */
 	public function get_channel_visibility( WC_Coupon $coupon ): ?string {
-		try {
-			return $this->meta_handler->get_visibility( $coupon );
-		} catch ( InvalidValue $exception ) {
+		$visibility = $this->meta_handler->get_visibility( $coupon );
+
+		if ( empty( $visibility ) ) {
 			do_action(
 				'woocommerce_gla_debug_message',
 				sprintf(
-					'Channel visibility forced to "%s" for invalid coupon (ID: %s).',
+					'Channel visibility forced to "%s" for visibility unknown (Post ID: %s).',
 					ChannelVisibility::DONT_SYNC_AND_SHOW,
 					$coupon->get_id()
 				),
 				__METHOD__
 			);
-
 			return ChannelVisibility::DONT_SYNC_AND_SHOW;
 		}
+
+		return $visibility;
 	}
 
 	/**
