699b1914c3b95cccba465f252777e6bf98842b1a
Eason Su <eason.su.tw@gmail.com>
2021-10-26 18:51:54 +0800

    Add a new hook useGoogleAuthorization for getting Google Oauth URL

---
+++ js/src/hooks/useGoogleAuthorization.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import { useMemo } from '@wordpress/element';
+import { addQueryArgs } from '@wordpress/url';
+
+/**
+ * Internal dependencies
+ */
+import { API_NAMESPACE } from '.~/data/constants';
+import useApiFetchCallback from './useApiFetchCallback';
+
+/**
+ * Request a Google Oauth URL.
+ *
+ * @param {'setup-mc'|'reconnect'} next Indicate the next page name to map the redirect URI when back from Google authorization.
+ * @param {string} [loginHint] Specify the email to be requested additional scopes. Set this parameter only if wants to request a partial oauth to Google.
+ * @see https://developers.google.com/identity/protocols/oauth2/openid-connect#login-hint
+ * @return {Array} The same structure as `useApiFetchCallback`.
+ */
+export default function useGoogleAuthorization( next, loginHint ) {
+	const fetchOption = useMemo( () => {
+		const query = { next, login_hint: loginHint };
+		const path = addQueryArgs( `${ API_NAMESPACE }/google/connect`, query );
+		return { path };
+	}, [ next, loginHint ] );
+
+	return useApiFetchCallback( fetchOption );
+}
