86c96a1fd909808a7118253f05d423cbca4bac5e
Eason Su <eason.su.tw@gmail.com>
2021-10-28 11:39:58 +0800

    Revert "Rename a variable to isGoogleConnectedInsufficientGMCScope"
    This reverts commit ed4424e1190652409f12f9a734adfb7245a9710b.

--- js/src/setup-mc/setup-stepper/setup-accounts/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/index.js
@@ -40,7 +40,7 @@ const SetupAccounts = ( props ) => {
 	const isGoogleMCAccountDisabled = ! (
 		isGoogleConnected && scope.gmcRequired
 	);
-	const isGoogleConnectedInsufficientGMCScope =
+	const maybePreviewExistingGoogleMCAccounts =
 		isGoogleConnected && ! scope.gmcRequired;
 	const isContinueButtonDisabled = googleMCAccount?.status !== 'connected';
 
@@ -71,7 +71,7 @@ const SetupAccounts = ( props ) => {
 			<GoogleMCAccount
 				disabled={ isGoogleMCAccountDisabled }
 				maybePreviewExistingAccounts={
-					isGoogleConnectedInsufficientGMCScope
+					maybePreviewExistingGoogleMCAccounts
 				}
 			/>
 			<Faqs />
