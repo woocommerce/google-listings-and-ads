86705dab9412b3cac61bd07cdb6159afe92271b5
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-10-29 01:34:15 +0800

    Code refactor with ConnectWPComAccountCard.

--- js/src/setup-mc/setup-stepper/setup-accounts/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/index.js
@@ -20,7 +20,7 @@ import WordPressDotComAccount from './wordpressdotcom-account';
 import GoogleAccountCard from '.~/components/google-account-card';
 import GoogleMCAccount from './google-mc-account';
 import Faqs from './faqs';
-import WPComAccountCard from './wordpressdotcom-account/wpcom-account-card';
+import ConnectWPComAccountCard from './wordpressdotcom-account/connect-wpcom-account-card';
 
 const SetupAccounts = ( props ) => {
 	const { onContinue = () => {} } = props;
@@ -64,7 +64,7 @@ const SetupAccounts = ( props ) => {
 			>
 				<VerticalGapLayout size="large">
 					<WordPressDotComAccount />
-					<WPComAccountCard />
+					<ConnectWPComAccountCard />
 					<GoogleAccountCard disabled={ isGoogleAccountDisabled } />
 				</VerticalGapLayout>
 			</Section>
--- js/src/setup-mc/setup-stepper/setup-accounts/wordpressdotcom-account/wpcom-account-card/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/wordpressdotcom-account/wpcom-account-card/index.js
@@ -7,32 +7,9 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import AccountCard from '.~/components/account-card';
-import AppButton from '.~/components/app-button';
-import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
-import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 import wpLogoURL from './wp-logo.svg';
 
-const WPComAccountCard = () => {
-	const { createNotice } = useDispatchCoreNotices();
-	const [ fetchJetpackConnect, { loading, data } ] = useApiFetchCallback( {
-		path: '/wc/gla/jetpack/connect',
-	} );
-
-	const handleConnectClick = async () => {
-		try {
-			const d = await fetchJetpackConnect();
-			window.location.href = d.url;
-		} catch ( error ) {
-			createNotice(
-				'error',
-				__(
-					'Unable to connect your WordPress.com account. Please try again later.',
-					'google-listings-and-ads'
-				)
-			);
-		}
-	};
-
+const WPComAccountCard = ( props ) => {
 	return (
 		<AccountCard
 			appearance={ {
@@ -49,20 +26,7 @@ const WPComAccountCard = () => {
 				),
 				title: 'WordPress.com',
 			} }
-			description={ __(
-				'Required to connect with Google',
-				'google-listings-and-ads'
-			) }
-			indicator={
-				<AppButton
-					isSecondary
-					loading={ loading || data }
-					eventName="gla_wordpress_account_connect_button_click"
-					onClick={ handleConnectClick }
-				>
-					{ __( 'Connect', 'google-listings-and-ads' ) }
-				</AppButton>
-			}
+			{ ...props }
 		/>
 	);
 };
