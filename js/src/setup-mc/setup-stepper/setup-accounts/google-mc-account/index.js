3e3a30ceeddcd3413c30a0a0898e48b8052228b3
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-04 03:32:35 +0800

    Display side tooltip in GoogleMCAccount.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/index.js
@@ -2,32 +2,51 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Tip } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Section from '.~/wcdl/section';
+import AppTooltip from '.~/components/app-tooltip';
 import DisabledDiv from '.~/components/disabled-div';
 import SectionContent from './section-content';
+import './index.scss';
 
 const GoogleMCAccount = ( props ) => {
 	const { disabled = false } = props;
 
 	return (
-		<Section
-			title={ __(
-				'Google Merchant Center account',
-				'google-listings-and-ads'
-			) }
-			description={ __(
-				'WooCommerce products synced to your Merchant Center product feed will allow you to list your products on Google.',
-				'google-listings-and-ads'
-			) }
-		>
-			<DisabledDiv disabled={ disabled }>
-				<SectionContent disabled={ disabled } />
-			</DisabledDiv>
-		</Section>
+		<div className="gla-google-mc-account">
+			<Section
+				description={
+					<Tip>
+						{ createInterpolateElement(
+							__(
+								'Google’s Shopping tab has an average of <strong>over 50% increase in clicks and over 100% increase in impressions</strong> across free listings and ads. <tooltip>Source</tooltip>',
+								'google-listings-and-ads'
+							),
+							{
+								strong: <strong></strong>,
+								tooltip: (
+									<AppTooltip
+										text={ __(
+											'Google Internal Data, July 2020, based on an A/B test comparing performance for users seeing the updated layout on the Shopping property vs a control group not seeing the new experience',
+											'google-listings-and-ads'
+										) }
+									></AppTooltip>
+								),
+							}
+						) }
+					</Tip>
+				}
+			>
+				<DisabledDiv disabled={ disabled }>
+					<SectionContent disabled={ disabled } />
+				</DisabledDiv>
+			</Section>
+		</div>
 	);
 };
 
