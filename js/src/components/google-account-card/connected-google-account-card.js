caa904758e2ad757ff8ad8b93e4745ae71bd9421
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-03 22:05:13 +0800

    Disconnect current Google account before connecting to a different one.

--- js/src/components/google-account-card/connected-google-account-card.js
+++ js/src/components/google-account-card/connected-google-account-card.js
@@ -7,29 +7,53 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { glaData } from '.~/constants';
+import { API_NAMESPACE } from '.~/data/constants';
 import AccountCard, { APPEARANCE } from '.~/components/account-card';
 import AppButton from '.~/components/app-button';
 import ConnectedIconLabel from '.~/components/connected-icon-label';
 import useGoogleAuthorization from '.~/hooks/useGoogleAuthorization';
 import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
 import Section from '.~/wcdl/section';
+import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 
 export default function ConnectedGoogleAccountCard( { googleAccount } ) {
 	const pageName = glaData.mcSetupComplete ? 'reconnect' : 'setup-mc';
-	const { createNotice } = useDispatchCoreNotices();
-	const [ fetchGoogleConnect, { loading, data } ] = useGoogleAuthorization(
-		pageName
-	);
+	const { createNotice, removeNotice } = useDispatchCoreNotices();
+	const [
+		fetchGoogleDisconnect,
+		{ loading: loadingGoogleDisconnect },
+	] = useApiFetchCallback( {
+		method: 'DELETE',
+		path: `${ API_NAMESPACE }/google/connect`,
+	} );
+	const [
+		fetchGoogleConnect,
+		{ loading: loadingGoogleConnect, data: dataGoogleConnect },
+	] = useGoogleAuthorization( pageName );
 
-	const handleConnect = async () => {
+	const handleSwitch = async () => {
+		let notice;
+
 		try {
+			const result = await createNotice(
+				'info',
+				__(
+					'Disconnecting your current Google account, please wait…',
+					'google-listings-and-ads'
+				)
+			);
+			notice = result.notice;
+
+			await fetchGoogleDisconnect();
+
 			const { url } = await fetchGoogleConnect();
 			window.location.href = url;
 		} catch ( error ) {
+			removeNotice( notice.id );
 			createNotice(
 				'error',
 				__(
-					'Unable to connect your Google account. Please try again later.',
+					'Unable to connect to a different Google account. Please try again later.',
 					'google-listings-and-ads'
 				)
 			);
@@ -45,12 +69,16 @@ export default function ConnectedGoogleAccountCard( { googleAccount } ) {
 			<Section.Card.Footer>
 				<AppButton
 					isLink
-					disabled={ loading || data }
+					disabled={
+						loadingGoogleDisconnect ||
+						loadingGoogleConnect ||
+						dataGoogleConnect
+					}
 					text={ __(
 						'Or, connect to a different Google account',
 						'google-listings-and-ads'
 					) }
-					onClick={ handleConnect }
+					onClick={ handleSwitch }
 				/>
 			</Section.Card.Footer>
 		</AccountCard>
