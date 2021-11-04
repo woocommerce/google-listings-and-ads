02799c3b90957c7e98264562500a929556b0d0f9
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-04 17:43:16 +0800

    Add code comment to explain using useApiFetchCallback instead of wp-data action in useSwitchGoogleAccount.

--- js/src/components/google-account-card/useSwitchGoogleAccount.js
+++ js/src/components/google-account-card/useSwitchGoogleAccount.js
@@ -30,6 +30,14 @@ import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 const useSwitchGoogleAccount = () => {
 	const pageName = glaData.mcSetupComplete ? 'reconnect' : 'setup-mc';
 	const { createNotice, removeNotice } = useDispatchCoreNotices();
+
+	/**
+	 * Note: we are manually calling `DELETE /google/connect` instead of using
+	 * `disconnectGoogleAccount` action from wp-data store
+	 * because `disconnectGoogleAccount` will cause a store update,
+	 * and the UI will display the Connect card for a brief moment,
+	 * before the browser redirects to the Google Auth page.
+	 */
 	const [
 		fetchGoogleDisconnect,
 		{ loading: loadingGoogleDisconnect },
@@ -37,6 +45,7 @@ const useSwitchGoogleAccount = () => {
 		path: `${ API_NAMESPACE }/google/connect`,
 		method: 'DELETE',
 	} );
+
 	const [
 		fetchGoogleConnect,
 		{ loading: loadingGoogleConnect, data: dataGoogleConnect },
