077d0e4f59f8ce7dead05bb52e0fe04c1b011628
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-10 02:49:10 +0800

    Improve UI of ReclaimUrlCard.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/reclaim-url-card/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/reclaim-url-card/index.js
@@ -2,14 +2,14 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement, useState } from '@wordpress/element';
-import { CheckboxControl } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
+import { CardDivider } from '@wordpress/components';
+import { Icon, link as linkIcon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import toAccountText from '.~/utils/toAccountText';
-import recordEvent from '.~/utils/recordEvent';
 import AppDocumentationLink from '.~/components/app-documentation-link';
 import AppButton from '.~/components/app-button';
 import Section from '.~/wcdl/section';
@@ -19,11 +19,12 @@ import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
 import { useAppDispatch } from '.~/data';
 import ContentButtonLayout from '.~/components/content-button-layout';
 import ReclaimUrlFailCard from './reclaim-url-fail-card';
+import AccountCard, { APPEARANCE } from '.~/components/account-card';
+import AppInputControl from '.~/components/app-input-control';
 import './index.scss';
 
 const ReclaimUrlCard = ( props ) => {
 	const { id, websiteUrl } = props;
-	const [ checked, setChecked ] = useState( false );
 	const { createNotice } = useDispatchCoreNotices();
 	const { invalidateResolution } = useAppDispatch();
 	const [
@@ -60,55 +61,37 @@ const ReclaimUrlCard = ( props ) => {
 		return <ReclaimUrlFailCard onRetry={ handleRetry } />;
 	}
 
-	const handleCheckboxChange = ( v ) => {
-		recordEvent( 'gla_mc_account_reclaim_url_agreement_check', {
-			checked: v,
-		} );
-		setChecked( v );
-	};
-
 	return (
-		<Section.Card className="gla-reclaim-url-card">
+		<AccountCard
+			className="gla-reclaim-url-card"
+			appearance={ APPEARANCE.GOOGLE_MERCHANT_CENTER }
+			description={ toAccountText( id ) }
+			indicator={
+				<AppButton isSecondary loading={ loading } onClick={ () => {} }>
+					{ __( 'Switch account', 'google-listings-and-ads' ) }
+				</AppButton>
+			}
+		>
+			<CardDivider />
 			<Section.Card.Body>
-				<Subsection.Title>{ toAccountText( id ) }</Subsection.Title>
+				<Subsection.Title>
+					{ __( 'Reclaim your URL', 'google-listings-and-ads' ) }
+				</Subsection.Title>
+				<p>
+					{ __(
+						'Your URL is currently claimed by another Merchant Center account.',
+						'google-listings-and-ads'
+					) }
+				</p>
 				<ContentButtonLayout>
-					<div>
-						<Subsection.Title>
-							{ createInterpolateElement(
-								__(
-									'Your URL, <url />, is currently claimed by another Merchant Center account. ',
-									'google-listings-and-ads'
-								),
-								{
-									url: <span>{ websiteUrl }</span>,
-								}
-							) }
-						</Subsection.Title>
-						<Subsection.HelperText className="gla-reclaim-url-card__warning">
-							{ createInterpolateElement(
-								__(
-									'<strong>Warning:</strong> If you reclaim this URL, it will cause any existing product listings or ads to stop running, and any existing shipping or tax configurations will be lost. The other verified account will be notified that they have lost their claim.',
-									'google-listings-and-ads'
-								),
-								{
-									strong: <strong />,
-								}
-							) }
-						</Subsection.HelperText>
-						<CheckboxControl
-							label={ __(
-								'Yes, I understand the implications of reclaiming my URL.',
-								'google-listings-and-ads'
-							) }
-							checked={ checked }
-							disabled={ loading }
-							onChange={ handleCheckboxChange }
-						></CheckboxControl>
-					</div>
+					<AppInputControl
+						disabled
+						prefix={ <Icon icon={ linkIcon } size={ 24 } /> }
+						value={ websiteUrl }
+					></AppInputControl>
 					<AppButton
 						isSecondary
 						isDestructive
-						disabled={ ! checked }
 						loading={ loading }
 						eventName="gla_mc_account_reclaim_url_button_click"
 						onClick={ handleReclaimClick }
@@ -116,20 +99,25 @@ const ReclaimUrlCard = ( props ) => {
 						{ __( 'Reclaim my URL', 'google-listings-and-ads' ) }
 					</AppButton>
 				</ContentButtonLayout>
-			</Section.Card.Body>
-			<Section.Card.Footer>
-				<AppDocumentationLink
-					context="setup-mc"
-					linkId="claim-url"
-					href="https://support.google.com/merchants/answer/176793"
-				>
-					{ __(
-						'Read more about claiming URLs',
-						'google-listings-and-ads'
+				<Subsection.HelperText>
+					{ createInterpolateElement(
+						__(
+							'If you reclaim this URL, it will cause any existing product listings or ads to stop running, and the other verified account will be notified that they have lost their claim. <link>Learn more</link>.',
+							'google-listings-and-ads'
+						),
+						{
+							link: (
+								<AppDocumentationLink
+									context="setup-mc"
+									linkId="claim-url"
+									href="https://support.google.com/merchants/answer/176793"
+								/>
+							),
+						}
 					) }
-				</AppDocumentationLink>
-			</Section.Card.Footer>
-		</Section.Card>
+				</Subsection.HelperText>
+			</Section.Card.Body>
+		</AccountCard>
 	);
 };
 
