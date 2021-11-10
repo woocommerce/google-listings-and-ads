1c2fdb7ad5191006510f9f713d33b9b68983e76d
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-10 18:02:30 +0800

    Improve UI of MC CreateAccountCard.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/create-account-card/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/create-account-card/index.js
@@ -2,48 +2,27 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import Section from '.~/wcdl/section';
-import TitleButtonLayout from '.~/components/title-button-layout';
 import CreateAccountButton from './create-account-button';
+import AccountCard, { APPEARANCE } from '.~/components/account-card';
 
 const CreateAccountCard = ( props ) => {
-	const {
-		allowShowExisting,
-		onShowExisting = () => {},
-		onCreateAccount,
-	} = props;
+	const { onCreateAccount } = props;
 
 	return (
-		<Section.Card>
-			<Section.Card.Body>
-				<TitleButtonLayout
-					title={ __(
-						'Create your Google Merchant Center account',
-						'google-listings-and-ads'
-					) }
-					button={
-						<CreateAccountButton
-							onCreateAccount={ onCreateAccount }
-						/>
-					}
-				></TitleButtonLayout>
-			</Section.Card.Body>
-			{ allowShowExisting && (
-				<Section.Card.Footer>
-					<Button isLink onClick={ onShowExisting }>
-						{ __(
-							'Or, use your existing Google Merchant Center account',
-							'google-listings-and-ads'
-						) }
-					</Button>
-				</Section.Card.Footer>
+		<AccountCard
+			appearance={ APPEARANCE.GOOGLE_MERCHANT_CENTER }
+			description={ __(
+				'Create your Google Merchant Center account',
+				'google-listings-and-ads'
 			) }
-		</Section.Card>
+			indicator={
+				<CreateAccountButton onCreateAccount={ onCreateAccount } />
+			}
+		></AccountCard>
 	);
 };
 
