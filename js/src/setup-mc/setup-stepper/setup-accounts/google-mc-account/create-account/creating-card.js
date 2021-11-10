cfafe67c9e12dbd95164b060707428ee9f154edd
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-10 18:07:23 +0800

    Improve UI of MC CreatingCard.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/creating-card.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/creating-card.js
@@ -7,10 +7,8 @@ import { useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import Section from '.~/wcdl/section';
-import ContentButtonLayout from '.~/components/content-button-layout';
-import Subsection from '.~/wcdl/subsection';
 import AppButton from '.~/components/app-button';
+import AccountCard, { APPEARANCE } from '.~/components/account-card';
 
 const CreatingCard = ( props ) => {
 	const { retryAfter, onRetry = () => {} } = props;
@@ -28,29 +26,18 @@ const CreatingCard = ( props ) => {
 	}, [ retryAfter, onRetry ] );
 
 	return (
-		<Section.Card>
-			<Section.Card.Body>
-				<ContentButtonLayout>
-					<div>
-						<Subsection.Title>
-							{ __(
-								'Create your Google Merchant Center account',
-								'google-listings-and-ads'
-							) }
-						</Subsection.Title>
-						<Subsection.HelperText>
-							{ __(
-								'This may take a few minutes, please wait a moment…',
-								'google-listings-and-ads'
-							) }
-						</Subsection.HelperText>
-					</div>
-					<AppButton loading>
-						{ __( 'Creating…', 'google-listings-and-ads' ) }
-					</AppButton>
-				</ContentButtonLayout>
-			</Section.Card.Body>
-		</Section.Card>
+		<AccountCard
+			appearance={ APPEARANCE.GOOGLE_MERCHANT_CENTER }
+			description={ __(
+				'This may take a few minutes, please wait a moment…',
+				'google-listings-and-ads'
+			) }
+			indicator={
+				<AppButton loading>
+					{ __( 'Creating…', 'google-listings-and-ads' ) }
+				</AppButton>
+			}
+		></AccountCard>
 	);
 };
 
