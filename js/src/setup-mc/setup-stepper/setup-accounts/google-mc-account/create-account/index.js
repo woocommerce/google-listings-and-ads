7d433e2b97d6489e26a615c57aff8a9c8e47ab79
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-14 19:58:10 +0800

    Code refactor with useCreateMCAccount hook.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/index.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/create-account/index.js
@@ -1,50 +1,17 @@
 /**
- * External dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Internal dependencies
  */
-import { useAppDispatch } from '.~/data';
-import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 import CreateAccountCard from './create-account-card';
 import CreatingCard from '../creating-card';
 import ReclaimUrlCard from '../reclaim-url-card';
-import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
+import useCreateMCAccount from '../useCreateMCAccount';
 
 const CreateAccount = ( props ) => {
 	const { allowShowExisting, onShowExisting } = props;
-	const { createNotice } = useDispatchCoreNotices();
-	const { invalidateResolution } = useAppDispatch();
 	const [
-		fetchCreateMCAccount,
+		handleCreateAccount,
 		{ loading, error, response },
-	] = useApiFetchCallback( {
-		path: `/wc/gla/mc/accounts`,
-		method: 'POST',
-	} );
-
-	const handleCreateAccount = async () => {
-		try {
-			await fetchCreateMCAccount( {
-				data: error?.id && { id: error.id },
-				parse: false,
-			} );
-			invalidateResolution( 'getGoogleMCAccount', [] );
-		} catch ( e ) {
-			if ( ! [ 403, 503 ].includes( e.status ) ) {
-				const body = await e.json();
-				const message =
-					body.message ||
-					__(
-						'Unable to create Merchant Center account. Please try again later.',
-						'google-listings-and-ads'
-					);
-				createNotice( 'error', message );
-			}
-		}
-	};
+	] = useCreateMCAccount();
 
 	if ( loading || ( response && response.status === 503 ) ) {
 		return (
---
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/useCreateMCAccount.js
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useAppDispatch } from '.~/data';
+import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
+import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
+
+const useCreateMCAccount = () => {
+	const { createNotice } = useDispatchCoreNotices();
+	const { invalidateResolution } = useAppDispatch();
+	const [ fetchCreateMCAccount, result ] = useApiFetchCallback( {
+		path: `/wc/gla/mc/accounts`,
+		method: 'POST',
+	} );
+
+	const handleCreateAccount = async () => {
+		try {
+			await fetchCreateMCAccount( {
+				data: result?.error?.id && { id: result.error.id },
+				parse: false,
+			} );
+			invalidateResolution( 'getGoogleMCAccount', [] );
+		} catch ( e ) {
+			if ( ! [ 403, 503 ].includes( e.status ) ) {
+				const body = await e.json();
+				const message =
+					body.message ||
+					__(
+						'Unable to create Merchant Center account. Please try again later.',
+						'google-listings-and-ads'
+					);
+				createNotice( 'error', message );
+			}
+		}
+	};
+
+	return [ handleCreateAccount, result ];
+};
+
+export default useCreateMCAccount;
