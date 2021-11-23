82c6e97b0a0e6c0729b5f8d41b75b38f5e411b50
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-11-23 21:30:40 +0800

    Use AccountCard disabled props.

--- js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/disabled-card.js
+++ js/src/setup-mc/setup-stepper/setup-accounts/google-mc-account/disabled-card.js
@@ -8,24 +8,22 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import AccountCard, { APPEARANCE } from '.~/components/account-card';
-import DisabledDiv from '.~/components/disabled-div';
 
 const DisabledCard = () => {
 	return (
-		<DisabledDiv>
-			<AccountCard
-				appearance={ APPEARANCE.GOOGLE_MERCHANT_CENTER }
-				description={ __(
-					'Required to sync products and list on Google',
-					'google-listings-and-ads'
-				) }
-				indicator={
-					<Button isSecondary disabled>
-						{ __( 'Connect', 'google-listings-and-ads' ) }
-					</Button>
-				}
-			/>
-		</DisabledDiv>
+		<AccountCard
+			disabled
+			appearance={ APPEARANCE.GOOGLE_MERCHANT_CENTER }
+			description={ __(
+				'Required to sync products and list on Google',
+				'google-listings-and-ads'
+			) }
+			indicator={
+				<Button isSecondary disabled>
+					{ __( 'Connect', 'google-listings-and-ads' ) }
+				</Button>
+			}
+		/>
 	);
 };
 
