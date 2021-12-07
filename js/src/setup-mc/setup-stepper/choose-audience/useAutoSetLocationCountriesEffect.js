66ed51137cd31e2d08989bab80e8dcdc96032b47
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-12-08 00:38:36 +0800

    Get target audience suggestions in useAutoSetLocationCountriesEffect.
    We use the countries value in the suggestion to populate the countries field in Setup MC Step 2 Choose Your Audience.

--- js/src/setup-mc/setup-stepper/choose-audience/useAutoSetLocationCountriesEffect.js
+++ js/src/setup-mc/setup-stepper/choose-audience/useAutoSetLocationCountriesEffect.js
@@ -6,39 +6,38 @@ import { useCallback, useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import useStoreCountry from '.~/hooks/useStoreCountry';
-import useGetCountries from '.~/hooks/useGetCountries';
+import useApiFetchEffect from '.~/hooks/useApiFetchEffect';
 
 /**
- * Automatically set `location` to be `selected` and `countries` to be `[ storeCountryCode ]`
- * when `values.location === null && values.countries.length === 0` (i.e. when users first visit the page).
+ * Automatically set `location` to be `selected`
+ * and `countries` to be the value from Target Audience Suggestion API
+ * when `values.location === null && values.countries.length === 0`
+ * (i.e. when users first visit the page).
  *
- * If the `storeCountryCode` is not in the list of the MC supported countries, then `countries` would be set to empty array `[]`.
- *
- * This is done by calling `onChange` from `getInputProps`, simulating user's manual input action.
+ * The value setting is done by calling `onChange` from `getInputProps`,
+ * simulating user's manual input action.
  *
  * @param {Object} formProps formProps.
  */
 const useAutoSetLocationCountriesEffect = ( formProps ) => {
 	const { values, getInputProps } = formProps;
-	const { code: storeCountryCode } = useStoreCountry();
-	const { data } = useGetCountries();
-
-	const hasNoLocationCountries =
-		values.location === null && values.countries.length === 0;
+	const { data } = useApiFetchEffect( {
+		path: `wc/gla/mc/target_audience/suggestions`,
+	} );
 
 	const setLocationCountries = useCallback( () => {
 		if ( ! data ) {
 			return;
 		}
 
-		const countriesValue = data[ storeCountryCode ]
-			? [ storeCountryCode ]
-			: [];
+		const countriesValue = data.countries;
 
 		getInputProps( 'location' ).onChange( 'selected' );
 		getInputProps( 'countries' ).onChange( countriesValue );
-	}, [ data, getInputProps, storeCountryCode ] );
+	}, [ data, getInputProps ] );
+
+	const hasNoLocationCountries =
+		values.location === null && values.countries.length === 0;
 
 	useEffect( () => {
 		if ( hasNoLocationCountries ) {
