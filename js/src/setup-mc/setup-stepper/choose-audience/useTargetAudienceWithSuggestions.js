5cdbd338ac382575de99e4b40475aca4599c70b2
Gan Eng Chin <ecgan@users.noreply.github.com>
2021-12-09 00:15:42 +0800

    Simplify useTargetAudienceWithSuggestions.
    We use useApiFetchEffect and make API requests in parallel instead of sequential. Overall, this would be faster and better for most users.

--- js/src/setup-mc/setup-stepper/choose-audience/useTargetAudienceWithSuggestions.js
+++ js/src/setup-mc/setup-stepper/choose-audience/useTargetAudienceWithSuggestions.js
@@ -1,29 +1,46 @@
 /**
- * External dependencies
- */
-import { useEffect } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
-import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 import useTargetAudience from '.~/hooks/useTargetAudience';
 import useGetCountries from '.~/hooks/useGetCountries';
+import useApiFetchEffect from '.~/hooks/useApiFetchEffect';
 
+/**
+ * @typedef {Object} TargetAudienceWithSuggestionsResult
+ * @property {boolean} loading A boolean indicating the network requests are still in progress.
+ * @property {Object} data Previously-saved target audience data, or target audience suggestions data.
+ */
+
+/**
+ * A hook that calls the following APIs in parallel:
+ *
+ * - Supported countries API, so that the list of supported countries
+ * are stored in the wp-data store and ready to be used by children components,
+ * e.g. MerchantCenterSelectControl.
+ * - Target audience API, to get the previously-saved target audience data.
+ * - Target audience suggestions API, to get the suggested target audience data
+ * based on WooCommerce store settings.
+ *
+ * It returns a result object with the following:
+ *
+ * - `loading`: a boolean indicating the network requests are still in progress.
+ * - `data`: if the target audience data does not have location and countries set
+ * (i.e. users are visiting Setup MC Step 2 for the first time),
+ * `data` will be the target audience suggestions data;
+ * otherwise, `data` will be the previously-saved target audience data.
+ *
+ * @return {TargetAudienceWithSuggestionsResult} A result object with `loading` and `data`.
+ */
 const useTargetAudienceWithSuggestions = () => {
 	const { hasFinishedResolution: hfrCountries } = useGetCountries();
 	const {
 		hasFinishedResolution: hfrTargetAudience,
 		data: dataTargetAudience,
 	} = useTargetAudience();
-	const [
-		fetchSuggestions,
-		{
-			loading: loadingSuggestions,
-			data: dataSuggestions,
-			response: responseSuggestions,
-		},
-	] = useApiFetchCallback( {
+	const {
+		loading: loadingSuggestions,
+		data: dataSuggestions,
+	} = useApiFetchEffect( {
 		path: `wc/gla/mc/target_audience/suggestions`,
 	} );
 
@@ -32,26 +49,9 @@ const useTargetAudienceWithSuggestions = () => {
 		dataTargetAudience?.location === null &&
 		dataTargetAudience?.countries === null;
 
-	const hasNotFetchedSuggestions =
-		! loadingSuggestions && ! responseSuggestions;
-
-	useEffect( () => {
-		if ( hasNoLocationCountries && hasNotFetchedSuggestions ) {
-			fetchSuggestions();
-		}
-	}, [ fetchSuggestions, hasNoLocationCountries, hasNotFetchedSuggestions ] );
-
-	if ( hasNoLocationCountries ) {
-		return {
-			loading:
-				! hfrCountries || ! hfrTargetAudience || ! responseSuggestions,
-			data: dataSuggestions || dataTargetAudience,
-		};
-	}
-
 	return {
-		loading: ! hfrCountries || ! hfrTargetAudience,
-		data: dataTargetAudience,
+		loading: ! hfrCountries || ! hfrTargetAudience || loadingSuggestions,
+		data: hasNoLocationCountries ? dataSuggestions : dataTargetAudience,
 	};
 };
 
