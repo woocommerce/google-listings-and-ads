c09b3673d60659a57a01620928a7f736ed7e4d0e
Jorge Mahecha <jorge.mahecha@a8c.com>
2021-12-08 19:15:00 +0100

    Added disable property to the audience country field

---
+++ js/src/components/paid-ads/audienceSection.test.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import AudienceSection from '.~/components/paid-ads/audience-section';
+
+jest.mock( '.~/hooks/useCountryKeyNameMap', () =>
+	jest.fn( () => ( {
+		GB: 'United Kingdom',
+		US: 'United States',
+		ES: 'Spain',
+	} ) )
+);
+
+jest.mock( '.~/hooks/useTargetAudienceFinalCountryCodes', () =>
+	jest.fn( () => ( { data: [ 'GB', 'US', 'ES' ] } ) )
+);
+
+describe( 'audienceSection', () => {
+	const defaultProps = {
+		formProps: {
+			getInputProps: () => ( {
+				checked: true,
+				className: null,
+				help: null,
+				onBlur: ( f ) => f,
+				onChange: undefined,
+				selected: [ 'GB' ],
+				value: [ 'GB' ],
+			} ),
+		},
+		countrySelectHelperText:
+			'Once a campaign has been created, you cannot change the target country.',
+	};
+
+	test( 'If Audience section is in edit mode the country field should be disabled', async () => {
+		render( <AudienceSection { ...defaultProps } disabled={ true } /> );
+
+		const dropdown = await screen.findByRole( 'combobox' );
+
+		expect( dropdown ).toBeDisabled();
+	} );
+
+	test( 'If Audience section is in create mode the country field should be enable', async () => {
+		render( <AudienceSection { ...defaultProps } disabled={ false } /> );
+
+		const dropdown = await screen.findByRole( 'combobox' );
+
+		expect( dropdown ).not.toBeDisabled();
+	} );
+} );
