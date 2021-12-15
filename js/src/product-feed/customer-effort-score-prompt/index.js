83ed21cd21ac886acd76216ab023cd7b0cffb59b
Ian Yu-Hsun Lin <ianlin@automattic.com>
2021-12-15 11:28:15 +0800

    Send CES feedback using event tracking

--- js/src/product-feed/customer-effort-score-prompt/index.js
+++ js/src/product-feed/customer-effort-score-prompt/index.js
@@ -3,12 +3,15 @@
  */
 import { __ } from '@wordpress/i18n';
 import CustomerEffortScore from '@woocommerce/customer-effort-score';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import { LOCAL_STORAGE_KEYS } from '.~/constants';
 import localStorage from '.~/utils/localStorage';
+
+const EVENT_CONTEXT = 'gla-setup';
 
 export default function CustomerEffortScorePrompt() {
 	const removeCesPromptFlagFromLocal = () => {
@@ -18,21 +21,31 @@ export default function CustomerEffortScorePrompt() {
 	};
 
 	const onNoticeShown = () => {
-		// TODO: record event
+		recordEvent( 'gla_ces_snackbar_open', {
+			context: EVENT_CONTEXT,
+		} );
 	};
 
 	const onNoticeDismissed = () => {
-		// TODO: record event
 		removeCesPromptFlagFromLocal();
+		recordEvent( 'gla_ces_snackbar_closed', {
+			context: EVENT_CONTEXT,
+		} );
 	};
 
 	const onModalShown = () => {
-		// TODO: record event
 		removeCesPromptFlagFromLocal();
+		recordEvent( 'gla_ces_modal_open', {
+			context: EVENT_CONTEXT,
+		} );
 	};
 
-	const recordScore = () => {
-		// TODO: record event
+	const recordScore = ( score, comments ) => {
+		recordEvent( 'gla_ces_feedback', {
+			context: EVENT_CONTEXT,
+			score,
+			comments: comments || '',
+		} );
 	};
 
 	return (
