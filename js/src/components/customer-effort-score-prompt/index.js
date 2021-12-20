29325459b02a2a95b2d1032917790e22a9edbde2
Ian Yu-Hsun Lin <ianlin@automattic.com>
2021-12-20 11:34:45 +0800

    Add docs for event records of CES prompts

--- js/src/components/customer-effort-score-prompt/index.js
+++ js/src/components/customer-effort-score-prompt/index.js
@@ -13,12 +13,39 @@ import { LOCAL_STORAGE_KEYS } from '.~/constants';
 import localStorage from '.~/utils/localStorage';
 
 /**
+ * CES prompt snackbar open
+ *
+ * @event gla_ces_snackbar_open
+ */
+/**
+ * CES prompt snackbar closed
+ *
+ * @event gla_ces_snackbar_closed
+ */
+/**
+ * CES modal open
+ *
+ * @event gla_ces_modal_open
+ */
+/**
+ * CES feedback recorded
+ *
+ * @event gla_ces_feedback
+ */
+
+/**
  * A CustomerEffortScore wrapper that uses tracks to track the selected
  * customer effort score.
+ *
+ * @fires gla_ces_snackbar_open whenever the CES snackbar (notice) is open
+ * @fires gla_ces_snackbar_closed whenever the CES snackbar (notice) is closed
+ * @fires gla_ces_modal_open whenever the CES modal is open
+ * @fires gla_ces_feedback whenever the CES feedback is recorded
  *
  * @param {Object} props React component props.
  * @param {string} props.eventContext Context to be used in the CES wrapper events.
  * @param {string} props.label Text to be displayed in the CES notice and modal.
+ * @return {JSX.Element} Rendered element.
  */
 const CustomerEffortScorePrompt = ( { eventContext, label } ) => {
 	const removeCESPromptFlagFromLocal = () => {
