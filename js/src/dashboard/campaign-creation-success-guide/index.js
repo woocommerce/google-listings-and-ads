739e8977a7ef58aacca1d678f473033439596cd1
Ian Yu-Hsun Lin <ianlin@automattic.com>
2021-12-24 17:19:17 +0800

    Decouple CustomerEffortScorePrompt and the CampaignCreationSuccessGuide

--- js/src/dashboard/campaign-creation-success-guide/index.js
+++ js/src/dashboard/campaign-creation-success-guide/index.js
@@ -1,12 +1,10 @@
 /**
  * External dependencies
  */
-import { getHistory, getNewPath, getQuery } from '@woocommerce/navigation';
 import {
 	createInterpolateElement,
 	useEffect,
 	useCallback,
-	useState,
 } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
@@ -15,59 +13,41 @@ import { recordEvent } from '@woocommerce/tracks';
 /**
  * Internal dependencies
  */
-import { getCreateCampaignUrl } from '.~/utils/urls';
-import isWCTracksEnabled from '.~/utils/isWCTracksEnabled';
 import AppModal from '.~/components/app-modal';
 import GuidePageContent, {
 	ContentLink,
 } from '.~/components/guide-page-content';
-import CustomerEffortScorePrompt from '.~/components/customer-effort-score-prompt';
 import { GUIDE_NAMES } from '.~/constants';
 import headerImageURL from './header.svg';
 import './index.scss';
+import {
+	CTA_CREATE_ANOTHER_CAMPAIGN,
+	CTA_CONFIRM,
+	CTA_DISMISS,
+} from '../constants';
 
-const CTA_CREATE_ANOTHER_CAMPAIGN = 'create-another-campaign';
-
-const handleCloseWithAction = ( e, specifiedAction ) => {
-	const action = specifiedAction || e.currentTarget.dataset.action;
-	const nextQuery = {
-		...getQuery(),
-		guide: undefined,
-	};
-	getHistory().replace( getNewPath( nextQuery ) );
-
-	if ( action === CTA_CREATE_ANOTHER_CAMPAIGN ) {
-		getHistory().push( getCreateCampaignUrl() );
-	}
-
-	recordEvent( 'gla_modal_closed', {
-		context: GUIDE_NAMES.CAMPAIGN_CREATION_SUCCESS,
-		action,
-	} );
-};
-
-const handleRequestClose = ( e ) => handleCloseWithAction( e, 'dismiss' );
-
-const GuideImplementation = ( { isOpen, setCESPromptOpen } ) => {
-	const handleGotItClick = useCallback(
-		( e ) => {
-			handleCloseWithAction( e );
-			setCESPromptOpen( true );
-		},
-		[ setCESPromptOpen ]
-	);
-
+/**
+ * Modal window to prompt the user at Dashboard, after successful completing the campaign creation.
+ *
+ * Show this guide modal by visiting the path with a specific query `guide=campaign-creation-success`.
+ * For example: `/wp-admin/admin.php?page=wc-admin&path=%2Fgoogle%2Fdashboard&guide=campaign-creation-success`.
+ *
+ * @param {Object} props React component props.
+ * @param {Function} props.onGuideRequestClose The function to be called when the guide is closed.
+ */
+export default function CampaignCreationSuccessGuide( {
+	onGuideRequestClose = () => {},
+} ) {
 	useEffect( () => {
-		if ( isOpen ) {
-			recordEvent( 'gla_modal_open', {
-				context: GUIDE_NAMES.CAMPAIGN_CREATION_SUCCESS,
-			} );
-		}
-	}, [ isOpen ] );
+		recordEvent( 'gla_modal_open', {
+			context: GUIDE_NAMES.CAMPAIGN_CREATION_SUCCESS,
+		} );
+	}, [] );
 
-	if ( ! isOpen ) {
-		return null;
-	}
+	const handleRequestClose = useCallback(
+		( e ) => onGuideRequestClose( e, CTA_DISMISS ),
+		[ onGuideRequestClose ]
+	);
 
 	return (
 		<AppModal
@@ -78,7 +58,7 @@ const GuideImplementation = ( { isOpen, setCESPromptOpen } ) => {
 					key="0"
 					isTertiary
 					data-action={ CTA_CREATE_ANOTHER_CAMPAIGN }
-					onClick={ handleCloseWithAction }
+					onClick={ onGuideRequestClose }
 				>
 					{ __(
 						'Create another campaign',
@@ -88,8 +68,8 @@ const GuideImplementation = ( { isOpen, setCESPromptOpen } ) => {
 				<Button
 					key="1"
 					isPrimary
-					data-action="confirm"
-					onClick={ handleGotItClick }
+					data-action={ CTA_CONFIRM }
+					onClick={ onGuideRequestClose }
 				>
 					{ __( 'Got it', 'google-listings-and-ads' ) }
 				</Button>,
@@ -128,36 +108,5 @@ const GuideImplementation = ( { isOpen, setCESPromptOpen } ) => {
 				) }
 			</GuidePageContent>
 		</AppModal>
-	);
-};
-
-/**
- * Modal window to prompt the user at Dashboard, after successful completing the campaign creation.
- *
- * Show this guide modal by visiting the path with a specific query `guide=campaign-creation-success`.
- * For example: `/wp-admin/admin.php?page=wc-admin&path=%2Fgoogle%2Fdashboard&guide=campaign-creation-success`.
- */
-export default function CampaignCreationSuccessGuide() {
-	const [ isCESPromptOpen, setCESPromptOpen ] = useState( false );
-
-	const isOpen = getQuery()?.guide === GUIDE_NAMES.CAMPAIGN_CREATION_SUCCESS;
-	const wcTracksEnabled = isWCTracksEnabled();
-
-	return (
-		<>
-			<GuideImplementation
-				isOpen={ isOpen }
-				setCESPromptOpen={ setCESPromptOpen }
-			/>
-			{ isCESPromptOpen && wcTracksEnabled && (
-				<CustomerEffortScorePrompt
-					label={ __(
-						'How easy was it to create a Google Ad campaign?',
-						'google-listings-and-ads'
-					) }
-					eventContext={ GUIDE_NAMES.CAMPAIGN_CREATION_SUCCESS }
-				/>
-			) }
-		</>
 	);
 }
