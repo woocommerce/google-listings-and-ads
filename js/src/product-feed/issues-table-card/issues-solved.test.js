6632d407085f2141ecf62a1c345630303ab7364b
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-01-14 19:41:42 +0100

    Refactor tests

--- js/src/product-feed/issues-table-card/issues-solved.test.js
+++ js/src/product-feed/issues-table-card/issues-solved.test.js
@@ -9,38 +9,32 @@ import { render } from '@testing-library/react';
 /**
  * Internal dependencies
  */
-import { ISSUE_TYPE_ACCOUNT, ISSUE_TYPE_PRODUCT } from '.~/constants';
 import useMCIssuesTypeFilter from '.~/hooks/useMCIssuesTypeFilter';
 import useActiveIssueType from '.~/hooks/useActiveIssueType';
 import IssuesSolved from '.~/product-feed/issues-table-card/issues-solved';
 
 describe( 'Issues Solved Message', () => {
-	describe( 'Renders when no issues', () => {
-		beforeEach( () => {
-			useMCIssuesTypeFilter.mockReturnValue( {
-				data: {
-					issues: [],
-					total: 0,
-				},
-				page: 1,
-				hasFinishedResolution: true,
-			} );
-		} );
-
-		it( 'When Issue Type Product', () => {
-			useActiveIssueType.mockReturnValue( ISSUE_TYPE_PRODUCT );
-
-			const { queryByText } = render( <IssuesSolved /> );
-
-			expect( queryByText( 'All product issues resolved' ) ).toBeTruthy();
-		} );
-
-		it( 'When Issue Type Account', () => {
-			useActiveIssueType.mockReturnValue( ISSUE_TYPE_ACCOUNT );
-
-			const { queryByText } = render( <IssuesSolved /> );
-
-			expect( queryByText( 'All account issues resolved' ) ).toBeTruthy();
+	beforeEach( () => {
+		useMCIssuesTypeFilter.mockReturnValue( {
+			data: {
+				issues: [],
+				total: 0,
+			},
+			page: 1,
+			hasFinishedResolution: true,
 		} );
 	} );
+
+	it.each( [ 'account', 'product' ] )(
+		'It renders when there are not issues of type %s',
+		( issueType ) => {
+			useActiveIssueType.mockReturnValue( issueType );
+
+			const { queryByText } = render( <IssuesSolved /> );
+
+			expect(
+				queryByText( `All ${ issueType } issues resolved` )
+			).toBeTruthy();
+		}
+	);
 } );
