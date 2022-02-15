f7ad55d2968f86b6979fe5d8164ffbed95210a0f
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-02-16 02:15:04 +0800

    Move useShippingRatesWithSavedSuggestions files, to be used in SetupFreeListings.

--- js/src/setup-mc/setup-stepper/setup-free-listings/shipping-rate/shipping-rate-setup/index.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/shipping-rate/shipping-rate-setup/index.js
@@ -5,7 +5,7 @@ import VerticalGapLayout from '.~/components/vertical-gap-layout';
 import AddRateButton from './add-rate-button';
 import CountriesPriceInputForm from './countries-price-input-form';
 import useStoreCurrency from '.~/hooks/useStoreCurrency';
-import useShippingRatesWithSavedSuggestions from './useShippingRatesWithSavedSuggestions';
+import useShippingRatesWithSavedSuggestions from '../../useShippingRatesWithSavedSuggestions';
 import groupShippingRatesByPriceCurrency from '.~/utils/groupShippingRatesByPriceCurrency';
 import AppSpinner from '.~/components/app-spinner';
 import useTargetAudienceFinalCountryCodes from '.~/hooks/useTargetAudienceFinalCountryCodes';
