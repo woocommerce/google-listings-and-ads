99df7f823c98d3e7c2578b15aed89252f691e6e4
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-02-17 00:24:52 +0800

    Use useDebouncedCallbackEffect in useAutoSaveShippingRatesEffect.

--- js/src/setup-mc/setup-stepper/setup-free-listings/useAutoSaveShippingRatesEffect.js
+++ js/src/setup-mc/setup-stepper/setup-free-listings/useAutoSaveShippingRatesEffect.js
@@ -1,21 +1,15 @@
 /**
- * External dependencies
- */
-import { useEffect } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
 import { useAppDispatch } from '.~/data';
 import useIsEqualRefValue from '.~/hooks/useIsEqualRefValue';
+import useDebouncedCallbackEffect from '.~/hooks/useDebouncedCallbackEffect';
 
 const useAutoSaveShippingRatesEffect = ( shippingRates ) => {
 	const { saveShippingRates } = useAppDispatch();
 	const shippingRatesRefValue = useIsEqualRefValue( shippingRates );
 
-	useEffect( () => {
-		saveShippingRates( shippingRatesRefValue );
-	}, [ saveShippingRates, shippingRatesRefValue ] );
+	useDebouncedCallbackEffect( shippingRatesRefValue, saveShippingRates );
 };
 
 export default useAutoSaveShippingRatesEffect;
