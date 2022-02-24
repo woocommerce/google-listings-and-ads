54163c575d847e512b302145f682b57e2395a43b
mikkamp <michael.kamp@automattic.com>
2022-02-24 15:36:09 +0000

    Remove creation operations

--- src/API/Google/AdsGroup.php
+++ src/API/Google/AdsGroup.php
@@ -29,9 +29,9 @@ use Google\Ads\GoogleAds\V9\Services\MutateOperation;
 /**
  * Class AdsGroup
  *
- * @since x.x.x Refactored to use batch requests when operating on campaigns.
+ * @since x.x.x Refactored to support deleting of a legacy SSC Ad Group.
  *
- * Used for Smart Shopping Campaigns (SSC)
+ * Used for legacy Smart Shopping Campaigns (SSC)
  * https://developers.google.com/google-ads/api/docs/samples/add-shopping-smart-ad
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Google
@@ -39,14 +39,6 @@ use Google\Ads\GoogleAds\V9\Services\MutateOperation;
 class AdsGroup implements OptionsAwareInterface {
 
 	use OptionsAwareTrait;
-
-	/**
-	 * Temporary ID to use within a batch job.
-	 * A negative number which is unique for all the created resources.
-	 *
-	 * @var int
-	 */
-	protected const TEMPORARY_ID = -3;
 
 	/**
 	 * The Google Ads Client.
@@ -72,22 +64,6 @@ class AdsGroup implements OptionsAwareInterface {
 	}
 
 	/**
-	 * Returns a set of operations to create an ad group.
-	 *
-	 * @param string $campaign_resource_name
-	 * @param string $campaign_name
-	 * @return array
-	 */
-	public function create_operations( string $campaign_resource_name, string $campaign_name ): array {
-		// Ad group must be created before listing group.
-		return [
-			$this->ad_group_create_operation( $campaign_resource_name, $campaign_name ),
-			$this->ad_group_ad_create_operation(),
-			$this->listing_group_create_operation(),
-		];
-	}
-
-	/**
 	 * Returns a set of operations to delete an ad group.
 	 *
 	 * @param string $campaign_resource_name
@@ -104,64 +80,6 @@ class AdsGroup implements OptionsAwareInterface {
 			$ad_group_ad_operations,
 			$ad_group_operations
 		);
-	}
-
-	/**
-	 * Returns an ad group create operation.
-	 *
-	 * @param string $campaign_resource_name
-	 * @param string $campaign_name
-	 *
-	 * @return MutateOperation
-	 */
-	protected function ad_group_create_operation( string $campaign_resource_name, string $campaign_name ): MutateOperation {
-		$ad_group = new AdGroup(
-			[
-				'resource_name' => $this->temporary_resource_name(),
-				'name'          => $campaign_name . ' Ad Group',
-				'campaign'      => $campaign_resource_name,
-				'type'          => AdGroupType::SHOPPING_SMART_ADS,
-				'status'        => AdGroupStatus::ENABLED,
-			]
-		);
-
-		$operation = ( new AdGroupOperation() )->setCreate( $ad_group );
-		return ( new MutateOperation() )->setAdGroupOperation( $operation );
-	}
-
-	/**
-	 * Returns an ad group ad create operation.
-	 *
-	 * @return MutateOperation
-	 */
-	protected function ad_group_ad_create_operation(): MutateOperation {
-		$ad_group_ad = new AdGroupAd(
-			[
-				'ad'       => new Ad( [ 'shopping_smart_ad' => new ShoppingSmartAdInfo() ] ),
-				'ad_group' => $this->temporary_resource_name(),
-			]
-		);
-
-		$operation = ( new AdGroupAdOperation() )->setCreate( $ad_group_ad );
-		return ( new MutateOperation() )->setAdGroupAdOperation( $operation );
-	}
-
-	/**
-	 * Returns an ad group criterion create operation.
-	 *
-	 * @return MutateOperation
-	 */
-	protected function listing_group_create_operation(): MutateOperation {
-		$ad_group_criterion = new AdGroupCriterion(
-			[
-				'ad_group'      => $this->temporary_resource_name(),
-				'status'        => AdGroupAdStatus::ENABLED,
-				'listing_group' => new ListingGroupInfo( [ 'type' => ListingGroupType::UNIT ] ),
-			]
-		);
-
-		$operation = ( new AdGroupCriterionOperation() )->setCreate( $ad_group_criterion );
-		return ( new MutateOperation() )->setAdGroupCriterionOperation( $operation );
 	}
 
 	/**
@@ -241,14 +159,5 @@ class AdsGroup implements OptionsAwareInterface {
 		}
 
 		return $operations;
-	}
-
-	/**
-	 * Return a temporary resource name for the ad group.
-	 *
-	 * @return string
-	 */
-	protected function temporary_resource_name() {
-		return ResourceNames::forAdGroup( $this->options->get_ads_id(), self::TEMPORARY_ID );
 	}
 }
