2b07743f4242b90da99b263ce67c970218b2fd51
Eason Su <eason.su.tw@gmail.com>
2022-02-24 14:38:12 +0800

    Fix that the `<input>` is still being triggered focused style when clicking on its `<label>`.
    Address https://github.com/woocommerce/google-listings-and-ads/pull/1268#discussion_r813123513

--- js/src/components/app-input-control/index.js
+++ js/src/components/app-input-control/index.js
@@ -10,11 +10,26 @@ import { __experimentalInputControl as InputControl } from '@wordpress/component
  */
 import './index.scss';
 
+const baseClassName = 'app-input-control';
+
+/**
+ * Renders <InputControl> with a wrapper to be applicable extend additional class names.
+ * noPointerEvents
+ *
+ * @param {Object} props React props to be forwarded to {@link InputControl}.
+ * @param {string} [props.className] Additional CSS class name to be appended.
+ * @param {boolean} [props.noPointerEvents] Whether disabled all pointer events. It will attach `pointer-events: none;` onto wrapper if true.
+ */
 const AppInputControl = forwardRef( ( props, ref ) => {
-	const { className, ...rest } = props;
+	const { className, noPointerEvents, ...rest } = props;
+	const wrapperClassName = classnames(
+		baseClassName,
+		noPointerEvents && `${ baseClassName }--no-pointer-events`,
+		className
+	);
 
 	return (
-		<div className={ classnames( 'app-input-control', className ) }>
+		<div className={ wrapperClassName }>
 			<InputControl ref={ ref } { ...rest } />
 		</div>
 	);
--- js/src/components/paid-ads/budget-section/index.js
+++ js/src/components/paid-ads/budget-section/index.js
@@ -13,6 +13,12 @@ import BudgetRecommendation from './budget-recommendation';
 import useGoogleAdsAccount from '.~/hooks/useGoogleAdsAccount';
 import AppInputPriceControl from '.~/components/app-input-price-control';
 
+const nonInteractableProps = {
+	noPointerEvents: true,
+	readOnly: true,
+	tabIndex: -1,
+};
+
 /**
  * Renders <Section> and <Section.Card> UI with campaign budget inputs.
  *
@@ -77,7 +83,7 @@ const BudgetSection = ( props ) => {
 								suffix={ currency }
 								{ ...getInputProps( 'amount' ) }
 								value={ amountValue }
-								readOnly={ disabled }
+								{ ...( disabled && nonInteractableProps ) }
 							/>
 							<AppInputPriceControl
 								disabled
