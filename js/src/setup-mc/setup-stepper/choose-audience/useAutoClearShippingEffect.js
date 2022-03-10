9d4bfafb9121034a92b1004353fafe1f60b2779f
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-03-11 03:23:31 +0800

    Add error handling in useAutoClearShippingEffect.
    This is because saveShippingRates does not have error handling in it by design.

--- js/src/setup-mc/setup-stepper/choose-audience/useAutoClearShippingEffect.js
+++ js/src/setup-mc/setup-stepper/choose-audience/useAutoClearShippingEffect.js
@@ -3,6 +3,7 @@
  */
 import { useEffect, useRef } from '@wordpress/element';
 import { useDebouncedCallback } from 'use-debounce';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -10,6 +11,7 @@ import { useDebouncedCallback } from 'use-debounce';
 import { useAppDispatch } from '.~/data';
 import useShippingTimes from '.~/hooks/useShippingTimes';
 import useSaveShippingRates from '.~/hooks/useSaveShippingRates';
+import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
 
 const wait = 500;
 
@@ -17,13 +19,26 @@ const useAutoClearShippingEffect = ( location, countries ) => {
 	const { data: shippingTimes } = useShippingTimes();
 	const { saveShippingRates } = useSaveShippingRates();
 	const { deleteShippingTimes } = useAppDispatch();
+	const { createNotice } = useDispatchCoreNotices();
 
 	const debouncedDelete = useDebouncedCallback( async () => {
-		saveShippingRates( [] );
+		try {
+			saveShippingRates( [] );
 
-		if ( shippingTimes.length ) {
-			const countryCodes = shippingTimes.map( ( el ) => el.countryCode );
-			deleteShippingTimes( countryCodes );
+			if ( shippingTimes.length ) {
+				const countryCodes = shippingTimes.map(
+					( el ) => el.countryCode
+				);
+				deleteShippingTimes( countryCodes );
+			}
+		} catch ( error ) {
+			createNotice(
+				'error',
+				__(
+					'Something went wrong while trying to clear your shipping data. Please try again later.',
+					'google-listings-and-ads'
+				)
+			);
 		}
 	}, wait );
 
