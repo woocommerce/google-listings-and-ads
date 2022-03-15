2936d59599c4f2b13d917843cd8033ae9db36dd7
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-03-15 16:43:23 +0400

    Update tags JSDoc

--- js/src/components/tree-select-control/tags.js
+++ js/src/components/tree-select-control/tags.js
@@ -10,10 +10,10 @@ import { Button } from '@wordpress/components';
  * A list of tags to display selected items.
  *
  * @param {Object} props The component props
- * @param {Object[]} props.tags The tags
+ * @param {Object[]} [props.tags=[]] The tags
  * @param {Function} props.onChange The method called when a tag is removed
  * @param {boolean} props.disabled True if the plugin is disabled
- * @param {number} props.maxVisibleTags The maximum number of tags to show. 0 or less than 0 evaluates to "Show All".
+ * @param {number} [props.maxVisibleTags=0] The maximum number of tags to show. 0 or less than 0 evaluates to "Show All".
  */
 const Tags = ( {
 	tags = [],
