f6db468086294023e6391f66aaef006781166914
Nima <nima.karimi@automattic.com>
2022-03-18 14:08:42 +0300

    Add LocationRate class
    This class represents a shipping rate belonging to a specific location. It basically maps a ShippingRate object to a Location object.

---
+++ src/Shipping/LocationRate.php
@@ -0,0 +1,89 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\Shipping;
+
+use JsonSerializable;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class LocationRate
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\Shipping
+ *
+ * @since x.x.x
+ */
+class LocationRate implements JsonSerializable {
+
+	/**
+	 * @var Location
+	 */
+	protected $location;
+
+	/**
+	 * @var ShippingRate
+	 */
+	protected $shipping_rate;
+
+	/**
+	 * LocationRate constructor.
+	 *
+	 * @param Location     $location
+	 * @param ShippingRate $shipping_rate
+	 */
+	public function __construct( Location $location, ShippingRate $shipping_rate ) {
+		$this->location      = $location;
+		$this->shipping_rate = $shipping_rate;
+	}
+
+	/**
+	 * @return Location
+	 */
+	public function get_location(): Location {
+		return $this->location;
+	}
+
+	/**
+	 * @param Location $location
+	 *
+	 * @return LocationRate
+	 */
+	public function set_location( Location $location ): LocationRate {
+		$this->location = $location;
+
+		return $this;
+	}
+
+	/**
+	 * @return ShippingRate
+	 */
+	public function get_shipping_rate(): ShippingRate {
+		return $this->shipping_rate;
+	}
+
+	/**
+	 * @param ShippingRate $shipping_rate
+	 *
+	 * @return LocationRate
+	 */
+	public function set_shipping_rate( ShippingRate $shipping_rate ): LocationRate {
+		$this->shipping_rate = $shipping_rate;
+
+		return $this;
+	}
+
+	/**
+	 * Specify data which should be serialized to JSON
+	 */
+	public function jsonSerialize() {
+		$rate_serialized = $this->shipping_rate->jsonSerialize();
+
+		return array_merge(
+			$rate_serialized,
+			[
+				'country' => $this->location->get_country(),
+			]
+		);
+	}
+}
