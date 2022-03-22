539ed3d954ec042dec1ef6f5e4975510cb123957
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-03-22 11:58:22 +0100

    Remove unused SPACE constant

--- js/src/components/tree-select-control/options.js
+++ js/src/components/tree-select-control/options.js
@@ -8,7 +8,7 @@ import classnames from 'classnames';
 /**
  * Internal dependencies
  */
-import { ARROW_LEFT, ARROW_RIGHT, ROOT_VALUE, SPACE } from './constants';
+import { ARROW_LEFT, ARROW_RIGHT, ROOT_VALUE } from './constants';
 import Checkbox from '.~/components/tree-select-control/checkbox';
 
 /**
