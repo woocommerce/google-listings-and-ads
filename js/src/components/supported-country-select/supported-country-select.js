5c4f005bb881cd05b3182a7cdae9baec4da51228
Eason Su <eason.su.tw@gmail.com>
2022-03-25 18:19:05 +0800

    Change SupportedCountrySelect to use TreeSelectControl

--- js/src/components/supported-country-select/supported-country-select.js
+++ js/src/components/supported-country-select/supported-country-select.js
@@ -1,15 +1,46 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useViewportMatch } from '@wordpress/compose';
+
+/**
  * Internal dependencies
  */
-import AppCountrySelect from '.~/components/app-country-select';
-import useMCCountries from '.~/hooks/useMCCountries';
+import TreeSelectControl from '.~/components/tree-select-control';
+import useMCCountryTreeOptions from './useMCCountryTreeOptions';
 
-const SupportedCountrySelect = ( props ) => {
-	const { data } = useMCCountries();
+/**
+ * @typedef {import('.~/data/actions').CountryCode} CountryCode
+ */
 
-	const options = data ? Object.keys( data ) : [];
+/**
+ * Returns a TreeSelectControl component with list of countries grouped by continents.
+ *
+ * This component is for selecting countries under Google Merchant Center supported countries.
+ * And the selectable countries can be further limited by the `countryCodes` prop.
+ *
+ * @param {Object} props React props.
+ * @param {Array<CountryCode>} [props.countryCodes] Country codes for converting to tree-based option structure. It will use all MC supported countries if not specified.
+ * @param {Object} props.restProps Props to be forwarded to TreeSelectControl.
+ */
+export default function SupportedCountrySelect( {
+	countryCodes,
+	...restProps
+} ) {
+	const maxVisibleTags = useViewportMatch( 'medium', '<' ) ? 5 : 10;
+	const treeOptions = useMCCountryTreeOptions( countryCodes );
 
-	return <AppCountrySelect options={ options } { ...props } />;
-};
-
-export default SupportedCountrySelect;
+	return (
+		<TreeSelectControl
+			placeholder={ __(
+				'Start typing to filter countries…',
+				'google-listings-and-ads'
+			) }
+			selectAllLabel={ __( 'All countries', 'google-listings-and-ads' ) }
+			maxVisibleTags={ maxVisibleTags }
+			options={ treeOptions }
+			{ ...restProps }
+		/>
+	);
+}
