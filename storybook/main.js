22f1adb2123b8466bca49e24152c4e4230643ad8
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-03-30 10:54:24 +0200

    ESLint

--- storybook/main.js
+++ storybook/main.js
@@ -1,15 +1,16 @@
 /**
  * External dependencies
  **/
-const path = require('path');
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require( 'path' );
+// eslint-disable-next-line import/no-extraneous-dependencies
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 
 const cssLoaders = [
 	{
 		loader: MiniCSSExtractPlugin.loader,
 	},
 	{
-		loader: require.resolve('css-loader'),
+		loader: require.resolve( 'css-loader' ),
 		options: {
 			sourceMap: process.env.NODE_ENV !== 'production',
 			modules: {
@@ -20,54 +21,57 @@ const cssLoaders = [
 ];
 
 module.exports = {
-	"stories": [
+	stories: [
 		// any file inside a stories folder in the JS dir
-		"../js/src/**/stories/*.@(js|jsx|ts|tsx)",
+		'../js/src/**/stories/*.@(js|jsx|ts|tsx)',
 	],
-	"addons": [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"@storybook/addon-docs"
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-docs',
 	],
 
-	webpackFinal: async (config, {configType}) => {
-		config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+	webpackFinal: async ( config, {} ) => {
+		config.resolve.modules = [
+			path.resolve( __dirname, '..' ),
+			'node_modules',
+		];
 		config.resolve.alias = {
 			...config.resolve.alias,
-			".~": path.resolve(__dirname, "../js/src")
+			'.~': path.resolve( __dirname, '../js/src' ),
 		};
 
 		config.module.rules = [
-			...config.module.rules, ...[
+			...config.module.rules,
+			...[
 				{
 					test: /\.(sc|sa)ss$/,
 					use: [
 						...cssLoaders,
 						{
-							loader: require.resolve('sass-loader'),
+							loader: require.resolve( 'sass-loader' ),
 							options: {
-								sourceMap: process.env.NODE_ENV === 'production',
+								sourceMap:
+									process.env.NODE_ENV === 'production',
 								additionalData:
 									'@import "@wordpress/base-styles/_colors"; ' +
 									'@import "@wordpress/base-styles/_variables"; ' +
 									'@import "@wordpress/base-styles/_mixins"; ' +
 									'@import "@wordpress/base-styles/_breakpoints"; ',
-							}
+							},
 						},
 					],
-
-				}
-			]
+				},
+			],
 		];
 
 		config.plugins.push(
-			new MiniCSSExtractPlugin({
+			new MiniCSSExtractPlugin( {
 				filename: `[name].css`,
-			})
+			} )
 		);
 
-		return config
-
-	}
-}
+		return config;
+	},
+};
