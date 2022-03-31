8a4607f7e18d9ae4c30501f4a69e220ea90f41e5
Eason Su <eason.su.tw@gmail.com>
2022-03-31 15:36:56 +0800

    Make ConnectWPComAccountCard call `jetpack/connect` API with the `next` parameter.

--- js/src/components/wpcom-account-card/connect-wpcom-account-card.js
+++ js/src/components/wpcom-account-card/connect-wpcom-account-card.js
@@ -2,10 +2,13 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies
  */
+import { glaData } from '.~/constants';
+import { API_NAMESPACE } from '.~/data/constants';
 import AppButton from '.~/components/app-button';
 import AccountCard, { APPEARANCE } from '.~/components/account-card';
 import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
@@ -13,8 +16,11 @@ import useApiFetchCallback from '.~/hooks/useApiFetchCallback';
 
 const ConnectWPComAccountCard = () => {
 	const { createNotice } = useDispatchCoreNotices();
+
+	const next = glaData.mcSetupComplete ? 'reconnect' : 'setup-mc';
+	const path = addQueryArgs( `${ API_NAMESPACE }/jetpack/connect`, { next } );
 	const [ fetchJetpackConnect, { loading, data } ] = useApiFetchCallback( {
-		path: '/wc/gla/jetpack/connect',
+		path,
 	} );
 
 	const handleConnectClick = async () => {
@@ -44,6 +50,7 @@ const ConnectWPComAccountCard = () => {
 					isSecondary
 					loading={ loading || data }
 					eventName="gla_wordpress_account_connect_button_click"
+					eventProps={ { context: next } }
 					onClick={ handleConnectClick }
 				>
 					{ __( 'Connect', 'google-listings-and-ads' ) }
