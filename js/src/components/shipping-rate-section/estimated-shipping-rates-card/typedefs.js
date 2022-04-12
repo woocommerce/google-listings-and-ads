104a9310b8a9f5699558b3db8aa0efa0636a9906
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-04-13 01:45:39 +0800

    Add ShippingRateGroup typedef.

---
+++ js/src/components/shipping-rate-section/estimated-shipping-rates-card/typedefs.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} ShippingRateGroup
+ * @property {Array<string>} countries Array of selected country codes.
+ * @property {string} currency Selected currency.
+ * @property {number} rate Rate value.
+ */
+
+export default {};
