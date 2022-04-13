1ec1bc0ad91dd6ea296d0806a26b19690353d0ac
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-04-13 17:27:02 +0800

    Add "method" property to ShippingRateGroup typedef.

--- js/src/components/shipping-rate-section/estimated-shipping-rates-card/typedefs.js
+++ js/src/components/shipping-rate-section/estimated-shipping-rates-card/typedefs.js
@@ -5,7 +5,8 @@
 /**
  * @typedef {Object} ShippingRateGroup
  * @property {Array<CountryCode>} countries Array of selected country codes.
- * @property {string} currency Selected currency.
+ * @property {string} method Shipping method, e.g. "flat_rate".
+ * @property {string} currency Currency.
  * @property {number} rate Rate value.
  */
 
