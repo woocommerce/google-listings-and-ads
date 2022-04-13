bb051983002fec5bb10eeca34c130bc85466acf4
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-04-13 17:14:37 +0800

    Use CountryCode typedef.

--- js/src/components/shipping-rate-section/estimated-shipping-rates-card/typedefs.js
+++ js/src/components/shipping-rate-section/estimated-shipping-rates-card/typedefs.js
@@ -1,6 +1,10 @@
 /**
+ * @typedef { import(".~/data/actions").CountryCode } CountryCode
+ */
+
+/**
  * @typedef {Object} ShippingRateGroup
- * @property {Array<string>} countries Array of selected country codes.
+ * @property {Array<CountryCode>} countries Array of selected country codes.
  * @property {string} currency Selected currency.
  * @property {number} rate Rate value.
  */
