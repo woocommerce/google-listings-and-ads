3327e54002f7762fbb5d2a0f1f1b0444f6ed90ad
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-04-18 12:38:51 +0400

    Fix PHPCS

--- src/Options/OptionsInterface.php
+++ src/Options/OptionsInterface.php
@@ -13,14 +13,14 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Value\PositiveInteger;
 interface OptionsInterface {
 
 
-	public const ADS_ACCOUNT_CURRENCY      = 'ads_account_currency';
-	public const ADS_ACCOUNT_STATE         = 'ads_account_state';
-	public const ADS_BILLING_URL           = 'ads_billing_url';
-	public const ADS_ID                    = 'ads_id';
-	public const ADS_CONVERSION_ACTION     = 'ads_conversion_action';
-	public const ADS_SETUP_COMPLETED_AT    = 'ads_setup_completed_at';
-	public const CONTACT_INFO_SETUP        = 'contact_info_setup';
-	public const DELAYED_ACTIVATE          = 'delayed_activate';
+	public const ADS_ACCOUNT_CURRENCY   = 'ads_account_currency';
+	public const ADS_ACCOUNT_STATE      = 'ads_account_state';
+	public const ADS_BILLING_URL        = 'ads_billing_url';
+	public const ADS_ID                 = 'ads_id';
+	public const ADS_CONVERSION_ACTION  = 'ads_conversion_action';
+	public const ADS_SETUP_COMPLETED_AT = 'ads_setup_completed_at';
+	public const CONTACT_INFO_SETUP     = 'contact_info_setup';
+	public const DELAYED_ACTIVATE       = 'delayed_activate';
 	public const DB_VERSION             = 'db_version';
 	public const FILE_VERSION           = 'file_version';
 	public const GOOGLE_CONNECTED       = 'google_connected';
@@ -54,13 +54,13 @@ interface OptionsInterface {
 		self::MERCHANT_ACCOUNT_STATE => true,
 		self::MERCHANT_CENTER        => true,
 		self::MERCHANT_ID            => true,
-		self::DELAYED_ACTIVATE          => true,
-		self::SHIPPING_RATES            => true,
-		self::SHIPPING_TIMES            => true,
-		self::REDIRECT_TO_ONBOARDING    => true,
-		self::SITE_VERIFICATION         => true,
-		self::TARGET_AUDIENCE           => true,
-		self::WP_TOS_ACCEPTED           => true,
+		self::DELAYED_ACTIVATE       => true,
+		self::SHIPPING_RATES         => true,
+		self::SHIPPING_TIMES         => true,
+		self::REDIRECT_TO_ONBOARDING => true,
+		self::SITE_VERIFICATION      => true,
+		self::TARGET_AUDIENCE        => true,
+		self::WP_TOS_ACCEPTED        => true,
 	];
 
 	public const OPTION_TYPES = [
