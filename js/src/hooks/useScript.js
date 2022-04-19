c426ee569882d2bb0811e08af012ada94b56822f
Ian Yu-Hsun Lin <ianlin@automattic.com>
2022-04-19 15:16:07 +0800

    Use lodash's noop as the default value of the callback param
    Also update JSDoc for the optional callback parameter.

--- js/src/hooks/useScript.js
+++ js/src/hooks/useScript.js
@@ -1,15 +1,16 @@
 /**
  * External dependencies
  */
+import { noop } from 'lodash';
 import { useEffect } from '@wordpress/element';
 
 /**
  * Load an external script and call the provided callback function when the script is loaded.
  *
- * @param {string}   url      The script's url.
- * @param {Function} callback A function to be called after the script is loaded.
+ * @param {string}   url             The script's url.
+ * @param {Function} [callback=noop] A function to be called after the script is loaded.
  */
-const useScript = ( url, callback = () => {} ) => {
+const useScript = ( url, callback = noop ) => {
 	useEffect( () => {
 		const script = document.createElement( 'script' );
 
