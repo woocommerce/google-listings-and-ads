6eb1e68e9eb5bcfacc2539f31c6b03fe956ca583
Ian Yu-Hsun Lin <ianlin@automattic.com>
2022-04-19 18:20:03 +0800

    Use CardHeader instead of a header class in get started video card

--- js/src/get-started-page/get-started-with-video-card/index.js
+++ js/src/get-started-page/get-started-with-video-card/index.js
@@ -3,7 +3,7 @@
  */
 import {
 	Card,
-	Flex,
+	CardHeader,
 	FlexBlock,
 	Tip,
 	__experimentalText as Text,
@@ -30,96 +30,89 @@ const GetStartedWithVideoCard = () => {
 
 	return (
 		<Card className="gla-get-started-with-video-card" isBorderless>
-			<Flex>
-				<FlexBlock className="header">
-					<Text variant="body" className="title">
-						{ __(
-							'The official extension for WooCommerce, built in collaboration with',
-							'google-listings-and-ads'
-						) }
-					</Text>
-					<div id="google-img">
-						<img
-							src={ googleLogoURL }
-							alt={ __(
-								'Google Logo',
-								'google-listings-and-ads'
-							) }
-							width="71"
-							height="24"
-						/>
-					</div>
-				</FlexBlock>
-				<FlexBlock className="motivation-video">
-					<WistiaVideo
-						id="lpvgtsjwrg"
-						src="https://fast.wistia.net/embed/iframe/lpvgtsjwrg?seo=false&videoFoam=true"
-						title="WooCommerce-Google-Listings-Ads"
-					/>
-				</FlexBlock>
-				<FlexBlock className="motivation-text">
-					<Text variant="title.medium" className="title">
-						{ __(
-							'Reach new customers across Google with free product listings and paid ads',
-							'google-listings-and-ads'
-						) }
-					</Text>
-					<Text variant="body" className="description">
-						{ __(
-							'Sync your products directly to Google, manage your product feed, and create Google Ad campaigns–without leaving your WooCommerce dashboard. The official extension, built in collaboration with Google.',
-							'google-listings-and-ads'
-						) }
-					</Text>
-					<AppButton
-						className="button"
-						isPrimary
-						disabled={ disableNextStep }
-						href={ getSetupMCUrl() }
-						eventName="gla_setup_mc"
-						eventProps={ {
-							target: 'set_up_free_listings',
-							trigger: 'click',
-						} }
-					>
-						{ __(
-							'Start listing products →',
-							'google-listings-and-ads'
-						) }
-					</AppButton>
-					<Text className="hint">
-						{ __(
-							'Estimated setup time: 15 min',
-							'google-listings-and-ads'
-						) }
-					</Text>
-					<Text
-						className="gla-get-started-with-video-card__terms-notice"
-						variant="body"
-					>
-						{ createInterpolateElement(
-							__(
-								'By clicking ‘Start listing products’, you agree to our <link>Terms of Service.</link>',
-								'google-listings-and-ads'
-							),
-							{
-								link: (
-									<AppDocumentationLink
-										context="get-started"
-										linkId="wp-terms-of-service"
-										href="https://wordpress.com/tos/"
-									/>
-								),
-							}
-						) }
-					</Text>
-				</FlexBlock>
-				<Tip>
+			<CardHeader>
+				<p>
 					{ __(
-						'If you’re already using another extension to manage your product feed with Google, make sure to deactivate or uninstall it first to prevent duplicate product feeds.',
+						'The official extension for WooCommerce, built in collaboration with',
 						'google-listings-and-ads'
 					) }
-				</Tip>
-			</Flex>
+				</p>
+				<img
+					src={ googleLogoURL }
+					alt={ __( 'Google Logo', 'google-listings-and-ads' ) }
+					width="71"
+					height="24"
+				/>
+			</CardHeader>
+			<FlexBlock className="motivation-video">
+				<WistiaVideo
+					id="lpvgtsjwrg"
+					src="https://fast.wistia.net/embed/iframe/lpvgtsjwrg?seo=false&videoFoam=true"
+					title="WooCommerce-Google-Listings-Ads"
+				/>
+			</FlexBlock>
+			<FlexBlock className="motivation-text">
+				<Text variant="title.medium" className="title">
+					{ __(
+						'Reach new customers across Google with free product listings and paid ads',
+						'google-listings-and-ads'
+					) }
+				</Text>
+				<Text variant="body" className="description">
+					{ __(
+						'Sync your products directly to Google, manage your product feed, and create Google Ad campaigns–without leaving your WooCommerce dashboard. The official extension, built in collaboration with Google.',
+						'google-listings-and-ads'
+					) }
+				</Text>
+				<AppButton
+					className="button"
+					isPrimary
+					disabled={ disableNextStep }
+					href={ getSetupMCUrl() }
+					eventName="gla_setup_mc"
+					eventProps={ {
+						target: 'set_up_free_listings',
+						trigger: 'click',
+					} }
+				>
+					{ __(
+						'Start listing products →',
+						'google-listings-and-ads'
+					) }
+				</AppButton>
+				<Text className="hint">
+					{ __(
+						'Estimated setup time: 15 min',
+						'google-listings-and-ads'
+					) }
+				</Text>
+				<Text
+					className="gla-get-started-with-video-card__terms-notice"
+					variant="body"
+				>
+					{ createInterpolateElement(
+						__(
+							'By clicking ‘Start listing products’, you agree to our <link>Terms of Service.</link>',
+							'google-listings-and-ads'
+						),
+						{
+							link: (
+								<AppDocumentationLink
+									context="get-started"
+									linkId="wp-terms-of-service"
+									href="https://wordpress.com/tos/"
+								/>
+							),
+						}
+					) }
+				</Text>
+			</FlexBlock>
+			<Tip>
+				{ __(
+					'If you’re already using another extension to manage your product feed with Google, make sure to deactivate or uninstall it first to prevent duplicate product feeds.',
+					'google-listings-and-ads'
+				) }
+			</Tip>
 		</Card>
 	);
 };
