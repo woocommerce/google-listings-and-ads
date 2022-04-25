e56b6350b60a4babe95965afbff2f5fdb3812468
Eason Su <eason.su.tw@gmail.com>
2022-04-25 18:43:58 +0800

    Add overflowY prop to AppModal

--- js/src/components/app-modal/index.js
+++ js/src/components/app-modal/index.js
@@ -9,18 +9,36 @@ import classnames from 'classnames';
  */
 import './index.scss';
 
+const overflowYStyleName = {
+	auto: 'app-modal__styled--overflow-y-auto',
+	visible: 'app-modal__styled--overflow-y-visible',
+};
+
 /**
  * Renders a Modal component with additional commonly used features.
  *
  * @param {Object} props React props.
  * @param {string} [props.className] Additional CSS class name to be appended.
+ * @param {'auto'|'visible'} [props.overflowY='auto'] Y-axis overflow style of modal.
  * @param {Array<JSX.Element>} [props.buttons] Buttons to be rendered at the modal footer.
  * @param {JSX.Element} props.children Content to be rendered.
  * @param {Object} props.rest Props to be forwarded to Modal.
  */
-const AppModal = ( { className, buttons = [], children, ...rest } ) => {
+const AppModal = ( {
+	className,
+	overflowY = 'auto',
+	buttons = [],
+	children,
+	...rest
+} ) => {
+	const modalClassName = classnames(
+		'app-modal',
+		overflowYStyleName[ overflowY ],
+		className
+	);
+
 	return (
-		<Modal className={ classnames( 'app-modal', className ) } { ...rest }>
+		<Modal className={ modalClassName } { ...rest }>
 			{ children }
 			{ buttons.length >= 1 && (
 				<div className="app-modal__footer">{ buttons }</div>
