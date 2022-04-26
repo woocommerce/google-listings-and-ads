1d833c50070fe01cbd53abc943053c3074a5c4ca
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-04-26 11:35:32 +0400

    Fix Cool-down period

--- src/Google/RequestReviewStatuses.php
+++ src/Google/RequestReviewStatuses.php
@@ -35,7 +35,8 @@ class RequestReviewStatuses implements Service {
 					isset( $region_status['reviewIneligibilityReasonDetails'] ) &&
 					isset( $region_status['reviewIneligibilityReasonDetails']['cooldownTime'] )
 				) {
-					$region_cooldown = intval( $region_status['reviewIneligibilityReasonDetails']['cooldownTime'] );
+					$region_cooldown = intval( strtotime( $region_status['reviewIneligibilityReasonDetails']['cooldownTime'] ) );
+
 					if ( ! $cooldown || $region_cooldown > $cooldown ) {
 						$cooldown = $region_cooldown;
 					}
@@ -77,8 +78,8 @@ class RequestReviewStatuses implements Service {
 
 		return [
 			'issues'   => array_map( 'strtolower', array_unique( $issues ) ),
-			'cooldown' => $cooldown,
-			'status'   => $status,
+			'cooldown' => $cooldown * 1000,
+			'status'   => $status
 		];
 	}
 
