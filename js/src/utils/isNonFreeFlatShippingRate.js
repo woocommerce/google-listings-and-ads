7a667eccd4e75a58414936e43364dff079fc42bc
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-04-26 22:41:14 +0800

    Add JSDoc for isNonFreeFlatShippingRate.

--- js/src/utils/isNonFreeFlatShippingRate.js
+++ js/src/utils/isNonFreeFlatShippingRate.js
@@ -1,3 +1,15 @@
+/**
+ * @typedef { import(".~/data/actions").ShippingRate } ShippingRate
+ */
+
+/**
+ * Check if a shipping rate is free or non-free.
+ *
+ * A shipping rate is free if its rate is 0, and non-free if its rate is greater than 0.
+ *
+ * @param {ShippingRate} shippingRate Shipping rate.
+ * @return {boolean} True if the shippingRate is non-free (rate is greater than 0).
+ */
 const isNonFreeFlatShippingRate = ( shippingRate ) => shippingRate.rate > 0;
 
 export default isNonFreeFlatShippingRate;
