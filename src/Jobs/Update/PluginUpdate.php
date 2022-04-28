f2ad92ccfc5e5a927eb309276bda17f358e8e0d4
Dima <dmytro.maksiuta@automattic.com>
2022-04-28 14:37:11 +0300

    Fixing code formatting.

--- src/Jobs/Update/PluginUpdate.php
+++ src/Jobs/Update/PluginUpdate.php
@@ -39,7 +39,7 @@ class PluginUpdate implements Service, InstallableInterface {
 	 * @var array
 	 */
 	private $updates = [
-		'1.0.1' => [
+		'1.0.1'  => [
 			CleanupProductTargetCountriesJob::class,
 			UpdateAllProducts::class,
 		],
