587422aaa78b275c173009c14ea87a5dc9498816
Ian Yu-Hsun Lin <ianlin@automattic.com>
2022-04-29 12:37:21 +0800

    Add get started card in new get started page

--- js/src/get-started-page/index.js
+++ js/src/get-started-page/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import BenefitsCard from './benefits-card';
 import CustomerQuotesCard from './customer-quotes-card';
 import FeaturesCard from './features-card';
+import GetStartedCard from './get-started-card';
 import GetStartedWithVideoCard from './get-started-with-video-card';
 import UnsupportedNotices from './unsupported-notices';
 
@@ -16,6 +17,7 @@ const GetStartedPage = () => {
 			<BenefitsCard />
 			<FeaturesCard />
 			<CustomerQuotesCard />
+			<GetStartedCard />
 		</div>
 	);
 };
