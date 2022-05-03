c7f2c538f5e385c87620ea22ee5220c04b1d1cde
Jorge M <jorge.mahecha@a8c.com>
2022-05-03 10:36:09 +0200

    Update JSDoc getConversioStatus

--- js/src/utils/getConversionCampaignStatusNotice.js
+++ js/src/utils/getConversionCampaignStatusNotice.js
@@ -10,7 +10,7 @@ import { CAMPAIGN_ADS_MIGRATION, MONTH_IN_SECONDS } from '.~/constants';
  * @param {string} adsCampaignConvertStatus.status Conversion status: unconverted|converted|not-applicable|unknown
  * @param {number} adsCampaignConvertStatus.updated Unix time in seconds of the last time that the status was checked. Once is converted the timestamp is not updated anymore.
  * @param {number} timestamp Timestamp that is used as a reference
- * @return {string} return the content to show in the banner
+ * @return {string|null} return the conversion campaign status or null if it is not in the migration period
  */
 const getConversionCampaignStatusNotice = (
 	adsCampaignConvertStatus,
