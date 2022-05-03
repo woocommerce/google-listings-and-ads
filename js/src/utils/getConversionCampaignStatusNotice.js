cb4ea2676e834b8db286108a22928b0c58e88479
Jorge M <jorge.mahecha@a8c.com>
2022-05-03 09:50:28 +0200

    Add getConversationStatus function

---
+++ js/src/utils/getConversionCampaignStatusNotice.js
@@ -0,0 +1,36 @@
+/**
+ * Internal dependencies
+ */
+import { CAMPAIGN_ADS_MIGRATION, MONTH_IN_SECONDS } from '.~/constants';
+
+/**
+ * Returns the conversion campagin status
+ *
+ * @param {Object} adsCampaignConvertStatus wp option with the information related to the conversion status
+ * @param {string} adsCampaignConvertStatus.status Conversion status: unconverted|converted|not-applicable|unknown
+ * @param {number} adsCampaignConvertStatus.updated Unix time in seconds of the last time that the status was checked. Once is converted the timestamp is not updated anymore.
+ * @param {number} timestamp Timestamp that is used as a reference
+ * @return {string} return the content to show in the banner
+ */
+const getConversionCampaignStatusNotice = (
+	adsCampaignConvertStatus,
+	timestamp = new Date().getTime()
+) => {
+	const isConverted = adsCampaignConvertStatus?.status === 'converted';
+	const conversionTimestamp = adsCampaignConvertStatus?.updated;
+
+	const isInMigrationPeriod =
+		conversionTimestamp &&
+		timestamp - conversionTimestamp * 1000 <
+			CAMPAIGN_ADS_MIGRATION.MAXIMUM_MONTHS_TO_DISPLAY_NOTICE *
+				MONTH_IN_SECONDS *
+				1000;
+
+	if ( ! isConverted ) return 'BEFORE_CONVERSION';
+	else if ( isConverted && isInMigrationPeriod ) {
+		return 'AFTER_CONVERSION';
+	}
+	return null;
+};
+
+export default getConversionCampaignStatusNotice;
