9a9efce020e8ab3d9fc2c838ef4ea87d02cfa828
Nima <nima.karimi@automattic.com>
2022-05-04 14:37:40 +0300

    Move `get_supported_countries_from_continent` method to GoogleHelper class

--- tests/Unit/Google/GoogleHelperTest.php
+++ tests/Unit/Google/GoogleHelperTest.php
@@ -7,6 +7,7 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Google\GoogleHelper;
 use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\WC;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\UnitTest;
 use PHPUnit\Framework\MockObject\MockObject;
+use WC_Countries;
 
 /**
  * Class GoogleHelperTest
@@ -89,6 +90,43 @@ class GoogleHelperTest extends UnitTest {
 
 		// If there are no subdivision codes for the country code.
 		$this->assertNull( $this->google_helper->find_subdivision_id_by_code( 'OR', 'LB' ) );
+	}
+
+	public function test_returns_supported_countries_from_continent() {
+		// Mock the WC_Countries class to return the list of countries for the EU continent.
+		$wc_countries = $this->createMock( WC_Countries::class );
+		$wc_countries->expects( $this->any() )
+					 ->method( 'get_continents' )
+					 ->willReturn( [
+						 'EU' => [
+							 'name'      => 'Europe',
+							 'countries' => [
+								 // A random country code, not supported by Merchant Center. This should be ignored.
+								 'OO1',
+								 // Another random country code, not supported by Merchant Center. This should be ignored.
+								 'OO2',
+								 // Countries supported by MC
+								 'GB',
+								 'FR',
+								 'DE',
+								 'DK',
+								 // And many more ...
+							 ],
+						 ],
+					 ] );
+		$this->wc->expects( $this->any() )
+				 ->method( 'get_wc_countries' )
+				 ->willReturn( $wc_countries );
+
+		$this->assertEqualSets(
+			[
+				'GB',
+				'FR',
+				'DE',
+				'DK',
+			],
+			$this->google_helper->get_supported_countries_from_continent( 'EU' )
+		);
 	}
 
 	/**
