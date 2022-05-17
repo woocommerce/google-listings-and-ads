bd087409ade52104db34362b1c3102af9aa31f8d
Jorge M <jorge.mahecha@a8c.com>
2022-05-17 23:42:24 +0200

    Update layour before convertion

--- src/Notes/BeforeCampaignMigration.php
+++ src/Notes/BeforeCampaignMigration.php
@@ -46,7 +46,7 @@ class BeforeCampaignMigration extends AbstractNote implements AdsAwareInterface 
 		);
 		$note->set_content_data( (object) [] );
 		$note->set_type( NoteEntry::E_WC_ADMIN_NOTE_INFORMATIONAL );
-		$note->set_layout( 'banner' );
+		$note->set_layout( 'plain' );
 		$note->set_image( '' );
 		$note->set_name( $this->get_name() );
 		$note->set_source( $this->get_slug() );
