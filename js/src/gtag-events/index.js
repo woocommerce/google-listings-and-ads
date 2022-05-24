908bba90a5648abe8c797bcc54ee240db1c12f79
mikkamp <michael.kamp@automattic.com>
2022-05-24 13:58:36 +0100

    Basic tracking for add_to_cart buttons

--- js/src/gtag-events/index.js
+++ js/src/gtag-events/index.js
@@ -1,24 +1,58 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { addAction } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
 import { namespace, actionPrefix } from './constants';
-import { getItemObject, trackEvent } from './utils';
+import { trackAddToCartEvent } from './utils';
 
 addAction(
 	`${ actionPrefix }-cart-add-item`,
 	namespace,
 	( { product, quantity = 1 } ) => {
-		trackEvent( 'add_to_cart', {
-			ecomm_pagetype: 'cart',
-			event_category: 'ecommerce',
-			event_label: __( 'Add to Cart', 'google-listings-and-ads' ),
-			items: [ getItemObject( product, quantity ) ],
-		} );
+		trackAddToCartEvent( product, quantity );
 	}
 );
+
+const addToCartClick = function ( event ) {
+	const data = event.target.dataset;
+
+	trackAddToCartEvent( { id: data.product_id }, data.quantity || 1 );
+};
+
+const singleAddToCartClick = function ( event ) {
+	const cartForm = event.target.closest( 'form.cart' );
+	if ( ! cartForm ) {
+		return;
+	}
+
+	const quantity = cartForm.querySelector( '[name=quantity]' );
+	const product = cartForm.querySelector( '[name=add-to-cart]' );
+	const variation = cartForm.querySelector( '[name=variation_id]' );
+
+	if ( product ) {
+		trackAddToCartEvent(
+			{ id: variation ? variation.value : product.value },
+			quantity ? parseInt( quantity.value, 10 ) : 1
+		);
+	}
+};
+
+window.onload = function () {
+	document
+		.querySelectorAll(
+			'.add_to_cart_button:not( .product_type_variable, .product_type_grouped )'
+		)
+		.forEach( ( button ) => {
+			button.addEventListener( 'click', addToCartClick );
+		} );
+
+	document
+		.querySelectorAll( '.single_add_to_cart_button' )
+		.forEach( ( button ) => {
+			button.addEventListener( 'click', singleAddToCartClick );
+		} );
+};
