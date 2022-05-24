99bfa1819e680a35aee18e1da0a9e2fa998db41f
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-05-24 16:29:47 +0400

    Tweak comment

--- js/src/edit-free-campaign/index.js
+++ js/src/edit-free-campaign/index.js
@@ -134,7 +134,7 @@ const EditFreeCampaign = () => {
 		savedShippingRates
 	);
 
-	// Dont take in consideration the order when comparing objects
+	// Dont take in consideration the order of the rules when comparing the Shipping times.
 	const didTimesChanged = ! isEqual(
 		new Set( shippingTimes ),
 		new Set( savedShippingTimes )
