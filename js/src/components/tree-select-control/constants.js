a918a13c4a6783140eb122a851f5663c0d84cd63
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-05-31 16:00:13 +0100

    Force Change in component

--- js/src/components/tree-select-control/constants.js
+++ js/src/components/tree-select-control/constants.js
@@ -6,3 +6,4 @@ export const ARROW_UP = 'ArrowUp';
 export const ARROW_DOWN = 'ArrowDown';
 export const ARROW_LEFT = 'ArrowLeft';
 export const ARROW_RIGHT = 'ArrowRight';
+export const TEST = '123';
