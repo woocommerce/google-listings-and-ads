1381e4fb801ff8412794dc5a5423401c466b64be
mikkamp <michael.kamp@automattic.com>
2022-05-31 14:51:18 +0100

    Add typedefs for object types

--- js/src/gtag-events/utils.js
+++ js/src/gtag-events/utils.js
@@ -26,7 +26,7 @@ export const trackEvent = ( eventName, eventParams ) => {
 /**
  * Track an add_to_cart event.
  *
- * @param {Object} product
+ * @param {Product} product
  * @param {number} quantity
  */
 export const trackAddToCartEvent = ( product, quantity = 1 ) => {
@@ -40,9 +40,9 @@ export const trackAddToCartEvent = ( product, quantity = 1 ) => {
 /**
  * Formats data into a cart Item object.
  *
- * @param {Object} product
+ * @param {Product} product
  * @param {number} quantity
- * @return {Object} Item object.
+ * @return {Item} Item object.
  */
 export const getCartItemObject = ( product, quantity ) => {
 	const item = {
@@ -72,7 +72,7 @@ export const getCartItemObject = ( product, quantity ) => {
  * Formats a regular price into a price object.
  *
  * @param {number} price
- * @return {Object} Price object.
+ * @return {ProductPrices} Price object.
  */
 export const getPriceObject = ( price ) => {
 	return {
@@ -84,8 +84,8 @@ export const getPriceObject = ( price ) => {
 /**
  * Formats a product object to include name and price from global data.
  *
- * @param {Object} product
- * @return {Object} Product object.
+ * @param {Product} product
+ * @return {Product} Product object with optional fields added.
  */
 export const getProductObject = ( product ) => {
 	if ( glaGtagData.products[ product.id ] ) {
@@ -100,7 +100,7 @@ export const getProductObject = ( product ) => {
 /**
  * Updates product data with the retrieved variation.
  *
- * @param {Object} variation
+ * @param {Variation} variation
  */
 export const retrievedVariation = ( variation ) => {
 	if ( ! variation?.variation_id ) {
@@ -112,3 +112,42 @@ export const retrievedVariation = ( variation ) => {
 		price: variation.display_price,
 	};
 };
+
+/**
+ * Product data to be included in tracking event.
+ *
+ * @typedef {Object} Product
+ * @property {number} id              ID number.
+ * @property {string} [name]          Name to display on the frontend.
+ * @property {Array} [categories]     List of product categories.
+ * @property {ProductPrices} [prices] Price data.
+ */
+
+/**
+ * Product prices.
+ *
+ * @typedef {Object} ProductPrices
+ * @property {number} price               Price in the smallest common currency unit.
+ * @property {number} currency_minor_unit The precision (decimal places).
+ */
+
+/**
+ * Item data to include in a tracked event.
+ *
+ * @typedef {Object} Item
+ * @property {string} id                       ID number including the `gla_` prefix.
+ * @property {number} [quantity]               Quantity of this item.
+ * @property {string} [name]                   Product name.
+ * @property {string} [category]               First product category name.
+ * @property {number} [price]                  Price as a decimal number.
+ * @property {string} google_business_vertical Set to `retail`.
+ */
+
+/**
+ * Variation data, sent when a variation has been selected to add to cart.
+ *
+ * @typedef {Object} Variation
+ * @property {number} variation_id    ID number.
+ * @property {string} [display_name]  Name to display on the frontend.
+ * @property {number} [display_price] Price value to display on the frontend.
+ */
