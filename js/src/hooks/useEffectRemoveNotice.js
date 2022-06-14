cbcb1fe13f4ebe7b6131988e2c5255bb72113244
Jorge M <jorge.mahecha@a8c.com>
2022-06-14 16:41:26 +0200

    Add useEffectRemoveNotice

---
+++ js/src/hooks/useEffectRemoveNotice.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import { useEffect } from '@wordpress/element';
+import { dispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import useNotices from '.~/hooks/useNotices';
+import { STORE_KEY } from '.~/data/constants';
+
+/**
+ * Removes a wp notice using its label when the component it is unmounted
+ *
+ * @param {string} label the notice label
+ * @param {string} [storeKey] the store
+ *
+ * @return {import('@wordpress/notices').Notice|null} The notice to be removed otherwise null if it is not found
+ */
+const useEffectRemoveNotice = ( label, storeKey = STORE_KEY ) => {
+	const notices = useNotices( storeKey );
+	const notice = notices.find( ( el ) => el.content === label );
+
+	useEffect( () => {
+		const { removeNotice } = dispatch( storeKey );
+
+		return () => {
+			if ( notice ) {
+				removeNotice( notice.id );
+			}
+		};
+	}, [ label, notice, storeKey ] );
+
+	return notice || null;
+};
+
+export default useEffectRemoveNotice;
