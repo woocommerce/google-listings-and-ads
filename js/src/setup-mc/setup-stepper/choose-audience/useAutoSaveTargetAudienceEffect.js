a2e05d7361a86efb33dd6dc2d86053ba62230039
Jorge M <jorge.mahecha@a8c.com>
2022-07-05 20:51:12 +0200

    Adjust useAutoSaveTargetAudienceEffect to catch the error

--- js/src/setup-mc/setup-stepper/choose-audience/useAutoSaveTargetAudienceEffect.js
+++ js/src/setup-mc/setup-stepper/choose-audience/useAutoSaveTargetAudienceEffect.js
@@ -1,8 +1,18 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import { useAppDispatch } from '.~/data';
 import useDebouncedCallbackEffect from '.~/hooks/useDebouncedCallbackEffect';
+import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
+
+/**
+ * @typedef { import(".~/data/actions").TargetAudienceData } TargetAudienceData
+ */
 
 /**
  * Automatically save settings value upon value change with a debounce delay.
@@ -11,12 +21,32 @@ import useDebouncedCallbackEffect from '.~/hooks/useDebouncedCallbackEffect';
  * because the data might be coming from the target audience suggestion API
  * which has not been saved before yet.
  *
- * @param {Object} value Settings value object to be saved.
+ * @param {TargetAudienceData} targetAudience Target audience value object to be saved.
  */
-const useAutoSaveTargetAudienceEffect = ( value ) => {
+const useAutoSaveTargetAudienceEffect = ( targetAudience ) => {
 	const { saveTargetAudience } = useAppDispatch();
+	const { createNotice } = useDispatchCoreNotices();
 
-	useDebouncedCallbackEffect( value, saveTargetAudience, {
+	/**
+	 * A `saveTargetAudienceCallback` callback that catches error and throws the error notice.
+	 *
+	 * @param {TargetAudienceData} value Target audience value object to be saved.
+	 */
+	const saveTargetAudienceCallback = async ( value ) => {
+		try {
+			await saveTargetAudience( value );
+		} catch ( error ) {
+			createNotice(
+				'error',
+				__(
+					'There was an error saving target audience data.',
+					'google-listings-and-ads'
+				)
+			);
+		}
+	};
+
+	useDebouncedCallbackEffect( targetAudience, saveTargetAudienceCallback, {
 		callOnFirstRender: true,
 	} );
 };
