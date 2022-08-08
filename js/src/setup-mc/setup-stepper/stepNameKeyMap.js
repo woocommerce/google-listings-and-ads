026abb498f495aa5dea12a900570c3350a74abed
Eason Su <eason.su.tw@gmail.com>
2022-08-08 16:22:53 +0800

    Adjust the mapping of step keys and add step 4 to the onboarding flow

--- js/src/setup-mc/setup-stepper/stepNameKeyMap.js
+++ js/src/setup-mc/setup-stepper/stepNameKeyMap.js
@@ -1,12 +1,8 @@
-// TODO:
-// Two keys have the same value '2' temporarily before the overall changes of
-// onboarding steps are completed. They would be mapped/changed to new step keys
-// and values in the subsequent PR.
 const stepNameKeyMap = {
 	accounts: '1',
-	target_audience: '2',
-	shipping_and_taxes: '2',
+	product_listings: '2',
 	store_requirements: '3',
+	paid_ads: '4',
 };
 
 export default stepNameKeyMap;
