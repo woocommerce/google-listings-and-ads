340a524876435634fea495ca977d082f74f67ea3
Eason Su <eason.su.tw@gmail.com>
2022-08-09 17:41:49 +0800

    Wrap the separator "," and the wording "and" into i18n functions.
    Address
    - https://github.com/woocommerce/google-listings-and-ads/pull/1602#discussion_r932082786
    - https://github.com/woocommerce/google-listings-and-ads/pull/1602#discussion_r933023431

--- js/src/utils/createMessageForMultipleErrors.js
+++ js/src/utils/createMessageForMultipleErrors.js
@@ -1,12 +1,7 @@
 /**
  * External dependencies
  */
-import { __, sprintf, _n } from '@wordpress/i18n';
-
-/**
- * Internal dependencies
- */
-import concatenateListOfWords from '.~/utils/concatenateListOfWords';
+import { __, sprintf, _n, _x } from '@wordpress/i18n';
 
 /**
  * Creates a message for a list of error messages. For example for a list of error messages: createMessageForMultipleErrors(['Target Audience', 'Shipping Rates']) will create the following message:
@@ -21,14 +16,27 @@ export default function createMessageForMultipleErrors(
 	isPartiallySuccessful = true
 ) {
 	if ( errorMessages.length ) {
-		const listErrors = concatenateListOfWords( errorMessages );
+		const separator = _x(
+			', ',
+			'the separator for concatenating the messages of failed actions',
+			'google-listings-and-ads'
+		);
+		const listErrors = sprintf(
+			// translators: 1: optional string when there are multiple failed actions, and it's a concatenated text of failed actions except for the last one. 2: the last one or the only failed action.
+			_n(
+				'There is an error in the following action: %1$s%2$s.',
+				'There are errors in the following actions: %1$s and %2$s.',
+				errorMessages.length,
+				'google-listings-and-ads'
+			),
+			errorMessages.slice( 0, -1 ).join( separator ),
+			errorMessages.at( -1 )
+		);
 
 		const content = sprintf(
-			// translators: 1: list of errors 2: optional text if some promises have been invoked successfully.
-			_n(
-				'There is an error in the following action: %1$s. %2$s Please try again later.',
-				'There are errors in the following actions: %1$s. %2$s Please try again later.',
-				errorMessages.length,
+			// translators: 1: text for the failed action(s). 2: optional text if some promises have been invoked successfully.
+			__(
+				'%1$s %2$s Please try again later.',
 				'google-listings-and-ads'
 			),
 			listErrors,
