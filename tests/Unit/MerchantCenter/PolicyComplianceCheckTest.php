abfea676b9c79d7201eac0f9774f5be169bb077c
Chen Song <songchen@google.com>
2022-08-09 11:57:57 -0700

    test fix

--- tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
+++ tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
@@ -3,14 +3,12 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\MerchantCenter;
 
-use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
 use Automattic\WooCommerce\GoogleListingsAndAds\Proxies\WC;
 use Automattic\WooCommerce\GoogleListingsAndAds\Google\GoogleHelper;
-use Automattic\WooCommerce\GoogleListingsAndAds\PluginHelper;
 use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\TargetAudience;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\UnitTest;
 use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\PolicyComplianceCheck;
-
+use WC_Payment_Gateway;
 use PHPUnit\Framework\MockObject\MockObject;
 
 defined( 'ABSPATH' ) || exit;
@@ -20,9 +18,10 @@ defined( 'ABSPATH' ) || exit;
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\MerchantCenter
  *
- * @property  MockObject|Merchant $merchant
- * @property  MockObject|Settings $google_settings
- * @property  PolicyComplianceCheck  $policy_compliance_check
+ * @property  MockObject|WC		$wc
+ * @property  MockObject|GoogleHelper	$google_helper
+ * @property  MockObject|TargetAudience	$target_audience
+ * @property  PolicyComplianceCheck	$policy_compliance_check
  */
 class PolicyComplianceCheckTest extends UnitTest {
 
@@ -31,52 +30,49 @@ class PolicyComplianceCheckTest extends UnitTest {
 	 */
 	public function setUp(): void {
 		parent::setUp();
-		$this->wc            = $this->createMock( WC::class );
-		$this->google_helper     = $this->createMock( GoogleHelper::class );
-		$this->target_audience     = $this->createMock( TargetAudience::class );
+		$this->wc		= $this->createMock( WC::class );
+		$this->google_helper	= $this->createMock( GoogleHelper::class );
+		$this->target_audience	= $this->createMock( TargetAudience::class );
 
 		$this->policy_compliance_check = new PolicyComplianceCheck( $this->wc, $this->google_helper, $this->target_audience );
 	}
 
-
-
 	public function test_website_accessible() {
 		$this->wc->expects( $this->any() )
 					   ->method( 'get_allowed_countries' )
-					   ->willReturn( ["AU" => "Australia", "AT" => "Austria", "CA" => "Canada", "US" => "United States"] );
+					   ->willReturn( ['AU' => 'Australia', 'AT' => 'Austria', 'CA' => 'Canada', 'US' => 'United States'] );
 		$this->target_audience->expects( $this->any() )
 					   ->method( 'get_target_countries' )
-					   ->willReturn( ["AU" => "Australia", "US" => "United States"] );
+					   ->willReturn( ['AU' , 'US' ] );
 
-		$this->assertEquals($this->policy_compliance_check->is_accessible(), true);
+		$this->assertTrue($this->policy_compliance_check->is_accessible());
 	}
-
 
 	public function test_website_not_accessible() {
 		$this->wc->expects( $this->any() )
 					   ->method( 'get_allowed_countries' )
-					   ->willReturn( ["AU" => "Australia", "AT" => "Austria", "CA" => "Canada", "US" => "United States"] );
+					   ->willReturn( ['AU' => 'Australia', 'AT' => 'Austria', 'CA' => 'Canada', 'US' => 'United States'] );
 		$this->target_audience->expects( $this->any() )
 					   ->method( 'get_target_countries' )
-					   ->willReturn( ["FR" => "France", "US" => "United States"] );
+					   ->willReturn( ['FR', 'US' ] );
 
-		$this->assertEquals($this->policy_compliance_check->is_accessible(), false);
+		$this->assertFalse($this->policy_compliance_check->is_accessible());
 	}
 
-
 	public function test_payment_gateways() {
-		$this->wc->expects( $this->any() )
-					   ->method( 'get_available_payment_gateways' )
-					   ->willReturn( ["PayPal", "Stripe"] );
+					   $payment_gateways = [
+						'stripe'	=> $this->createMock( WC_Payment_Gateway::class ),
+						'paypal'	=> $this->createMock( WC_Payment_Gateway::class ),
+					];
 
-		$this->assertEquals($this->policy_compliance_check->has_payment_gateways(), true);
+		$this->assertTrue($this->policy_compliance_check->has_payment_gateways());
 	}
 
 	public function test_empty_payment_gateways() {
 		$this->wc->expects( $this->any() )
 					   ->method( 'get_available_payment_gateways' )
-					   ->willReturn( []);
+					   ->willReturn( [] );
 
-		$this->assertEquals($this->policy_compliance_check->has_payment_gateways(), false);
+		$this->assertFalse($this->policy_compliance_check->has_payment_gateways());
 	}
 }
