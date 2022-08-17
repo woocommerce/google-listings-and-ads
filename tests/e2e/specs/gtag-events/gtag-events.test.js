5479c3b7cc8161b765d7420e54d54d069fa9a3f1
Eason Su <eason.su.tw@gmail.com>
2022-08-17 16:37:10 +0800

    Increase the default timeout for all gtag e2e tests to reduce the possibility of timeout errors.

--- tests/e2e/specs/gtag-events/gtag-events.test.js
+++ tests/e2e/specs/gtag-events/gtag-events.test.js
@@ -34,7 +34,7 @@ let simpleProductID;
 
 describe( 'GTag events', () => {
 	beforeAll( async () => {
-		page.setDefaultTimeout( 3000 );
+		page.setDefaultTimeout( 10000 );
 		await saveConversionID();
 		await createBlockShopPage();
 		simpleProductID = await createSimpleProduct();
