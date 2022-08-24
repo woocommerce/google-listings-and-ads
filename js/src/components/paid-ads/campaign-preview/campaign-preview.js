1bde4a92504e8d86eeafd88358aa5e87ce8900a4
Eason Su <eason.su.tw@gmail.com>
2022-08-24 18:26:54 +0800

    Add the container for ad previews

---
+++ js/src/components/paid-ads/campaign-preview/campaign-preview.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import './campaign-preview.scss';
+
+export default function CampaignPreview() {
+	return <div className="gla-campaign-preview" />;
+}
---
+++ js/src/components/paid-ads/campaign-preview/index.js
@@ -0,0 +1,13 @@
+export { default } from './campaign-preview';
+
+/**
+ * Data for compositing ad preview mockups.
+ *
+ * @typedef {Object} AdPreviewData
+ * @property {string} title Price of the product.
+ * @property {string} price Price of the product including currency symbol, e.g. "$24.99".
+ * @property {string} shopName Shop name of merchant's website.
+ * @property {string} coverUrl Cover image URL of the product.
+ * @property {string} shopLogoUrl Logo URL of merchant's website.
+ * @property {string} shopUrl URL of merchant's website.
+ */
