8e72981c45e5ba8bc62f09bb2c1bb8bf1ebb7fbb
Eason Su <eason.su.tw@gmail.com>
2022-08-30 12:07:15 +0800

    Fix a typo in the JSDoc of js/src/components/paid-ads/campaign-preview/index.js
    Address https://github.com/woocommerce/google-listings-and-ads/pull/1650#discussion_r957330825

--- js/src/components/paid-ads/campaign-preview/index.js
+++ js/src/components/paid-ads/campaign-preview/index.js
@@ -4,7 +4,7 @@ export { default } from './campaign-preview';
  * Data for compositing ad preview mockups.
  *
  * @typedef {Object} AdPreviewData
- * @property {string} title Price of the product.
+ * @property {string} title Title of the product.
  * @property {string} price Price of the product including currency symbol, e.g. "$24.99".
  * @property {string} shopName Shop name of merchant's website.
  * @property {string} coverUrl Cover image URL of the product.
