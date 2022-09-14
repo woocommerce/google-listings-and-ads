e83cedbf5803c9076b60b2f22866b7ec902a4f5f
Chen Song <songchen@google.com>
2022-09-13 18:23:59 -0700

    test

--- tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
+++ tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
@@ -112,6 +112,8 @@ class PolicyComplianceCheckTest extends WPRequestUnitTest {
 	}
 
 	public function test_with_store_ssl() {
+		define('RELOCATE',true);
+		define('WP_HOME','https://example.org');
 		define( 'WP_SITEURL', 'https://example.org' );
 		update_option( 'siteurl', 'https://example.org' );
 		$this->assertTrue($this->policy_compliance_check->get_is_store_ssl());
