dccec5949607c2af5dd158a2c9ce8dd01bd2de72
Chen Song <songchen@google.com>
2022-09-13 18:27:20 -0700

    test

--- tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
+++ tests/Unit/MerchantCenter/PolicyComplianceCheckTest.php
@@ -112,9 +112,9 @@ class PolicyComplianceCheckTest extends WPRequestUnitTest {
 	}
 
 	public function test_with_store_ssl() {
-		define('WP_HOME','https://example.org');
+		define( 'WP_HOME', 'https://example.org' );
 		$this->assertTrue($this->policy_compliance_check->get_is_store_ssl());
-		define('WP_HOME','http://example.org');
+		define( 'WP_HOME', 'http://example.org' );
 	}
 
 	public function test_not_has_refund_page() {
