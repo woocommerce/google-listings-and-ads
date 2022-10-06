cfa618d308d21207d70333e7d701e48b73f380a6
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-10-06 16:15:11 +0400

    Fix name for MPN

--- src/Product/Attributes/MPN.php
+++ src/Product/Attributes/MPN.php
@@ -59,6 +59,6 @@ class MPN extends AbstractAttribute implements WithMappingInterface {
 	 * @return string
 	 */
 	public static function get_name(): string {
-		return __( 'GTIN', 'google-listings-and-ads' );
+		return __( 'MPN', 'google-listings-and-ads' );
 	}
 }
