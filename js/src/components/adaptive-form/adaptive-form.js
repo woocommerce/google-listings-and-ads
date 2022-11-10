7eff6d8dfc4bb9e7c3c7e3626a58b600a93b5390
Eason <eason.su.tw@gmail.com>
2022-11-10 22:21:36 +0800

    Add code comments for a special use case that directly mutates React state in AdaptiveForm

--- js/src/components/adaptive-form/adaptive-form.js
+++ js/src/components/adaptive-form/adaptive-form.js
@@ -121,9 +121,12 @@ function AdaptiveForm( { children, ...props }, ref ) {
 
 				// Related to WC 6.9. Only one delegate can be consumed at a time in this render prop to
 				// ensure the updating states will always be the latest when calling.
-				//const { batchQueue } = adapterRef.current;
 				if ( batchQueue.length ) {
 					// Use `setImmediate` to avoid the warning of request state updates while rendering.
+					// Mutating a React hook state is an anti-pattern in most cases. Here is done intentionally
+					// because it's necessary to ensure this component will be triggered re-rendering through
+					// `setBatchQueue`, but also to avoid calling `settingBatchQueue` here and triggering
+					// additional rendering again.
 					setImmediate( () => setDelegation( batchQueue.shift() ) );
 				}
 
