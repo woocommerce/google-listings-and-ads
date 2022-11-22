2abfbbcc8ed74b23d002f521ac16454d1bafe283
Jorge M <jorge.mahecha@a8c.com>
2022-11-22 18:11:25 +0100

    Update get_final_urls_suggestions to use search query

--- src/Ads/AssetSuggestionsService.php
+++ src/Ads/AssetSuggestionsService.php
@@ -29,9 +29,12 @@ class AssetSuggestionsService implements Service {
 	/**
 	 * Get posts that can be used to suggest assets
 	 *
+	 * @param string $search The search query
+	 * @param int    $per_page Number of items per page
+	 *
 	 * @return array
 	 */
-	protected function get_post_suggestions(): array {
+	protected function get_post_suggestions( $search, $per_page ): array {
 		$post_suggestions    = [];
 		$excluded_post_types = [ 'attachment' ];
 
@@ -47,8 +50,9 @@ class AssetSuggestionsService implements Service {
 
 		$args = [
 			'post_type'      => $filtered_post_types,
-			'posts_per_page' => -1,
+			'posts_per_page' => $per_page,
 			'post_status'    => 'publish',
+			's'              => $search,
 		];
 
 		$posts = $this->wp->get_posts( $args );
@@ -63,9 +67,12 @@ class AssetSuggestionsService implements Service {
 	/**
 	 * Get terms that can be used to suggest assets
 	 *
+	 * @param string $search The search query
+	 * @param int    $per_page Number of items per page
+	 *
 	 * @return array
 	 */
-	protected function get_terms_suggestion(): array {
+	protected function get_terms_suggestion( $search, $per_page ): array {
 		$terms_suggestions = [];
 
 		// Get all taxonomies that are public, show_in_menu = true helps to exclude taxonomies such as "product_shipping_class".
@@ -79,7 +86,10 @@ class AssetSuggestionsService implements Service {
 		// Skip empty terms
 		$terms = $this->wp->get_terms(
 			[
-				'taxonomy' => $taxonomies,
+				'taxonomy'   => $taxonomies,
+				'hide_empty' => false,
+				'number'     => $per_page,
+				'name__like' => $search,
 			]
 		);
 
@@ -95,12 +105,53 @@ class AssetSuggestionsService implements Service {
 	/**
 	 * Return a list of final urls that can be used to suggest assets.
 	 *
+	 * @param string $search The search query
+	 * @param int    $per_page Number of items per page
+	 * @param string $order_by Order by: id, type, title, url
+	 *
 	 * @return array Array of final urls with their title, id & type.
 	 */
-	public function get_final_urls_suggestions(): array {
-		$posts = $this->get_post_suggestions();
-		$terms = $this->get_terms_suggestion();
-		return array_merge( $posts, $terms );
+	public function get_final_urls_suggestions( $search = '', $per_page = 30, $order_by = 'title' ): array {
+		if ( $per_page <= 0 ) {
+			return [];
+		}
+
+		// Split possible results between posts and terms.
+		$per_page_posts = ceil( $per_page / 2 );
+		$per_page_terms = $per_page - $per_page_posts;
+
+		$posts = $this->get_post_suggestions( $search, $per_page_posts );
+
+		// If not posts results, try to get all results from terms.
+		if ( count( $posts ) === 0 ) {
+			$result = $this->get_terms_suggestion( $search, $per_page );
+			return $this->order_results( $result, $order_by );
+		}
+
+		// Try to get more results using the terms
+		$terms  = $this->get_terms_suggestion( $search, $per_page_terms );
+		$result = array_merge( $posts, $terms );
+
+		return $this->order_results( $result, $order_by );
+
+	}
+
+	/**
+	 *  Order suggestions alphabetically
+	 *
+	 *  @param array  $array associative array
+	 *  @param string $field Sort by a specific field
+	 */
+	public function order_results( $array, $field ) {
+		usort(
+			$array,
+			function ( $a, $b ) use ( $field ) {
+				return strcmp( (string) $a[ $field ], (string) $b[ $field ] );
+			}
+		);
+
+		return $array;
+
 	}
 
 	/**
