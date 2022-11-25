2617fb6f8ab5242b4b1493d64844e14d6800d975
Eason Su <eason.su.tw@gmail.com>
2022-11-25 23:41:35 +0800

    Add step 2 to the campaign creation page

--- js/src/pages/create-paid-ads-campaign/create-paid-ads-campaign-form.js
+++ js/src/pages/create-paid-ads-campaign/create-paid-ads-campaign-form.js
@@ -2,10 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement, useState } from '@wordpress/element';
-import { Form } from '@woocommerce/components';
-import { getHistory } from '@woocommerce/navigation';
-import { recordEvent } from '@woocommerce/tracks';
+import { createInterpolateElement } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -15,115 +12,58 @@ import StepContentHeader from '.~/components/stepper/step-content-header';
 import StepContentFooter from '.~/components/stepper/step-content-footer';
 import AppDocumentationLink from '.~/components/app-documentation-link';
 import AppButton from '.~/components/app-button';
-import useDispatchCoreNotices from '.~/hooks/useDispatchCoreNotices';
-import useTargetAudienceFinalCountryCodes from '.~/hooks/useTargetAudienceFinalCountryCodes';
-import { useAppDispatch } from '.~/data';
+import { useAdaptiveFormContext } from '.~/components/adaptive-form';
 import CreateCampaignFormContent from '.~/components/paid-ads/create-campaign-form-content';
-import validateForm from '.~/utils/paid-ads/validateForm';
-import { getDashboardUrl } from '.~/utils/urls';
 
 /**
- * @fires gla_launch_paid_campaign_button_click on submit
+ * Renders the form container for campaign management.
+ *
+ * Please note that this component relies on an AdaptiveForm's context, so it expects
+ * a context provider component (`AdaptiveForm`) to existing in its parents.
+ *
  * @fires gla_documentation_link_click with `{ context: 'create-ads', link_id: 'see-what-ads-look-like', href: 'https://support.google.com/google-ads/answer/6275294' }`
+ *
+ * @param {Object} props React props.
+ * @param {() => void} props.onContinue Callback called once continue button is clicked.
  */
-const CreatePaidAdsCampaignForm = () => {
-	const [ loading, setLoading ] = useState( false );
-	const { createAdsCampaign } = useAppDispatch();
-	const { createNotice } = useDispatchCoreNotices();
-	const { data: targetAudience } = useTargetAudienceFinalCountryCodes();
-
-	const handleValidate = ( values ) => {
-		return validateForm( values );
-	};
-
-	const handleSubmit = async ( values ) => {
-		setLoading( true );
-
-		try {
-			const { amount, countryCodes } = values;
-
-			recordEvent( 'gla_launch_paid_campaign_button_click', {
-				audiences: countryCodes.join( ',' ),
-				budget: amount,
-			} );
-
-			await createAdsCampaign( amount, countryCodes );
-
-			createNotice(
-				'success',
-				__(
-					'You’ve successfully created a paid campaign!',
-					'google-listings-and-ads'
-				)
-			);
-		} catch ( e ) {
-			setLoading( false );
-			return;
-		}
-
-		getHistory().push( getDashboardUrl() );
-	};
-
-	if ( ! targetAudience ) {
-		return null;
-	}
+const CreatePaidAdsCampaignForm = ( { onContinue } ) => {
+	const formContext = useAdaptiveFormContext();
+	const { isValidForm } = formContext;
 
 	return (
-		<Form
-			initialValues={ {
-				amount: 0,
-				countryCodes: targetAudience,
-			} }
-			validate={ handleValidate }
-			onSubmit={ handleSubmit }
-		>
-			{ ( formProps ) => {
-				const {
-					isValidForm,
-					handleSubmit: handleLaunchCampaignClick,
-				} = formProps;
-
-				return (
-					<StepContent>
-						<StepContentHeader
-							title={ __(
-								'Create your paid campaign',
-								'google-listings-and-ads'
-							) }
-							description={ createInterpolateElement(
-								__(
-									'Paid Performance Max campaigns are automatically optimized for you by Google. <link>See what your ads will look like.</link>',
-									'google-listings-and-ads'
-								),
-								{
-									link: (
-										<AppDocumentationLink
-											context="create-ads"
-											linkId="see-what-ads-look-like"
-											href="https://support.google.com/google-ads/answer/6275294"
-										/>
-									),
-								}
-							) }
-						/>
-						<CreateCampaignFormContent formProps={ formProps } />
-						<StepContentFooter>
-							<AppButton
-								isPrimary
-								disabled={ ! isValidForm }
-								loading={ loading }
-								onClick={ handleLaunchCampaignClick }
-							>
-								{ __(
-									'Launch paid campaign',
-									'google-listings-and-ads'
-								) }
-							</AppButton>
-						</StepContentFooter>
-					</StepContent>
-				);
-			} }
-		</Form>
+		<StepContent>
+			<StepContentHeader
+				title={ __(
+					'Create your paid campaign',
+					'google-listings-and-ads'
+				) }
+				description={ createInterpolateElement(
+					__(
+						'Paid Performance Max campaigns are automatically optimized for you by Google. <link>See what your ads will look like.</link>',
+						'google-listings-and-ads'
+					),
+					{
+						link: (
+							<AppDocumentationLink
+								context="create-ads"
+								linkId="see-what-ads-look-like"
+								href="https://support.google.com/google-ads/answer/6275294"
+							/>
+						),
+					}
+				) }
+			/>
+			<CreateCampaignFormContent formProps={ formContext } />
+			<StepContentFooter>
+				<AppButton
+					isPrimary
+					disabled={ ! isValidForm }
+					onClick={ onContinue }
+				>
+					{ __( 'Continue', 'google-listings-and-ads' ) }
+				</AppButton>
+			</StepContentFooter>
+		</StepContent>
 	);
 };
 
