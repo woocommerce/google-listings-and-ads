f070c7311730fc1e81a5cd529d28a5178afd0ef8
Jorge M <jorge.mahecha@a8c.com>
2022-12-16 13:33:21 +0100

    Add AssetGroupController schema

--- src/API/Site/Controllers/Ads/AssetGroupController.php
+++ src/API/Site/Controllers/Ads/AssetGroupController.php
@@ -48,10 +48,27 @@ class AssetGroupController extends BaseController {
 					'methods'             => TransportMethods::READABLE,
 					'callback'            => $this->get_asset_groups_assets_callback(),
 					'permission_callback' => $this->get_permission_callback(),
+					'args'                => $this->get_asset_group_params(),
 				],
 				'schema' => $this->get_api_response_schema_callback(),
 			]
 		);
+	}
+
+	/**
+	 * Get the assets suggestions params.
+	 *
+	 * @return array
+	 */
+	public function get_asset_group_params(): array {
+		return [
+			'id' => [
+				'description'       => __( 'Campaign ID.', 'google-listings-and-ads' ),
+				'type'              => 'number',
+				'sanitize_callback' => 'absint',
+				'validate_callback' => 'rest_validate_request_arg',
+			],
+		];
 	}
 
 
@@ -61,8 +78,15 @@ class AssetGroupController extends BaseController {
 	protected function get_asset_groups_assets_callback(): callable {
 		return function( Request $request ) {
 			try {
-				$campaign_id = absint( $request['id'] );
-				return $this->ads_asset_group->get_asset_groups_by_campaign_id( $campaign_id );
+				$campaign_id = $request['id'];
+				return array_map(
+					function( $item ) use ( $request ) {
+						$data = $this->prepare_item_for_response( $item, $request );
+						return $this->prepare_response_for_collection( $data );
+					},
+					$this->ads_asset_group->get_asset_groups_by_campaign_id( $campaign_id )
+				);
+
 			} catch ( Exception $e ) {
 				return $this->response_from_exception( $e );
 			}
@@ -76,7 +100,88 @@ class AssetGroupController extends BaseController {
 	 * @return array
 	 */
 	protected function get_schema_properties(): array {
-		return [];
+		return [
+			'id'               => [
+				'type'        => 'number',
+				'description' => __( 'Asset Group ID', 'google-listings-and-ads' ),
+				'context'     => [ 'view' ],
+			],
+			'final_urls'       => [
+				'type'        => 'array',
+				'description' => __( 'A list of final URLs', 'google-listings-and-ads' ),
+				'context'     => [ 'view' ],
+				'enum'        => [ 'post', 'term' ],
+
+			],
+			'display_url_path' => [
+				'type'        => 'array',
+				'description' => __( 'Text that may appear appended to the url displayed in the ad.', 'google-listings-and-ads' ),
+				'context'     => [ 'view' ],
+			],
+			'assets'           => [
+				'type'        => 'array',
+				'description' => __( 'Asset is a part of an ad which can be shared across multiple ads. It can be an image (ImageAsset), headlines, descriptions, etc.', 'google-listings-and-ads' ),
+				'items'       => [
+					'type'       => 'object',
+					'properties' => [
+						'square_marketing_image'   => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'marketing_image'          => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'logo'                     => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'business_name'            => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'headline'                 => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'description'              => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'long_headline'            => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+						'call_to_action_selection' => [
+							'type'  => 'array',
+							'items' => $this->get_schema_asset(),
+						],
+					],
+				],
+			],
+
+		];
+	}
+
+	/**
+	 * Get the item schema for the asset.
+	 *
+	 * @return array
+	 */
+	protected function get_schema_asset() {
+		return [
+			'type'       => 'object',
+			'properties' => [
+				'id'      => [
+					'type'        => 'number',
+					'description' => __( 'Asset ID', 'google-listings-and-ads' ),
+				],
+				'content' => [
+					'type'        => 'string',
+					'description' => __( 'Asset content', 'google-listings-and-ads' ),
+				],
+			],
+		];
 	}
 
 	/**
