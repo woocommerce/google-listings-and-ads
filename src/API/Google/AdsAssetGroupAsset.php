a3287eb767816bd502a3709b2e20a2eb6b5ddf77
Jorge M <jorge.mahecha@a8c.com>
2022-12-20 19:09:48 +0100

    Add try/catch for asset group assets

--- src/API/Google/AdsAssetGroupAsset.php
+++ src/API/Google/AdsAssetGroupAsset.php
@@ -10,6 +10,8 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsAwareInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\OptionsAwareTrait;
 use Google\Ads\GoogleAds\V11\Services\GoogleAdsRow;
 use Google\Ads\GoogleAds\V11\Resources\AssetGroupAsset;
+use Google\ApiCore\ApiException;
+use Automattic\WooCommerce\GoogleListingsAndAds\Exception\ExceptionWithResponseData;
 
 
 
@@ -26,6 +28,7 @@ use Google\Ads\GoogleAds\V11\Resources\AssetGroupAsset;
 class AdsAssetGroupAsset implements OptionsAwareInterface {
 
 	use OptionsAwareTrait;
+	use ApiExceptionTrait;
 
 	/**
 	 * The Google Ads Client.
@@ -76,31 +79,46 @@ class AdsAssetGroupAsset implements OptionsAwareInterface {
 	 * @param array $asset_groups_ids The asset groups ids.
 	 *
 	 * @return array The assets for the asset groups.
+	 * @throws ExceptionWithResponseData When an ApiException is caught.
 	 */
 	public function get_assets_by_asset_group_ids( array $asset_groups_ids ): array {
-		if ( empty( $asset_groups_ids ) ) {
-			return [];
+		try {
+			if ( empty( $asset_groups_ids ) ) {
+				return [];
+			}
+
+			$asset_group_assets = [];
+			$asset_results      = ( new AdsAssetGroupAssetQuery() )
+				->set_client( $this->client, $this->options->get_ads_id() )
+				->add_columns( [ 'asset_group.id' ] )
+				->where( 'asset_group.id', $asset_groups_ids, 'IN' )
+				->where( 'asset_group_asset.field_type', $this->get_asset_field_types_query(), 'IN' )
+				->where( 'asset_group_asset.status', 'REMOVED', '!=' )
+				->get_results();
+
+			/** @var GoogleAdsRow $row */
+			foreach ( $asset_results->iterateAllElements() as $row ) {
+
+				/** @var AssetGroupAsset $asset_group_asset */
+				$asset_group_asset = $row->getAssetGroupAsset();
+
+				$field_type = AssetFieldType::label( $asset_group_asset->getFieldType() );
+				$asset_group_assets[ $row->getAssetGroup()->getId() ][ $field_type ][] = $this->asset->convert_asset( $row );
+			}
+
+			return $asset_group_assets;
+		} catch ( ApiException $e ) {
+			do_action( 'woocommerce_gla_ads_client_exception', $e, __METHOD__ );
+
+			$errors = $this->get_api_exception_errors( $e );
+			throw new ExceptionWithResponseData(
+				/* translators: %s Error message */
+				sprintf( __( 'Error retrieving asset groups assets: %s', 'google-listings-and-ads' ), reset( $errors ) ),
+				$this->map_grpc_code_to_http_status_code( $e ),
+				null,
+				[ 'errors' => $errors ]
+			);
 		}
 
-		$asset_group_assets = [];
-		$asset_results      = ( new AdsAssetGroupAssetQuery() )
-			->set_client( $this->client, $this->options->get_ads_id() )
-			->add_columns( [ 'asset_group.id' ] )
-			->where( 'asset_group.id', $asset_groups_ids, 'IN' )
-			->where( 'asset_group_asset.field_type', $this->get_asset_field_types_query(), 'IN' )
-			->where( 'asset_group_asset.status', 'REMOVED', '!=' )
-			->get_results();
-
-		/** @var GoogleAdsRow $row */
-		foreach ( $asset_results->iterateAllElements() as $row ) {
-
-			/** @var AssetGroupAsset $asset_group_asset */
-			$asset_group_asset = $row->getAssetGroupAsset();
-
-			$field_type = AssetFieldType::label( $asset_group_asset->getFieldType() );
-			$asset_group_assets[ $row->getAssetGroup()->getId() ][ $field_type ][] = $this->asset->convert_asset( $row );
-		}
-
-		return $asset_group_assets;
 	}
 }
