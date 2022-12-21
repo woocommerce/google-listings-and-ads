c74e27d7d77db1e288e728120bb3ed513e47da79
mikkamp <michael.kamp@automattic.com>
2022-12-21 14:48:24 +0000

    Get default parameters from WP_REST_Request

--- src/API/Site/Controllers/BaseReportsController.php
+++ src/API/Site/Controllers/BaseReportsController.php
@@ -120,30 +120,16 @@ abstract class BaseReportsController extends BaseController {
 	 * @return array
 	 */
 	protected function prepare_query_arguments( Request $request ): array {
-		$params   = $this->get_collection_params();
-		$defaults = $this->get_defaults( $params );
-		$args     = wp_parse_args( array_intersect_key( $request->get_query_params(), $params ), $defaults );
+		$args = wp_parse_args(
+			array_intersect_key(
+				$request->get_query_params(),
+				$this->get_collection_params()
+			),
+			$request->get_default_params()
+		);
 
 		$this->normalize_timezones( $args );
 		return $args;
-	}
-
-	/**
-	 * Get parameter defaults.
-	 *
-	 * @param array $params List of parameters.
-	 *
-	 * @return array
-	 */
-	protected function get_defaults( array $params ): array {
-		$defaults = [];
-		foreach ( $params as $key => $param ) {
-			if ( isset( $param['default'] ) ) {
-				$defaults[ $key ] = $param['default'];
-			}
-		}
-
-		return $defaults;
 	}
 
 	/**
