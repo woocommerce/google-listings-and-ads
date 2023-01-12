8eb6bc7a88cccd4c03bb5c47ca72e9ead660aefa
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2023-01-12 13:16:03 +0700

    Adding tests for Merchant Center Select Control

---
+++ js/src/components/merchant-center-select-control/index.test.js
@@ -0,0 +1,38 @@
+jest.mock( '.~/hooks/useExistingGoogleMCAccounts', () => ( {
+	__esModule: true,
+	default: jest
+		.fn()
+		.mockName( 'useExistingGoogleMCAccounts' )
+		.mockImplementation( () => {
+			return {
+				data: [
+					{ id: 1, name: 'Account 1', domain: 'https://example.com' },
+					{
+						id: 2,
+						name: 'Account 2',
+						domain: 'https://example2.com',
+					},
+				],
+			};
+		} ),
+} ) );
+
+/**
+ * External dependencies
+ */
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+/**
+ * Internal dependencies
+ */
+import MerchantCenterSelectControl from '.~/components/merchant-center-select-control/index';
+
+describe( 'Merchant Center Select Control', () => {
+	test( 'First option selected by default', () => {
+		const { queryAllByRole } = render( <MerchantCenterSelectControl /> );
+		const options = queryAllByRole( 'option' );
+		expect( options ).toHaveLength( 2 );
+		expect( options[ 0 ] ).toHaveAttribute( 'value', '1' );
+	} );
+} );
