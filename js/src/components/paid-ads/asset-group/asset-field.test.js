0861a23c05aa5a8fd69269d358cb57e1de297933
Eason Su <eason.su.tw@gmail.com>
2023-01-16 17:12:55 +0800

    Add tests for AssetField component

---
+++ js/src/components/paid-ads/asset-group/asset-field.test.js
@@ -0,0 +1,109 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+/**
+ * Internal dependencies
+ */
+import AssetField from './asset-field';
+
+describe( 'AssetField', () => {
+	function getToggleButton() {
+		return screen.getByRole( 'button', { name: /toggle/i } );
+	}
+
+	function getHelpButton() {
+		return screen.getByRole( 'button', { name: /open popover/i } );
+	}
+
+	it( 'Should render the heading', () => {
+		render( <AssetField heading="Heading" numOfIssues={ 0 } /> );
+
+		expect( screen.getByText( 'Heading' ) ).toBeInTheDocument();
+	} );
+
+	it( 'Should render the subheading', () => {
+		render( <AssetField subheading="Subheading" numOfIssues={ 0 } /> );
+
+		expect( screen.getByText( 'Subheading' ) ).toBeInTheDocument();
+	} );
+
+	it( 'When `numOfIssues` > 0, it should render the number of issues', () => {
+		const { rerender } = render( <AssetField numOfIssues={ 1 } /> );
+
+		expect( screen.getByText( '1 issue' ) ).toBeInTheDocument();
+
+		rerender( <AssetField numOfIssues={ 2 } /> );
+
+		expect( screen.getByText( '2 issues' ) ).toBeInTheDocument();
+	} );
+
+	it( 'When `numOfIssues` = 0, it should not render the number of issues', () => {
+		render( <AssetField numOfIssues={ 0 } /> );
+
+		expect( screen.queryByText( '0 issues' ) ).not.toBeInTheDocument();
+	} );
+
+	it( 'When not setting `initialExpanded`, it should collapse to hide the children', () => {
+		render( <AssetField numOfIssues={ 0 }>Children</AssetField> );
+
+		expect( screen.queryByText( 'Children' ) ).not.toBeInTheDocument();
+	} );
+
+	it( 'When setting `initialExpanded`, it should expand to render the children', () => {
+		render(
+			<AssetField numOfIssues={ 0 } initialExpanded>
+				Children
+			</AssetField>
+		);
+
+		expect( screen.getByText( 'Children' ) ).toBeInTheDocument();
+	} );
+
+	it( 'When disabling, it should also collapse to hide the children', () => {
+		const { rerender } = render(
+			<AssetField numOfIssues={ 0 } initialExpanded>
+				Children
+			</AssetField>
+		);
+
+		expect( screen.getByText( 'Children' ) ).toBeInTheDocument();
+
+		rerender(
+			<AssetField numOfIssues={ 0 } initialExpanded disabled>
+				Children
+			</AssetField>
+		);
+
+		expect( screen.queryByText( 'Children' ) ).not.toBeInTheDocument();
+	} );
+
+	it( 'When disabled, it should also disable the toggle button and help button', async () => {
+		const { rerender } = render( <AssetField numOfIssues={ 0 } /> );
+
+		expect( getToggleButton() ).toBeEnabled();
+		expect( getHelpButton() ).toBeEnabled();
+
+		rerender( <AssetField numOfIssues={ 0 } disabled /> );
+
+		expect( getToggleButton() ).toBeDisabled();
+		expect( getHelpButton() ).toBeDisabled();
+	} );
+
+	it( 'When clicking on the toggle button, it should toggle to show or hide the children', async () => {
+		render( <AssetField numOfIssues={ 0 }>Children</AssetField> );
+
+		expect( screen.queryByText( 'Children' ) ).not.toBeInTheDocument();
+
+		await userEvent.click( getToggleButton() );
+
+		expect( screen.getByText( 'Children' ) ).toBeInTheDocument();
+
+		await userEvent.click( getToggleButton() );
+
+		expect( screen.queryByText( 'Children' ) ).not.toBeInTheDocument();
+	} );
+} );
