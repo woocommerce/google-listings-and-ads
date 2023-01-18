83d77426d64b00b2a1288719e104ab54f982e37c
mikkamp <michael.kamp@automattic.com>
2023-01-18 08:03:38 +0000

    Improve prefix of autoloads

--- bin/prefix-vendor-namespace.php
+++ bin/prefix-vendor-namespace.php
@@ -102,25 +102,8 @@ foreach ( $packages as $package ) {
 		process_file( $file, $package, false, true );
 	}
 
-	// Update the namespace in the composer.json files, recursively finding all files named explicitly "composer.json".
-	$composer_files = get_dir_contents(
-		"{$vendor_dir}/{$package['package']}",
-		'/' . preg_quote( DIRECTORY_SEPARATOR, '/' ) . 'composer.json$/'
-	);
-
-	array_map(
-		function( $file ) use ( $package, $namespace_prefix ) {
-			return replace_in_json_file( $file, $package['namespace'] );
-		},
-		$composer_files
-	);
-
-	// Update the namespace in vendor/composer/installed.json
-	// This file is used to generate the classmaps.
-	replace_in_json_file( "{$vendor_dir}/composer/installed.json", $package['namespace'] );
-
-	// Remove file autoloads from vendor/composer/installed.json
-	remove_file_autoloads(
+	// Prefix autoloads and remove file autoloads from vendor/composer/installed.json
+	update_autoloads(
 		"{$vendor_dir}/composer/installed.json",
 		$composer_json['autoload']['files'] ?? [],
 		$package
@@ -149,11 +132,6 @@ function process_file( $file, $package, $prefix_namespace = true, $prefix_uses =
 
 	$contents     = file_get_contents( $file );
 	$content_hash = md5( $contents );
-
-	// Check to see whether a replacement has already run for this namespace. Just in case.
-	if ( false !== strpos( $contents, "{$namespace_prefix}\\{$package['namespace']}" ) ) {
-		return;
-	}
 
 	$namespace_change = 0;
 	$uses_change      = 0;
@@ -378,44 +356,22 @@ function find_dependent_files( string $package ): array {
 }
 
 /**
- * Replace namespace strings with a JSON file.
- *
- * @since 1.2.0
- *
- * @param string $file      Filename to replace the strings.
- * @param string $namespace Namespace to search for.
- */
-function replace_in_json_file( string $file, string $namespace ) {
-	global $namespace_prefix;
-
-	if ( ! file_exists( $file ) ) {
-		return;
-	}
-
-	$contents = file_get_contents( $file );
-	file_put_contents(
-		$file,
-		str_replace(
-			addslashes( "{$namespace}\\" ),
-			addslashes( "{$namespace_prefix}\\{$namespace}\\" ),
-			$contents
-		)
-	);
-}
-
-/**
  * Removes any autoload files from a package, and confirms they are loaded from the main composer.json file.
  * This ensures that the generated file vendor/composer/autoload_files.php will only autoload the files once,
  * using the new namespace. Autoloading the files from our main composer.json ensures we use a unique hash so
  * we don't conflict with other extensions autoloading the same files.
  *
- * @since 1.4.2
+ * The second task of this function is to prefix any autoloads with our custom namespace prefix.
+ *
+ * @since x.x.x
  *
  * @param string $file              Generated file containing information about all the installed packages
  * @param array  $composer_autoload List of autoloaded files in composer.json
  * @param array  $package           Package prefix configuration.
  */
-function remove_file_autoloads( string $file, array $composer_autoload, array $package ) {
+function update_autoloads( string $file, array $composer_autoload, array $package ) {
+	global $namespace_prefix;
+
 	if ( ! file_exists( $file ) ) {
 		return;
 	}
@@ -427,36 +383,57 @@ function remove_file_autoloads( string $file, array $composer_autoload, array $p
 
 	$modified = false;
 	foreach ( $json['packages'] as $key => $dep_package ) {
-		if ( $package['package'] !== $dep_package['name'] ) {
+
+		// If strict confirm that full package name matches.
+		if ( $package['strict'] && $dep_package['name'] !== $package['package'] ) {
 			continue;
 		}
 
-		if ( empty( $dep_package['autoload']['files'] ) ) {
+		// Check if start of package name matches.
+		if ( 0 !== stripos( $dep_package['name'], $package['package'] ) ) {
 			continue;
 		}
 
-		foreach ( $dep_package['autoload']['files'] as $autoload_file ) {
-
-			// Confirm we already include this autoload in the main composer file.
-			$filename = "vendor/{$dep_package['name']}/{$autoload_file}";
-			if ( in_array( $filename, $composer_autoload, true ) ) {
-				continue;
-			}
-
-			// Confirm this file isn't being excluded.
-			if ( in_array( $autoload_file, $package['exclude_autoload'], true ) ) {
-				continue;
-			}
-
-			printf(
-				'Autoloaded file "%s" should be included in composer.json' . PHP_EOL,
-				$filename
-			);
-			exit( 1 );
+		if ( empty( $dep_package['autoload'] ) ) {
+			continue;
 		}
 
-		$json['packages'][ $key ]['autoload']['files'] = [];
-		$modified = true;
+		// Remove any file autoloads and ensure they are included in the main composer file
+		if ( ! empty( $dep_package['autoload']['files'] ) ) {
+			foreach ( $dep_package['autoload']['files'] as $autoload_file ) {
+
+				// Confirm we already include this autoload in the main composer file.
+				$filename = "vendor/{$dep_package['name']}/{$autoload_file}";
+				if ( in_array( $filename, $composer_autoload, true ) ) {
+					continue;
+				}
+
+				// Confirm this file isn't being excluded.
+				if ( in_array( $autoload_file, $package['exclude_autoload'], true ) ) {
+					continue;
+				}
+
+				printf(
+					'Autoloaded file "%s" should be included in composer.json' . PHP_EOL,
+					$filename
+				);
+				exit( 1 );
+			}
+
+			$json['packages'][ $key ]['autoload']['files'] = [];
+			$modified = true;
+		}
+
+		// Prefix any of the autoloads for this specific package.
+		foreach ( $json['packages'][ $key ]['autoload'] as $type => $mappings ) {
+			foreach ( $mappings as $namespace => $path ) {
+				if ( 0 === stripos( (string) $namespace, $package['namespace'] ) ) {
+					unset( $json['packages'][ $key ]['autoload'][ $type ][ $namespace ] );
+					$json['packages'][ $key ]['autoload'][ $type ][ "{$namespace_prefix}\\{$namespace}" ] = $path;
+					$modified = true;
+				}
+			}
+		}
 	}
 
 	if ( $modified ) {
