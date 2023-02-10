e2878dd3bb1a3fd9a137f534764a9ee4e9358606
Eason Su <eason.su.tw@gmail.com>
2023-02-10 16:14:45 +0800

    Fix the uncaught error occurred when querying the non-existing tour in the `getTour` resolver.
    Address https://github.com/woocommerce/google-listings-and-ads/pull/1752#discussion_r1101082488

--- js/src/data/resolvers.js
+++ js/src/data/resolvers.js
@@ -17,7 +17,7 @@ import TYPES from './action-types';
 import { API_NAMESPACE } from './constants';
 import { getReportKey } from './utils';
 import { adaptAdsCampaign } from './adapters';
-import { fetchWithHeaders } from './controls';
+import { fetchWithHeaders, awaitPromise } from './controls';
 
 import {
 	handleFetchError,
@@ -470,11 +470,30 @@ export function* getStoreCategories() {
  * @param {string} tourId The tour to get
  */
 export function* getTour( tourId ) {
-	const response = yield apiFetch( {
-		path: `${ API_NAMESPACE }/tours/${ tourId }`,
-	} );
+	try {
+		const { data } = yield fetchWithHeaders( {
+			path: `${ API_NAMESPACE }/tours/${ tourId }`,
+		} );
 
-	yield receiveTour( response );
+		yield receiveTour( data );
+	} catch ( response ) {
+		// Intentionally silence the specific error since the tour API will respond with
+		// a 404 error if the querying tour ID doesn't exist.
+		if ( response.status === 404 ) {
+			return;
+		}
+
+		const bodyPromise = response?.json() || response?.text();
+		const error = yield awaitPromise( bodyPromise );
+
+		yield handleFetchError(
+			error,
+			__(
+				'There was an error getting the tour.',
+				'google-listings-and-ads'
+			)
+		);
+	}
 }
 
 /**
