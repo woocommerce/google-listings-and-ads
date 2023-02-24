d32d1d72fc1283d67fc540cecdd0907553203b30
Eason Su <eason.su.tw@gmail.com>
2023-02-24 11:11:32 +0800

    Make `Tooltip` handle the close of tooltip properly in `AppTooltip`.

--- js/src/components/app-tooltip/index.js
+++ js/src/components/app-tooltip/index.js
@@ -1,9 +1,8 @@
 /**
  * External dependencies
  */
-import classnames from 'classnames';
 import { Tooltip } from 'extracted/@wordpress/components';
-import { Children, isValidElement } from '@wordpress/element';
+import { Children } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -27,30 +26,19 @@ import './index.scss';
  */
 const AppTooltip = ( props ) => {
 	const { children, ...rest } = props;
-	const containerClasses = [ 'gla-tooltip__children-container' ];
 
-	// `Tooltip` component won't attach an `onMouseLeave` handler to a disabled child to
-	// hide the popover. It may result in leaving the popover still open after the mouse
-	// moves out of the disabled child.
+	// `Tooltip` component will create an event catcher floating upon the child when it finds
+	// the child is disabled. Therefore, this component needs to forward the `disabled` prop
+	// of the actual child to the wrapper <div> in order to make `Tooltip` able to catch the
+	// `onMouseLeave` event and close the popover properly.
 	//
-	// Ref: https://github.com/WordPress/gutenberg/blob/%40wordpress/components%4019.17.0/packages/components/src/tooltip/index.js#L193-L201
-	//
-	// Although this component wraps a <div> as the passed child to make `Tooltip` bind
-	// the `onMouseLeave` handler to the wrapper <div> instead, a disabled child element
-	// such as <button> still won't fire the `onMouseLeave` event nor further propagate
-	// to the wrapper.
-	//
-	// Therefore, here uses a trick that adds an additional placeholder to float upon and
-	// cover the whole disabled child, so that it will be possible to directly trigger the
-	// `onMouseLeave` event from the wrapper.
+	// Ref:
+	// - https://github.com/WordPress/gutenberg/blob/%40wordpress/components%4019.17.0/packages/components/src/tooltip/index.js#L253-L257
+	// - https://github.com/WordPress/gutenberg/blob/%40wordpress/components%4019.17.0/packages/components/src/tooltip/index.js#L35-L51
+	let childDisabled;
 	const renderingChildren = Children.toArray( children );
 	if ( renderingChildren.length === 1 ) {
-		const child = renderingChildren[ 0 ];
-
-		if ( isValidElement( child ) && child.props.disabled ) {
-			const baseClass = containerClasses[ 0 ];
-			containerClasses.push( `${ baseClass }--hover-placeholder` );
-		}
+		childDisabled = renderingChildren[ 0 ].props?.disabled;
 	}
 
 	return (
@@ -59,7 +47,12 @@ const AppTooltip = ( props ) => {
 			This inline-block div is needed for the tooltip to show up correctly.
 			If we use span, the tooltip will not wrap ToggleControl nicely.
 			*/ }
-			<div className={ classnames( containerClasses ) }>{ children }</div>
+			<div
+				className="gla-tooltip__children-container"
+				disabled={ childDisabled }
+			>
+				{ children }
+			</div>
 		</Tooltip>
 	);
 };
