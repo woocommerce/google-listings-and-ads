f763449a5b47efffe9b3ddd3a9a12dc711563a56
Eason Su <eason.su.tw@gmail.com>
2023-07-25 11:22:24 +0800

    Change to adopt the force update approach as proposed in the React official document for `AppInputNumberControl`
    Address https://github.com/woocommerce/google-listings-and-ads/pull/2019#discussion_r1272059836

--- js/src/components/app-input-number-control/index.js
+++ js/src/components/app-input-number-control/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useState } from '@wordpress/element';
+import { useReducer } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -44,13 +44,13 @@ const AppInputNumberControl = ( props ) => {
 	// The `InputControl` in `@wordpress/components` doesn't take into account the context
 	// in which the `value` of a controlled component is updated via `onBlur`, resulting in
 	// the incoming `value` prop may not syncing to the <input> element's display value.
-	// Therefore, a synchronization needle is used to make the value synchronized via the
+	// Therefore, an updater of `useReducer` is used to make the value synchronized via the
 	// onChange callback.
 	//
 	// Ref:
 	// - https://github.com/WordPress/gutenberg/blob/%40wordpress/components%4019.17.0/packages/components/src/input-control/index.tsx#L59-L62
 	// - https://github.com/WordPress/gutenberg/blob/%40wordpress/components%4019.17.0/packages/components/src/input-control/utils.ts#L71-L108
-	const [ , setSyncNeedle ] = useState( 0 );
+	const [ , forceUpdate ] = useReducer( ( x ) => x + 1, 0 );
 
 	/**
 	 * Value to be displayed to the user in the UI.
@@ -90,7 +90,7 @@ const AppInputNumberControl = ( props ) => {
 	const handleChange = ( val ) => {
 		const numberValue = getNumberFromString( val );
 		onChange( numberValue );
-		setSyncNeedle( ( count ) => count + 1 );
+		forceUpdate();
 	};
 
 	const handleBlur = ( e ) => {
