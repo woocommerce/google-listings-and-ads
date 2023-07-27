29f2e3695cd64b23762c0eb0c05e5daa0d98a815
Tomek Wytrębowicz <tomalecpub@gmail.com>
2023-07-27 16:05:55 +0200

    Add tests for register and content methods.

--- tests/Unit/TaskList/CompleteSetupTaskTest.php
+++ tests/Unit/TaskList/CompleteSetupTaskTest.php
@@ -3,6 +3,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\tasks;
 
+use Automattic\WooCommerce\Admin\Features\OnboardingTasks\TaskLists;
 use Automattic\WooCommerce\GoogleListingsAndAds\MerchantCenter\MerchantCenterService;
 use Automattic\WooCommerce\GoogleListingsAndAds\TaskList\CompleteSetupTask;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\UnitTest;
@@ -28,10 +29,28 @@ class CompleteSetupTaskTest extends UnitTest {
 	 */
 	public function setUp(): void {
 		parent::setUp();
+		// Mock tasks list.
+		TaskLists::clear_lists();
+		TaskLists::add_list( [ 'id' => 'extended' ] );
+		// Mock MC setup.
 		$this->merchant_center = $this->createMock( MerchantCenterService::class );
 
 		$this->task = new CompleteSetupTask();
 		$this->task->set_merchant_center_object( $this->merchant_center );
+	}
+
+	/**
+	 * Test that after the task is registered and `init` fired,
+	 * the task is added to the `extended` list and the `add_woocommerce_extended_task_list_item` action is called.
+	 */
+	public function test_register() {
+		$this->task->register();
+		do_action( 'init', 1 );
+
+		$this->assertGreaterThan( 0, did_action( 'add_woocommerce_extended_task_list_item' ) );
+
+		$this->assertNotNull( TaskLists::get_list( 'extended' )->get_task( $this->task->get_id() ) );
+		$this->assertEquals( $this->task, TaskLists::get_list( 'extended' )->get_task( $this->task->get_id() ) );
 	}
 
 	public function test_id() {
@@ -42,9 +61,14 @@ class CompleteSetupTaskTest extends UnitTest {
 		$this->assertEquals( 'Set up Google Listings & Ads', $this->task->get_title() );
 	}
 
+	public function test_content() {
+		$this->assertEquals( '', $this->task->get_content() );
+	}
+
 	public function test_time() {
 		$this->assertEquals( '20 minutes', $this->task->get_time() );
 	}
+
 	public function test_dismissable() {
 		$this->assertTrue( $this->task->is_dismissable() );
 	}
