1b7b5b2ba4e1157ee6fbed56656f2ef170a39c58
mikkamp <michael.kamp@automattic.com>
2023-08-03 10:41:22 +0100

    Turn off parallel processing

--- tests/e2e/config/playwright.config.js
+++ tests/e2e/config/playwright.config.js
@@ -15,17 +15,11 @@ module.exports = defineConfig( {
 		timeout: 20 * 1000,
 	},
 
-	/* Run tests in files in parallel */
-	fullyParallel: true,
-
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !! process.env.CI,
 
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 4 : undefined,
 
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
