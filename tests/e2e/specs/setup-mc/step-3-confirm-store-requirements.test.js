b8e35c9b0713b5263b61962db1e96f667a3cc9bc
Ian Yu-Hsun Lin <ianlin@automattic.com>
2023-09-11 14:58:13 +0800

    Add e2e tests for onboarding step 3 phone verification

---
+++ tests/e2e/specs/setup-mc/step-3-confirm-store-requirements.test.js
@@ -0,0 +1,173 @@
+/**
+ * External dependencies
+ */
+import { parsePhoneNumberFromString as parsePhoneNumber } from 'libphonenumber-js';
+const { test, expect } = require( '@playwright/test' );
+
+/**
+ * Internal dependencies
+ */
+import StoreRequirements from '../../utils/pages/setup-mc/step-3-confirm-store-requirements';
+
+test.use( { storageState: process.env.ADMINSTATE } );
+
+test.describe.configure( { mode: 'serial' } );
+
+/**
+ * @type {import('../../utils/pages/setup-mc/step-1-set-up-accounts.js').default} storeRequirements
+ */
+let storeRequirements = null;
+
+/**
+ * @type {import('@playwright/test').Page} page
+ */
+let page = null;
+
+test.describe( 'Confirm store requirements', () => {
+	test.beforeAll( async ( { browser } ) => {
+		page = await browser.newPage();
+		storeRequirements = new StoreRequirements( page );
+		await Promise.all( [
+			// Mock Jetpack as connected
+			storeRequirements.mockJetpackConnected(),
+
+			// Mock google as connected.
+			storeRequirements.mockGoogleConnected(),
+
+			// Mock Merchant Center as connected
+			storeRequirements.mockMCConnected(),
+
+			// Mock MC step as store_requirements
+			storeRequirements.mockMCSetup( 'incomplete', 'store_requirements' ),
+
+			// Mock MC contact information
+			storeRequirements.mockContactInformation(),
+		] );
+	} );
+
+	test.afterAll( async () => {
+		await storeRequirements.closePage();
+	} );
+
+	test( 'should see the heading and the texts below', async () => {
+		await storeRequirements.goto();
+
+		await expect(
+			page.getByRole( 'heading', {
+				name: 'Confirm store requirements',
+			} )
+		).toBeVisible();
+
+		await expect(
+			page.getByText(
+				'Review and confirm that your store meets Google Merchant Center requirements.'
+			)
+		).toBeVisible();
+	} );
+
+	test.describe( 'Phone verification', () => {
+		test.beforeAll( async () => {
+			await storeRequirements.fulfillPhoneVerificationRequest( {
+				verification_id: 'abc-123-def',
+			} );
+		} );
+
+		test( 'should see the correct texts in phone verification card', async () => {
+			const phoneNumberDescriptionRow =
+				storeRequirements.getPhoneNumberDescriptionRow();
+			await expect( phoneNumberDescriptionRow ).toContainText(
+				'Please enter a phone number to be used for verification.'
+			);
+		} );
+
+		test( 'should see the "Send verfication code" button to be disabled', async () => {
+			const sendCodeButton =
+				storeRequirements.getSendVerificationCodeButton();
+			await expect( sendCodeButton ).toBeDisabled();
+		} );
+
+		test( 'should see the "Send verfication code" button to be disabled when enter a phone number with wrong format', async () => {
+			const sendCodeButton =
+				storeRequirements.getSendVerificationCodeButton();
+			await storeRequirements.selectCountryCodeOption(
+				'United States (US) (+1)'
+			);
+			await expect( sendCodeButton ).toBeDisabled();
+			await storeRequirements.fillPhoneNumber( '9999999999' );
+			await expect( sendCodeButton ).toBeDisabled();
+		} );
+
+		test( 'should see the "Send verfication code" button to be enabled when country code and phone number are entered', async () => {
+			const sendCodeButton =
+				storeRequirements.getSendVerificationCodeButton();
+			await storeRequirements.selectCountryCodeOption(
+				'United States (US) (+1)'
+			);
+			await expect( sendCodeButton ).toBeDisabled();
+			await storeRequirements.fillPhoneNumber( '8888888888' );
+			await expect( sendCodeButton ).toBeEnabled();
+		} );
+
+		test( 'should see "Verify phone number" button is disabled after clicking "Send verification code" button', async () => {
+			await storeRequirements.clickSendVerificationCodeButton();
+			const verifyNumberButton =
+				storeRequirements.getVerifyPhoneNumberButton();
+			await expect( verifyNumberButton ).toBeDisabled();
+		} );
+
+		test( 'should see "Verify phone number" button is enabled after entering the verification code', async () => {
+			await storeRequirements.fillVerificationCode();
+			const verifyNumberButton =
+				storeRequirements.getVerifyPhoneNumberButton();
+			await expect( verifyNumberButton ).toBeEnabled();
+		} );
+
+		test( 'should see an error notice after entering wrong verification code', async () => {
+			// Mock phone verification request as failed
+			await storeRequirements.fulfillPhoneVerificationVerifyRequest(
+				{
+					message: '[verificationCode] Wrong code.',
+					reason: 'badRequest',
+				},
+				400
+			);
+			await storeRequirements.clickVerifyPhoneNumberButon();
+
+			const errorNotice =
+				storeRequirements.getPhoneNumberErrorNotice();
+			await expect( errorNotice ).toContainText(
+				'Wrong verification code. Please try again.'
+			);
+		} );
+
+		test( 'should see phone number and the "Edit" button after entering correct verification code', async () => {
+			// Mock MC contact information
+			await storeRequirements.mockContactInformation( {
+				phoneNumber: '+18888888888',
+				phoneVerificationStatus: 'verified',
+			} );
+
+			// Mock phone verification request as successful
+			await storeRequirements.fulfillPhoneVerificationVerifyRequest(
+				null,
+				204
+			);
+
+			await storeRequirements.fillVerificationCode( '654321' );
+			await storeRequirements.clickVerifyPhoneNumberButon();
+
+			const parsedNumber = parsePhoneNumber( '8888888888', 'US' );
+			const expectedNumber = parsedNumber.formatInternational();
+
+			const phoneNumberDescriptionRow =
+				storeRequirements.getPhoneNumberDescriptionRow();
+			await expect( phoneNumberDescriptionRow ).toContainText(
+				expectedNumber
+			);
+
+			const phoneNumberEditButton =
+				storeRequirements.getPhoneNumberEditButton();
+			await expect( phoneNumberEditButton ).toBeVisible();
+		} );
+	} );
+} );
---
+++ tests/e2e/utils/pages/setup-mc/step-3-confirm-store-requirements.js
@@ -0,0 +1,254 @@
+/**
+ * Internal dependencies
+ */
+import { LOAD_STATE } from '../../constants';
+import MockRequests from '../../mock-requests';
+
+/**
+ * Configure product listings page object class.
+ */
+export default class StoreRequirements extends MockRequests {
+	/**
+	 * @param {import('@playwright/test').Page} page
+	 */
+	constructor( page ) {
+		super( page );
+		this.page = page;
+	}
+
+	/**
+	 * Close the current page.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async closePage() {
+		await this.page.close();
+	}
+
+	/**
+	 * Go to the set up mc page.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async goto() {
+		await this.page.goto(
+			'/wp-admin/admin.php?page=wc-admin&path=%2Fgoogle%2Fsetup-mc&google-mc=connected',
+			{ waitUntil: LOAD_STATE.NETWORK_IDLE }
+		);
+	}
+
+	/**
+	 * Get components-card class.
+	 *
+	 * @return {import('@playwright/test').Locator} Get components-card class.
+	 */
+	getComponentsCard() {
+		return this.page.locator( '.components-card' );
+	}
+
+	/**
+	 * Get phone verification card.
+	 *
+	 * @return {import('@playwright/test').Locator} Get phone verification card.
+	 */
+	getPhoneVerificationCard() {
+		return this.getComponentsCard().first();
+	}
+
+	/**
+	 * Get store address card.
+	 *
+	 * @return {import('@playwright/test').Locator} Get store address card.
+	 */
+	getStoreAddressCard() {
+		return this.getComponentsCard().nth( 1 );
+	}
+
+	/**
+	 * Get checklist card.
+	 *
+	 * @return {import('@playwright/test').Locator} Get checklist card.
+	 */
+	getChecklistCard() {
+		return this.getComponentsCard().nth( 2 );
+	}
+
+	/**
+	 * Get phone number description text.
+	 *
+	 * @return {import('@playwright/test').Locator} Get checklist card.
+	 */
+	getPhoneNumberDescriptionRow() {
+		return this.getPhoneVerificationCard().locator(
+			'.gla-account-card__description'
+		);
+	}
+
+	/**
+	 * Get "Send verification code" button.
+	 *
+	 * @return {import('@playwright/test').Locator} Get "Send verification code" button.
+	 */
+	getSendVerificationCodeButton() {
+		return this.getPhoneVerificationCard().getByRole( 'button', {
+			name: 'Send verification code',
+			exact: true,
+		} );
+	}
+
+	/**
+	 * Get "Verify phone number" button.
+	 *
+	 * @return {import('@playwright/test').Locator} Get "Verify phone number" button.
+	 */
+	getVerifyPhoneNumberButton() {
+		return this.getPhoneVerificationCard().getByRole( 'button', {
+			name: 'Verify phone number',
+			exact: true,
+		} );
+	}
+
+	/**
+	 * Get country code input box.
+	 *
+	 * @return {import('@playwright/test').Locator} Get country code input box.
+	 */
+	getCountryCodeInputBox() {
+		return this.getPhoneVerificationCard().locator(
+			'input[id*="woocommerce-select-control"]'
+		);
+	}
+
+	/**
+	 * Get phone number input box.
+	 *
+	 * @return {import('@playwright/test').Locator} Get phone number input box.
+	 */
+	getPhoneNumberInputBox() {
+		return this.getPhoneVerificationCard().locator(
+			'input[id*="inspector-input-control"]'
+		);
+	}
+
+	/**
+	 * Get verification code input boxes.
+	 *
+	 * @return {import('@playwright/test').Locator} Get verification code input box.
+	 */
+	getVerificationCodeInputBoxes() {
+		return this.getPhoneVerificationCard().locator(
+			'.wcdl-subsection .app-input-control input'
+		);
+	}
+
+	/**
+	 * Get phone number error notice.
+	 *
+	 * @return {import('@playwright/test').Locator} Get phone number error notice.
+	 */
+	getPhoneNumberErrorNotice() {
+		return this.getPhoneVerificationCard().locator(
+			'.components-notice.is-error'
+		);
+	}
+
+	/**
+	 * Get phone number edit button.
+	 *
+	 * @return {import('@playwright/test').Locator} Get phone number edit button.
+	 */
+	getPhoneNumberEditButton() {
+		return this.getPhoneVerificationCard().getByRole( 'button', {
+			name: 'Edit',
+			exact: true,
+		} );
+	}
+
+	/**
+	 * Fill country code.
+	 *
+	 * @param {string} code
+	 *
+	 * @return {Promise<void>}
+	 */
+	async fillCountryCode( code = 'United States (US) (+1)' ) {
+		const countryCodeInputBox = this.getCountryCodeInputBox();
+		await countryCodeInputBox.fill( code );
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+
+	/**
+	 * Select country code option.
+	 *
+	 * @param {string} code
+	 *
+	 * @return {Promise<void>}
+	 */
+	async selectCountryCodeOption( code = 'United States (US) (+1)' ) {
+		await this.fillCountryCode( code );
+		const countryCodeOption = this.page.getByRole( 'option', {
+			name: code,
+		} );
+		await countryCodeOption.click();
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+
+	/**
+	 * Fill phone number.
+	 *
+	 * @param {string} number
+	 *
+	 * @return {Promise<void>}
+	 */
+	async fillPhoneNumber( number = '8888888888' ) {
+		const phoneNumberInputBox = this.getPhoneNumberInputBox();
+		await phoneNumberInputBox.fill( number );
+
+		// A hack to finish typing in the input box, similar to pressing anywhere in the page.
+		await phoneNumberInputBox.press( 'Tab' );
+
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+
+	/**
+	 * Click send verification code button.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async clickSendVerificationCodeButton() {
+		const sendCodeButton = this.getSendVerificationCodeButton();
+		await sendCodeButton.click();
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+
+	/**
+	 * Fill verification code.
+	 *
+	 * @param {string} code
+	 *
+	 * @return {Promise<void>}
+	 */
+	async fillVerificationCode( code = '123456' ) {
+		const verificationInputBoxes = this.getVerificationCodeInputBoxes();
+		const count = await verificationInputBoxes.count();
+
+		for ( let i = 0; i < count; i++ ) {
+			const input = await verificationInputBoxes.nth( i );
+			const digit = code[ i ];
+			await input.fill( digit );
+		}
+
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+
+	/**
+	 * Click verify phone number button.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async clickVerifyPhoneNumberButon() {
+		const verifyPhoneNumberButton = this.getVerifyPhoneNumberButton();
+		await verifyPhoneNumberButton.click();
+		await this.page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
+	}
+}
