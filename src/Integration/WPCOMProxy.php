dc1fcb640e4e33c16154d858f3e10ddb562c6ed5
Jorge M <jorge.mahecha@a8c.com>
2023-11-10 01:15:22 +0100

    Rename file

--- src/Integration/WPCOMProxy.php
+++ src/Integration/WPCOMProxy.php
@@ -5,19 +5,19 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\Integration;
 
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Registerable;
 use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Service;
-use Automattic\WooCommerce\GoogleListingsAndAds\Infrastructure\Conditional;
 use WP_REST_Response;
+use WP_REST_Request;
 
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Class WooCoommerceProxy
+ * Class WPCOMProxy
  *
- * Initializes the hooks to filter the data sent to the proxy depending on the query parameter gla_syncable.
+ * Initializes the hooks to filter the data sent to the WPCOM proxy depending on the query parameter gla_syncable.
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\Integration
  */
-class WooCoommerceProxy implements Service, Registerable, Conditional {
+class WPCOMProxy implements Service, Registerable {
 
 	/**
 	 * The Post types to be filtered.
@@ -53,41 +53,10 @@ class WooCoommerceProxy implements Service, Registerable, Conditional {
 	];
 
 	/**
-	 * Check if the request is a API request.
-	 *
-	 * @return bool Whether the class is needed.
-	 */
-	public static function is_needed(): bool {
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		return self::is_request_to_rest_api() && isset( $_GET['gla_syncable'] );
-	}
-
-	/**
-	 * Check if is request to the WC REST API.
-	 *
-	 * This function is based on the WooCommerce function is_rest_api_request() but also checks for rest_route=/wc/ regardless the permalink settings.
-	 *
-	 * @see https://github.com/woocommerce/woocommerce/blob/bae3a34501047bfdff4d79c240396f38cdcd1f25/plugins/woocommerce/includes/class-wc-rest-authentication.php#L62
-	 * @see https://github.com/woocommerce/woocommerce/issues/41191
-	 *
-	 * @return bool Whether the request is to the WC REST API.
-	 */
-	public static function is_request_to_rest_api(): bool {
-		if ( empty( $_SERVER['REQUEST_URI'] ) ) {
-			return false;
-		}
-
-		$rest_prefix = trailingslashit( rest_get_url_prefix() );
-		$request_uri = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
-
-		// Check if the request is to the WC API endpoints.
-		return ( false !== strpos( $request_uri, $rest_prefix . 'wc/' ) ) || ( false !== strpos( $request_uri, 'rest_route=/wc/' ) );
-	}
-
-	/**
 	 * Register all filters.
 	 */
 	public function register(): void {
+		// Allow to filter by gla_syncable.
 		add_filter(
 			'woocommerce_rest_query_vars',
 			function( $valid_vars ) {
@@ -127,6 +96,17 @@ class WooCoommerceProxy implements Service, Registerable, Conditional {
 	}
 
 	/**
+	 * Whether the data should be filtered.
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 *
+	 * @return bool
+	 */
+	protected function should_filter_data( WP_REST_Request $request ): bool {
+		return $request->get_param( 'gla_syncable' ) !== null;
+	}
+
+	/**
 	 * Check if a single item is syncable.
 	 *
 	 * @param WP_REST_Response $response The response object.
@@ -135,9 +115,13 @@ class WooCoommerceProxy implements Service, Registerable, Conditional {
 	 *
 	 * @return WP_REST_Response The response object updated.
 	 */
-	public function check_item_is_syncable( $response, $object, $request ): WP_REST_Response {
+	public function check_item_is_syncable( $response, $object, WP_REST_Request $request ): WP_REST_Response {
+		if ( ! $this->should_filter_data( $request ) ) {
+			return $response;
+		}
+
 		$route               = $request->get_route();
-		$pattern             = '/(?P<resource>[\w]+)\/(?P<id>[\d]+)/';
+		$pattern             = '/(?P<resource>[\w]+)\/(?P<id>[\d]+$)/';
 		$protected_resources = [
 			'products',
 			'coupons',
@@ -172,11 +156,16 @@ class WooCoommerceProxy implements Service, Registerable, Conditional {
 	/**
 	 * Query items with specific args for example where _wc_gla_visibility is set to sync-and-show.
 	 *
-	 * @param array $args The query args.
+	 * @param array           $args The query args.
+	 * @param WP_REST_Request $request The request object.
 	 *
 	 * @return array The query args updated.
 	 * */
-	public function filter_by_metaquery( array $args ): array {
+	public function filter_by_metaquery( array $args, WP_REST_Request $request ): array {
+		if ( ! $this->should_filter_data( $request ) ) {
+			return $args;
+		}
+
 		$post_type         = $args['post_type'];
 		$post_type_filters = self::$post_types_to_be_filter[ $post_type ];
 
@@ -193,10 +182,16 @@ class WooCoommerceProxy implements Service, Registerable, Conditional {
 	 * Filter the metadata of an object.
 	 *
 	 * @param WP_REST_Response $response The response object.
+	 * @param mixed            $object   The object.
+	 * @param WP_REST_Request  $request  The request object.
 	 *
 	 * @return WP_REST_Response The response object updated.
 	 */
-	public function filter_metadata( WP_REST_Response $response ): WP_REST_Response {
+	public function filter_metadata( WP_REST_Response $response, $object, WP_REST_Request $request ): WP_REST_Response {
+		if ( ! $this->should_filter_data( $request ) ) {
+			return $response;
+		}
+
 		$data = $response->get_data();
 
 		if ( ! isset( $data['meta_data'] ) ) {
