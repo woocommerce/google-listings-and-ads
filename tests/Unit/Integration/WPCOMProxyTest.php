391b84836cfc81c4c5ac6a87b5dd6ccbcfe2234b
Jorge M <jorge.mahecha@a8c.com>
2023-11-14 11:09:21 +0100

    Use the ChannelVisibility constants

--- tests/Unit/Integration/WPCOMProxyTest.php
+++ tests/Unit/Integration/WPCOMProxyTest.php
@@ -5,6 +5,8 @@ namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\API\Site\Contro
 use Automattic\WooCommerce\RestApi\UnitTests\Helpers\CouponHelper;
 use Automattic\WooCommerce\RestApi\UnitTests\Helpers\ProductHelper;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\RESTControllerUnitTest;
+use Automattic\WooCommerce\GoogleListingsAndAds\Value\ChannelVisibility;
+use Automattic\WooCommerce\GoogleListingsAndAds\Integration\WPCOMProxy;
 use WC_Meta_Data;
 use WP_REST_Response;
 
@@ -46,14 +48,14 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 	 *
 	 * @return array
 	 */
-	protected function get_test_metadata( $visibility = 'sync-and-show' ): array {
+	protected function get_test_metadata( $visibility = ChannelVisibility::SYNC_AND_SHOW ): array {
 		$args = [
 			'_private_meta' => 'private',
 			'public_meta'   => 'public',
 		];
 
 		if ( $visibility ) {
-			$args['_wc_gla_visibility'] = $visibility;
+			$args[ WPCOMProxy::KEY_VISIBILITY ] = $visibility;
 		}
 		return $args;
 	}
@@ -94,7 +96,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$product_2 = ProductHelper::create_simple_product();
 
 		$this->add_metadata( $product_1->get_id(), $this->get_test_metadata() );
-		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( 'dont-sync-and-show' ) );
+		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ) );
 
 		$response = $this->do_request( '/wc/v3/products', 'GET', [ 'gla_syncable' => '1' ] );
 
@@ -102,8 +104,8 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertCount( 1, $response->get_data() );
 
 		$expected_metadata = [
-			'public_meta'        => 'public',
-			'_wc_gla_visibility' => 'sync-and-show',
+			'public_meta'              => 'public',
+			WPCOMProxy::KEY_VISIBILITY => ChannelVisibility::SYNC_AND_SHOW,
 		];
 
 		$this->assertEquals( $product_1->get_id(), $response->get_data()[0]['id'] );
@@ -115,7 +117,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$product_2 = ProductHelper::create_simple_product();
 
 		$this->add_metadata( $product_1->get_id(), $this->get_test_metadata() );
-		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( 'dont-sync-and-show' ) );
+		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ) );
 
 		$response = $this->do_request( '/wc/v3/products', 'GET', [ 'gla_syncable' => '0' ] );
 
@@ -128,7 +130,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertArrayHasKey( $product_2->get_id(), $response_mapped );
 
 		$this->assertEquals( $this->get_test_metadata(), $this->format_metadata( $response_mapped[ $product_1->get_id() ]['meta_data'] ) );
-		$this->assertEquals( $this->get_test_metadata( 'dont-sync-and-show' ), $this->format_metadata( $response_mapped[ $product_2->get_id() ]['meta_data'] ) );
+		$this->assertEquals( $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ), $this->format_metadata( $response_mapped[ $product_2->get_id() ]['meta_data'] ) );
 	}
 
 	public function test_get_products_without_gla_visibility_metadata() {
@@ -144,8 +146,8 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertCount( 1, $response->get_data() );
 
 		$expected_metadata = [
-			'public_meta'        => 'public',
-			'_wc_gla_visibility' => 'sync-and-show',
+			'public_meta'              => 'public',
+			WPCOMProxy::KEY_VISIBILITY => ChannelVisibility::SYNC_AND_SHOW,
 		];
 
 		$this->assertEquals( $product_2->get_id(), $response->get_data()[0]['id'] );
@@ -191,7 +193,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$product_1 = ProductHelper::create_simple_product();
 		$product_2 = ProductHelper::create_simple_product();
 		$this->add_metadata( $product_1->get_id(), $this->get_test_metadata() );
-		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( 'dont-sync-and-show' ) );
+		$this->add_metadata( $product_2->get_id(), $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ) );
 
 		$response = $this->do_request( '/wc/v3/products', 'GET' );
 
@@ -204,7 +206,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertArrayHasKey( $product_2->get_id(), $response_mapped );
 
 		$this->assertEquals( $this->get_test_metadata(), $this->format_metadata( $response_mapped[ $product_1->get_id() ]['meta_data'] ) );
-		$this->assertEquals( $this->get_test_metadata( 'dont-sync-and-show' ), $this->format_metadata( $response_mapped[ $product_2->get_id() ]['meta_data'] ) );
+		$this->assertEquals( $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ), $this->format_metadata( $response_mapped[ $product_2->get_id() ]['meta_data'] ) );
 	}
 
 	public function test_get_variations() {
@@ -262,7 +264,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 
 	public function test_get_coupons() {
 		$coupon_1 = CouponHelper::create_coupon( 'dummycoupon-1', 'publish', $this->get_test_metadata() );
-		$coupon_2 = CouponHelper::create_coupon( 'dummycoupon-2', 'publish', $this->get_test_metadata( 'dont-sync-and-show' ) );
+		$coupon_2 = CouponHelper::create_coupon( 'dummycoupon-2', 'publish', $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ) );
 
 		delete_post_meta( $coupon_1->get_id(), 'customer_email' );
 		delete_post_meta( $coupon_2->get_id(), 'customer_email' );
@@ -273,8 +275,8 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertCount( 1, $response->get_data() );
 
 		$expected_metadata = [
-			'public_meta'        => 'public',
-			'_wc_gla_visibility' => 'sync-and-show',
+			'public_meta'              => 'public',
+			WPCOMProxy::KEY_VISIBILITY => ChannelVisibility::SYNC_AND_SHOW,
 		];
 
 		$this->assertEquals( $coupon_1->get_id(), $response->get_data()[0]['id'] );
@@ -294,8 +296,8 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertCount( 1, $response->get_data() );
 
 		$expected_metadata = [
-			'public_meta'        => 'public',
-			'_wc_gla_visibility' => 'sync-and-show',
+			'public_meta'              => 'public',
+			WPCOMProxy::KEY_VISIBILITY => ChannelVisibility::SYNC_AND_SHOW,
 		];
 
 		$this->assertEquals( $coupon->get_id(), $response->get_data()[0]['id'] );
@@ -315,8 +317,8 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 		$this->assertCount( 1, $response->get_data() );
 
 		$expected_metadata = [
-			'public_meta'        => 'public',
-			'_wc_gla_visibility' => 'sync-and-show',
+			'public_meta'              => 'public',
+			WPCOMProxy::KEY_VISIBILITY => ChannelVisibility::SYNC_AND_SHOW,
 		];
 
 		$this->assertEquals( $coupon->get_id(), $response->get_data()[0]['id'] );
@@ -358,7 +360,7 @@ class WPCOMProxyTest extends RESTControllerUnitTest {
 
 	public function test_get_coupons_without_gla_syncable_param() {
 		$coupon_1 = CouponHelper::create_coupon( 'dummycoupon-1', 'publish', $this->get_test_metadata() );
-		$coupon_2 = CouponHelper::create_coupon( 'dummycoupon-2', 'publish', $this->get_test_metadata( 'dont-sync-and-show' ) );
+		$coupon_2 = CouponHelper::create_coupon( 'dummycoupon-2', 'publish', $this->get_test_metadata( ChannelVisibility::DONT_SYNC_AND_SHOW ) );
 
 		$response = $this->do_request( '/wc/v3/coupons', 'GET' );
 
