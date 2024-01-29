41d1a8ccf083f2aec35e897ab44c0e4400efd92d
Joe McGill <joe.mcgill@10up.com>
2024-01-29 13:24:18 -0600

    Define Ads object for MC Service tets

--- tests/Unit/MerchantCenter/AccountServiceTest.php
+++ tests/Unit/MerchantCenter/AccountServiceTest.php
@@ -3,6 +3,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\MerchantCenter;
 
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Ads;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Merchant;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Middleware;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\SiteVerification;
