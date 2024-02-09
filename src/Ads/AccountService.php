864d74b24fde4e6deeef4d44a281f881a48b821f
Asvin Balloo <asvin.balloo@gmail.com>
2024-02-09 17:37:06 +0400

    Hopefully last of PHP fixes.

--- src/Ads/AccountService.php
+++ src/Ads/AccountService.php
@@ -293,12 +293,12 @@ class AccountService implements OptionsAwareInterface, Service {
 
 	/**
 	 * Updates the enhanced ads conversion status.
-	 * 
+	 *
 	 * @param string $status Status which should be updated to. Possible values are: pending, enabled and disabled.
 	 *
 	 * @return array
 	 */
-	public function update_enhanced_conversion_status(string $status): array {
+	public function update_enhanced_conversion_status( string $status ): array {
 		$updated_status = $this->container->get( Ads::class )->update_enhanced_conversion_status( $status );
 
 		return [
