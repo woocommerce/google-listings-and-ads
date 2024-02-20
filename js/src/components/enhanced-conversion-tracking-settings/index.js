14f926b5987faa66de86877fb1ae52b3e4294a21
Asvin Balloo <asvin.balloo@gmail.com>
2024-02-20 16:55:50 +0400

    Make use of adsConnected property og glaData.

--- js/src/components/enhanced-conversion-tracking-settings/index.js
+++ js/src/components/enhanced-conversion-tracking-settings/index.js
@@ -6,12 +6,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import {
-	GOOGLE_ADS_ACCOUNT_STATUS,
-	ENHANCED_ADS_CONVERSION_STATUS,
-} from '.~/constants';
-import SpinnerCard from '.~/components/spinner-card';
-import useGoogleAdsAccount from '.~/hooks/useGoogleAdsAccount';
+import { ENHANCED_ADS_CONVERSION_STATUS, glaData } from '.~/constants';
 import useAllowEnhancedConversions from '.~/hooks/useAllowEnhancedConversions';
 import Section from '.~/wcdl/section';
 import PendingNotice from '.~/components/enhanced-conversion-tracking-settings/pending-notice';
@@ -33,35 +28,26 @@ const TITLE = __( 'Enhanced Conversion Tracking', 'google-listings-and-ads' );
  * Renders the settings panel for enhanced conversion tracking
  */
 const EnhancedConversionTrackingSettings = () => {
-	const { googleAdsAccount, hasFinishedResolution } = useGoogleAdsAccount();
 	const { allowEnhancedConversions } = useAllowEnhancedConversions();
 
-	if (
-		( ! googleAdsAccount ||
-			googleAdsAccount?.status !==
-				GOOGLE_ADS_ACCOUNT_STATUS.CONNECTED ) &&
-		hasFinishedResolution
-	) {
+	if ( ! glaData.adsConnected ) {
 		return null;
 	}
 
 	return (
 		<Section title={ TITLE } description={ DESCRIPTION }>
-			{ ! hasFinishedResolution && <SpinnerCard /> }
+			<VerticalGapLayout size="large">
+				<Section.Card>
+					<Section.Card.Body>
+						{ allowEnhancedConversions ===
+							ENHANCED_ADS_CONVERSION_STATUS.PENDING && (
+							<PendingNotice />
+						) }
 
-			{ hasFinishedResolution && (
-				<VerticalGapLayout size="large">
-					<Section.Card>
-						<Section.Card.Body>
-							{ allowEnhancedConversions ===
-								ENHANCED_ADS_CONVERSION_STATUS.PENDING && (
-								<PendingNotice />
-							) }
-							<CTA />
-						</Section.Card.Body>
-					</Section.Card>
-				</VerticalGapLayout>
-			) }
+						<CTA />
+					</Section.Card.Body>
+				</Section.Card>
+			</VerticalGapLayout>
 		</Section>
 	);
 };
