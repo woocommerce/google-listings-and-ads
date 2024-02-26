ff5e5f8d41283f12b7b79e3a33eeae8d1256244e
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2024-02-26 12:23:54 -0300

    Add tests

--- tests/Unit/Jobs/ProductNotificationJobTest.php
+++ tests/Unit/Jobs/ProductNotificationJobTest.php
@@ -36,4 +36,26 @@ class ProductNotificationJobTest extends AbstractItemNotificationJobTest {
 	public function create_item() {
 		return WC_Helper_Product::create_simple_product();
 	}
+
+	public function test_mark_as_unsynced_when_delete() {
+		/** @var \WC_Product $product */
+		$product = WC_Helper_Product::create_simple_product();
+		$id     = $product->get_id();
+
+		$this->product_helper->expects( $this->once() )
+			->method( 'should_trigger_delete_notification' )
+			->with( $product )
+			->willReturn( true );
+
+		$this->product_helper->expects( $this->exactly( 3 ) )
+			->method( 'get_wc_product' )
+			->with( $id )
+			->willReturn( $product );
+
+		$this->notification_service->expects( $this->once() )->method( 'notify' )->willReturn( true );
+		$this->product_helper->expects( $this->once() )
+			->method( 'mark_as_unsynced' );
+
+		$this->job->handle_process_items_action( [ $id, 'product.delete' ] );
+	}
 }
