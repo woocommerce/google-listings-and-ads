b61d133b74f4f07d11277606b630e7dafe694330
Ankit Gade <ankit.gade@10up.com>
2024-02-28 16:57:53 +0530

    Update options name with ADS_ prefix

--- tests/Unit/API/Site/Controllers/Ads/AccountControllerTest.php
+++ tests/Unit/API/Site/Controllers/Ads/AccountControllerTest.php
@@ -241,6 +241,13 @@ class AccountControllerTest extends RESTControllerUnitTest {
 		$this->assertEquals( $expected_response, $response->get_data() );
 	}
 
+	public function test_update_enhanced_conversion_status_with_invalid_status() {
+		$response = $this->do_request( self::ROUTE_UPDATED_EC_STATUS, 'POST', [ 'status' => 'invalid' ] );
+
+		$this->assertEquals( 400, $response->get_status() );
+		// $this->assertEquals( $expected_response, $response->get_data() );
+	}
+
 	public function test_get_enhanced_conversion_status() {
 		$expected_response = [ 'status' => 'pending' ];
 		$this->account->expects( $this->once() )
