e61f9612496150cbf3638ab754355790537f6fd7
Joe McGill <joe.mcgill@10up.com>
2024-03-14 14:41:55 -0500

    Update claim modal and notice

--- js/src/components/google-ads-account-card/claim-account-modal.js
+++ js/src/components/google-ads-account-card/claim-account-modal.js
@@ -38,7 +38,7 @@ const ClaimAccountModal = ( { onRequestClose = noop } ) => {
 	return (
 		<AppModal
 			title={ __(
-				'Accept invitation to claim your Google Ads account',
+				'Claim your Google Ads account',
 				'google-listings-and-ads'
 			) }
 			buttons={ [
@@ -48,34 +48,23 @@ const ClaimAccountModal = ( { onRequestClose = noop } ) => {
 					eventName="gla_ads_accept_invitation_button_click" // @todo: review event name
 					onClick={ handleAcceptInvitationClick }
 				>
-					{ __( 'Accept invitation', 'google-listings-and-ads' ) }
+					{ __(
+						'Claim account in Google Ads',
+						'google-listings-and-ads'
+					) }
 				</AppButton>,
 			] }
 			onRequestClose={ onRequestClose }
 		>
 			<p>
-				<strong>
-					{ __(
-						'Your new Google Ads Account has been created! Accept the invitation to access your account and continue onboarding',
-						'google-listings-and-ads'
-					) }
-				</strong>
-			</p>
-			<p>
 				{ __(
-					'Claiming your account is easy! Simply click “Accept Invitation” below, then click “Continue” in the pop-up screen with the invitation. This will give you access to your new account, and will allow us to automatically set-up conversion tracking for you.',
+					'Claiming your account lets you access Google Ads and sets up conversion measurement. You must claim your account in the next 20 days.',
 					'google-listings-and-ads'
 				) }
 			</p>
 			<p>
 				{ __(
-					'This step is required to continue setting up Google Listings & Ads. If you do not claim access to your account in 20 days, your invite will expire and you will no longer be able to access your newly created account.',
-					'google-listings-and-ads'
-				) }
-			</p>
-			<p>
-				{ __(
-					'Once you have accepted the claim, you will also be directed to set-up billing. While billing setup is required for creating campaigns, you do not need to complete this step at this time and may close the pop-up after clicking “Continue” to claim your account.',
+					'When you claim your account, you’ll be asked to set up billing. This step is optional and you only need to complete it if you want to create Google Ads campaigns. If you don’t want to set up billing, close the window after you’ve clicked ‘Continue’ on the next page.',
 					'google-listings-and-ads'
 				) }
 			</p>
