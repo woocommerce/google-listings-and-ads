1bfd26d2906b6039bd45c199de993a876e59f91f
Jorge M <jorge.mahecha@a8c.com>
2024-03-14 18:57:15 +0100

    Refactor useMCProductStatistics

--- js/src/hooks/useMCProductStatistics.js
+++ js/src/hooks/useMCProductStatistics.js
@@ -1,15 +1,43 @@
 /**
+ * External dependencies
+ */
+import { useEffect } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
-import useAppSelectDispatch from '.~/hooks/useAppSelectDispatch';
+import useAppSelectDispatch from './useAppSelectDispatch';
+import useCountdown from './useCountdown';
 
 /**
  * Call `useAppSelectDispatch` with `"getMCProductStatistics"`.
+ * If the background process is loading, start a countdown and invalidate the resolution to refresh the data.
  *
  * @return {useAppSelectDispatch} Result of useAppSelectDispatch.
  */
 const useMCProductStatistics = () => {
-	return useAppSelectDispatch( 'getMCProductStatistics' );
+	const { second, callCount, startCountdown } = useCountdown();
+	const { data, hasFinishedResolution, invalidateResolution, ...rest } =
+		useAppSelectDispatch( 'getMCProductStatistics' );
+
+	const isLoading = hasFinishedResolution && data?.loading ? true : false;
+
+	useEffect( () => {
+		if (
+			( second === 0 && callCount > 0 && isLoading === true ) ||
+			( isLoading === true && callCount === 0 )
+		) {
+			startCountdown( 30 );
+			invalidateResolution( 'getMCProductStatistics', [] );
+		}
+	}, [ second, callCount, isLoading, invalidateResolution, startCountdown ] );
+
+	return {
+		data,
+		invalidateResolution,
+		hasFinishedResolution,
+		...rest,
+	};
 };
 
 export default useMCProductStatistics;
