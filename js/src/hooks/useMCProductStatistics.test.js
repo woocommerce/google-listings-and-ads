f12c22562b54fb4e9fa1eba5d198fb92b367a037
Jorge M <jorge.mahecha@a8c.com>
2024-03-17 17:01:06 +0100

    Add tests for useMCProductStatistics

---
+++ js/src/hooks/useMCProductStatistics.test.js
@@ -0,0 +1,127 @@
+/**
+ * External dependencies
+ */
+import { renderHook } from '@testing-library/react-hooks';
+
+/**
+ * Internal dependencies
+ */
+import useMCProductStatistics from '.~/hooks/useMCProductStatistics';
+import useCountdown from '.~/hooks/useCountdown';
+import useAppSelectDispatch from '.~/hooks/useAppSelectDispatch';
+
+jest.mock( '.~/hooks/useAppSelectDispatch' );
+jest.mock( '.~/hooks/useCountdown' );
+
+describe( 'useMCProductStatistics', () => {
+	const startCountdown = jest.fn();
+	const invalidateResolution = jest.fn();
+
+	const statsData = {
+		loading: false,
+		statistics: {
+			active: 10,
+			expiring: 20,
+			pending: 30,
+			disapproved: 2132,
+			not_synced: 1074,
+		},
+		error: null,
+	};
+
+	it( 'The update_merchant_product_statuses job is completed and the response contains the statistics', () => {
+		useCountdown.mockImplementation( () => {
+			return {
+				second: 0,
+				callCount: 0,
+				startCountdown,
+			};
+		} );
+
+		useAppSelectDispatch.mockImplementation( () => {
+			return {
+				hasFinishedResolution: true,
+				invalidateResolution,
+				data: statsData,
+			};
+		} );
+
+		const { result } = renderHook( () => useMCProductStatistics() );
+
+		expect( startCountdown ).not.toHaveBeenCalled();
+		expect( invalidateResolution ).not.toHaveBeenCalled();
+		expect( result.current.data ).toEqual( statsData );
+		expect( result.current.hasFinishedResolution ).toEqual( true );
+	} );
+
+	describe( 'Retry two times until the update_merchant_product_statuses job is completed', () => {
+		beforeAll( () => {
+			jest.clearAllMocks();
+
+			useCountdown.mockImplementation( () => {
+				return {
+					second: 0,
+					callCount: 0,
+					startCountdown,
+				};
+			} );
+		} );
+		it( "The first time the job is in progress/scheduled and the response doesn't contains statistics", () => {
+			useAppSelectDispatch.mockImplementation( () => {
+				return {
+					hasFinishedResolution: true,
+					invalidateResolution,
+					data: {
+						loading: true,
+						statistics: {},
+						error: null,
+					},
+				};
+			} );
+
+			renderHook( () => useMCProductStatistics() );
+
+			expect( startCountdown ).toHaveBeenCalledTimes( 1 );
+			expect( startCountdown ).toHaveBeenCalledWith( 30 );
+			expect( invalidateResolution ).not.toHaveBeenCalled();
+		} );
+		it( 'The countdown is completed, so we invalidate the call and try again', () => {
+			useCountdown.mockImplementation( () => {
+				return {
+					second: 0,
+					callCount: 1,
+					startCountdown,
+				};
+			} );
+
+			renderHook( () => useMCProductStatistics() );
+
+			expect( startCountdown ).toHaveBeenCalledTimes( 2 );
+			expect( startCountdown ).toHaveBeenCalledWith( 30 );
+			expect( invalidateResolution ).toHaveBeenCalledTimes( 1 );
+			expect( invalidateResolution ).toHaveBeenCalledWith(
+				'getMCProductStatistics',
+				[]
+			);
+		} );
+		it( 'The third time the job is completed and the response contains the statistics', () => {
+			useAppSelectDispatch.mockImplementation( () => {
+				return {
+					hasFinishedResolution: true,
+					invalidateResolution,
+					data: statsData,
+				};
+			} );
+
+			renderHook( () => useMCProductStatistics() );
+
+			expect( startCountdown ).toHaveBeenCalledTimes( 3 );
+			expect( startCountdown ).toHaveBeenLastCalledWith( 0 );
+			expect( invalidateResolution ).toHaveBeenCalledTimes( 1 );
+			expect( invalidateResolution ).toHaveBeenCalledWith(
+				'getMCProductStatistics',
+				[]
+			);
+		} );
+	} );
+} );
