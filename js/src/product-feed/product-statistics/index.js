85c8fb8b5fa803f219f2421c2030f2c0279c99e5
Jorge M <jorge.mahecha@a8c.com>
2024-03-17 14:52:58 +0100

    Use SummaryListPlaceholder instead of AppSpinner

--- js/src/product-feed/product-statistics/index.js
+++ js/src/product-feed/product-statistics/index.js
@@ -24,7 +24,6 @@ import ProductStatusHelpPopover from './product-status-help-popover';
 import SyncStatus from '.~/product-feed/product-statistics/status-box/sync-status';
 import FeedStatus from '.~/product-feed/product-statistics/status-box/feed-status';
 import AccountStatus from '.~/product-feed/product-statistics/status-box/account-status';
-import AppSpinner from '.~/components/app-spinner';
 import Text from '.~/components/app-text';
 import './index.scss';
 
@@ -37,6 +36,8 @@ const ProductStatistics = () => {
 			'google-listings-and-ads'
 		);
 	}
+
+	const isLoading = ! hasFinishedResolution || data?.loading;
 
 	return (
 		<Card className="gla-product-statistics">
@@ -52,70 +53,49 @@ const ProductStatistics = () => {
 				className="gla-product-statistics__summaries"
 				size={ null }
 			>
-				{ ! hasFinishedResolution && (
-					<SummaryListPlaceholder numberOfItems={ 5 } />
-				) }
-				{ hasFinishedResolution && data && (
+				{ isLoading && <SummaryListPlaceholder numberOfItems={ 5 } /> }
+				{ hasFinishedResolution && ! data?.loading && (
 					<SummaryList>
 						{ () => [
 							<SummaryNumber
-								children={ data.loading && <AppSpinner /> }
 								key="active"
 								label={ __(
 									'Active',
 									'google-listings-and-ads'
 								) }
-								value={
-									data.loading ? '' : data.statistics.active
-								}
+								value={ data.statistics.active }
 							/>,
 							<SummaryNumber
-								children={ data.loading && <AppSpinner /> }
 								key="expiring"
 								label={ __(
 									'Expiring',
 									'google-listings-and-ads'
 								) }
-								value={
-									data.loading ? '' : data.statistics.expiring
-								}
+								value={ data.statistics.expiring }
 							/>,
 							<SummaryNumber
-								children={ data.loading && <AppSpinner /> }
 								key="pending"
 								label={ __(
 									'Pending',
 									'google-listings-and-ads'
 								) }
-								value={
-									data.loading ? '' : data.statistics.pending
-								}
+								value={ data.statistics.pending }
 							/>,
 							<SummaryNumber
-								children={ data.loading && <AppSpinner /> }
 								key="disapproved"
 								label={ __(
 									'Disapproved',
 									'google-listings-and-ads'
 								) }
-								value={
-									data.loading
-										? ''
-										: data.statistics.disapproved
-								}
+								value={ data.statistics.disapproved }
 							/>,
 							<SummaryNumber
-								children={ data.loading && <AppSpinner /> }
 								key="not_synced"
 								label={ __(
 									'Not Synced',
 									'google-listings-and-ads'
 								) }
-								value={
-									data.loading
-										? ''
-										: data.statistics.not_synced
-								}
+								value={ data.statistics.not_synced }
 							/>,
 						] }
 					</SummaryList>
