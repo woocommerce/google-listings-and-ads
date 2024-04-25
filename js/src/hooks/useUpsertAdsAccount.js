65f240f0c4a504af0a116b0b7cfe098b9d3f60b4
Joe McGill <joe.mcgill@10up.com>
2024-04-25 08:28:37 -0500

    Remove unneccessary inline comment

--- js/src/hooks/useUpsertAdsAccount.js
+++ js/src/hooks/useUpsertAdsAccount.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useCallback, useState } from '@wordpress/element'; // Add this line
+import { useCallback, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
