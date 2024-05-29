b415243df690ffd05380d11fa59002e0526b56bd
Eason Su <eason.su.tw@gmail.com>
2024-05-29 20:06:52 +0800

    Add event tracking to `ClaimAccountButton`.

--- js/src/components/google-ads-account-card/claim-account-button.js
+++ js/src/components/google-ads-account-card/claim-account-button.js
@@ -8,10 +8,22 @@ import { noop } from 'lodash';
  */
 import AppButton from '.~/components/app-button';
 import getWindowFeatures from '.~/utils/getWindowFeatures';
+import { FILTER_ONBOARDING } from '.~/utils/tracks';
 import useGoogleAdsAccountStatus from '.~/hooks/useGoogleAdsAccountStatus';
+import useEventPropertiesFilter from '.~/hooks/useEventPropertiesFilter';
+
+/**
+ * Clicking on the button to open the invitation page for claiming the newly created Google Ads account.
+ *
+ * @event gla_open_ads_account_claim_invitation_button_click
+ * @property {string} [context] Indicates the place where the button is located.
+ * @property {string} [step] Indicates the step in the onboarding process.
+ */
 
 /**
  * Renders a button for opening a pop-up window to claim the newly created Google Ads account.
+ *
+ * @fires gla_open_ads_account_claim_invitation_button_click When the user clicks on the button to claim the account with `{ context: 'setup-mc' | 'setup-ads' }`.
  *
  * @param {Object} props React props.
  * @param {Function} [props.onClick] Function called back when the button is clicked.
@@ -19,6 +31,7 @@ import useGoogleAdsAccountStatus from '.~/hooks/useGoogleAdsAccountStatus';
  */
 const ClaimAccountButton = ( { onClick = noop, ...restProps } ) => {
 	const { inviteLink } = useGoogleAdsAccountStatus();
+	const getEventProps = useEventPropertiesFilter( FILTER_ONBOARDING );
 
 	const handleClaimAccountClick = ( event ) => {
 		const { defaultView } = event.target.ownerDocument;
@@ -29,7 +42,14 @@ const ClaimAccountButton = ( { onClick = noop, ...restProps } ) => {
 		onClick( event );
 	};
 
-	return <AppButton { ...restProps } onClick={ handleClaimAccountClick } />;
+	return (
+		<AppButton
+			{ ...restProps }
+			eventName="gla_open_ads_account_claim_invitation_button_click"
+			eventProps={ getEventProps() }
+			onClick={ handleClaimAccountClick }
+		/>
+	);
 };
 
 export default ClaimAccountButton;
--- js/src/components/google-ads-account-card/claim-account-button.test.js
+++ js/src/components/google-ads-account-card/claim-account-button.test.js
@@ -4,16 +4,25 @@
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import ClaimAccountButton from './claim-account-button';
 import useGoogleAdsAccountStatus from '.~/hooks/useGoogleAdsAccountStatus';
+import { FILTER_ONBOARDING } from '.~/utils/tracks';
+import expectEventWithPropertiesFilter from '.~/tests/expectEventWithPropertiesFilter';
 
 jest.mock( '.~/hooks/useGoogleAdsAccountStatus', () =>
 	jest.fn().mockName( 'useGoogleAdsAccountStatus' )
 );
+
+jest.mock( '@woocommerce/tracks', () => {
+	return {
+		recordEvent: jest.fn().mockName( 'recordEvent' ),
+	};
+} );
 
 describe( 'ClaimAccountButton', () => {
 	let windowOpen;
@@ -28,6 +37,7 @@ describe( 'ClaimAccountButton', () => {
 
 	afterEach( () => {
 		windowOpen.mockReset();
+		recordEvent.mockClear();
 	} );
 
 	it( 'should render the specified text in the button', () => {
@@ -42,23 +52,23 @@ describe( 'ClaimAccountButton', () => {
 		);
 	} );
 
-	it( 'should forward onClick callback', () => {
+	it( 'should forward onClick callback', async () => {
 		const onClick = jest.fn();
 		render( <ClaimAccountButton onClick={ onClick } /> );
 
 		expect( onClick ).toHaveBeenCalledTimes( 0 );
 
-		userEvent.click( screen.getByRole( 'button' ) );
+		await userEvent.click( screen.getByRole( 'button' ) );
 
 		expect( onClick ).toHaveBeenCalledTimes( 1 );
 	} );
 
-	it( 'should open the invitation link in a pop-up window', () => {
+	it( 'should open the invitation link in a pop-up window', async () => {
 		render( <ClaimAccountButton /> );
 
 		expect( windowOpen ).toHaveBeenCalledTimes( 0 );
 
-		userEvent.click( screen.getByRole( 'button' ) );
+		await userEvent.click( screen.getByRole( 'button' ) );
 
 		expect( windowOpen ).toHaveBeenCalledTimes( 1 );
 		expect( windowOpen ).toHaveBeenCalledWith(
@@ -66,6 +76,19 @@ describe( 'ClaimAccountButton', () => {
 			'_blank',
 			// Ignore the value of the window features.
 			expect.any( String )
+		);
+	} );
+
+	it( 'should record click events and be aware of extra event properties from filters', async () => {
+		await expectEventWithPropertiesFilter(
+			ClaimAccountButton,
+			FILTER_ONBOARDING,
+			() => screen.getByRole( 'button' ),
+			'gla_open_ads_account_claim_invitation_button_click',
+			[
+				{ context: 'setup-mc', step: '1' },
+				{ context: 'setup-ads', step: '2' },
+			]
 		);
 	} );
 } );
