1179f5b3cd616c6af5720f3309bee5869283beb6
Eason Su <eason.su.tw@gmail.com>
2024-06-05 16:15:51 +0800

    Adjust the JSDoc of the `useEventPropertiesFilter` hook.
    Address:
    - https://github.com/woocommerce/google-listings-and-ads/pull/2419#discussion_r1626485954
    - https://github.com/woocommerce/google-listings-and-ads/pull/2419#discussion_r1626519421

--- js/src/hooks/useEventPropertiesFilter.js
+++ js/src/hooks/useEventPropertiesFilter.js
@@ -11,14 +11,14 @@ import useIsEqualRefValue from './useIsEqualRefValue';
 import { NAMESPACE, hooks, filterPropertiesMap } from '.~/utils/tracks';
 
 /**
- * Hook to handle event properties across multiple layers.
+ * Hook to propagate event properties across multiple layers.
  *
  * For example, a button that is rendering a child layer requires state or context
  * held by the parent component when recording event tracking, and they span many
  * layers. Use this hook to avoid prop drilling.
  *
  * @param {string} filterName The name of the filter to use.
- * @param {Object} extraProperties The extra event properties to be added. Only valid properties are processed.
+ * @param {Object} extraProperties The extra event properties to be added. Only property names listed in the respective `filterPropertiesMap` are processed.
  * @return {(properties: Object) => Object} The function to call to filter the event properties.
  */
 export default function useEventPropertiesFilter(
