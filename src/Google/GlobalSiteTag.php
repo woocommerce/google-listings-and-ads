fa87c96b7c8f7f7705963f9cd225952a43c003e4
Martyn Jones <martynmjones@gmail.com>
2024-06-07 18:52:37 +0100

    Add WP Consent API integration script

--- src/Google/GlobalSiteTag.php
+++ src/Google/GlobalSiteTag.php
@@ -191,9 +191,23 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 
 		$this->assets_handler->register( $gtag_events );
 
+		$wp_consent_api = new ScriptWithBuiltDependenciesAsset(
+			'gla-wp-consent-api',
+			'js/build/wp-consent-api',
+			"{$this->get_root_dir()}/js/build/wp-consent-api.asset.php",
+			new BuiltScriptDependencyArray(
+				[
+					'dependencies' => [ 'wp-consent-api' ],
+					'version'      => $this->get_version(),
+				]
+			)
+		);
+
+		$this->assets_handler->register( $wp_consent_api );
+
 		add_action(
 			'wp_footer',
-			function () use ( $gtag_events ) {
+			function () use ( $gtag_events, $wp_consent_api ) {
 				$gtag_events->add_localization(
 					'glaGtagData',
 					[
@@ -204,6 +218,10 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 
 				$this->register_js_for_fast_refresh_dev();
 				$this->assets_handler->enqueue( $gtag_events );
+				
+				if ( ! class_exists( '\WC_Google_Gtag_JS' ) && function_exists( 'wp_has_consent' ) ) {
+					$this->assets_handler->enqueue( $wp_consent_api );
+				}
 			}
 		);
 	}
