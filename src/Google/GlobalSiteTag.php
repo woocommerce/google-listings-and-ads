d7d8805375a2f193ac0ea4478adee2c672b28826
Martyn Jones <martynmjones@gmail.com>
2024-06-07 18:53:56 +0100

    Add `wait_for_update` setting to consent default

--- src/Google/GlobalSiteTag.php
+++ src/Google/GlobalSiteTag.php
@@ -311,6 +311,7 @@ class GlobalSiteTag implements Service, Registerable, Conditional, OptionsAwareI
 				ad_user_data: 'denied',
 				ad_personalization: 'denied',
 				region: ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 'CH'],
+				wait_for_update: 500,
 			} );";
 		/**
 		 * Filters the default gtag consent mode configuration.
