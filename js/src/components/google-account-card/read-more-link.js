679db1015b964931a28bfe85b562d25390f9e8a9
Miguel Pérez Pellicer <5908855+puntope@users.noreply.github.com>
2024-08-03 19:51:18 +0400

    Change link for required google permissions

--- js/src/components/google-account-card/read-more-link.js
+++ js/src/components/google-account-card/read-more-link.js
@@ -7,7 +7,7 @@ const readMoreLink = (
 	<AppDocumentationLink
 		context="setup-mc-accounts"
 		linkId="required-google-permissions"
-		href="https://woocommerce.com/document/google-for-woocommerce/get-started/requirements/#general-requirements"
+		href="https://woocommerce.com/document/google-for-woocommerce/get-started/setup-and-configuration/#required-google-permissions"
 	/>
 );
 
