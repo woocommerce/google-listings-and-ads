757b5c90b2bd64320483ed8beda8a0472eba5b84
Eason Su <eason.su.tw@gmail.com>
2024-08-19 18:01:46 +0800

    Make `<AppTooltip>` compatible with the `placement` prop introduced by WordPress 6.4 to replace `position`.

--- js/src/tests/dependencies/woocommerce/settings.js
+++ js/src/tests/dependencies/woocommerce/settings.js
@@ -5,3 +5,15 @@ export default function getSettings() {
 export function getSetting( key, backup ) {
 	return global.wcSettings[ key ] || backup;
 }
+
+// Jest is currently running tests with npm packages bundled with WordPress 6.4.
+export function isWpVersion( version, operator ) {
+	if ( version === '6.4' && operator === '<' ) {
+		return false;
+	}
+
+	// eslint-disable-next-line no-console
+	console.warn(
+		`Please specify the mocking result of \`isWpVersion( '${ version }', '${ operator }' )\` for your test case.`
+	);
+}
