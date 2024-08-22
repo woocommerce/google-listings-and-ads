c9a259fadc079c01303d7edc7c5535163783f6f5
Eason Su <eason.su.tw@gmail.com>
2024-08-22 11:51:07 +0800

    Add a more detailed description for the mock `isWpVersion` function.
    Ref: https://github.com/woocommerce/google-listings-and-ads/pull/2544#discussion_r1725007682

--- js/src/tests/dependencies/woocommerce/settings.js
+++ js/src/tests/dependencies/woocommerce/settings.js
@@ -6,6 +6,9 @@ export function getSetting( key, backup ) {
 	return global.wcSettings[ key ] || backup;
 }
 
+// This mock function is used for jest testing only and therefore doesn't
+// implement the actual comparison logic. Whenever any implementation relies on
+// `isWpVersion`, add the corresponding return value when writing tests for it.
 // Jest is currently running tests with npm packages bundled with WordPress 6.4.
 export function isWpVersion( version, operator ) {
 	if ( version === '6.4' && operator === '<' ) {
