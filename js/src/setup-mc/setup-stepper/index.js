068f4c4d6e9358c954ac9428a161ea266448beda
asvinb <asvin.balloo@gmail.com>
2024-09-10 13:18:17 +0400

    Revert changes.

--- js/src/setup-mc/setup-stepper/index.js
+++ js/src/setup-mc/setup-stepper/index.js
@@ -26,12 +26,12 @@ const SetupStepper = () => {
 
 	const { status, step } = mcSetup;
 
-	// if ( status === 'complete' ) {
-	// 	getHistory().replace( getNewPath( {}, '/google/dashboard' ) );
-	// 	return null;
-	// }
+	if ( status === 'complete' ) {
+		getHistory().replace( getNewPath( {}, '/google/dashboard' ) );
+		return null;
+	}
 
-	return <SavedSetupStepper savedStep={ stepNameKeyMap.paid_ads } />;
+	return <SavedSetupStepper savedStep={ stepNameKeyMap[ step ] } />;
 };
 
 export default SetupStepper;
