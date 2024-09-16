dc08372197176163e853158bcb0312aeb22b98de
Karthik Thayyil <thayyilkarthik@gmail.com>
2024-09-16 10:37:28 +0100

    E2E test for with ad account.

--- tests/e2e/specs/add-paid-campaigns/add-paid-campaigns-has-account.test.js
+++ tests/e2e/specs/add-paid-campaigns/add-paid-campaigns-has-account.test.js
@@ -59,7 +59,7 @@ let setupBudgetPage = null;
  */
 let page = null;
 
-test.describe( 'Set up Ads account', () => {
+test.describe( 'Set up paid campaign', () => {
 	// The campaign budget
 	let budget = null;
 
@@ -71,6 +71,15 @@ test.describe( 'Set up Ads account', () => {
 		await setupAdsAccounts.mockAdsAccountsResponse( [] );
 		await dashboardPage.mockRequests();
 		await dashboardPage.goto();
+
+		// Add a connected Ads account.
+		await setupAdsAccounts.fulfillAdsConnection( {
+			id: ADS_ACCOUNTS[ 0 ].id,
+			currency: 'USD',
+			symbol: '$',
+			status: 'connected',
+			step: '',
+		} );
 	} );
 
 	test.afterAll( async () => {
@@ -89,205 +98,14 @@ test.describe( 'Set up Ads account', () => {
 		await expect( adsConnectionButton ).toBeEnabled();
 	} );
 
-	test.describe( 'Set up your accounts page', async () => {
+	test.describe( 'Create your paid campaign', () => {
 		test.beforeAll( async () => {
-			await setupAdsAccounts.mockAdsAccountsResponse( [] );
+			setupBudgetPage = new SetupBudgetPage( page );
 			await adsConnectionButton.click();
 			await page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
 		} );
 
-		test( 'Page header should be "Set up your accounts"', async () => {
-			await expect(
-				page.getByRole( 'heading', { name: 'Set up your accounts' } )
-			).toBeVisible();
-			await expect(
-				page.getByText(
-					'Connect your Google account and your Google Ads account to set up a paid Performance Max campaign.'
-				)
-			).toBeVisible();
-		} );
-
-		test( 'Google Account should show as connected', async () => {
-			await expect(
-				page.getByText(
-					'This Google account is connected to your store’s product feed.'
-				)
-			).toBeVisible();
-		} );
-
-		test( 'Continue Button should be disabled', async () => {
-			await expect( setupAdsAccounts.getContinueButton() ).toBeDisabled();
-		} );
-	} );
-
-	test.describe( 'Add paid campaigns with no Ads account', async () => {
-		test( 'Create an account should be visible', async () => {
-			const createAccountButton = page.getByRole( 'button', {
-				name: 'Create account',
-			} );
-
-			await expect( createAccountButton ).toBeVisible();
-
-			await expect( setupAdsAccounts.getContinueButton() ).toBeDisabled();
-
-			await expect(
-				page.getByText(
-					'Required to set up conversion measurement and create campaigns.'
-				)
-			).toBeVisible();
-
-			await createAccountButton.click();
-		} );
-
-		test( 'Create account button should be disable if the ToS have not been accepted.', async () => {
-			await expect(
-				page.getByRole( 'heading', {
-					name: 'Create Google Ads Account',
-				} )
-			).toBeVisible();
-
-			await expect(
-				page.getByText(
-					'By creating a Google Ads account, you agree to the following terms and conditions:'
-				)
-			).toBeVisible();
-
-			await expect(
-				setupAdsAccounts.getCreateAdsAccountButtonModal()
-			).toBeDisabled();
-		} );
-
-		test( 'Accept terms and conditions to enable the create account button', async () => {
-			await setupAdsAccounts.getAcceptTermCreateAccount().check();
-
-			await expect(
-				setupAdsAccounts.getCreateAdsAccountButtonModal()
-			).toBeEnabled();
-		} );
-
-		test( 'Create an Ads account', async () => {
-			// Intercept Ads connection request.
-			const connectAdsAccountRequest =
-				setupAdsAccounts.registerConnectAdsAccountRequests();
-
-			await setupAdsAccounts.mockAdsAccountsResponse( ADS_ACCOUNTS );
-
-			// Mock request to fulfill Ads connection.
-			await setupAdsAccounts.fulfillAdsConnection( {
-				id: ADS_ACCOUNTS[ 0 ].id,
-				currency: 'USD',
-				symbol: '$',
-				status: 'incomplete',
-				step: 'account_access',
-			} );
-
-			await setupAdsAccounts.mockAdsStatusNotClaimed();
-
-			await setupAdsAccounts.getCreateAdsAccountButtonModal().click();
-
-			await connectAdsAccountRequest;
-
-			const modal = setupAdsAccounts.getAcceptAccountModal();
-			await expect( modal ).toBeVisible();
-		} );
-
-		test( 'Show Unclaimed Ads account', async () => {
-			await setupAdsAccounts.clickCloseAcceptAccountButtonFromModal();
-
-			const claimButton = setupAdsAccounts.getAdsClaimAccountButton();
-			const claimText = setupAdsAccounts.getAdsClaimAccountText();
-
-			await expect( claimButton ).toBeVisible();
-			await expect( claimText ).toBeVisible();
-
-			await expect( setupAdsAccounts.getContinueButton() ).toBeDisabled();
-		} );
-
-		test( 'Show Claimed Ads account', async () => {
-			// Intercept Ads connection request.
-			await setupAdsAccounts.fulfillAdsConnection( {
-				id: ADS_ACCOUNTS[ 0 ].id,
-				currency: 'USD',
-				symbol: '$',
-				status: 'connected',
-				step: '',
-			} );
-
-			await setupAdsAccounts.mockAdsStatusClaimed();
-
-			await page.reload();
-
-			await expect( setupAdsAccounts.getContinueButton() ).toBeEnabled();
-
-			await expect(
-				page.getByRole( 'link', {
-					name: `Account ${ ADS_ACCOUNTS[ 0 ].id }`,
-				} )
-			).toBeVisible();
-
-			await expect( setupAdsAccounts.getContinueButton() ).toBeEnabled();
-		} );
-	} );
-
-	test.describe( 'Add paid campaigns with existing Ads accounts', () => {
-		test.beforeAll( async () => {
-			await setupAdsAccounts.mockAdsAccountsResponse( ADS_ACCOUNTS );
-			//Disconnect the account from the previous test
-			setupAdsAccounts.fulfillAdsConnection( {
-				id: ADS_ACCOUNTS[ 1 ].id,
-				currency: 'EUR',
-				symbol: '\u20ac',
-				status: 'disconnected',
-			} );
-
-			await page.reload();
-		} );
-
-		test( 'Select one existing account', async () => {
-			const adsAccountSelected = `${ ADS_ACCOUNTS[ 1 ].id }`;
-
-			await expect(
-				setupAdsAccounts.getConnectAdsButton()
-			).toBeDisabled();
-
-			await setupAdsAccounts.selectAnExistingAdsAccount(
-				adsAccountSelected
-			);
-
-			await expect(
-				setupAdsAccounts.getConnectAdsButton()
-			).toBeEnabled();
-
-			//Intercept Ads connection request
-			const connectAdsAccountRequest =
-				setupAdsAccounts.registerConnectAdsAccountRequests(
-					adsAccountSelected
-				);
-
-			//Mock request to fulfill Ads connection
-			setupAdsAccounts.fulfillAdsConnection( {
-				id: ADS_ACCOUNTS[ 1 ].id,
-				currency: 'EUR',
-				symbol: '\u20ac',
-				status: 'connected',
-			} );
-
-			await setupAdsAccounts.clickConnectAds();
-			await connectAdsAccountRequest;
-
-			await expect( setupAdsAccounts.getContinueButton() ).toBeEnabled();
-		} );
-	} );
-
-	test.describe( 'Create your paid campaign', () => {
-		test.beforeAll( async () => {
-			setupBudgetPage = new SetupBudgetPage( page );
-		} );
-
-		test( 'Continue to create paid campaign', async () => {
-			await setupAdsAccounts.clickContinue();
-			await page.waitForLoadState( LOAD_STATE.DOM_CONTENT_LOADED );
-
+		test( 'Create paid campaign page', async () => {
 			await expect(
 				page.getByRole( 'heading', {
 					name: 'Create your paid campaign',
