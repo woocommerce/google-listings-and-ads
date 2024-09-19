eb70d33b996f43eb48ce5da786d0c5de57ce8bdf
asvinb <asvin.balloo@gmail.com>
2024-09-19 11:57:19 +0400

    Do not trigger fetch if there are no countryCodes.

--- js/src/hooks/useFetchBudgetRecommendationEffect.js
+++ js/src/hooks/useFetchBudgetRecommendationEffect.js
@@ -19,11 +19,19 @@ import useApiFetchEffect from './useApiFetchEffect';
  * @param {Array<CountryCode>} countryCodes Country code array.
  * @return {Object} Budget recommendation.
  */
-const useFetchBudgetRecommendationEffect = ( countryCodes ) => {
-	const url = `${ API_NAMESPACE }/ads/campaigns/budget-recommendation`;
-	const query = { country_codes: countryCodes };
-	const path = addQueryArgs( url, query );
-	return useApiFetchEffect( { path } );
+const useFetchBudgetRecommendationEffect = ( countryCodes = [] ) => {
+	let args;
+
+	// If there are no country codes, undefined will be passed to useApiFetchEffect which won't trigger the fetch.
+	if ( countryCodes.length > 0 ) {
+		const url = `${ API_NAMESPACE }/ads/campaigns/budget-recommendation`;
+		const query = { country_codes: countryCodes };
+		const path = addQueryArgs( url, query );
+
+		args = { path };
+	}
+
+	return useApiFetchEffect( args );
 };
 
 export default useFetchBudgetRecommendationEffect;
