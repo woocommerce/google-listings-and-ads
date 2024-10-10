2564a42648754191c155cb8d53e2ad9ac3df25a9
Karthik Thayyil <thayyilkarthik@gmail.com>
2024-10-10 03:30:35 +0100

    fix review feedback

--- js/src/components/paid-ads/continue-button.js
+++ js/src/components/paid-ads/continue-button.js
@@ -13,16 +13,16 @@ import AppButton from '.~/components/app-button';
  *
  * @param {Object} props Props
  * @param {Object} props.formProps Form props forwarded from `Form` component.
- * @param {Function} props.handleContinueClick Function to handle the continue button click.
- * @return {import(".~/components/paid-ads/continue-button").default} The button.
+ * @param {Function} props.onClick Function to handle the continue button click.
+ * @return {import(".~/components/app-button").default} The button.
  */
-const ContinueButton = ( { formProps, handleContinueClick } ) => {
+const ContinueButton = ( { formProps, onClick } ) => {
 	return (
 		<AppButton
 			isPrimary
 			text={ __( 'Continue', 'google-listings-and-ads' ) }
 			disabled={ ! formProps.isValidForm }
-			onClick={ () => handleContinueClick() }
+			onClick={ onClick }
 		/>
 	);
 };
