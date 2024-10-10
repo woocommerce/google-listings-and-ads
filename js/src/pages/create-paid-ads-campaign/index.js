dc716ec66000cf3add2986e2dbb0b525aa27fb00
Karthik Thayyil <thayyilkarthik@gmail.com>
2024-10-10 03:32:33 +0100

    change prop name

--- js/src/pages/create-paid-ads-campaign/index.js
+++ js/src/pages/create-paid-ads-campaign/index.js
@@ -150,7 +150,7 @@ const CreatePaidAdsCampaign = () => {
 									continueButton={ ( props ) => (
 										<ContinueButton
 											{ ...props }
-											handleContinueClick={ () =>
+											onClick={ () =>
 												handleContinueClick(
 													STEP.ASSET_GROUP
 												)
