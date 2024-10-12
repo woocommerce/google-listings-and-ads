3922ab1180c66c723e9f1ac89cfcae0778e0e06b
Karthik Thayyil <thayyilkarthik@gmail.com>
2024-10-12 01:59:23 +0100

    CreateCampaignNotice component

--- js/src/product-feed/product-statistics/create-campaign-notice.js
+++ js/src/product-feed/product-statistics/create-campaign-notice.js
@@ -2,26 +2,56 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Notice } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
+import AddPaidCampaignButton from '.~/components/paid-ads/add-paid-campaign-button';
 import useMCProductStatistics from '.~/hooks/useMCProductStatistics';
+import useAdsCampaigns from '.~/hooks/useAdsCampaigns';
 
 const CreateCampaignNotice = () => {
-	const { hasFinishedResolution, data } = useMCProductStatistics();
+	const { hasFinishedResolution: productsResolution, data: products } =
+		useMCProductStatistics();
+	const { loaded: campaignsLoaded, data: campaigns } = useAdsCampaigns();
 
-	if ( hasFinishedResolution && ! data ) {
-		return __(
-			'An error occurred while retrieving your product feed. Please try again later.',
-			'google-listings-and-ads'
-		);
-	}
-	const isLoading = ! hasFinishedResolution || data?.loading;
+	const isLoading =
+		! productsResolution || products?.loading || ! campaignsLoaded;
+	const inValidData =
+		( productsResolution && ! products ) ||
+		( campaignsLoaded && ! campaigns );
 
-	if ( isLoading ) {
+	if (
+		isLoading ||
+		inValidData ||
+		products?.statistics?.active === 0 ||
+		campaigns?.length > 0
+	) {
 		return null;
 	}
+
+	return (
+		<Notice className="gla-create-campaign-notice" isDismissible={ false }>
+			<section>
+				<p>
+					{ __(
+						'You have approved products. Create a Google Ads campaign to reach more customers and drive more sales.',
+						'google-listings-and-ads'
+					) }
+				</p>
+				<AddPaidCampaignButton
+					eventProps={ {
+						context: 'product-feed-overview-promotion',
+					} }
+					children={ __(
+						'Create Campaign',
+						'google-listings-and-ads'
+					) }
+				/>
+			</section>
+		</Notice>
+	);
 };
 
 export default CreateCampaignNotice;
