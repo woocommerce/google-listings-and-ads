6fa4d1506066aeff0228a2aa36ab31414432e722
Eason Su <eason.su.tw@gmail.com>
2025-05-13 18:28:59 +0800

    Remove the hardcoded data of the free ads credits and the related UI.

--- js/src/components/free-ad-credit/index.js
+++ js/src/components/free-ad-credit/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement, useState } from '@wordpress/element';
+import { createInterpolateElement } from '@wordpress/element';
 import GridiconGift from 'gridicons/dist/gift';
 
 /**
@@ -10,31 +10,11 @@ import GridiconGift from 'gridicons/dist/gift';
  */
 import AppDocumentationLink from '~/components/app-documentation-link';
 import './index.scss';
-import TrackableLink from '~/components/trackable-link';
-import CountryModal from './country-modal';
 
 /**
- * Clicking on the link to view free ad credit value by country.
- *
- * @event gla_free_ad_credit_country_click
- * @property {string} context Indicates which page the link is in.
- */
-
-/**
- * @fires gla_free_ad_credit_country_click with `{ context: 'setup-ads' }`.
  * @fires gla_documentation_link_click with `{ context: 'setup-ads', link_id: 'free-ad-credit-terms', href: 'https://www.google.com/ads/coupons/terms/' }`
  */
 const FreeAdCredit = () => {
-	const [ showModal, setShowModal ] = useState( false );
-
-	const handleClick = () => {
-		setShowModal( true );
-	};
-
-	const handleRequestClose = () => {
-		setShowModal( false );
-	};
-
 	return (
 		<div className="gla-free-ad-credit">
 			<GridiconGift />
@@ -48,21 +28,10 @@ const FreeAdCredit = () => {
 				<div className="gla-free-ad-credit__description">
 					{ createInterpolateElement(
 						__(
-							'New to Google Ads? Get $500 in ad credit when you spend $500 within your first 60 days. Check how much credit you can receive in your country <checkLink>here</checkLink>. <termLink>Terms and conditions apply</termLink>.',
+							'New to Google Ads? Get $500 in ad credit when you spend $500 within your first 60 days. <termLink>Terms and conditions apply</termLink>.',
 							'google-listings-and-ads'
 						),
 						{
-							checkLink: (
-								<TrackableLink
-									eventName="gla_free_ad_credit_country_click"
-									eventProps={ {
-										context: 'setup-ads',
-									} }
-									href="#"
-									type="wp-admin"
-									onClick={ handleClick }
-								/>
-							),
 							termLink: (
 								<AppDocumentationLink
 									context="setup-ads"
@@ -73,9 +42,6 @@ const FreeAdCredit = () => {
 						}
 					) }
 				</div>
-				{ showModal && (
-					<CountryModal onRequestClose={ handleRequestClose } />
-				) }
 			</div>
 		</div>
 	);
