30f07fdaf29183a4e8cf0f18eb64663650c797a9
Eason Su <eason.su.tw@gmail.com>
2025-06-12 17:42:53 +0800

    Update Google Ads API related `use` statements and references from v18 to v20.

--- src/API/Google/BudgetMetrics.php
+++ src/API/Google/BudgetMetrics.php
@@ -12,14 +12,14 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsAwareInterface
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsAwareTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\PluginHelper;
-use Google\Ads\GoogleAds\V18\Enums\AdvertisingChannelTypeEnum\AdvertisingChannelType;
-use Google\Ads\GoogleAds\V18\Enums\BiddingStrategyTypeEnum\BiddingStrategyType;
-use Google\Ads\GoogleAds\V18\Enums\RecommendationTypeEnum\RecommendationType;
-use Google\Ads\GoogleAds\V18\Resources\Recommendation\CampaignBudgetRecommendation;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest\AssetGroupInfo;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest\BiddingInfo;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest\BudgetInfo;
+use Google\Ads\GoogleAds\V20\Enums\AdvertisingChannelTypeEnum\AdvertisingChannelType;
+use Google\Ads\GoogleAds\V20\Enums\BiddingStrategyTypeEnum\BiddingStrategyType;
+use Google\Ads\GoogleAds\V20\Enums\RecommendationTypeEnum\RecommendationType;
+use Google\Ads\GoogleAds\V20\Resources\Recommendation\CampaignBudgetRecommendation;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest\AssetGroupInfo;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest\BiddingInfo;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest\BudgetInfo;
 use Google\ApiCore\ApiException;
 
 /**
--- src/API/Google/BudgetRecommendations.php
+++ src/API/Google/BudgetRecommendations.php
@@ -12,18 +12,18 @@ use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsAwareInterface
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsAwareTrait;
 use Automattic\WooCommerce\GoogleListingsAndAds\Options\TransientsInterface;
 use Automattic\WooCommerce\GoogleListingsAndAds\PluginHelper;
-use Google\Ads\GoogleAds\V18\Enums\AdvertisingChannelTypeEnum\AdvertisingChannelType;
-use Google\Ads\GoogleAds\V18\Enums\BiddingStrategyTypeEnum\BiddingStrategyType;
-use Google\Ads\GoogleAds\V18\Enums\RecommendationTypeEnum\RecommendationType;
-use Google\Ads\GoogleAds\V18\Resources\Recommendation\CampaignBudgetRecommendation;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest\AssetGroupInfo;
-use Google\Ads\GoogleAds\V18\Services\GenerateRecommendationsRequest\BiddingInfo;
+use Google\Ads\GoogleAds\V20\Enums\AdvertisingChannelTypeEnum\AdvertisingChannelType;
+use Google\Ads\GoogleAds\V20\Enums\BiddingStrategyTypeEnum\BiddingStrategyType;
+use Google\Ads\GoogleAds\V20\Enums\RecommendationTypeEnum\RecommendationType;
+use Google\Ads\GoogleAds\V20\Resources\Recommendation\CampaignBudgetRecommendation;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest\AssetGroupInfo;
+use Google\Ads\GoogleAds\V20\Services\GenerateRecommendationsRequest\BiddingInfo;
 use Google\ApiCore\ApiException;
 
 /**
  * Class BudgetRecommendations
- * https://developers.google.com/google-ads/api/rest/reference/rest/v18/Recommendation#CampaignBudgetRecommendation
+ * https://developers.google.com/google-ads/api/rest/reference/rest/v20/Recommendation#CampaignBudgetRecommendation
  *
  * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Google
  */
