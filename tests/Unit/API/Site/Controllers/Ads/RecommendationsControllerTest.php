4d40efa01019b74dc222f02eeaa9e8f4e22a5188
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-07-02 11:54:00 +0530

    Update unit tests

--- tests/Unit/API/Site/Controllers/Ads/RecommendationsControllerTest.php
+++ tests/Unit/API/Site/Controllers/Ads/RecommendationsControllerTest.php
@@ -2,10 +2,12 @@
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\Tests\Unit\API\Site\Controllers\Ads;
 
+use Automattic\WooCommerce\GoogleListingsAndAds\Ads\AdsRecommendationsService;
 use Automattic\WooCommerce\GoogleListingsAndAds\Ads\AccountService;
 use Automattic\WooCommerce\GoogleListingsAndAds\API\Site\Controllers\Ads\RecommendationsController;
 use Automattic\WooCommerce\GoogleListingsAndAds\Exception\AccountReconnect;
 use Automattic\WooCommerce\GoogleListingsAndAds\Tests\Framework\RESTControllerUnitTest;
+use Automattic\WooCommerce\GoogleListingsAndAds\Vendor\League\Container\Container;
 use Exception;
 use PHPUnit\Framework\MockObject\MockObject;
 use WP_REST_Response as Response;
@@ -23,19 +25,57 @@ class RecommendationsControllerTest extends RESTControllerUnitTest {
 	/** @var RecommendationsController $controller */
 	protected $controller;
 
+	/** @var MockObject|AdsRecommendationsService */
+	protected $recommendations;
+
+	/** @var Container */
+	protected $container;
+
 	protected const ROUTE_RECOMMENDATIONS = '/wc/gla/ads/recommendations';
 
 	public function setUp(): void {
 		parent::setUp();
 
+		$this->recommendations = $this->createMock( AdsRecommendationsService::class );
+
+		// Mock the container to return the mocked AdsRecommendationsService.
+		$this->container = new Container();
+		$this->container->addShared( AdsRecommendationsService::class, $this->recommendations );
+
 		$this->account    = $this->createMock( AccountService::class );
 		$this->controller = new RecommendationsController( $this->server, $this->account );
+		$this->controller->set_container( $this->container );
 		$this->controller->register();
 	}
 
 	public function test_get_recommendations_returns_all_recommendations() {
 		$this->account->method( 'get_connected_account' )
 			->willReturn( [ 'status' => 'connected' ] );
+
+		$mock_recommendations_data = [
+			[
+				'id'              => 1,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/1',
+				'campaign_id'     => 100,
+				'campaign_name'   => 'Test Campaign',
+				'campaign_status' => 'ENABLED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+			[
+				'id'              => 2,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/2',
+				'campaign_id'     => 101,
+				'campaign_name'   => 'Another Campaign',
+				'campaign_status' => 'PAUSED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+		];
+
+		$this->recommendations->expects( $this->once() )
+			->method( 'get_recommendations' )
+			->willReturn( $mock_recommendations_data );
 
 		$response = $this->do_request( self::ROUTE_RECOMMENDATIONS, 'GET' );
 
@@ -86,6 +126,48 @@ class RecommendationsControllerTest extends RESTControllerUnitTest {
 			'type' => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
 		];
 
+		$mock_recommendations_data = [
+			[
+				'id'              => 1,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/1',
+				'campaign_id'     => 100,
+				'campaign_name'   => 'Test Campaign',
+				'campaign_status' => 'ENABLED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+			[
+				'id'              => 2,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/2',
+				'campaign_id'     => 101,
+				'campaign_name'   => 'Another Campaign',
+				'campaign_status' => 'PAUSED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+			[
+				'id'              => 3,
+				'type'            => 'CAMPAIGN_BUDGET',
+				'resource_name'   => 'customers/124/recommendations/3',
+				'campaign_id'     => 103,
+				'campaign_name'   => 'Another Campaign 03',
+				'campaign_status' => 'PAUSED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+		];
+
+		// Only return the recommendation matching the filter.
+		$filtered_mock_data = array_filter(
+			$mock_recommendations_data,
+			function ( $rec ) use ( $filter_by_type ) {
+				return $rec['type'] === $filter_by_type['type'];
+			}
+		);
+
+		$this->recommendations->expects( $this->once() )
+			->method( 'get_recommendations' )
+			->willReturn( $filtered_mock_data );
+
 		// Filter by a type that does not exist in the stubbed recommendations.
 		$response = $this->do_request( self::ROUTE_RECOMMENDATIONS, 'GET', $filter_by_type );
 
@@ -104,11 +186,43 @@ class RecommendationsControllerTest extends RESTControllerUnitTest {
 		$this->account->method( 'get_connected_account' )
 			->willReturn( [ 'status' => 'connected' ] );
 
+		$mock_recommendations_data = [
+			[
+				'id'              => 1,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/1',
+				'campaign_id'     => 100,
+				'campaign_name'   => 'Test Campaign',
+				'campaign_status' => 'ENABLED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+			[
+				'id'              => 2,
+				'type'            => 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH',
+				'resource_name'   => 'customers/123/recommendations/2',
+				'campaign_id'     => 101,
+				'campaign_name'   => 'Another Campaign',
+				'campaign_status' => 'PAUSED',
+				'last_synced'     => gmdate( 'c' ),
+			],
+		];
+
 		$filter_by_id = [
 			'id' => 2,
 		];
 
-		// Filter by a type that does not exist in the stubbed recommendations.
+		// Only return the recommendation matching the filter.
+		$filtered_mock_data = array_filter(
+			$mock_recommendations_data,
+			function ( $rec ) use ( $filter_by_id ) {
+				return $rec['id'] === $filter_by_id['id'];
+			}
+		);
+
+		$this->recommendations->expects( $this->once() )
+			->method( 'get_recommendations' )
+			->willReturn( array_values( $filtered_mock_data ) );
+
 		$response = $this->do_request( self::ROUTE_RECOMMENDATIONS, 'GET', $filter_by_id );
 
 		$this->assertEquals( 200, $response->get_status() );
