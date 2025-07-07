5676ebb0b96531b917bfbd97be62c11ffb0d9d1b
Mukesh Panchal <mukeshpanchal27@gmail.com>
2025-07-07 10:00:13 +0530

    Cleanup AdsRecommendationsQuery

--- src/API/Google/Query/AdsRecommendationsQuery.php
+++ src/API/Google/Query/AdsRecommendationsQuery.php
@@ -18,46 +18,12 @@ defined( 'ABSPATH' ) || exit;
 class AdsRecommendationsQuery extends AdsQuery {
 
 	/**
-	 * Client which handles the query.
-	 *
-	 * @var GoogleAdsClient
-	 */
-	protected $client = null;
-
-	/**
-	 * Ads Account ID.
-	 *
-	 * @var int
-	 */
-	protected $id = null;
-
-	/**
 	 * AdsRecommendationsQuery constructor.
 	 */
 	public function __construct() {
 		parent::__construct( 'recommendation' );
 		$this->set_initial_columns();
 		$this->where( 'recommendation.type', 'IMPROVE_PERFORMANCE_MAX_AD_STRENGTH' );
-	}
-
-	/**
-	 * Set the client which will handle the query.
-	 *
-	 * @param GoogleAdsClient $client Client instance.
-	 * @param int             $id     Account ID.
-	 *
-	 * @return QueryInterface
-	 * @throws InvalidProperty If the ID is empty.
-	 */
-	public function set_client( GoogleAdsClient $client, int $id ): QueryInterface {
-		if ( empty( $id ) ) {
-			throw InvalidProperty::not_null( get_class( $this ), 'id' );
-		}
-
-		$this->client = $client;
-		$this->id     = $id;
-
-		return $this;
 	}
 
 	/**
