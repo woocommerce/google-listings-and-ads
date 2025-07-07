b3b79617c8d7de9362a8d285be33503030e5320d
jjgrainger <github@jjgrainger.co.uk>
2025-07-07 11:05:01 +0100

    Only update settings value in the databse if it does not matched the current stored value

--- src/API/Site/Controllers/Ads/AdsSettingsController.php
+++ src/API/Site/Controllers/Ads/AdsSettingsController.php
@@ -92,7 +92,14 @@ class AdsSettingsController extends BaseOptionsController {
 					continue;
 				}
 
-				$result = $this->options->update( $key, (bool) $value );
+				$stored = (bool) $this->options->get( $key );
+
+				if ( ! is_null( $stored ) && $stored === $value ) {
+					$settings[ $key ] = $value;
+					continue;
+				}
+
+				$result = $this->options->update( $key, $value );
 
 				if ( false === $result ) {
 					return new WP_REST_Response(
