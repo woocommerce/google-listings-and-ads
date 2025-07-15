e5f5a92c93071d030b43c56596e95ad47e95fe75
jjgrainger <github@jjgrainger.co.uk>
2025-07-15 17:17:58 +0100

    Fix js lint issues

--- tests/e2e/specs/notification-manager.test.js
+++ tests/e2e/specs/notification-manager.test.js
@@ -11,10 +11,7 @@ const { test, expect } = require( '@playwright/test' );
 /**
  * Internal dependencies
  */
-import {
-	clearOnboardedMerchant,
-	setOnboardedMerchant,
-} from '../utils/api';
+import { clearOnboardedMerchant, setOnboardedMerchant } from '../utils/api';
 import DashboardPage from '../utils/pages/dashboard';
 
 test.use( { storageState: process.env.ADMINSTATE } );
@@ -34,7 +31,6 @@ let page = null;
 test.use( { storageState: process.env.ADMINSTATE } );
 
 test.describe( 'notification-manager', () => {
-
 	test.beforeAll( async ( { browser } ) => {
 		page = await browser.newPage();
 		dashboardPage = new DashboardPage( page );
@@ -48,35 +44,49 @@ test.describe( 'notification-manager', () => {
 		await page.close();
 	} );
 
-	test( 'Should display notification count on Marketing menu by default', ( async ( { browser } ) => {
+	test( 'Should display notification count on Marketing menu by default', async ( {
+		browser,
+	} ) => {
 		page = await browser.newPage();
 
-		await page.goto(
-			'/wp-admin/index.php',
-			{ waitUntil: LOAD_STATE.DOM_CONTENT_LOADED }
-		);
+		await page.goto( '/wp-admin/index.php', {
+			waitUntil: LOAD_STATE.DOM_CONTENT_LOADED,
+		} );
 
-		const badge = page.getByRole('link', { name: 'Marketing' }).locator('span.update-plugins').filter({ hasText: '1' })
+		const badge = page
+			.getByRole( 'link', { name: 'Marketing' } )
+			.locator( 'span.update-plugins' )
+			.filter( { hasText: '1' } );
 
-		expect(badge).toBeVisible();
-	} ) );
-
-
-	test( 'Should display notification count on Google for WooCommerce when inside sub-menu', () => {
-		const badge = dashboardPage.page.getByRole('link', { name: 'Google for WooCommerce' }).locator('span.update-plugins').filter({ hasText: '1' })
-
-		expect(badge).toBeVisible();
+		expect( badge ).toBeVisible();
 	} );
 
-	test( 'Badge should move to the correct location on menu change', ( async () => {
-		const badge = dashboardPage.page.getByRole('link', { name: 'Google for WooCommerce' }).locator('span.update-plugins').filter({ hasText: '1' })
+	test( 'Should display notification count on Google for WooCommerce when inside sub-menu', () => {
+		const badge = dashboardPage.page
+			.getByRole( 'link', { name: 'Google for WooCommerce' } )
+			.locator( 'span.update-plugins' )
+			.filter( { hasText: '1' } );
 
-		expect(badge).toBeVisible();
+		expect( badge ).toBeVisible();
+	} );
 
-		await dashboardPage.page.getByRole('link', { name: 'Analytics' }).click();
+	test( 'Badge should move to the correct location on menu change', async () => {
+		const badge = dashboardPage.page
+			.getByRole( 'link', { name: 'Google for WooCommerce' } )
+			.locator( 'span.update-plugins' )
+			.filter( { hasText: '1' } );
 
-		const badgeMoved = dashboardPage.page.getByRole('link', { name: 'Marketing' }).locator('span.update-plugins').filter({ hasText: '1' })
+		expect( badge ).toBeVisible();
 
-		expect(badgeMoved).toBeVisible();
-	} ) );
+		await dashboardPage.page
+			.getByRole( 'link', { name: 'Analytics' } )
+			.click();
+
+		const badgeMoved = dashboardPage.page
+			.getByRole( 'link', { name: 'Marketing' } )
+			.locator( 'span.update-plugins' )
+			.filter( { hasText: '1' } );
+
+		expect( badgeMoved ).toBeVisible();
+	} );
 } );
