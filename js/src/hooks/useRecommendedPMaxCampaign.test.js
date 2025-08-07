45fafa9e4eb2a52847c667a46079243c45e04c66
asvinb <asvin.balloo@gmail.com>
2025-08-07 12:09:13 +0400

    Save WIP.

--- js/src/hooks/useRecommendedPMaxCampaign.test.js
+++ js/src/hooks/useRecommendedPMaxCampaign.test.js
@@ -8,20 +8,15 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import useRecommendedPMaxCampaign from './useRecommendedPMaxCampaign';
-import useAdsCampaigns from '~/hooks/useAdsCampaigns';
 
 jest.mock( '@wordpress/data/src/components/use-select', () => jest.fn() );
-
-jest.mock( '~/hooks/useAdsCampaigns', () =>
-	jest.fn().mockName( 'useAdsCampaigns' )
-);
 
 jest.mock( '@wordpress/data', () => ( {
 	__esModule: true,
 	useSelect: jest.fn(),
 } ) );
 
-const mockedCampaigns = [
+const mockedRecommendations = [
 	{
 		id: 1,
 		name: 'Campaign 1',
@@ -45,108 +40,16 @@ const mockedCampaigns = [
 	},
 ];
 
-const mockedRecommendations = [
-	{
-		campaign_id: 2,
-	},
-];
-
 describe( 'usePmaxAssetOptimizationRecommendedCampaign', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
 	} );
 
-	it( 'returns null campaign and hasFinishedResolution=false if campaigns not loaded', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: mockedCampaigns,
-			loaded: true,
-		} );
-		useSelect.mockImplementation( () => ( {
-			campaign: null,
-			hasFinishedResolution: false,
-		} ) );
-
-		const { result } = renderHook( () => useRecommendedPMaxCampaign() );
-		expect( result.current ).toEqual( {
-			campaign: null,
-			hasFinishedResolution: false,
-		} );
-	} );
-
-	it( 'returns null campaign and hasFinishedResolution=true if no enabled PMax campaigns', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: [
-				{
-					id: 3,
-					name: 'Campaign 3',
-					type: 'SEARCH',
-					status: 'enabled',
-					amount: 300,
-				},
-			],
-			loaded: true,
-		} );
+	it( 'returns null campaign and hasFinishedResolution=true if no recommendations', () => {
 		useSelect.mockImplementation( ( cb ) => {
 			return cb( () => ( {
 				hasFinishedResolution: () => true,
-				getAdsRecommendations: () => mockedRecommendations,
-			} ) );
-		} );
-
-		const { result } = renderHook( () => useRecommendedPMaxCampaign() );
-		expect( result.current ).toEqual( {
-			campaign: null,
-			hasFinishedResolution: true,
-		} );
-	} );
-
-	it( 'returns null campaign if no recommendations for highest spending campaign', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: mockedCampaigns,
-			loaded: true,
-		} );
-		useSelect.mockImplementation( ( cb ) => {
-			return cb( () => ( {
-				hasFinishedResolution: () => true,
-				getAdsRecommendations: () => [ { campaign_id: 4 } ],
-			} ) );
-		} );
-
-		const { result } = renderHook( () => useRecommendedPMaxCampaign() );
-		expect( result.current ).toEqual( {
-			campaign: null,
-			hasFinishedResolution: true,
-		} );
-	} );
-
-	it( 'returns null campaign and hasFinishedResolution=false if recommendations are loading', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: mockedCampaigns,
-			loaded: true,
-		} );
-		useSelect.mockImplementation( ( cb ) => {
-			return cb( () => ( {
-				hasFinishedResolution: () => false,
-				getAdsRecommendations: () => null,
-			} ) );
-		} );
-
-		const { result } = renderHook( () => useRecommendedPMaxCampaign() );
-		expect( result.current ).toEqual( {
-			campaign: null,
-			hasFinishedResolution: false,
-		} );
-	} );
-
-	it( 'returns null campaign if recommendations do not match highest spending campaign', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: mockedCampaigns,
-			loaded: true,
-		} );
-		useSelect.mockImplementation( ( cb ) => {
-			return cb( () => ( {
-				hasFinishedResolution: () => true,
-				getAdsRecommendations: () => [ { campaign_id: 1 } ],
+				getAdsRecommendations: () => [],
 			} ) );
 		} );
 
@@ -158,10 +61,6 @@ describe( 'usePmaxAssetOptimizationRecommendedCampaign', () => {
 	} );
 
 	it( 'returns highest spending enabled PMax campaign with recommendation', () => {
-		useAdsCampaigns.mockReturnValue( {
-			data: mockedCampaigns,
-			loaded: true,
-		} );
 		useSelect.mockImplementation( ( cb ) => {
 			return cb( () => ( {
 				hasFinishedResolution: () => true,
@@ -171,7 +70,7 @@ describe( 'usePmaxAssetOptimizationRecommendedCampaign', () => {
 
 		const { result } = renderHook( () => useRecommendedPMaxCampaign() );
 		expect( result.current ).toEqual( {
-			campaign: mockedCampaigns[ 1 ],
+			campaign: mockedRecommendations[ 1 ],
 			hasFinishedResolution: true,
 		} );
 	} );
