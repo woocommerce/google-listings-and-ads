508b848a36ea5cf4122bbd9dbd6208535168bb95
asvinb <asvin.balloo@gmail.com>
2025-08-14 14:40:21 +0400

    Simplify code.

--- js/src/components/paid-ads/campaign-assets-form.js
+++ js/src/components/paid-ads/campaign-assets-form.js
@@ -158,14 +158,14 @@ export default function CampaignAssetsForm( {
 					: initialAssetGroup;
 				let hasNonEmptyAssets = false;
 
-				const updatedContextValues = {};
-				Object.keys( emptyAssetGroup ).forEach( ( key ) => {
-					if ( assetGroup && assetGroup[ key ]?.length ) {
-						hasNonEmptyAssets = true;
-					}
-
-					updatedContextValues[ key ] = nextAssetGroup[ key ];
-				} );
+				const updatedContextValues = Object.fromEntries(
+					Object.keys( emptyAssetGroup ).map( ( key ) => {
+						if ( assetGroup?.[ key ]?.length ) {
+							hasNonEmptyAssets = true;
+						}
+						return [ key, nextAssetGroup[ key ] ];
+					} )
+				);
 
 				if ( Object.keys( updatedContextValues ).length ) {
 					formContext.setValues( updatedContextValues );
