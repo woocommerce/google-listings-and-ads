a620c4ab70b9887ba47760088aae4f4c1d3f44f1
Ankit Gade <ankit.gade@10up.com>
2025-09-02 12:14:07 +0530

    Fix: PHPCS Issue.

--- src/Coupon/WCCouponAdapter.php
+++ src/Coupon/WCCouponAdapter.php
@@ -385,6 +385,7 @@ class WCCouponAdapter extends GooglePromotion implements Validatable {
 	/**
 	 *
 	 * @param string $targetCountry
+	 *
      *            phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 	 */
 	public function setTargetCountry( $targetCountry ) {
