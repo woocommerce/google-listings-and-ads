7684ba10b89d535dca4b17cb3dc1886e8f06b4b8
Ankit Gade <ankit.gade@10up.com>
2025-09-02 12:26:44 +0530

    Revert coupon adapter changes.

--- src/Coupon/WCCouponAdapter.php
+++ src/Coupon/WCCouponAdapter.php
@@ -385,8 +385,7 @@ class WCCouponAdapter extends GooglePromotion implements Validatable {
 	/**
 	 *
 	 * @param string $targetCountry
-	 *
-     * phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+     *            phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
 	 */
 	public function setTargetCountry( $targetCountry ) {
 		// set the new target country
