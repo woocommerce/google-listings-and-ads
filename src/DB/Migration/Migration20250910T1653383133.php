b82317da64250ef60f37d7b5c9890e8b95f9ed1e
jjgrainger <github@jjgrainger.co.uk>
2025-09-12 15:02:41 +0100

    Remove scheduled jobs and database file and table

--- src/DB/Migration/Migration20250910T1653383133.php
+++ src/DB/Migration/Migration20250910T1653383133.php
@@ -3,7 +3,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\GoogleListingsAndAds\DB\Migration;
 
-use Automattic\WooCommerce\GoogleListingsAndAds\DB\Table\AdsRecommendationsTable;
+use Automattic\WooCommerce\GoogleListingsAndAds\ActionScheduler\ActionScheduler;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -19,21 +19,19 @@ defined( 'ABSPATH' ) || exit;
 class Migration20250910T1653383133 extends AbstractMigration {
 
 	/**
-	 * @var AdsRecommendationsTable
+	 * @var ActionScheduler
 	 */
-	protected $ads_recommendations_table;
+	protected $action_scheduler;
 
 	/**
 	 * Migration constructor.
 	 *
 	 * @param \wpdb                   $wpdb
-	 * @param AdsRecommendationsTable $ads_recommendations_table
 	 */
-	public function __construct( \wpdb $wpdb, AdsRecommendationsTable $ads_recommendations_table ) {
+	public function __construct( \wpdb $wpdb, ActionScheduler $action_scheduler ) {
 		parent::__construct( $wpdb );
-		$this->ads_recommendations_table = $ads_recommendations_table;
+		$this->action_scheduler = $action_scheduler;
 	}
-
 
 	/**
 	 * Returns the version to apply this migration for.
@@ -50,8 +48,21 @@ class Migration20250910T1653383133 extends AbstractMigration {
 	 * @return void
 	 */
 	public function apply(): void {
-		if ( $this->ads_recommendations_table->exists() ) {
-			$this->wpdb->query( "DROP TABLE IF EXISTS `{$this->wpdb->_escape( $this->ads_recommendations_table->get_name() )}`" ); // phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		// Remove the ads_recommendation table if it exists.
+		$this->wpdb->query( "DROP TABLE IF EXISTS {$this->wpdb->prefix}gla_ads_recommendations" );
+
+		if ( function_exists( 'as_unschedule_all_actions' ) ) {
+			// Remove any scheduled actions that use the ads recommendations table.
+			$job_hooks = [
+				'gla/jobs/update_ads_recommendations/start',
+				'gla/jobs/update_ads_recommendations/process_item',
+			];
+
+			foreach ( $job_hooks as $hook ) {
+				if ( $this->action_scheduler->has_scheduled_action( $hook ) ) {
+					as_unschedule_all_actions( $hook );
+				}
+			}
 		}
 	}
 }
