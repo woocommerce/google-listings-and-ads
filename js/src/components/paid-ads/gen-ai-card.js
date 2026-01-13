6cb816d2766ce093bea5fa6680688ca863cbdf32
Ankit Gade <ankit.gade@10up.com>
2026-01-13 13:05:28 +0530

    Update GenAICard to latest designs.

--- js/src/components/paid-ads/gen-ai-card.js
+++ js/src/components/paid-ads/gen-ai-card.js
@@ -2,24 +2,16 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { addQueryArgs } from '@wordpress/url';
-import { external as externalIcon } from '@wordpress/icons';
-import { createInterpolateElement } from '@wordpress/element';
-import { Icon, Flex, FlexBlock, CardBody } from '@wordpress/components';
+import { Flex, FlexBlock, CardBody, Notice } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import Badge from '~/components/badge';
 import Section from '~/components/section';
-import AppButton from '~/components/app-button';
 import useGoogleAdsAccount from '~/hooks/useGoogleAdsAccount';
-import AppDocumentationLink from '~/components/app-documentation-link';
 import genAIImageURL from '~/images/pmax-assets-improvements/gen-ai.svg';
-import { GOOGLE_ADS_ACCOUNT_STATUS } from '~/constants';
+import genAICheckMark from '~/images/pmax-assets-improvements/gen-ai-check-notice.svg';
 import './gen-ai-card.scss';
-
-const { CONNECTED, INCOMPLETE } = GOOGLE_ADS_ACCOUNT_STATUS;
 
 /**
  * GenAICard component displays a promotional card for Google AI-powered asset generation
@@ -31,9 +23,6 @@ const { CONNECTED, INCOMPLETE } = GOOGLE_ADS_ACCOUNT_STATUS;
  */
 const GenAICard = () => {
 	const { googleAdsAccount } = useGoogleAdsAccount();
-	const hasAdsAccount = [ CONNECTED, INCOMPLETE ].includes(
-		googleAdsAccount?.status
-	);
 	const queryArgs = {};
 
 	if ( googleAdsAccount?.ocid ) {
@@ -41,11 +30,6 @@ const GenAICard = () => {
 	} else if ( googleAdsAccount?.id ) {
 		queryArgs.ecid = googleAdsAccount.id;
 	}
-
-	const recommendationsURL = addQueryArgs(
-		'https://ads.google.com/aw/recommendations',
-		queryArgs
-	);
 
 	return (
 		<Section.Card className="gla-gen-ai-card">
@@ -58,63 +42,49 @@ const GenAICard = () => {
 				>
 					<FlexBlock>
 						<Flex direction="column" gap={ 4 } align="start">
-							<Badge>
-								{ __(
-									'Now available',
-									'google-listings-and-ads'
-								) }
-							</Badge>
-
 							<div>
 								<Section.Card.Title>
 									{ __(
-										'You can use Google AI to help build Performance Max assets with a few clicks.',
+										'Review Your AI Suggestions',
 										'google-listings-and-ads'
 									) }
 								</Section.Card.Title>
 								<div>
-									{ createInterpolateElement(
-										__(
-											'Starting with your website, Google AI will understand what you’re advertising and can generate or suggest text, image, logo, and video assets for you. <link>Learn more</link>',
-											'google-listings-and-ads'
-										),
-										{
-											link: (
-												<AppDocumentationLink
-													href="https://support.google.com/google-ads/answer/14150602"
-													context="pmax-assets-improvements"
-													linkId="pmax-gen-ai-card"
-												/>
-											),
-										}
+									{ __(
+										'Google AI analyzed your campaign’s URL to automatically generate your ad assets. Please review the suggested text and images below to ensure they align with your brand.',
+										'google-listings-and-ads'
 									) }
 								</div>
 							</div>
 
-							<AppButton
-								icon={ <Icon icon={ externalIcon } /> }
-								iconPosition="right"
-								href={ recommendationsURL }
-								disabled={ ! hasAdsAccount }
-								target="_blank"
-								isSecondary
-							>
-								{ __(
-									'Generate assets with GenAI',
-									'google-listings-and-ads'
-								) }
-							</AppButton>
+							<Notice status="success" isDismissible={ false }>
+								<img
+									src={ genAICheckMark }
+									alt={ __(
+										'Checkmark indicating success',
+										'google-listings-and-ads'
+									) }
+									width="24"
+									height="24"
+								/>
+								<div>
+									{ __(
+										'Text assets were auto-populate with Google AI',
+										'google-listings-and-ads'
+									) }
+								</div>
+							</Notice>
 						</Flex>
 					</FlexBlock>
-					<FlexBlock>
+					<FlexBlock className="gla-gen-ai-card__image-block">
 						<img
 							src={ genAIImageURL }
 							alt={ __(
 								'Drawing of a person who successfully launched a campaign',
 								'google-listings-and-ads'
 							) }
-							width="212"
-							height="212"
+							width="92"
+							height="90"
 						/>
 					</FlexBlock>
 				</Flex>
