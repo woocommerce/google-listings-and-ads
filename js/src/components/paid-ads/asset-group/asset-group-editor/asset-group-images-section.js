b415f3f26196769b9da97d71aace345a7366ad97
asvinb <asvin.balloo@gmail.com>
2026-01-14 16:37:52 +0400

    Use existing Tip component and remove GenAIImagesNotice component.

--- js/src/components/paid-ads/asset-group/asset-group-editor/asset-group-images-section.js
+++ js/src/components/paid-ads/asset-group/asset-group-editor/asset-group-images-section.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Flex, FlexItem, Tip } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -11,7 +12,6 @@ import ImagesSelector from './images-selector';
 import AssetField from './asset-field';
 import Section from '~/components/section';
 import AppDocumentationLink from '~/components/app-documentation-link';
-import GenAIImagesNotice from '../../gen-ai-images-notice';
 import { ASSET_IMAGE_SPECS } from '../../assetSpecs';
 
 /**
@@ -34,7 +34,8 @@ const AssetGroupImagesSection = ( {
 	getNumOfIssues,
 	renderErrors,
 } ) => {
-	const { values, getInputProps } = useAdaptiveFormContext();
+	const { values, getInputProps, adapter } = useAdaptiveFormContext();
+	const showTip = adapter.hasImportedAssets;
 
 	return (
 		<Section
@@ -75,38 +76,54 @@ const AssetGroupImagesSection = ( {
 				</div>
 			}
 		>
-			<GenAIImagesNotice />
 			<div className="gla-asset-group-section__content">
-				{ ASSET_IMAGE_SPECS.map( ( spec ) => {
-					const initialImageUrls = initialValues[ spec.key ];
-					const imageProps = getInputProps( spec.key );
-
-					return (
-						<AssetField
-							key={ spec.key }
-							ref={ refFirstErrorField.bind( spec.key ) }
-							heading={ spec.heading }
-							subheading={ spec.subheading }
-							help={ spec.help }
-							numOfIssues={ getNumOfIssues( spec.key ) }
-							markOptional={ spec.min === 0 }
-							disabled={ ! isSelectedFinalUrl }
-							initialExpanded={ isSelectedFinalUrl }
-						>
-							<ImagesSelector
-								initialImageUrls={ initialImageUrls }
-								maxNumberOfImages={ spec.getMax( values ) }
-								reachedMaxNumberTip={ spec.getMaxNumberTip(
-									values
+				<Flex direction="column" gap={ 4 }>
+					{ showTip && (
+						<FlexItem>
+							<Tip>
+								{ __(
+									"We've used your final URL to auto-populate images…",
+									'google-listings-and-ads'
 								) }
-								imageConfig={ spec.imageConfig }
-								onChange={ imageProps.onChange }
-							>
-								{ renderErrors( spec.key ) }
-							</ImagesSelector>
-						</AssetField>
-					);
-				} ) }
+							</Tip>
+						</FlexItem>
+					) }
+
+					<FlexItem>
+						{ ASSET_IMAGE_SPECS.map( ( spec ) => {
+							const initialImageUrls = initialValues[ spec.key ];
+							const imageProps = getInputProps( spec.key );
+
+							return (
+								<AssetField
+									key={ spec.key }
+									ref={ refFirstErrorField.bind( spec.key ) }
+									heading={ spec.heading }
+									subheading={ spec.subheading }
+									help={ spec.help }
+									numOfIssues={ getNumOfIssues( spec.key ) }
+									markOptional={ spec.min === 0 }
+									disabled={ ! isSelectedFinalUrl }
+									initialExpanded={ isSelectedFinalUrl }
+								>
+									<ImagesSelector
+										initialImageUrls={ initialImageUrls }
+										maxNumberOfImages={ spec.getMax(
+											values
+										) }
+										reachedMaxNumberTip={ spec.getMaxNumberTip(
+											values
+										) }
+										imageConfig={ spec.imageConfig }
+										onChange={ imageProps.onChange }
+									>
+										{ renderErrors( spec.key ) }
+									</ImagesSelector>
+								</AssetField>
+							);
+						} ) }
+					</FlexItem>
+				</Flex>
 			</div>
 		</Section>
 	);
