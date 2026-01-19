e6c8f8ab9059b8e5733e4bebd9d681f495e6fd25
asvinb <asvin.balloo@gmail.com>
2026-01-19 16:20:19 +0400

    Tweak to styling.

--- js/src/components/paid-ads/gen-ai-progress.js
+++ js/src/components/paid-ads/gen-ai-progress.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 // eslint-disable-next-line import/named, @woocommerce/dependency-group
-import { Flex, FlexBlock, ProgressBar } from '@wordpress/components';
+import { ProgressBar } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -20,20 +20,21 @@ const GenAIProgress = () => {
 				width={ 212 }
 				height={ 212 }
 			/>
-			<Flex direction="column" align="center" gap={ 2 }>
-				<FlexBlock className="gen-ai-progress__text-content">
-					<h2>
-						{ __( 'Generating assets', 'google-listings-and-ads' ) }
-					</h2>
-					<ProgressBar className="gen-ai-progress__bar" />
-					<p>
-						{ __(
-							'Google AI is analyzing your campaign’s URL to automatically generate your ad assets',
-							'google-listings-and-ads'
-						) }
-					</p>
-				</FlexBlock>
-			</Flex>
+
+			<div className="gen-ai-progress__text-content">
+				<h2>
+					{ __( 'Generating assets', 'google-listings-and-ads' ) }
+				</h2>
+
+				<ProgressBar className="gen-ai-progress__bar" />
+
+				<p>
+					{ __(
+						'Google AI is analyzing your campaign’s URL to automatically generate your ad assets',
+						'google-listings-and-ads'
+					) }
+				</p>
+			</div>
 		</div>
 	);
 };
