f6c847741205d5d9aee9b9f69a2d6f05c8bd2fe0
Ankit Gade <ankit.gade@10up.com>
2026-01-22 00:27:18 +0530

    Update copy.

--- js/src/pages/settings/linked-accounts-section-wrapper.js
+++ js/src/pages/settings/linked-accounts-section-wrapper.js
@@ -7,15 +7,25 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import Section from '~/components/section';
+import { glaData } from '~/constants';
 
 export default function LinkedAccountsSectionWrapper( props ) {
+	const { serviceBasedMerchant } = glaData;
+
+	const description = serviceBasedMerchant
+		? __(
+				'A WordPress.com account, Google account and Google Ads account are required to use this extension in WooCommerce.',
+				'google-listings-and-ads'
+		  )
+		: __(
+				'A WordPress.com account, Google account, Google Merchant Center account, and Google Ads account are required to use this extension in WooCommerce.',
+				'google-listings-and-ads'
+		  );
+
 	return (
 		<Section
 			title={ __( 'Linked accounts', 'google-listings-and-ads' ) }
-			description={ __(
-				'A WordPress.com account, Google account, Google Merchant Center account, and Google Ads account are required to use this extension in WooCommerce.',
-				'google-listings-and-ads'
-			) }
+			description={ description }
 			{ ...props }
 		/>
 	);
