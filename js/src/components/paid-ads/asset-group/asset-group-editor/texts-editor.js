de395f7109519cf1c6c4d1d6ade7d90a3eb5917c
asvinb <asvin.balloo@gmail.com>
2026-01-21 16:32:42 +0400

    refactor(asset-group): Extract asset slot filling utility

--- js/src/components/paid-ads/asset-group/asset-group-editor/texts-editor.js
+++ js/src/components/paid-ads/asset-group/asset-group-editor/texts-editor.js
@@ -16,6 +16,7 @@ import AppInputControl from '~/components/app-input-control';
 import AssetItemActionButton, {
 	ACTION_TYPES,
 } from './asset-item-action-button';
+import fillEmptyAssetSlots from './utils/fill-empty-asset-slots';
 import './texts-editor.scss';
 
 function normalizeNumberOfTexts( texts, minNumberOfTexts, maxNumberOfTexts ) {
@@ -26,61 +27,6 @@ function normalizeNumberOfTexts( texts, minNumberOfTexts, maxNumberOfTexts ) {
 		sliceArgs.push( maxNumberOfTexts );
 	}
 	return texts.concat( supplement ).slice( ...sliceArgs );
-}
-
-/**
- * Result returned by fillEmptyAssetSlotsWithUniqueValues.
- *
- * @typedef {Object} FillEmptyAssetSlotsResult
- * @property {string[]} assets Updated asset list.
- * @property {number} updatedCount Number of empty ("") slots that were filled.
- */
-
-/**
- * Fill empty asset slots (represented by empty strings "") with unique
- * generated values.
- *
- * Existing non-empty values are preserved.
- * Empty slots that cannot be filled remain as "".
- *
- * @param {string[]} currentAssets Current asset values, where "" represents an empty slot.
- * @param {string[]} generatedAssets Newly generated candidate asset values.
- *
- * @return {FillEmptyAssetSlotsResult} Result containing updated assets and count of filled slots.
- */
-export function fillEmptyAssetSlotsWithUniqueValues(
-	currentAssets,
-	generatedAssets
-) {
-	const existingAssetValues = new Set( currentAssets.filter( Boolean ) );
-
-	let generatedIndex = 0;
-	let updatedCount = 0;
-
-	const assets = currentAssets.map( ( assetValue ) => {
-		if ( assetValue !== '' ) {
-			return assetValue;
-		}
-
-		while (
-			generatedIndex < generatedAssets.length &&
-			existingAssetValues.has( generatedAssets[ generatedIndex ] )
-		) {
-			generatedIndex++;
-		}
-
-		if ( generatedIndex < generatedAssets.length ) {
-			const nextGeneratedValue = generatedAssets[ generatedIndex ];
-			existingAssetValues.add( nextGeneratedValue );
-			generatedIndex++;
-			updatedCount++;
-			return nextGeneratedValue;
-		}
-
-		return '';
-	} );
-
-	return { assets, updatedCount };
 }
 
 /**
@@ -166,11 +112,10 @@ export default function TextsEditor( {
 			const response = await fetchGenAITextAssets( finalUrl, assetKey );
 			const generatedTextAssets = response?.data?.[ assetKey ] ?? [];
 
-			const { assets: updatedTexts, updatedCount } =
-				fillEmptyAssetSlotsWithUniqueValues(
-					texts,
-					generatedTextAssets
-				);
+			const { assets: updatedTexts, updatedCount } = fillEmptyAssetSlots(
+				texts,
+				generatedTextAssets
+			);
 
 			if ( updatedCount > 0 ) {
 				updateTexts( updatedTexts );
