24b97c157a772844edd420a404a848eed61261dc
Ankit Gade <ankit.gade@10up.com>
2026-01-22 23:09:59 +0530

    Remove Google Merchant Center references during onboarding.

--- js/src/components/google-combo-account-card/connect-google-combo-account-card.js
+++ js/src/components/google-combo-account-card/connect-google-combo-account-card.js
@@ -15,6 +15,7 @@ import {
 	useGoogleConnectFlow,
 } from '~/components/google-account-card';
 import AppDocumentationLink from '../app-documentation-link';
+import { glaData } from '~/constants';
 
 /**
  * Renders a card to connect to Google Account.
@@ -34,6 +35,54 @@ const ConnectGoogleComboAccountCard = ( { disabled } ) => {
 	const [ handleConnect, { loading, data } ] =
 		useGoogleConnectFlow( pageName );
 	const [ termsAccepted, setTermsAccepted ] = useState( false );
+	const { serviceBasedMerchant } = glaData;
+
+	const description = serviceBasedMerchant
+		? __( 'Required to sync with Google Ads.', 'google-listings-and-ads' )
+		: __(
+				'Required to sync with Google Merchant Center and Google Ads.',
+				'google-listings-and-ads'
+		  );
+
+	// Build the terms label. If serviceBasedMerchant is true, omit Merchant Center link.
+	const termsLabel = serviceBasedMerchant
+		? createInterpolateElement(
+				__(
+					'I accept the terms and conditions of <linkAds>Google Ads</linkAds>',
+					'google-listings-and-ads'
+				),
+				{
+					linkAds: (
+						<AppDocumentationLink
+							context="setup-ads"
+							linkId="google-ads-terms-of-service"
+							href="https://support.google.com/adspolicy/answer/54818"
+						/>
+					),
+				}
+		  )
+		: createInterpolateElement(
+				__(
+					'I accept the terms and conditions of <linkMerchant>Merchant Center</linkMerchant> and <linkAds>Google Ads</linkAds>',
+					'google-listings-and-ads'
+				),
+				{
+					linkMerchant: (
+						<AppDocumentationLink
+							context="setup-mc-accounts"
+							linkId="google-mc-terms-of-service"
+							href="https://support.google.com/merchants/answer/160173"
+						/>
+					),
+					linkAds: (
+						<AppDocumentationLink
+							context="setup-ads"
+							linkId="google-ads-terms-of-service"
+							href="https://support.google.com/adspolicy/answer/54818"
+						/>
+					),
+				}
+		  );
 
 	return (
 		<AccountCard
@@ -43,35 +92,9 @@ const ConnectGoogleComboAccountCard = ( { disabled } ) => {
 			className="gla-google-combo-service-account-card--google"
 			description={
 				<>
-					<p>
-						{ __(
-							'Required to sync with Google Merchant Center and Google Ads.',
-							'google-listings-and-ads'
-						) }
-					</p>
+					<p>{ description }</p>
 					<CheckboxControl
-						label={ createInterpolateElement(
-							__(
-								'I accept the terms and conditions of <linkMerchant>Merchant Center</linkMerchant> and <linkAds>Google Ads</linkAds>',
-								'google-listings-and-ads'
-							),
-							{
-								linkMerchant: (
-									<AppDocumentationLink
-										context="setup-mc-accounts"
-										linkId="google-mc-terms-of-service"
-										href="https://support.google.com/merchants/answer/160173"
-									/>
-								),
-								linkAds: (
-									<AppDocumentationLink
-										context="setup-ads"
-										linkId="google-ads-terms-of-service"
-										href="https://support.google.com/adspolicy/answer/54818"
-									/>
-								),
-							}
-						) }
+						label={ termsLabel }
 						checked={ termsAccepted }
 						onChange={ setTermsAccepted }
 						disabled={ disabled }
