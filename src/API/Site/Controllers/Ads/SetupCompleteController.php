4777975bd4422e5f669f9ef82a52b45fb104bbb3
James Morrison <james.morrison@fueled.com>
2026-01-26 15:08:16 +0000

    Undo PHPCS fixes.

--- src/API/Site/Controllers/Ads/SetupCompleteController.php
+++ src/API/Site/Controllers/Ads/SetupCompleteController.php
@@ -62,7 +62,7 @@ class SetupCompleteController extends BaseController {
 	 * @return callable
 	 */
 	protected function get_setup_complete_callback(): callable {
-		return function ( Request $request ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+		return function ( Request $request ) {
 			do_action( 'woocommerce_gla_ads_setup_completed' );
 
 			/**
--- tests/Unit/API/Google/MiddlewareTest.php
+++ tests/Unit/API/Google/MiddlewareTest.php
@@ -427,7 +427,7 @@ class MiddlewareTest extends UnitTest {
 	public function test_get_sdi_merchant_update_endpoint_with_site_url_having_path() {
 		add_filter(
 			'woocommerce_gla_site_url',
-			function ( $home_url ) { //phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+			function ( $home_url ) {
 				return 'http://example.org/shop';
 			}
 		);
