f52cb6705751c554c5b0b9c6fe89b6779f84affb
asvinb <asvin.balloo@gmail.com>
2026-01-29 12:06:03 +0400

    style(onboarding): Correct budget label casing

--- js/src/pages/onboarding/setup-stepper/saved-ads-only-setup-stepper/index.js
+++ js/src/pages/onboarding/setup-stepper/saved-ads-only-setup-stepper/index.js
@@ -172,7 +172,7 @@ const SavedAdsOnlySetupStepper = ( { savedStep } ) => {
 				},
 				{
 					key: ADS_ONLY_STEP_NAME_KEY_MAP.create_campaign,
-					label: __( 'Set your Budget', 'google-listings-and-ads' ),
+					label: __( 'Set your budget', 'google-listings-and-ads' ),
 					content: (
 						<SetupPaidAds
 							onSubmit={ handleSetupPaidAdsSubmit }
