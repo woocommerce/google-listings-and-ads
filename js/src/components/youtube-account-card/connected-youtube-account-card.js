f79c4ad5b9bd67567c6f5b37ca266650e3478ded
asvinb <asvin.balloo@gmail.com>
2026-01-30 10:43:14 +0400

    feat(youtube): Implement incomplete account setup state

--- js/src/components/youtube-account-card/connected-youtube-account-card.js
+++ js/src/components/youtube-account-card/connected-youtube-account-card.js
@@ -3,8 +3,6 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Notice } from '@wordpress/components';
-import { getQuery, getHistory } from '@woocommerce/navigation';
-import { useEffect, useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -13,58 +11,73 @@ import AccountCard, { APPEARANCE } from '~/components/account-card';
 import ConnectedIconLabel from '~/components/connected-icon-label';
 import Section from '~/components/section';
 import DisconnectAccount from './disconnect-account';
+import AppButton from '~/components/app-button';
 import useYouTubeSetupCompleteCallback from '~/hooks/useYouTubeSetupCompleteCallback';
-import { getSettingsUrl } from '~/utils/urls';
+import { YOUTUBE_ACCOUNT_STATUS } from '~/constants';
 
 /**
  * @typedef { import('./youtube-account-card.js').YouTubeAccount } YouTubeAccount
  */
 
 /**
+ * Clicking on the button to link the YouTube account.
+ *
+ * @event gla_link_youtube_account_button_click
+ * @property {string} context Indicates from which page the button was clicked. Possible value: 'settings-youtube'.
+ */
+
+/**
  * Component to display a connected YouTube account.
  * Detects if setup completion is needed via URL query (youtube=connected) and triggers it.
+ *
+ * @fires gla_link_youtube_account_button_click When the user clicks on the button to link the YouTube account.
  *
  * @param {Object} props
  * @param {YouTubeAccount} props.youTubeAccount The connected YouTube account.
  */
 const ConnectedYouTubeAccountCard = ( { youTubeAccount } ) => {
-	const youTubeConnected = getQuery()?.youtube === 'connected';
-	const [ handleFinishSetup ] = useYouTubeSetupCompleteCallback();
-	const hasCompletedSetupRef = useRef( false );
+	const [ handleFinishSetup, { loading, error } ] =
+		useYouTubeSetupCompleteCallback();
+	const shouldLinkYouTubeAccount =
+		youTubeAccount.status === YOUTUBE_ACCOUNT_STATUS.INCOMPLETE;
 
-	useEffect( () => {
-		async function completeSetup() {
-			hasCompletedSetupRef.current = true;
-			await handleFinishSetup();
-			getHistory().replace( getSettingsUrl() );
-		}
+	let accountCardProps = {
+		description: youTubeAccount.channel.label,
+		indicator: <ConnectedIconLabel />,
+	};
 
-		if ( youTubeConnected && ! hasCompletedSetupRef.current ) {
-			completeSetup();
-		}
-	}, [ youTubeConnected, handleFinishSetup ] );
-
-	let accountCardProps = {};
-	if ( youTubeAccount?.channel?.id ) {
+	if ( shouldLinkYouTubeAccount ) {
 		accountCardProps = {
-			description: youTubeAccount.channel.label,
-			indicator: <ConnectedIconLabel />,
-		};
-	} else {
-		accountCardProps = {
-			actions: (
+			indicator: (
+				<AppButton
+					eventName="gla_link_youtube_account_button_click"
+					eventProps={ { context: 'settings-youtube' } }
+					onClick={ handleFinishSetup }
+					disabled={ loading }
+					loading={ loading }
+					isSecondary
+				>
+					{ __( 'Complete setup', 'google-listings-and-ads' ) }
+				</AppButton>
+			),
+			detail: error?.message ? (
 				<Notice status="error" isDismissible={ false }>
-					{ __(
-						'No channels found (or permission not granted).',
-						'google-listings-and-ads'
-					) }
+					{ error.message }
 				</Notice>
+			) : undefined,
+			description: __(
+				'Your YouTube account is connected, but setup isn’t complete yet.',
+				'google-listings-and-ads'
 			),
 		};
 	}
 
 	return (
-		<AccountCard appearance={ APPEARANCE.YOUTUBE } { ...accountCardProps }>
+		<AccountCard
+			appearance={ APPEARANCE.YOUTUBE }
+			expandedDetail
+			{ ...accountCardProps }
+		>
 			<Section.Card.Footer>
 				<DisconnectAccount />
 			</Section.Card.Footer>
--- js/src/hooks/useYouTubeSetupCompleteCallback.js
+++ js/src/hooks/useYouTubeSetupCompleteCallback.js
@@ -7,10 +7,8 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import useApiFetchCallback from '~/hooks/useApiFetchCallback';
-import useDispatchCoreNotices from '~/hooks/useDispatchCoreNotices';
 
 export default function useYouTubeSetupCompleteCallback() {
-	const { createNotice } = useDispatchCoreNotices();
 	const [ fetchCompleteYouTubeSetup, result ] = useApiFetchCallback( {
 		path: '/wc/gla/youtube/setup/complete',
 		method: 'POST',
@@ -19,15 +17,7 @@ export default function useYouTubeSetupCompleteCallback() {
 	const handleFinishSetup = async () => {
 		try {
 			await fetchCompleteYouTubeSetup();
-		} catch ( error ) {
-			const message =
-				error.message ||
-				__(
-					'Unable to complete your YouTube setup. Please try again later.',
-					'google-listings-and-ads'
-				);
-			createNotice( 'error', message );
-		}
+		} catch ( error ) {}
 	};
 
 	return [ handleFinishSetup, result ];
