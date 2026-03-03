7fac899ee23f362a3d341ae7b56892f09122e8c9
Alejandro Perez Martin <aleperez92@gmail.com>
2026-03-03 10:45:48 +0100

    Update faqs accordion

--- js/src/components/paid-ads/ads-campaign/faqs.js
+++ js/src/components/paid-ads/ads-campaign/faqs.js
@@ -10,48 +10,46 @@ import FaqsPanel from '~/components/faqs-panel';
 
 const faqItems = [
 	{
-		trackId: 'how-does-google-ads-work',
-		question: __( 'How does Google Ads work?', 'google-listings-and-ads' ),
-		answer: __(
-			'Google Ads works by displaying your ad when people search online for the products and services you offer. By leveraging smart technology, Google Ads helps get your ads in front of potential customers at just the moment they’re ready to take action.',
-			'google-listings-and-ads'
-		),
-	},
-	{
-		trackId: 'what-is-a-product-feed',
-		question: __( 'What is a service feed?', 'google-listings-and-ads' ),
-		answer: __(
-			'Your service feed is the central data source that contains a list of products you want to advertise through Merchant Center. By default, Google syncs all active products from your WooCommerce inventory. You can choose to exclude products later after this setup.',
-			'google-listings-and-ads'
-		),
-	},
-	{
-		trackId: 'how-much-does-ads-cost',
-		question: __( 'How much does ads cost?', 'google-listings-and-ads' ),
-		answer: __(
-			'With Google Ads, you decide how much to spend. There’s no minimum spend, and no time commitment. Your costs may vary from day to day, but you won’t be charged more than your daily budget times the number of days in a month. You pay only for the actual clicks and calls that your ad receives.',
-			'google-listings-and-ads'
-		),
-	},
-	{
-		trackId: 'where-will-my-service-appear',
+		trackId: 'where-will-my-services-appear',
 		question: __(
-			'Where will my service appear?',
+			'Where will my services appear?',
 			'google-listings-and-ads'
 		),
 		answer: __(
-			'If you’re running a Performance Max Campaign, your approved products can appear on Google Search, Google Maps, the Shopping tab, Gmail, Youtube, the Google Display Network, and Discover feed.',
+			'If you’re running a Performance Max Campaign, your services can appear on Google Search, Google Maps, Gmail, Youtube, the Google Display Network, and Discover feed. This will only be applicable in the locations your services are offered.',
 			'google-listings-and-ads'
 		),
 	},
 	{
-		trackId: 'how-long-until-i-see-results-with-google-ads',
+		trackId: 'what-is-pmax-campaign-for-services',
 		question: __(
-			'How long until I see results with Google Ads?',
+			'What is a PMax campaign for services?',
 			'google-listings-and-ads'
 		),
 		answer: __(
-			'Google’s Performance Max campaigns are powered by machine learning models. These models train and adapt based on the data you provide in your campaign. This means performance optimization can take time. Typically, this learning process takes 1—2 weeks.',
+			"PMax campaign for services uses your website's landing pages and creative assets (images, videos, text) to find leads across Search, YouTube, Display, and Maps.",
+			'google-listings-and-ads'
+		),
+	},
+	{
+		trackId: 'can-i-use-this-without-products',
+		question: __(
+			"I don't have physical products. Can I still use this?",
+			'google-listings-and-ads'
+		),
+		answer: __(
+			'Yes! It’s designed specifically for service providers ({give_examples}, etc.). It treats your "Services" as the offer and optimizes for leads or appointments.',
+			'google-listings-and-ads'
+		),
+	},
+	{
+		trackId: 'what-makes-these-ads-different-from-product-ads',
+		question: __(
+			'What makes these ads different from product ads?',
+			'google-listings-and-ads'
+		),
+		answer: __(
+			'Product ads are focused on a tangible product being sold. These ads are for services-based businesses largely for lead generation (i.e. appointment booking). Online sales (non-feed) is also a possible objective.',
 			'google-listings-and-ads'
 		),
 	},
