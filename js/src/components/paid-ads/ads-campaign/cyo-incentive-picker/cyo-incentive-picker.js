975264a0495d714f6b3c9a8dd3e56ddb9a4e7759
asvinb <asvin.balloo@gmail.com>
2026-04-24 14:21:40 +0400

    Minor code tweaks

--- js/src/components/paid-ads/ads-campaign/cyo-incentive-picker/cyo-incentive-picker.js
+++ js/src/components/paid-ads/ads-campaign/cyo-incentive-picker/cyo-incentive-picker.js
@@ -24,7 +24,30 @@ import { recordGlaEvent } from '~/utils/tracks';
 import './cyo-incentive-picker.scss';
 
 /**
+ * @event gla_cyo_incentive_picker_shown
+ * @description Fired when the CYO incentive picker is shown to the user.
+ * @param {string} context - The context in which the incentive picker is shown, e.g. 'create-ads', 'edit-ads', 'setup-ads', 'setup-mc', or 'setup-ads-only'.
+ */
+
+/**
+ * @event gla_cyo_incentive_selected
+ * @description Fired when the user selects an incentive offer.
+ * @param {string} context - The context in which the incentive offer is selected.
+ * @param {string} level - The level of the selected incentive offer, e.g. 'low', 'medium', or 'high'.
+ */
+
+/**
+ * @event gla_cyo_incentive_apply_incentive_retry_click
+ * @description Fired when the user clicks the retry button after a failed attempt to apply an incentive.
+ * @param {string} context - The context in which the retry action is taken.
+ */
+
+/**
  * Renders the component for picking a "Choose Your Own" incentive for ads campaigns, which allows merchants to select from different ads credit offers based on their expected ad spend.
+ *
+ * @fires gla_cyo_incentive_picker_shown when the incentive picker is shown to the user.
+ * @fires gla_cyo_incentive_selected when the user selects an incentive offer.
+ * @fires gla_cyo_incentive_apply_incentive_retry_click when the user clicks the retry button after a failed attempt to apply an incentive.
  *
  * @param {Object}   props React props.
  * @param {string}   props.context The context in which this component is used, e.g. 'create-ads', 'edit-ads', 'setup-ads', 'setup-mc', or 'setup-ads-only'. This is used for tracking purposes and may also be used to conditionally render content within the component.
@@ -38,6 +61,7 @@ const CyoIncentivePicker = ( { context, incentiveResult, onRetry = noop } ) => {
 	const { formatAmount } = useAdsCurrency();
 	const {
 		value: selectedIncentiveOffer,
+		checked, // we don't actually need this prop for radio control, but we need to destructure it here to avoid passing it down to RadioControl which will cause a warning since RadioControl doesn't expect a checked prop
 		onChange,
 		...restInputProps
 	} = getInputProps( 'incentiveOffer' );
@@ -73,24 +97,19 @@ const CyoIncentivePicker = ( { context, incentiveResult, onRetry = noop } ) => {
 				offer: item.offer,
 				spendAmount: item.requirement.spend.requiredAmount.units,
 				awardAmount: item.requirement.spend.awardAmount.units,
-				radioProps: {
-					...restInputProps,
-					onChange,
-					checked: selectedIncentiveOffer === item.offer,
-					value: item.offer,
-				},
+				radioProps: restInputProps,
 			} );
 		}
 		return acc;
 	}, [] );
 
 	const handleIncentiveChange = ( offer ) => {
-		onChange( offer );
-
 		recordGlaEvent( 'gla_cyo_incentive_selected', {
 			context,
 			level: offer,
 		} );
+
+		onChange( offer );
 	};
 
 	return (
@@ -127,11 +146,8 @@ const CyoIncentivePicker = ( { context, incentiveResult, onRetry = noop } ) => {
 								id,
 								spendAmount,
 								awardAmount,
-								radioProps: {
-									selected,
-									value,
-									...restRadioProps
-								},
+								offer,
+								radioProps,
 							} ) => {
 								const formattedSpendAmount =
 									formatAmount( spendAmount );
@@ -153,10 +169,10 @@ const CyoIncentivePicker = ( { context, incentiveResult, onRetry = noop } ) => {
 
 								return (
 									<RadioControl
-										{ ...restRadioProps }
+										{ ...radioProps }
 										key={ id }
 										className="gla-cyoi-radio-control__radio-control"
-										options={ [ { value, label } ] }
+										options={ [ { label, value: offer } ] }
 										help={ sprintf(
 											/* translators: %s: amount in users' currency */
 											__(
@@ -166,6 +182,7 @@ const CyoIncentivePicker = ( { context, incentiveResult, onRetry = noop } ) => {
 											formattedSpendAmount
 										) }
 										onChange={ handleIncentiveChange }
+										selected={ selectedIncentiveOffer }
 										hideLabelFromVision
 									/>
 								);
