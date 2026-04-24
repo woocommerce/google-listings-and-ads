e786774e64d9f3c1d1eeb4ccd33aee38d30b5335
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2026-04-24 18:35:16 +0200

    Add markets tab, page and dashboard placeholder
    Signed-off-by: Ihor Vorotnov <ihor.vorotnov@gmail.com>

---
+++ js/src/pages/markets/markets-dashboard/index.js
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+const MarketsDashboard = () => {
+	return <h1>{ __( 'Markets', 'google-listings-and-ads' ) }</h1>;
+};
+
+export default MarketsDashboard;
