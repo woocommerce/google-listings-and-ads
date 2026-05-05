746a6fba1d5a6ddeb1337395404152637103682a
asvinb <asvin.balloo@gmail.com>
2026-05-05 12:16:42 +0400

    Save WIP

--- js/src/pages/markets/markets-dashboard/edit-market-modal/index.js
+++ js/src/pages/markets/markets-dashboard/edit-market-modal/index.js
@@ -1,14 +1,13 @@
 /**
  * External dependencies
  */
-import { pick } from 'lodash';
 import { __ } from '@wordpress/i18n';
 import { useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { TARGET_AUDIENCE_FIELDS } from '~/components/free-listings/choose-audience-section/constants';
+import { PRIMARY_MARKET_ID } from '../../constants';
 import AdaptiveForm from '~/components/adaptive-form';
 import AppModal from '~/components/app-modal';
 import AppButton from '~/components/app-button';
@@ -16,6 +15,19 @@ import useTargetAudienceFinalCountryCodes from '~/hooks/useTargetAudienceFinalCo
 import ValidationErrors from '~/components/validation-errors';
 import EditPrimaryFeed from './edit-primary-feed';
 import AppSpinner from '~/components/app-spinner';
+
+const checkErrors = ( values ) => {
+	const errors = {};
+
+	if ( values.countries.length === 0 ) {
+		errors.countries = __(
+			'Please select at least one country.',
+			'google-listings-and-ads'
+		);
+	}
+
+	return errors;
+};
 
 /**
  * Placeholder for the Edit Market modal.
@@ -29,68 +41,74 @@ import AppSpinner from '~/components/app-spinner';
  * @param {() => void} props.onRequestClose Called when the user closes the modal.
  */
 const EditMarketModal = ( { market, onRequestClose } ) => {
+	const { id } = market;
+	const isPrimaryMarket = id === PRIMARY_MARKET_ID;
 	const formRef = useRef();
 	const { targetAudience, getFinalCountries, loaded } =
 		useTargetAudienceFinalCountryCodes();
+
+	const handleValidate = ( values ) => {
+		return checkErrors( values );
+	};
 
 	const extendAdapter = ( formContext ) => {
 		return {
 			audienceCountries: getFinalCountries( formContext.values ),
 			renderRequestedValidation( key ) {
-				if ( formContext.adapter.requestedShowValidation ) {
-					return (
-						<ValidationErrors
-							messages={ formContext.errors[ key ] }
-						/>
-					);
-				}
-				return null;
+				return (
+					<ValidationErrors messages={ formContext.errors[ key ] } />
+				);
 			},
 		};
 	};
 
-	const handleChange = ( change, values ) => {
-		if ( TARGET_AUDIENCE_FIELDS.includes( change.name ) ) {
-			console.log( pick( values, TARGET_AUDIENCE_FIELDS ) );
-		}
-	};
+	if ( ! loaded ) {
+		return <AppSpinner />;
+	}
 
 	return (
-		<AppModal
-			title={ __( 'Edit market', 'google-listings-and-ads' ) }
-			onRequestClose={ onRequestClose }
-			buttons={ [
-				<AppButton
-					key="close"
-					variant="tertiary"
-					onClick={ onRequestClose }
-				>
-					{ __( 'Cancel', 'google-listings-and-ads' ) }
-				</AppButton>,
-				<AppButton
-					key="save"
-					variant="primary"
-					onClick={ onRequestClose }
-				>
-					{ __( 'Save', 'google-listings-and-ads' ) }
-				</AppButton>,
-			] }
+		<AdaptiveForm
+			ref={ formRef }
+			initialValues={ {
+				countries: targetAudience.countries || [],
+			} }
+			extendAdapter={ extendAdapter }
+			validate={ handleValidate }
 		>
-			{ loaded && (
-				<AdaptiveForm
-					ref={ formRef }
-					initialValues={ {
-						countries: targetAudience.countries || [],
-					} }
-					extendAdapter={ extendAdapter }
-					onChange={ handleChange }
-				>
-					<EditPrimaryFeed />
-				</AdaptiveForm>
-			) }
+			{ ( formContext ) => {
+				const { isValidForm, handleSubmit, isDirty } = formContext;
+				console.log( 'Form values:', formContext );
 
-			{ ! loaded && <AppSpinner /> }
-		</AppModal>
+				return (
+					<AppModal
+						title={ __( 'Edit market', 'google-listings-and-ads' ) }
+						onRequestClose={ onRequestClose }
+						overflow="visible"
+						buttons={ [
+							<AppButton
+								key="close"
+								variant="tertiary"
+								onClick={ onRequestClose }
+							>
+								{ __( 'Cancel', 'google-listings-and-ads' ) }
+							</AppButton>,
+							<AppButton
+								key="save"
+								variant="primary"
+								onClick={ handleSubmit }
+								disabled={ ! isValidForm || ! isDirty }
+							>
+								{ __( 'Save', 'google-listings-and-ads' ) }
+							</AppButton>,
+						] }
+					>
+						{ loaded && isPrimaryMarket && <EditPrimaryFeed /> }
+
+						{ ! loaded && <AppSpinner /> }
+					</AppModal>
+				);
+			} }
+		</AdaptiveForm>
 	);
 };
 
