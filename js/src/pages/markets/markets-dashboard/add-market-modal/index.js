171c637f11dfe739d81f63333fa20e59efcd4912
asvinb <asvin.balloo@gmail.com>
2026-05-11 12:26:14 +0400

    feat(markets): Populate initial shipping rates in Add Market modal

--- js/src/pages/markets/markets-dashboard/add-market-modal/index.js
+++ js/src/pages/markets/markets-dashboard/add-market-modal/index.js
@@ -14,8 +14,9 @@ import AppButton from '~/components/app-button';
 import AdaptiveForm from '~/components/adaptive-form';
 import MultiLingualPluginPrompt from './multilingual-plugin-prompt';
 import ValidationErrors from '~/components/validation-errors';
-import Inputs from './inputs';
 import useSettings from '~/hooks/useSettings';
+import useShippingRates from '~/hooks/useShippingRates';
+import MarketFields from './market-fields';
 
 /**
  * Placeholder for the Add Market modal.
@@ -32,7 +33,7 @@ import useSettings from '~/hooks/useSettings';
 const AddMarketModal = ( { onRequestClose } ) => {
 	const formRef = useRef();
 	const { settings } = useSettings();
-	console.log( 'Current settings:', settings );
+	const { data: shippingRates } = useShippingRates();
 
 	const extendAdapter = ( formContext ) => {
 		return {
@@ -53,8 +54,10 @@ const AddMarketModal = ( { onRequestClose } ) => {
 			ref={ formRef }
 			initialValues={ {
 				countries: [], // @TODO: to remove since checkErrors depends on it for now.
-				country: null,
-				flat_shipping_rate: null,
+				offer_free_shipping:
+					shippingRates?.[ 0 ]?.options?.free_shipping_threshold > 0,
+				flat_shipping_rate: shippingRates?.[ 0 ]?.rate,
+				shipping_country_rates: shippingRates, // for backwards compatibility with existing controls; to be removed once all controls are migrated to use flat_shipping_rate and offer_free_shipping directly.
 			} }
 			extendAdapter={ extendAdapter }
 			validate={ checkErrors }
@@ -90,7 +93,7 @@ const AddMarketModal = ( { onRequestClose } ) => {
 						onRequestClose={ onRequestClose }
 						buttons={ buttons }
 					>
-						<Inputs />
+						<MarketFields />
 						<MultiLingualPluginPrompt />
 					</AppModal>
 				);
--- js/src/pages/markets/markets-dashboard/add-market-modal/market-fields.js
+++ js/src/pages/markets/markets-dashboard/add-market-modal/market-fields.js
@@ -7,7 +7,10 @@ import { Flex, Notice } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import { useAdaptiveFormContext } from '~/components/adaptive-form';
+import {
+	useAdaptiveFormContext,
+	useAdaptiveFormInputProps,
+} from '~/components/adaptive-form';
 import MarketSelectControl from './market-select-control';
 import LanguageSelectControl from './language-select-control';
 import CurrencySelectControl from './currency-select-control';
@@ -15,8 +18,13 @@ import ShippingRateInputControl from '~/components/shipping-rate-section/estimat
 import FreeShippingThresholdControl from '~/components/order-value-condition-section/minimum-order-card/free-shipping-threshold-control';
 import CountriesTimeInput from '~/components/free-listings/configure-product-listings/shipping-time-setup/countries-time-input';
 
-const Inputs = () => {
-	const { getInputProps } = useAdaptiveFormContext();
+const MarketFields = () => {
+	const { getInputProps, values } = useAdaptiveFormContext();
+	const freeShippingInputProps = useAdaptiveFormInputProps(
+		'shipping_country_rates',
+		'free_shipping_threshold'
+	);
+	const shouldDisplayFreeShippingThreshold = values.flat_shipping_rate > 0;
 
 	return (
 		<Flex direction="column" gap={ 6 }>
@@ -40,11 +48,13 @@ const Inputs = () => {
 				{ ...getInputProps( 'flat_shipping_rate' ) }
 			/>
 
-			<FreeShippingThresholdControl value={ [] } onChange={ () => {} } />
+			{ shouldDisplayFreeShippingThreshold && (
+				<FreeShippingThresholdControl { ...freeShippingInputProps } />
+			) }
 
 			<CountriesTimeInput />
 		</Flex>
 	);
 };
 
-export default Inputs;
+export default MarketFields;
