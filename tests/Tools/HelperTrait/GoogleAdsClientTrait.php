7e712f881c8c514174ef109dff4b05390a9fbaf9
James Morrison <james.morrison@fueled.com>
2026-05-18 16:50:20 +0100

    Keep variable name as start_date

--- tests/Tools/HelperTrait/GoogleAdsClientTrait.php
+++ tests/Tools/HelperTrait/GoogleAdsClientTrait.php
@@ -424,15 +424,15 @@ trait GoogleAdsClientTrait {
 		$setting = $this->createMock( ShoppingSetting::class );
 		$setting->method( 'getFeedLabel' )->willReturn( $data['country'] );
 
-		$start_date_time = $data['start_date'] ?? null;
-		$campaign        = $this->createMock( Campaign::class );
+		$start_date = $data['start_date'] ?? null;
+		$campaign   = $this->createMock( Campaign::class );
 		$campaign->method( 'getId' )->willReturn( $data['id'] );
 		$campaign->method( 'getName' )->willReturn( $data['name'] );
 		$campaign->method( 'getStatus' )->willReturn( CampaignStatus::number( $data['status'] ) );
 		$campaign->method( 'getAdvertisingChannelType' )->willReturn( CampaignType::number( $data['type'] ) );
 		$campaign->method( 'getShoppingSetting' )->willReturn( $setting );
-		$campaign->method( 'hasStartDateTime' )->willReturn( $start_date_time !== null && $start_date_time !== '' );
-		$campaign->method( 'getStartDateTime' )->willReturn( $start_date_time !== null && $start_date_time !== '' ? $start_date_time : '' );
+		$campaign->method( 'hasStartDateTime' )->willReturn( $start_date !== null && $start_date !== '' );
+		$campaign->method( 'getStartDateTime' )->willReturn( $start_date !== null && $start_date !== '' ? $start_date : '' );
 		$campaign->method( 'getBrandGuidelinesEnabled' )->willReturn( $data['brand_guidelines_enabled'] ?? false );
 
 		$budget = $this->createMock( CampaignBudget::class );
