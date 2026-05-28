6c9017d00d79b858bfbdeb333545bcaeb805199f
Alejandro Perez Martin <aleperez92@gmail.com>
2026-05-28 18:14:46 +0200

    Add loading spinner to audience section loading

--- js/src/components/free-listings/choose-audience-section/choose-audience-section.js
+++ js/src/components/free-listings/choose-audience-section/choose-audience-section.js
@@ -8,9 +8,10 @@ import { __ } from '@wordpress/i18n';
  */
 import { useAdaptiveFormContext } from '~/components/adaptive-form';
 import AppRadioContentControl from '~/components/app-radio-content-control';
-import Section from '~/components/section';
-import Subsection from '~/components/subsection';
 import RadioHelperText from '~/components/radio-helper-text';
+import Section from '~/components/section';
+import SpinnerCard from '~/components/spinner-card';
+import Subsection from '~/components/subsection';
 import SupportedCountrySelect from '~/components/supported-country-select';
 import VerticalGapLayout from '~/components/vertical-gap-layout';
 import useGoogleMCAccount from '~/hooks/useGoogleMCAccount';
@@ -23,7 +24,8 @@ import './choose-audience-section.scss';
  * Does not provide any save strategy, this is to be bound externally.
  */
 const ChooseAudienceSection = () => {
-	const { hasGoogleMCConnection } = useGoogleMCAccount();
+	const { hasGoogleMCConnection, hasFinishedResolution } =
+		useGoogleMCAccount();
 	const {
 		getInputProps,
 		adapter: { renderRequestedValidation },
@@ -68,51 +70,60 @@ const ChooseAudienceSection = () => {
 				title={ __( 'Audience', 'google-listings-and-ads' ) }
 				description={ <p>{ description }</p> }
 			>
-				<Section.Card>
-					<Section.Card.Body>
-						<Subsection>
-							<Subsection.Title>
-								{ __( 'Location', 'google-listings-and-ads' ) }
-							</Subsection.Title>
-							<Subsection.HelperText>
-								{ titleHelper }
-							</Subsection.HelperText>
-							<VerticalGapLayout size="medium">
-								<AppRadioContentControl
-									{ ...getInputProps( 'location' ) }
-									collapsible={ true }
-									label={ __(
-										'Selected countries only',
+				{ ! hasFinishedResolution ? (
+					<SpinnerCard />
+				) : (
+					<Section.Card>
+						<Section.Card.Body>
+							<Subsection>
+								<Subsection.Title>
+									{ __(
+										'Location',
 										'google-listings-and-ads'
 									) }
-									value="selected"
-								>
-									<SupportedCountrySelect
-										multiple
-										{ ...getInputProps( 'countries' ) }
-										help={ __(
-											'Can’t find a country? Only supported countries can be selected.',
+								</Subsection.Title>
+								<Subsection.HelperText>
+									{ titleHelper }
+								</Subsection.HelperText>
+								<VerticalGapLayout size="medium">
+									<AppRadioContentControl
+										{ ...getInputProps( 'location' ) }
+										collapsible={ true }
+										label={ __(
+											'Selected countries only',
 											'google-listings-and-ads'
 										) }
-									/>
-									{ renderRequestedValidation( 'countries' ) }
-								</AppRadioContentControl>
-								<AppRadioContentControl
-									{ ...getInputProps( 'location' ) }
-									label={ __(
-										'All countries',
-										'google-listings-and-ads'
-									) }
-									value="all"
-								>
-									<RadioHelperText>
-										{ radioHelper }
-									</RadioHelperText>
-								</AppRadioContentControl>
-							</VerticalGapLayout>
-						</Subsection>
-					</Section.Card.Body>
-				</Section.Card>
+										value="selected"
+									>
+										<SupportedCountrySelect
+											multiple
+											{ ...getInputProps( 'countries' ) }
+											help={ __(
+												'Can’t find a country? Only supported countries can be selected.',
+												'google-listings-and-ads'
+											) }
+										/>
+										{ renderRequestedValidation(
+											'countries'
+										) }
+									</AppRadioContentControl>
+									<AppRadioContentControl
+										{ ...getInputProps( 'location' ) }
+										label={ __(
+											'All countries',
+											'google-listings-and-ads'
+										) }
+										value="all"
+									>
+										<RadioHelperText>
+											{ radioHelper }
+										</RadioHelperText>
+									</AppRadioContentControl>
+								</VerticalGapLayout>
+							</Subsection>
+						</Section.Card.Body>
+					</Section.Card>
+				) }
 			</Section>
 		</>
 	);
