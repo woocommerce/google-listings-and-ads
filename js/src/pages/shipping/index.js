ab984a4b697176e2de7ea2ac0067e52a99cf543c
Alejandro Perez Martin <aleperez92@gmail.com>
2026-05-28 18:02:53 +0200

    Set default value for settings to null to avoid rendering empty state

--- js/src/pages/shipping/index.js
+++ js/src/pages/shipping/index.js
@@ -189,8 +189,8 @@ export default function Shipping() {
 		}
 	};
 
-	const initialAudience = targetAudience?.countries ? targetAudience : {};
-	const initialSettings = settings?.shipping_rate ? settings : {};
+	const initialAudience = targetAudience?.location ? targetAudience : null;
+	const initialSettings = settings?.shipping_rate ? settings : null;
 	const initialRates = hasResolvedShippingRates ? savedShippingRates : {};
 	const initialTimes = hasResolvedShippingTimes ? savedShippingTimes : {};
 
