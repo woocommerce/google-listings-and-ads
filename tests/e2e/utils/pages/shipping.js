594ba826f7f0a4e9f31901f341afc4a35bf4fa0d
Alejandro Perez Martin <aleperez92@gmail.com>
2026-06-04 11:22:19 +0200

    Fix selector

--- tests/e2e/utils/pages/shipping.js
+++ tests/e2e/utils/pages/shipping.js
@@ -129,7 +129,7 @@ export default class ShippingPage extends MockRequests {
 	 * @return {Promise<void>}
 	 */
 	async fillCountriesShippingTimeInput( min, max ) {
-		const timesLocator = this.page.locator( '.countries-time input' );
+		const timesLocator = this.page.locator( '.gla-countries-time-input input' );
 		await timesLocator.first().fill( min );
 		await timesLocator.last().fill( max );
 	}
