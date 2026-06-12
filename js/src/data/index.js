ca4b48b615dce75d1cc4720f0b3ddbb12a398b5a
Mikael Tomović <mikael.tomovic@gmail.com>
2026-06-12 10:47:39 +0100

    Reverting to remove the if conditional around store registration.

--- js/src/data/index.js
+++ js/src/data/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { registerStore, select, useDispatch, dispatch } from '@wordpress/data';
+import { registerStore, useDispatch, dispatch } from '@wordpress/data';
 import apiFetch from '@wordpress/api-fetch';
 import { getHistory } from '@woocommerce/navigation';
 
@@ -18,52 +18,49 @@ import reducer from './reducer';
 import { createErrorResponseCatcher } from './apiFetchMiddlewares';
 import { getReconnectAccountUrl } from '~/utils/urls';
 
-// Register the store if it's not already registered.
-if ( ! select( STORE_KEY ) ) {
-	registerStore( STORE_KEY, {
-		actions,
-		selectors,
-		resolvers,
-		controls,
-		reducer,
-	} );
+registerStore( STORE_KEY, {
+	actions,
+	selectors,
+	resolvers,
+	controls,
+	reducer,
+} );
 
-	dispatch( STORE_KEY ).hydratePrefetchedData( glaData.initialWpData );
+dispatch( STORE_KEY ).hydratePrefetchedData( glaData.initialWpData );
 
-	apiFetch.use(
-		createErrorResponseCatcher( ( response ) => {
-			if ( glaData.mcSetupComplete && response.status === 401 ) {
-				return ( response.json || response.text )
-					.call( response )
-					.then( ( errorInfo ) => {
-						if ( typeof errorInfo === 'string' ) {
-							return { message: errorInfo };
-						}
-						return errorInfo;
-					} )
-					.then( ( errorInfo ) => {
-						const url = getReconnectAccountUrl( errorInfo.code );
+apiFetch.use(
+	createErrorResponseCatcher( ( response ) => {
+		if ( glaData.mcSetupComplete && response.status === 401 ) {
+			return ( response.json || response.text )
+				.call( response )
+				.then( ( errorInfo ) => {
+					if ( typeof errorInfo === 'string' ) {
+						return { message: errorInfo };
+					}
+					return errorInfo;
+				} )
+				.then( ( errorInfo ) => {
+					const url = getReconnectAccountUrl( errorInfo.code );
 
-						if ( url ) {
-							getHistory().replace( url );
-						}
+					if ( url ) {
+						getHistory().replace( url );
+					}
 
-						return errorInfo;
-					} )
-					.then( ( errorInfo ) => {
-						// Inject the status code to let the subsequent handlers can identify the 401 response error.
-						return Promise.reject( {
-							...errorInfo,
-							statusCode: response.status,
-						} );
+					return errorInfo;
+				} )
+				.then( ( errorInfo ) => {
+					// Inject the status code to let the subsequent handlers can identify the 401 response error.
+					return Promise.reject( {
+						...errorInfo,
+						statusCode: response.status,
 					} );
-			}
+				} );
+		}
 
-			// Throws error response to subsequent middlewares
-			throw response;
-		} )
-	);
-}
+		// Throws error response to subsequent middlewares
+		throw response;
+	} )
+);
 
 export { STORE_KEY };
 
