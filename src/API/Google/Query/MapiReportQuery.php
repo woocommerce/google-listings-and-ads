db3f45bb58125007e1a1897da047ae33a0f77c6f
alaca <ante.laca@gmail.com>
2026-06-15 13:03:04 +0200

    Introduce `MapiReportQuery` to support Merchant API report queries, replace existing query implementations, and streamline client handling.

---
+++ src/API/Google/Query/MapiReportQuery.php
@@ -0,0 +1,84 @@
+<?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query;
+
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Mapi\MapiPaths;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Mapi\MerchantApiClient;
+use Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Mapi\MerchantApiException;
+use Automattic\WooCommerce\GoogleListingsAndAds\Exception\InvalidProperty;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class MapiReportQuery
+ *
+ * Runs a report query against the Merchant API accounts.reports.search resource.
+ *
+ * @package Automattic\WooCommerce\GoogleListingsAndAds\API\Google\Query
+ */
+abstract class MapiReportQuery extends Query {
+
+	/**
+	 * Client which handles the query.
+	 *
+	 * @var MerchantApiClient
+	 */
+	protected $client = null;
+
+	/**
+	 * Merchant account ID.
+	 *
+	 * @var int
+	 */
+	protected $id = null;
+
+	/**
+	 * Arguments to add to the search query.
+	 *
+	 * @var array
+	 */
+	protected $search_args = [];
+
+	/**
+	 * Set the client which will handle the query.
+	 *
+	 * @param MerchantApiClient $client Client instance.
+	 * @param int               $id     Account ID.
+	 *
+	 * @return QueryInterface
+	 */
+	public function set_client( MerchantApiClient $client, int $id ): QueryInterface {
+		$this->client = $client;
+		$this->id     = $id;
+
+		return $this;
+	}
+
+	/**
+	 * Perform the query and save it to the results.
+	 *
+	 * @throws InvalidProperty      If the client is not set.
+	 * @throws MerchantApiException On a non-2xx response.
+	 */
+	protected function query_results() {
+		if ( ! $this->client || ! $this->id ) {
+			throw InvalidProperty::not_null( get_class( $this ), 'client' );
+		}
+
+		$body = [ 'query' => $this->build_query() ];
+
+		if ( ! empty( $this->search_args['pageSize'] ) ) {
+			$body['pageSize'] = $this->search_args['pageSize'];
+		}
+
+		if ( ! empty( $this->search_args['pageToken'] ) ) {
+			$body['pageToken'] = $this->search_args['pageToken'];
+		}
+
+		$this->results = $this->client->post(
+			sprintf( '%s/accounts/%d/reports:search', MapiPaths::REPORTS, $this->id ),
+			$body
+		);
+	}
+}
