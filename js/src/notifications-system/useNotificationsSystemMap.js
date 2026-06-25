7958f94db7aba040beb1bc588440036f58e4b02f
Maggie Serino <maggie.serino@fueled.com>
2026-06-25 15:54:39 +0200

    GOOWOO-672 change coupon URL in case of coupons not synced

--- js/src/notifications-system/useNotificationsSystemMap.js
+++ js/src/notifications-system/useNotificationsSystemMap.js
@@ -16,6 +16,7 @@ import {
 	getSetupAdsUrl,
 	getWCAdvancedSettingsUrl,
 	getOnboardingUrl,
+	getCouponsUrl,
 } from '~/utils/urls';
 
 /**
@@ -31,6 +32,7 @@ const dashboardUrl = getDashboardUrl();
 const settingsUrl = getSettingsUrl();
 const wcAdvancedSettingsUrl = getWCAdvancedSettingsUrl();
 const onboardingUrl = getOnboardingUrl();
+const couponsUrl = getCouponsUrl();
 
 /**
  * Static notification configs — created once at module level, never re-created on render.
@@ -340,7 +342,7 @@ const useNotificationsSystemMap = () => {
 				actions: [
 					{
 						id: 'review-coupon-settings',
-						href: settingsUrl,
+						href: couponsUrl,
 						children: __(
 							'Review coupon settings',
 							'google-listings-and-ads'
