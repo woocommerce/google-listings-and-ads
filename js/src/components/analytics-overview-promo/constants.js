8648321d2b8ec74c31148d4fec7bf8c56eef294f
Alejandro Perez Martin <aleperez92@gmail.com>
2026-09-11 14:11:46 +0200

    Remove unused export

--- js/src/components/analytics-overview-promo/constants.js
+++ js/src/components/analytics-overview-promo/constants.js
@@ -1,9 +1,4 @@
 /**
- * Tracking context for events fired from the Analytics Overview promo.
- */
-export const ANALYTICS_OVERVIEW_PROMO_CONTEXT = 'analytics-overview-promo';
-
-/**
  * The `@wordpress/preferences` key used to persist the promo's dismissed state.
  */
 export const ANALYTICS_OVERVIEW_PROMO_DISMISSED_KEY =
